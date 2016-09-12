4eb74efebc86c2e06aab43e261062fb0f861b20c
wren romano <wren@community.haskell.org>
2016-09-12 13:21:33 -0700

    re2/build.sh: removed debugging output stuff
    We may want to reintroduce it to make the build process prettier
    and easier to debug; but we should do it for all the libraries we're
    fuzzing, not just this one.

--- re2/build.sh
+++ re2/build.sh
@@ -17,22 +17,6 @@
 
 cd /src/re2
 
-function _start()   { echo -e "\n\033[1;33m➜\033[0m \033[1m$*\033[0m"; }
-function _success() { echo -e "\033[1;32m✔\033[0m \033[1m$*\033[0m"; }
-function _failure() { echo -e "\n\033[1;31m✖\033[0m \033[1m$*\033[0m"; }
-
-function _try() {
-	_start "$*"
-	$@
-	if [ $? -eq 0 ]; then
-		_success "$*"
-	else
-		_failure "$*"
-		exit $?
-	fi
-}
-
-
 # First, build the re2 library.
 # N.B., we don't follow the standard incantation for building re2
 # (i.e., `make && make test && make install && make testinstall`),
@@ -41,12 +25,12 @@ function _try() {
 # really need for our fuzzer, so that's all we build. Hopefully
 # this won't cause the fuzzer to fail erroneously due to not running
 # upstream's tests first to be sure things compiled correctly.
-_try make clean
-_try make obj/libre2.a
+make clean
+make obj/libre2.a
 
 
 # Second, build our fuzzers.
-_try $CXX $CXXFLAGS -std=c++11 -I. \
+$CXX $CXXFLAGS -std=c++11 -I. \
 	/src/oss-fuzz/re2/re2_fuzzer.cc -o /out/re2_fuzzer \
 	/work/libfuzzer/*.o ./obj/libre2.a $LDFLAGS
 
