1f6691451cd689b6f1d8b5acb618b48fbc7562c6
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-10-06 15:23:38 -0700

    Update all.sh

--- infra/base-images/all.sh
+++ infra/base-images/all.sh
@@ -15,7 +15,7 @@
 #
 ################################################################################
 
-docker build --pull -t ossfuzz/base infra/base-images/base
-docker build -t ossfuzz/base-clang infra/base-images/base-clang
-docker build -t ossfuzz/base-libfuzzer infra/base-images/base-libfuzzer
-docker build -t ossfuzz/libfuzzer-runner infra/base-images/libfuzzer-runner
+docker build --pull -t ossfuzz/base $@ infra/base-images/base
+docker build -t ossfuzz/base-clang $@ infra/base-images/base-clang
+docker build -t ossfuzz/base-libfuzzer $@ infra/base-images/base-libfuzzer
+docker build -t ossfuzz/libfuzzer-runner $@ infra/base-images/libfuzzer-runner
