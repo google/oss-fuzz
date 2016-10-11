fd2ccea312c65e2f623b3802120258f02e420111
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-10-11 14:23:29 -0700

    Update build.sh

--- nss/build.sh
+++ nss/build.sh
@@ -19,6 +19,9 @@
 mkdir -p /work/nss
 hg clone https://hg.mozilla.org/projects/nspr
 hg clone https://hg.mozilla.org/projects/nss
+
+ls -alR
+
 cd /work/nss/nss
 make BUILD_OPT=1 USE_64=1 NSS_DISABLE_GTESTS=1 CC="$CC $CFLAGS" \
     CXX="$CXX $CXXFLAGS" LD="$CC $CFLAGS" ZDEFS_FLAG= clean nss_build_all
