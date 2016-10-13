211d9a3f9f314091ca38dc82106bb4b97762210b
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-10-12 23:59:14 -0700

    Update libfuzzer-pipeline.groovy

--- infra/libfuzzer-pipeline.groovy
+++ infra/libfuzzer-pipeline.groovy
@@ -42,7 +42,7 @@ def call(body) {
       echo "Building $dockerTag"
 
       // rm files with docker first to use the same user
-      sh "docker run --rm -v $workspace/out:/out ubuntu /bin/sh -c 'rm -rf /out/*'"
+      sh "docker run --rm -v $workspace/out:/out ubuntu /bin/sh -c 'rm -rvf /out/*'"
       sh "rm -rf $workspace/out"
       sh "mkdir -p $workspace/out"
 
