1e501e66b05345bbba081fa941035cd67864f318
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-10-14 14:10:29 -0700

    Update libfuzzer-pipeline-test.groovy

--- infra/libfuzzer-pipeline-test.groovy
+++ infra/libfuzzer-pipeline-test.groovy
@@ -127,7 +127,7 @@ def call(body) {
             }
           }
           sh "ls -al $resultsDir/"
-          step([$class: 'JUnitResultArchiver', testResults: '**/TEST-*.xml'])
+          step([$class: 'JUnitResultArchiver', testResults: '$resultsDir/TEST-*.xml'])
           echo "Tested $fuzzersFound fuzzer"
           if (!fuzzersFound) {
             error "no fuzzers found";
