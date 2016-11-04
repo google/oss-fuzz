991552b4a9404c12d0b84237a791f597d09a2b0c
Kuang-che Wu <kcwu@chromium.org>
2016-11-04 18:56:47 +0800

    Add fuzzer for libarchive
    Fixes #44

---
+++ targets/libarchive/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd /src/libarchive
+
+# build the target.
+./build/autogen.sh
+./configure
+make -j$(nproc) all
+
+# build your fuzzer(s)
+$CXX $CXXFLAGS -Ilibarchive \
+    /src/libarchive_fuzzer.cc -o /out/libarchive_fuzzer \
+    -lfuzzer .libs/libarchive.a $FUZZER_LDFLAGS \
+    -lbz2 -llzo2 -llzma -lxml2 -lz -lcrypto -llz4
