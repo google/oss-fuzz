cef739b4277bf9123fcfc06ce491172481e38849
Kuang-che Wu <kcwu@chromium.org>
2016-11-05 16:56:49 +0800

    Add fuzzers for lcms. Fixes #43
    cmsIT8_load_fuzzer is ported from
    https://github.com/google/libfuzzer-bot/tree/master/dockerfiles/lcms2
    
    cms_transform_fuzzer is simplified from pdfium's usage
    https://pdfium.googlesource.com/pdfium/+/master/testing/libfuzzer/pdf_codec_icc_fuzzer.cc

---
+++ targets/lcms/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd /src/lcms
+
+# build the target.
+./configure
+make -j$(nproc) all
+
+# build your fuzzer(s)
+FUZZERS="cmsIT8_load_fuzzer cms_transform_fuzzer"
+for F in $FUZZERS; do
+    $CC $CFLAGS -Iinclude \
+        /src/$F.c -o /out/$F \
+        -lfuzzer src/.libs/liblcms2.a $FUZZER_LDFLAGS
+done
