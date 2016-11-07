d8ce066f2b9be384906bf551e2a5f2f4c6c116a8
Alex Gaynor <alex.gaynor@gmail.com>
2016-11-07 18:26:17 -0500

    use a unique_ptr and new, this is C++!

--- targets/libjpeg-turbo/libjpeg_turbo_fuzzer.cc
+++ targets/libjpeg-turbo/libjpeg_turbo_fuzzer.cc
@@ -36,12 +36,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         return 0;
     }
 
-    // TODO: this can't possibly be right?
-    void *buf = malloc(width * height * 3);
+    std::unique_ptr<unsigned char[]> buf(new unsigned char[width * height * 3]);
     tjDecompress2(
-        jpegDecompressor, data, size, reinterpret_cast<unsigned char *>(buf), width, 0, height, TJPF_RGB, 0);
+        jpegDecompressor, data, size, buf.get(), width, 0, height, TJPF_RGB, 0);
 
-    free(buf);
     tjDestroy(jpegDecompressor);
 
     return 0;
