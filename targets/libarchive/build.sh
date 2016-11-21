dd4578322aab699dc621103fc1bd06a815d870ee
Mike Aizatsky <mike.aizatsky@gmail.com>
2016-11-21 12:32:26 -0800

    [libarchive] replacing dynamic with static linkage

--- targets/libarchive/build.sh
+++ targets/libarchive/build.sh
@@ -24,4 +24,5 @@ make -j$(nproc) all
 $CXX $CXXFLAGS -Ilibarchive \
     $SRC/libarchive_fuzzer.cc -o $OUT/libarchive_fuzzer \
     -lfuzzer .libs/libarchive.a \
-    -lbz2 -llzo2 -llzma -lxml2 -lz -lcrypto -llz4
+    -Wl,-Bstatic -lbz2 -llzo2  -lxml2 -llzma -lz -lcrypto -llz4 -licuuc \
+    -licudata -Wl,-Bdynamic
