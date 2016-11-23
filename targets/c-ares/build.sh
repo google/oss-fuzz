fcdf7c700453247330823dd1564beb61495a44fc
Max Moroz <mmoroz@chromium.org>
2016-11-23 11:33:02 +0100

    [c-ares] Fixing build flags to have proper coverage + stack-traces.

--- targets/c-ares/build.sh
+++ targets/c-ares/build.sh
@@ -17,9 +17,9 @@
 
 # Build the target.
 ./buildconf
-./configure
+./configure CC="$CC $CFLAGS"
 make clean
-make -j$(nproc) V=1 all
+make CFLAGS= -j$(nproc) V=1 all
 
 # Build the fuzzer.
 $CXX $CXXFLAGS -std=c++11 -I. \
