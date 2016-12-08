d0902b97fd84b7202ea4124a3efe39f1fdf200e3
Caolán McNamara <caolanm@redhat.com>
2016-12-08 13:51:38 +0000

    --disable-gui now works

--- projects/libreoffice/build.sh
+++ projects/libreoffice/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 
+git pull -r
+
 #shuffle CXXFLAGS -stdlib=libc++ arg into CXX as well because we use
 #the CXX as the linker and need to pass -stdlib=libc++ to build
 export CXX="$CXX -stdlib=libc++"
@@ -18,24 +20,6 @@ $SRC/libreoffice/autogen.sh --with-distro=LibreOfficeOssFuzz
 make build-nocheck
 
 #copy in linked dependencies on build platform not in deployment platform
-#we need to get --disable-gui working again to drop this clutter
-cp /usr/lib/x86_64-linux-gnu/libX11.so.6 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXau.so.6 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXdmcp.so.6 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libX11-xcb.so.1 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXext.so.6 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXdamage.so.1 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXfixes.so.3 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb.so.1 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-shm.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-render.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-glx.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-dri2.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-dri3.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-present.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxcb-sync.so.1 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libxshmfence.so.1 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXrender.so.1 instdir/program
 cp /usr/lib/x86_64-linux-gnu/libfontconfig.so.1 instdir/program
 cp /usr/lib/x86_64-linux-gnu/libfreetype.so.6 instdir/program
 cp /usr/lib/x86_64-linux-gnu/libicu*.so.* instdir/program/
@@ -47,10 +31,6 @@ cp /usr/lib/x86_64-linux-gnu/libnspr4.so instdir/program/
 cp /usr/lib/x86_64-linux-gnu/libplds4.so instdir/program/
 cp /usr/lib/x86_64-linux-gnu/libplc4.so instdir/program/
 cp /usr/lib/x86_64-linux-gnu/libpng12.so.0 instdir/program/
-cp /usr/lib/x86_64-linux-gnu/libglapi.so.0 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libXxf86vm.so.1 instdir/program
-cp /usr/lib/x86_64-linux-gnu/libdrm.so.2 instdir/program
-cp /usr/lib/x86_64-linux-gnu/mesa/libGL.so.1 instdir/program
 
 #save the output under instdir, which is what we want to keep
 rm -rf ../instdir
