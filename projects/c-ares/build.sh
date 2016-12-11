515111403d2a584f8566705a51d396ea9ac62edc
Abhishek Arya <inferno@chromium.org>
2016-12-11 06:19:10 -0800

    Use c-ares fuzzer from its repo.

--- projects/c-ares/build.sh
+++ projects/c-ares/build.sh
@@ -22,7 +22,11 @@ make clean
 make -j$(nproc) V=1 all
 
 # Build the fuzzer.
-$CXX $CXXFLAGS -std=c++11 -I. \
-    $SRC/c_ares_ares_create_query_fuzzer.cc \
-    -o $OUT/c_ares_ares_create_query_fuzzer \
+$CC $CFLAGS -I. -c $SRC/c-ares/test/ares-test-fuzz.c -o $WORK/ares-test-fuzz.o
+$CXX $CXXFLAGS -std=c++11 \
+    $WORK/ares-test-fuzz.o \
+    -o $OUT/ares_parse_reply_fuzzer \
     -lFuzzingEngine $SRC/c-ares/.libs/libcares.a
+
+# Archive and copy to $OUT seed corpus if the build succeeded.
+zip -j $OUT/ares_parse_reply_fuzzer_seed_corpus.zip $SRC/c-ares/test/fuzzinput/*
