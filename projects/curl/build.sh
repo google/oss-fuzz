20c48c6e08f26b5dd16d964e8a4cf9b8607d4e17
inferno-chromium <inferno@chromium.org>
2016-12-12 10:19:45 -0800

    Add seed corpus for curl fuzzer. Also update length for tests. (#172)

--- projects/curl/build.sh
+++ projects/curl/build.sh
@@ -18,11 +18,14 @@
 ./buildconf
 ./configure --disable-shared --enable-debug --enable-maintainer-mode --disable-symbol-hiding --disable-threaded-resolver --enable-ipv6 --with-random=/dev/null
 make -j$(nproc)
+
+# Build the fuzzer.
 $CXX $CXXFLAGS $SRC/curl_fuzzer.cc -Iinclude lib/.libs/libcurl.a \
   -o $OUT/curl_fuzzer \
   -Wl,-Bstatic -lssl -lcrypto -lz -lFuzzingEngine -Wl,-Bdynamic
 
-#  /usr/lib/x86_64-linux-gnu/libssl.a \
-#  /usr/lib/x86_64-linux-gnu/libcrypto.a \
-
+# Copy dictionary and options file to $OUT.
 cp $SRC/*.dict $SRC/*.options $OUT/
+
+# Archive and copy to $OUT seed corpus if the build succeeded.
+zip -j $OUT/curl_fuzzer_seed_corpus.zip $SRC/curl/tests/data/test*
