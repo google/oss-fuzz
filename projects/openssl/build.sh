713113690c442bbcfef03dd79d380aeab0fd6cb9
Kurt Roeckx <kurt@roeckx.be>
2016-12-15 20:53:11 +0100

    Openssl: Enable more configure options
    This enables all the options that are off by default but that we still
    want to test.

--- projects/openssl/build.sh
+++ projects/openssl/build.sh
@@ -15,7 +15,7 @@
 #
 ################################################################################
 
-./config enable-fuzz-libfuzzer -DPEDANTIC -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION no-shared --with-fuzzer-lib=/usr/lib/libFuzzingEngine $CFLAGS
+./config enable-fuzz-libfuzzer -DPEDANTIC -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION no-shared enable-tls1_3 enable-rc5 enable-md2 enable-ec_nistp_64_gcc_128 enable-ssl3 enable-ssl3-method enable-nextprotoneg enable-weak-ssl-ciphers --with-fuzzer-lib=/usr/lib/libFuzzingEngine $CFLAGS -fno-sanitize=alignment
 make -j$(nproc) EX_LIBS="-ldl /usr/local/lib/libc++.a"
 
 fuzzers=$(find fuzz -executable -type f '!' -name \*.py '!' -name \*-test)
