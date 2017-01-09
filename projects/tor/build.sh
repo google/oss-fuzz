81d84c7ab7a43528bfbca307bfc9b996d674d3ee
Nick Mathewson <nickm@torproject.org>
2017-01-09 10:59:11 -0500

    More work

--- projects/tor/build.sh
+++ projects/tor/build.sh
@@ -2,6 +2,11 @@
 
 sh autogen.sh
 
-./configure --disable-asciidoc
+# We need to run configure with leak-checking disabled, or many of the
+# test functions will fail.
+export ASAN_OPTIONS=detect_leaks=0
+
+./configure --disable-asciidoc --enable-oss-fuzz
 make clean
-make -j$(nproc) all
+make -j$(nproc) oss-fuzz-fuzzers
+cp src/test/fuzz/oss-fuzz-* $(OUT)
