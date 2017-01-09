a8f378ea639191c767a2f0edb9a62c3161f4da3e
Nick Mathewson <nickm@torproject.org>
2017-01-09 13:58:13 -0500

    more fixes for tor

--- projects/tor/build.sh
+++ projects/tor/build.sh
@@ -9,4 +9,24 @@ export ASAN_OPTIONS=detect_leaks=0
 ./configure --disable-asciidoc --enable-oss-fuzz
 make clean
 make -j$(nproc) oss-fuzz-fuzzers
-cp src/test/fuzz/oss-fuzz-* $(OUT)
+
+TORLIBS="src/or/libtor-testing.a"
+TORLIBS="$TORLIBS src/common/libor-crypto-testing.a"
+TORLIBS="$TORLIBS src/ext/keccak-tiny/libkeccak-tiny.a"
+TORLIBS="$TORLIBS src/common/libcurve25519_donna.a"
+TORLIBS="$TORLIBS src/ext/ed25519/ref10/libed25519_ref10.a"
+TORLIBS="$TORLIBS src/ext/ed25519/donna/libed25519_donna.a"
+TORLIBS="$TORLIBS src/common/libor-testing.a"
+TORLIBS="$TORLIBS src/common/libor-ctime-testing.a"
+TORLIBS="$TORLIBS src/common/libor-event-testing.a"
+TORLIBS="$TORLIBS src/trunnel/libor-trunnel-testing.a"
+TORLIBS="$TORLIBS -lz -lm -levent -lssl -lcrypto"
+
+for fuzzer in src/test/fuzz/*.a; do
+    output="${fuzzer%.a}"
+    output="${output##*lib}"
+    ${CXX} ${CXXFLAGS} -std=c++11 -lFuzzingEngine ${fuzzer} ${TORLIBS} -o ${OUT}/${output}
+done
+
+cd $WORK
+
