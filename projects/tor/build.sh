9f68c71dd98269b49294239004dd3b77c4c1f859
Nick Mathewson <nickm@torproject.org>
2017-01-09 14:14:42 -0500

    copyright notice, corpora.

--- projects/tor/build.sh
+++ projects/tor/build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+##############################################################################
+
+cd ${SRC}/tor
 
 sh autogen.sh
 
@@ -26,7 +43,7 @@ for fuzzer in src/test/fuzz/*.a; do
     output="${fuzzer%.a}"
     output="${output##*lib}"
     ${CXX} ${CXXFLAGS} -std=c++11 -lFuzzingEngine ${fuzzer} ${TORLIBS} -o ${OUT}/${output}
+    zip ${OUT}/${output}_seed_corpus.zip ${SRC}/tor-fuzz-corpora/${output#oss-fuzz-}
 done
 
-cd $WORK
 
