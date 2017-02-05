6492d4dc56898e36203fb14f298a70c2b22410a0
Jonathan Foote <foote@fastly.com>
2017-02-04 19:05:16 -0500

    remove h2o upstream workaround patching; add dictionary support

--- projects/h2o/build.sh
+++ projects/h2o/build.sh
@@ -3,7 +3,6 @@
 set -x
 
 pushd $SRC/h2o
-git apply $SRC/fixup.patch
 cmake -DBUILD_FUZZER=ON -DOSS_FUZZ=ON -DOPENSSL_USE_STATIC_LIBS=TRUE .
 make
 cp ./h2o-fuzzer-http* $OUT/
@@ -11,5 +10,5 @@ cp ./h2o-fuzzer-http* $OUT/
 zip -jr $OUT/h2o-fuzzer-http1_seed_corpus.zip $SRC/h2o/fuzz/http1-corpus
 zip -jr $OUT/h2o-fuzzer-http2_seed_corpus.zip $SRC/h2o/fuzz/http2-corpus
 
-cp $SRC/*.options $OUT/
+cp $SRC/*.options $SRC/h2o/fuzz/*.dict $OUT/
 popd
