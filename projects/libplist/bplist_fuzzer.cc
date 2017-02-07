c31697bf9c982d8312adb5580435c1628ca22464
Abhishek Arya <inferno@chromium.org>
2017-02-07 15:24:00 -0800

    Fix UBSAN error with function argument casting.

--- projects/libplist/bplist_fuzzer.cc
+++ projects/libplist/bplist_fuzzer.cc
@@ -19,10 +19,10 @@
 #include <plist/plist.h>
 #include <stdio.h>
 
-extern "C" int LLVMFuzzerTestOneInput(const char* data, size_t size)
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size)
 {
 	plist_t root_node = NULL;
-	plist_from_bin(data, size, &root_node);
+	plist_from_bin(reinterpret_cast<const char*>(data), size, &root_node);
 	plist_free(root_node);
 
 	return 0;
