2aff2eedcff0fb6d1cdef62d0eac8e4c8a7052d9
Abhishek Arya <inferno@chromium.org>
2017-03-10 09:02:02 -0800

    Fix for bug 802.
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=802#c4

--- projects/sqlite3/build.sh
+++ projects/sqlite3/build.sh
@@ -24,6 +24,7 @@ export ASAN_OPTIONS=detect_leaks=0
 # Also limit max memory page count to avoid creating large databases.
 export CFLAGS="$CFLAGS -DSQLITE_MAX_LENGTH=128000000 \
                -DSQLITE_MAX_SQL_LENGTH=128000000 \
+               -DSQLITE_MAX_MEMORY=25000000 \
                -DSQLITE_PRINTF_PRECISION_LIMIT=1048576 \
                -DSQLITE_MAX_PAGE_COUNT=16384"
 ../configure
