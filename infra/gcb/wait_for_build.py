23854bcf00cc972dfc54bed50d6a06da4df4a623
Oliver Chang <ochang@chromium.org>
2017-03-10 17:35:54 -0800

    actually exit

--- infra/gcb/wait_for_build.py
+++ infra/gcb/wait_for_build.py
@@ -8,6 +8,7 @@ Usage: wait_for_build.py <build_id>
 import json
 import sys
 import time
+import thread
 import threading
 
 from google.cloud import logging
@@ -15,7 +16,8 @@ from google.cloud import pubsub
 from googleapiclient.discovery import build
 from oauth2client.client import GoogleCredentials
 
-POLL_INTERVAL = 30
+POLL_INTERVAL = 15
+status = None
 
 
 def usage():
@@ -34,6 +36,7 @@ def create_log_subscription(log_topic, build_id):
 
 
 def poll_build_status_thread(build_id):
+  global status
   credentials = GoogleCredentials.get_application_default()
   cloudbuild = build('cloudbuild', 'v1', credentials=credentials)
 
@@ -41,13 +44,9 @@ def poll_build_status_thread(build_id):
     build_info = cloudbuild.projects().builds().get(
         projectId='clusterfuzz-external', id=build_id).execute()
     status = build_info['status']
-    if status == 'SUCCESS':
-      print status
-      exit(0)
-
-    if status == 'FAILURE':
-      print status
-      exit(1)
+    if status == 'SUCCESS' or status == 'FAILURE':
+      thread.interrupt_main()
+      return
 
     time.sleep(POLL_INTERVAL)
 
@@ -62,15 +61,26 @@ def main():
   log_topic = pubsub_client.topic('build-logs-' + build_id)
   assert log_topic.exists()
 
-  status_thread = threading.Thread(target=poll_build_status_thread, args=(build_id,))
+  status_thread = threading.Thread(target=poll_build_status_thread,
+                                   args=(build_id,))
+  status_thread.daemon = True
   status_thread.start()
 
-  log_sub = create_log_subscription(log_topic, build_id)
-  while True:
-    pulled = log_sub.pull(max_messages=32)
-    for ack_id, message in pulled:
-      print json.loads(message.data)['textPayload']
-      log_sub.acknowledge([ack_id])
+  try:
+    log_sub = create_log_subscription(log_topic, build_id)
+    while True:
+      pulled = log_sub.pull(max_messages=32)
+      for ack_id, message in pulled:
+        print json.loads(message.data)['textPayload']
+        log_sub.acknowledge([ack_id])
+  except KeyboardInterrupt:
+    if status:
+      print status
+
+    if status == 'SUCCESS':
+      sys.exit(0)
+
+    sys.exit(1)
 
 
 if __name__ == "__main__":
