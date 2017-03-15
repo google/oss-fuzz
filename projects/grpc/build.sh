6364c0f8326dd1b9b1af000d993339d5ea8a93ca
matt-kwong <matt-kwong@users.noreply.github.com>
2017-03-15 14:43:36 -0700

    gRPC - add ssl_server_fuzzer and remove options (#458)
    * CC less people for gRPC issues
    
    * Enable ssl_server_fuzzer and remove options

--- projects/grpc/build.sh
+++ projects/grpc/build.sh
@@ -28,9 +28,8 @@ test/core/transport/chttp2/hpack_parser_fuzzer_test.c \
 test/core/end2end/fuzzers/api_fuzzer.c \
 test/core/end2end/fuzzers/client_fuzzer.c \
 test/core/end2end/fuzzers/server_fuzzer.c \
+test/core/security/ssl_server_fuzzer.c \
 "
-# TODO: enable ssl server corpus after Bazel fuzzer rules written
-# test/core/security/ssl_server_fuzzer.c \
 
 FUZZER_DICTIONARIES="\
 test/core/end2end/fuzzers/api_fuzzer.dictionary \
@@ -76,21 +75,20 @@ for dict in $FUZZER_DICTIONARIES; do
   cp $dict $OUT/
 done
 
-cp $SRC/*.options $OUT/
+cp $SRC/grpc/tools/fuzzer/options/*.options $OUT/
 
 # We don't have a consistent naming convention between fuzzer files and corpus
 # directories so we resort to hard coding zipping corpuses
-zip $OUT/fuzzer_seed_corpus.zip test/core/json/corpus
-zip $OUT/uri_fuzzer_test_seed_corpus.zip test/core/client_channel/uri_corpus
-zip $OUT/request_fuzzer_seed_corpus.zip test/core/http/request_corpus
-zip $OUT/response_fuzzer_seed_corpus.zip test/core/http/response_corpus
-zip $OUT/fuzzer_response_seed_corpus.zip test/core/nanopb/corpus_response
-zip $OUT/fuzzer_serverlist_seed_corpus.zip test/core/nanopb/corpus_serverlist
-zip $OUT/percent_decode_fuzzer_seed_corpus.zip test/core/slice/percent_decode_corpus
-zip $OUT/percent_encode_fuzzer_seed_corpus.zip test/core/slice/percent_encode_corpus
-zip $OUT/hpack_parser_fuzzer_test_seed_corpus.zip test/core/transport/chttp2/hpack_parser_corpus
-zip $OUT/api_fuzzer_seed_corpus.zip test/core/end2end/fuzzers/api_fuzzer_corpus
-zip $OUT/client_fuzzer_seed_corpus.zip test/core/end2end/fuzzers/client_fuzzer_corpus
-zip $OUT/server_fuzzer_seed_corpus.zip test/core/end2end/fuzzers/server_fuzzer_corpus
-# TODO: zip ssl server corpus after Bazel fuzzer rules written
-# test/core/security/corpus/ssl_server_corpus
+zip $OUT/fuzzer_seed_corpus.zip test/core/json/corpus/*
+zip $OUT/uri_fuzzer_test_seed_corpus.zip test/core/client_channel/uri_corpus/*
+zip $OUT/request_fuzzer_seed_corpus.zip test/core/http/request_corpus/*
+zip $OUT/response_fuzzer_seed_corpus.zip test/core/http/response_corpus/*
+zip $OUT/fuzzer_response_seed_corpus.zip test/core/nanopb/corpus_response/*
+zip $OUT/fuzzer_serverlist_seed_corpus.zip test/core/nanopb/corpus_serverlist/*
+zip $OUT/percent_decode_fuzzer_seed_corpus.zip test/core/slice/percent_decode_corpus/*
+zip $OUT/percent_encode_fuzzer_seed_corpus.zip test/core/slice/percent_encode_corpus/*
+zip $OUT/hpack_parser_fuzzer_test_seed_corpus.zip test/core/transport/chttp2/hpack_parser_corpus/*
+zip $OUT/api_fuzzer_seed_corpus.zip test/core/end2end/fuzzers/api_fuzzer_corpus/*
+zip $OUT/client_fuzzer_seed_corpus.zip test/core/end2end/fuzzers/client_fuzzer_corpus/*
+zip $OUT/server_fuzzer_seed_corpus.zip test/core/end2end/fuzzers/server_fuzzer_corpus/*
+zip $OUT/ssl_server_fuzzer_seed_corpus.zip test/core/security/corpus/ssl_server_corpus/*
