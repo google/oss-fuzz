47aa4cb9dd3aebda93ee047b73e3f227bebe0bcc
David Tardon <dtardon@redhat.com>
2017-03-30 01:21:58 +0200

    integrate libmspub (#485)
    * add libmspub
    
    * remove sanitizers field
    
    * integrate libmspub

---
+++ projects/libmspub/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+CXX="$CXX -std=c++11 -stdlib=libc++"
+
+# HACK to link with static icu
+mkdir icu
+cp -p /usr/lib/*/libicu*.a icu
+iculib=$(pwd)/icu
+
+pushd librevenge
+./autogen.sh
+./configure --without-docs --disable-shared --enable-static --disable-tests
+make -j$(nproc)
+rvnginc=$(pwd)/inc
+rvnglib=$(pwd)/src/lib
+popd
+
+./autogen.sh
+./configure --without-docs --disable-shared --enable-static --disable-tools --enable-fuzzers \
+    ICU_CFLAGS="$(pkg-config --cflags icu-i18n)" \
+    ICU_LIBS="-L$iculib $(pkg-config --libs icu-i18n)" \
+    REVENGE_CFLAGS=-I$rvnginc REVENGE_LIBS="-L$rvnglib -lrevenge-0.0" \
+    REVENGE_STREAM_CFLAGS=-I$rvnginc REVENGE_STREAM_LIBS="-L$rvnglib -lrevenge-stream-0.0" \
+    REVENGE_GENERATORS_CFLAGS=-I$rvnginc REVENGE_GENERATORS_LIBS="-L$rvnglib -lrevenge-generators-0.0"
+make -j$(nproc)
+
+cp src/fuzz/*fuzzer $OUT
+cp $SRC/*_seed_corpus.zip $OUT
