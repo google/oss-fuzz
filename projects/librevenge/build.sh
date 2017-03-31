7b4b0f78061882e26c7effc976512432b6a385aa
David Tardon <dtardon@redhat.com>
2017-03-31 14:32:27 +0200

    c++11 is not needed

--- projects/librevenge/build.sh
+++ projects/librevenge/build.sh
@@ -15,8 +15,6 @@
 #
 ################################################################################
 
-CXX="$CXX -std=c++11"
-
 ./autogen.sh
 ./configure --without-docs --disable-shared --enable-static --disable-tests --disable-generators --enable-fuzzers
 make -j$(nproc)
