76bd4a5443a921fed541dc064df13c04b7161efc
David Tardon <dtardon@redhat.com>
2017-04-05 16:45:17 +0200

    add abw corpus (#515)

--- projects/dlplibs/build.sh
+++ projects/dlplibs/build.sh
@@ -168,5 +168,18 @@ pushd libe-book
 make -j$(nproc)
 popd
 
+pushd libabw
+./autogen.sh
+./configure --without-docs --disable-shared --enable-static --disable-tools --enable-fuzzers \
+    LDFLAGS=-L$staticlib \
+    LIBXML_LIBS="-lxml2 -llzma -licuuc -licudata" \
+    REVENGE_CFLAGS=-I$rvnginc REVENGE_LIBS="-L$rvnglib -lrevenge-0.0" \
+    REVENGE_STREAM_CFLAGS=-I$rvnginc REVENGE_STREAM_LIBS="-L$rvnglib -lrevenge-stream-0.0" \
+    REVENGE_GENERATORS_CFLAGS=-I$rvnginc REVENGE_GENERATORS_LIBS="-L$rvnglib -lrevenge-generators-0.0"
+make -j$(nproc)
+popd
+
 cp */src/fuzz/*fuzzer $OUT
 cp *_seed_corpus.zip $OUT
+cp *.options $OUT
+cp *.dict $OUT
