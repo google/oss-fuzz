3aa1b52a2d1b0282baef7675c13225da13b404b5
Alex Gaynor <alex.gaynor@gmail.com>
2017-04-17 19:11:39 -0400

    [WIP] Added libssh fuzzer (#418)

---
+++ projects/libssh/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+mkdir -p build
+pushd build
+cmake -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
+    -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+    -DWITH_STATIC_LIB=ON ..
+make "-j$(nproc)"
+popd
+
+$CXX $CXXFLAGS -std=c++11 -Iinclude/ \
+    "$SRC/libssh_server_fuzzer.cc" -o "$OUT/libssh_server_fuzzer" \
+    -lFuzzingEngine ./build/src/libssh.a -Wl,-Bstatic -lcrypto -lz -Wl,-Bdynamic
