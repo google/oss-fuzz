bab0e6c54d680cf325b192655cbb3a0f0f7ddc1d
Oliver Chang <oliverchang@users.noreply.github.com>
2017-04-24 14:59:36 -0700

    Update build.sh

--- projects/brotli/build.sh
+++ projects/brotli/build.sh
@@ -5,7 +5,7 @@ make clean
 make -j$(nproc) brotlidec
 
 $CXX $CXXFLAGS -std=c++11 -I. \
-    c/fuzz/decode_fuzzer.cc -I./include -o $OUT/decode_fuzzer \
+    c/fuzz/decode_fuzzer.cc -I./c/include -o $OUT/decode_fuzzer \
     -lFuzzingEngine ./libbrotlidec.a ./libbrotlicommon.a
 
 cp java/org/brotli/integration/fuzz_data.zip $OUT/decode_fuzzer_seed_corpus.zip
