d5df9df1aaa0de1be39c17226f27745e5f3112fb
Kevin Lubick <kjlubick@google.com>
2017-05-09 17:05:03 -0400

    [skia] fix build

--- projects/skia/build.sh
+++ projects/skia/build.sh
@@ -21,7 +21,8 @@ $SRC/depot_tools/gn gen out/Fuzz\
     --args='cc="'$CC'"
     cxx="'$CXX'"
     is_debug=false
-    extra_cflags=["'"$CXXFLAGS_ARR"'","-DIS_FUZZING"]
+    extra_cflags=["'"$CXXFLAGS_ARR"'","-DIS_FUZZING",
+        "-Wno-zero-as-null-pointer-constant", "-Wno-unused-template"]
     skia_use_system_freetype2=false
     skia_use_fontconfig=false
     skia_enable_gpu=false
