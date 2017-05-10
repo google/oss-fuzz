e5b8e544a5468206ed7a33434a810efe1d6ef360
Abhishek Arya <inferno@chromium.org>
2017-05-10 12:29:21 -0700

    Remove unneeded quotes, see boringssl/build.sh

--- projects/openssl/build.sh
+++ projects/openssl/build.sh
@@ -16,7 +16,7 @@
 ################################################################################
 
 CONFIGURE_FLAGS=""
-if [[ $CFLAGS = "*sanitize=memory*" ]]
+if [[ $CFLAGS = *sanitize=memory* ]]
 then
   CONFIGURE_FLAGS="no-asm"
 fi
