3666c6bf985aea62bbb145acda1528bff936a689
Abhishek Arya <inferno@chromium.org>
2017-05-10 12:47:59 -0700

    Add missing space.

--- projects/openssl/build.sh
+++ projects/openssl/build.sh
@@ -21,7 +21,7 @@ then
   CONFIGURE_FLAGS="no-asm"
 fi
 
-./config --debug enable-fuzz-libfuzzer -DPEDANTIC -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION no-shared enable-tls1_3 enable-rc5 enable-md2 enable-ec_nistp_64_gcc_128 enable-ssl3 enable-ssl3-method enable-nextprotoneg enable-weak-ssl-ciphers --with-fuzzer-lib=/usr/lib/libFuzzingEngine $CFLAGS -fno-sanitize=alignment$CONFIGURE_FLAGS
+./config --debug enable-fuzz-libfuzzer -DPEDANTIC -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION no-shared enable-tls1_3 enable-rc5 enable-md2 enable-ec_nistp_64_gcc_128 enable-ssl3 enable-ssl3-method enable-nextprotoneg enable-weak-ssl-ciphers --with-fuzzer-lib=/usr/lib/libFuzzingEngine $CFLAGS -fno-sanitize=alignment $CONFIGURE_FLAGS
 
 make -j$(nproc) LDCMD="clang++ $CXXFLAGS"
 
