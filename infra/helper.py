47be7a1b7d349a21ebc56647abb1b72ba1c7b06d
Abhishek Arya <inferno@chromium.org>
2017-05-10 14:09:55 -0700

    Add valgrind support in infra/helper.py, fixes #592.

--- infra/helper.py
+++ infra/helper.py
@@ -87,11 +87,13 @@ def main():
 
   reproduce_parser = subparsers.add_parser(
       'reproduce', help='Reproduce a crash.')
+  reproduce_parser.add_argument('--valgrind', action='store_true',
+                       help='run with valgrind')
   reproduce_parser.add_argument('project_name', help='name of the project')
   reproduce_parser.add_argument('fuzzer_name', help='name of the fuzzer')
   reproduce_parser.add_argument('testcase_path', help='path of local testcase')
   reproduce_parser.add_argument('fuzzer_args', help='arguments to pass to the fuzzer',
-                      nargs=argparse.REMAINDER)
+                       nargs=argparse.REMAINDER)
 
   shell_parser = subparsers.add_parser(
       'shell', help='Run /bin/bash in an image.')
@@ -383,10 +385,21 @@ def reproduce(args):
   if not _check_fuzzer_exists(args.project_name, args.fuzzer_name):
     return 1
 
-  run_args = [
+  debugger = ''
+  env = []
+  image_name = 'base-runner'
+
+  if args.valgrind:
+    debugger = 'valgrind --tool=memcheck --track-origins=yes --leak-check=full'
+
+  if debugger:
+    image_name = 'base-runner-debug'
+    env += ['DEBUGGER=' + debugger]
+
+  run_args = sum([['-e', v] for v in env], []) + [
       '-v', '%s:/out' % os.path.join(BUILD_DIR, 'out', args.project_name),
       '-v', '%s:/testcase' % _get_absolute_path(args.testcase_path),
-      '-t', 'gcr.io/oss-fuzz-base/base-runner',
+      '-t', 'gcr.io/oss-fuzz-base/%s' % image_name,
       'reproduce',
       args.fuzzer_name,
       '-runs=100',
