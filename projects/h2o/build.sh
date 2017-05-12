c4054c4d8e3f0b01223cef9a05ce4b9553338f91
Jonathan Foote <jmfoote@loyola.edu>
2017-05-12 18:48:55 -0400

    add url handler fuzz target (#601)

--- projects/h2o/build.sh
+++ projects/h2o/build.sh
@@ -18,10 +18,11 @@
 pushd $SRC/h2o
 cmake -DBUILD_FUZZER=ON -DOSS_FUZZ=ON -DOPENSSL_USE_STATIC_LIBS=TRUE .
 make
-cp ./h2o-fuzzer-http* $OUT/
+cp ./h2o-fuzzer-* $OUT/
 
 zip -jr $OUT/h2o-fuzzer-http1_seed_corpus.zip $SRC/h2o/fuzz/http1-corpus
 zip -jr $OUT/h2o-fuzzer-http2_seed_corpus.zip $SRC/h2o/fuzz/http2-corpus
+zip -jr $OUT/h2o-fuzzer-url_seed_corpus.zip $SRC/h2o/fuzz/url-corpus
 
 cp $SRC/*.options $SRC/h2o/fuzz/*.dict $OUT/
 popd
