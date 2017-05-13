c06153a2f0b88b2eb892aa955f2b6fa1e52fdc09
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2017-05-12 18:01:34 -0700

    add corpora, fix a type in find command

--- projects/example/build.sh
+++ projects/example/build.sh
@@ -20,5 +20,7 @@ make clean  # Not strictly necessary, since we are building in fresh dir.
 make -j$(nproc) all    # Build the fuzz targets.
 make -j$(nproc) check  # Sanity check, not strictly required, but nice to have.
 
-# Copy the fuzzer executables to $OUT
-find . -name '*_fuzzer' -exec cp -v '{}' $OUT
+# Copy the fuzzer executables and zip-ed corpora to $OUT
+find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
+find . -name '*_fuzzer_seed_corpus.zip' -exec cp -v '{}' $OUT ';'
+
