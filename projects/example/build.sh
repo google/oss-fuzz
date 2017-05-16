9b6ba4dc148c38e85c3f71d8d468d92b28f9e353
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2017-05-15 17:53:08 -0700

    Update build.sh

--- projects/example/build.sh
+++ projects/example/build.sh
@@ -20,6 +20,8 @@ make clean  # Not strictly necessary, since we are building in a fresh dir.
 make -j$(nproc) all    # Build the fuzz targets.
 make -j$(nproc) check  # Sanity check, not strictly required, but nice to have.
 
-# Copy the fuzzer executables and zip-ed corpora to $OUT
+# Copy the fuzzer executables, zip-ed corpora, option and dictionary files to $OUT
 find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
+find . -name '*_fuzzer.dict' -exec cp -v '{}' $OUT ';'
+find . -name '*_fuzzer.options' -exec cp -v '{}' $OUT ';'
 find . -name '*_fuzzer_seed_corpus.zip' -exec cp -v '{}' $OUT ';'
