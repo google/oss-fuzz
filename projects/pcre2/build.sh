google/oss-fuzz

commit c2cdffc0c1178fcaf75b2144d8127274180c0127

Update build.sh (#623)
I have moved the cp command to the end of the file, as requested.

projects/pcre2/build.sh
@@ -24,3 +24,6 @@ make -j$(nproc) all
 # build fuzzer
 $CXX $CXXFLAGS -o $OUT/pcre2_fuzzer \
     -lFuzzingEngine .libs/libpcre2-fuzzsupport.a .libs/libpcre2-8.a
+
+# set up dictionary and options to use it
+cp $SRC/pcre2_fuzzer.options $SRC/pcre2_fuzzer.dict $OUT/

commit 11adc754c7b45f5d1e84cc93e5a110b6753fc9cb

Fix build breakage after #623.

projects/pcre2/build.sh
@@ -26,4 +26,4 @@ $CXX $CXXFLAGS -o $OUT/pcre2_fuzzer \
     -lFuzzingEngine .libs/libpcre2-fuzzsupport.a .libs/libpcre2-8.a
 
 # set up dictionary and options to use it
-cp $SRC/pcre2_fuzzer.options $SRC/pcre2_fuzzer.dict $OUT/
+cp pcre2_fuzzer.options pcre2_fuzzer.dict $OUT/