0de3b4f23d0cc6993d30793d8b50390fbafc7b2e
Paras Chetal <paras.chetal@gmail.com>
2017-06-09 05:05:08 +0530

    Add the outline for fuzzer build

--- projects/qubes-os/build.sh
+++ projects/qubes-os/build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 export BACKEND_VMM=xen
+
+
+cd $SRC/qubes-os/linux-utils/
+git checkout origin/fuzz
+
+cd qrexec-lib
+
+$CC $CFLAGS -c ioall.c
+$CC $CFLAGS -c copy-file.c
+$CC $CFLAGS -c crc32.c
+$CC $CFLAGS -c pack.c
+$CC $CFLAGS -c unpack.c
+ar rcs libqubes-rpc-filecopy.a ioall.o copy-file.o crc32.o unpack.o pack.o
+
+$CXX $CXXFLAGS -o $OUT/fuzzer-qubes-rpc-filecopy -I. -I./fuzzer fuzzer/fuzzer.cc -lFuzzingEngine libqubes-rpc-filecopy.a
+
