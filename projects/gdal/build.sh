2ba5aa07c7cf72f7811affded7834d93080ae42c
Even Rouault <even.rouault@spatialys.com>
2017-06-15 11:22:49 +0200

    GDAL: move download of curl and netcdf sources in Dockerfile

--- projects/gdal/build.sh
+++ projects/gdal/build.sh
@@ -16,9 +16,6 @@
 ################################################################################
 
 # build libcurl.a (builing against Ubuntu libcurl.a doesn't work easily)
-if [[ ! -d curl ]]; then
-    git clone --depth 1 https://github.com/curl/curl.git
-fi
 cd curl
 ./buildconf
 ./configure --disable-shared --prefix=$SRC/install
@@ -28,15 +25,9 @@ make install
 cd ..
 
 # build libnetcdf.a
-if [[ ! -d netcdf-4.4.1.1 ]]; then
-    curl ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.1.1.tar.gz > netcdf-4.4.1.1.tar.gz
-    tar xzf netcdf-4.4.1.1.tar.gz
-    cd netcdf-4.4.1.1
-    patch -p0 < $SRC/NC4_put_propattr_leak_fix.patch
-    mkdir build
-    cd ..
-fi
-cd netcdf-4.4.1.1/build
+cd netcdf-4.4.1.1
+mkdir -p build
+cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$SRC/install -DHDF5_C_LIBRARY=libhdf5_serial.a -DHDF5_HL_LIBRARY=libhdf5_serial_hl.a -DHDF5_INCLUDE_DIR=/usr/include/hdf5/serial -DENABLE_DAP:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_UTILITIES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DENABLE_TESTS:BOOL=OFF
 make clean -s
 make -j$(nproc) -s
