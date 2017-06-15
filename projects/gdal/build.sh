606d84b729cea785b2b51dcde0c1ddb3995f89fe
Even Rouault <even.rouault@mines-paris.org>
2017-06-15 17:37:04 +0200

    GDAL: add libcurl dependency (#677)
    * GDAL: add libcurl dependency
    
    * GDAL: move download of curl and netcdf sources in Dockerfile
    
    * GDAL: add patch for libnetcdf
    
    This fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2265
    
    Patch submitted in libnetcdf upstream per
    https://github.com/Unidata/netcdf-c/pull/427

--- projects/gdal/build.sh
+++ projects/gdal/build.sh
@@ -15,12 +15,18 @@
 #
 ################################################################################
 
+# build libcurl.a (builing against Ubuntu libcurl.a doesn't work easily)
+cd curl
+./buildconf
+./configure --disable-shared --prefix=$SRC/install
+make clean -s
+make -j$(nproc) -s
+make install
+cd ..
+
 # build libnetcdf.a
-curl ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.1.1.tar.gz > netcdf-4.4.1.1.tar.gz
-tar xzf netcdf-4.4.1.1.tar.gz
 cd netcdf-4.4.1.1
-patch -p0 < $SRC/NC4_put_propattr_leak_fix.patch
-mkdir build
+mkdir -p build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$SRC/install -DHDF5_C_LIBRARY=libhdf5_serial.a -DHDF5_HL_LIBRARY=libhdf5_serial_hl.a -DHDF5_INCLUDE_DIR=/usr/include/hdf5/serial -DENABLE_DAP:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_UTILITIES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DENABLE_TESTS:BOOL=OFF
 make clean -s
@@ -31,7 +37,7 @@ cd ../..
 # build gdal
 cd gdal
 export LDFLAGS=${CXXFLAGS}
-./configure --without-libtool --with-liblzma --with-expat --with-sqlite3 --with-xerces --with-webp --with-netcdf=$SRC/install --without-curl --without-hdf5 --with-jpeg=internal
+./configure --without-libtool --with-liblzma --with-expat --with-sqlite3 --with-xerces --with-webp --with-netcdf=$SRC/install --with-curl=$SRC/install --without-hdf5 --with-jpeg=internal
 make clean -s
 make -j$(nproc) -s static-lib
 
@@ -40,6 +46,8 @@ export EXTRA_LIBS="-Wl,-Bstatic -lwebp -llzma -lexpat -lsqlite3 -lgif -lpng12 -l
 export EXTRA_LIBS="$EXTRA_LIBS -lxerces-c -licuuc -licudata"
 # netCDF related
 export EXTRA_LIBS="$EXTRA_LIBS -L$SRC/install/lib -lnetcdf -lhdf5_serial_hl -lhdf5_serial -lsz -laec -lz"
+# curl related
+export EXTRA_LIBS="$EXTRA_LIBS -L$SRC/install/lib -lcurl -lssl -lcrypto -lz"
 export EXTRA_LIBS="$EXTRA_LIBS -Wl,-Bdynamic -ldl -lpthread"
 ./fuzzers/build_google_oss_fuzzers.sh
 ./fuzzers/build_seed_corpus.sh
