58e0dd5542c95a5a0a86a6c705f2163f322abb20
Paras Chetal <paras.chetal@gmail.com>
2017-06-16 16:23:21 +0530

    Integrate libqubes-rpc-filecopy

--- projects/qubes-os/build.sh
+++ projects/qubes-os/build.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
-
+#!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
 export BACKEND_VMM=xen
 
 
 cd $SRC/qubes-os/linux-utils/
-git checkout origin/fuzz
 
 cd qrexec-lib
 
@@ -15,5 +28,6 @@ $CC $CFLAGS -c pack.c
 $CC $CFLAGS -c unpack.c
 ar rcs libqubes-rpc-filecopy.a ioall.o copy-file.o crc32.o unpack.o pack.o
 
-$CXX $CXXFLAGS -o $OUT/fuzzer-qubes-rpc-filecopy -I. -I./fuzzer fuzzer/fuzzer.cc -lFuzzingEngine libqubes-rpc-filecopy.a
+$CXX $CXXFLAGS -o $OUT/libqubes-rpc-filecopy -I. -I./fuzzer fuzzer/fuzzer.cc -lFuzzingEngine libqubes-rpc-filecopy.a
 
+cp $SRC/*.options $OUT/
