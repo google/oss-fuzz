270f4d7f9f93ca3265eb100e7f3ff6bbc043e183
levi-wolfssl <levi@wolfssl.com>
2017-07-14 14:39:47 -0600

    [wolfSSL] build files (#715)
    * [wolfSSL] build files
    
    * [wolfSSL] rework build file to use external make

---
+++ projects/wolfssl/Dockerfile
@@ -0,0 +1,26 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+FROM gcr.io/oss-fuzz-base/base-builder
+MAINTAINER levi@wolfssl.com
+
+RUN apt-get update && apt-get install -y make autoconf automake libtool zip
+RUN git clone https://github.com/wolfssl/wolfssl --depth 1 $SRC/wolfssl
+RUN git clone https://github.com/wolfssl/oss-fuzz-targets --depth 1 $SRC/fuzz-targets
+
+WORKDIR wolfssl
+
+COPY build.sh $SRC/
---
+++ projects/wolfssl/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eu
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# target_dir determined by Dockerfile
+target_dir="$SRC/fuzz-targets"
+
+# build wolfssl
+./autogen.sh
+./configure --enable-static --disable-shared --prefix=/usr CC="clang"
+make -j "$(nproc)" all
+make install
+
+# put linker arguments into the environment, appending to any existing ones
+export LDFLAGS="${LDFLAGS-""}"
+export LDLIBS="${LDLIBS-""} -lwolfssl -lFuzzingEngine"
+
+# make and export targets to $OUT; environment overridding internal variables
+cd "${target_dir}"
+make -e all
+make -e export prefix="$OUT"
