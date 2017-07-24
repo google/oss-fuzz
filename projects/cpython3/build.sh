f54086c22ae7d8fb0635970211c899e4f0e09e2b
Devin Jeanpierre <jeanpierreda@google.com>
2017-07-24 03:00:40 -0700

    Fix issues pointed out in https://github.com/google/oss-fuzz/pull/731:
    * It's 2017
    * $CC, not clang.
    
    Did not (yet) fix anything else -- e.g. where the script should live, or if it
    should use cpp or m4 or something instead of Python. ;)

--- projects/cpython3/build.sh
+++ projects/cpython3/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2016 Google Inc.
+# Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 ################################################################################
 
 # Workaround for distutils, which doesn't copy $CC except on OSX.
-export LDSHARED="clang -shared"
+export LDSHARED="$CC -shared"
 # Workaround for PGen, which doesn't use CFLAGS for linking
 export LDFLAGS="$CFLAGS"
 # _freeze_importlib, and presumably all other binaries (including the fuzzer),
