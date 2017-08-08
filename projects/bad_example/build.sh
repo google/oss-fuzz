f81476ca75b3fec3b74a3e068e5c96b1f05059cb
Max Moroz <dor3s1@gmail.com>
2017-08-08 10:54:53 -0700

    [infra] Add post build checks for catching and reporting bad builds + add test projects. (#754)
    * [WIP] Add post build checks for catching and reporting bad builds + test projects.
    
    * Move all type of issues into a single project. Tested all sanitizers.
    
    * First version of the script for performing bad build checks. Still not ready to commit.\
    
    * Added a valid example that should pass all the checks + fixed instrumentation check.
    
    * Use SIGTERM and the handler calling _exit(0) for the startup crash case.
    
    * Small fixes.
    
    * Ignore startup_crash check_for fuzzing engines other than libFuzzer.
    
    * Use "local" for local variables in the script, fix container name.
    
    * Add COPY step for bad_build_check into base-runner Dockerfile.
    
    * Make bad_build_check temporary noop unless EXPERIMENTAL_BAD_BUILD_CHECK is set to 1.
    
    * Temporary remove test binaries step.
    
    * Remove empty line.
    
    * Remove unnecessary env variable.

---
+++ projects/bad_example/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -eu
+
+# Testcase 1. Valid fuzzer build.
+################################################################################
+./configure
+make -j$(nproc) clean
+make -j$(nproc) all
+
+$CXX $CXXFLAGS -std=c++11 -I. \
+    $SRC/bad_example_fuzzer.cc -o $OUT/bad_example_valid_build \
+    -lFuzzingEngine ./libz.a
+
+
+# Testcase 2. Silent startup crash.
+################################################################################
+./configure
+make -j$(nproc) clean
+make -j$(nproc) all
+
+$CXX $CXXFLAGS -std=c++11 -I. -DINTENTIONAL_STARTUP_CRASH \
+    $SRC/bad_example_fuzzer.cc -o $OUT/bad_example_startup_crash \
+    -lFuzzingEngine ./libz.a
+
+
+# Testcase 3. Ignore the flags provided by OSS-Fuzz.
+################################################################################
+export CFLAGS="-O1"
+export CXXFLAGS="-O1 -stdlib=libc++"
+
+./configure
+make -j$(nproc) clean
+make -j$(nproc) all
+
+$CXX -fsanitize=$SANITIZER $CXXFLAGS -std=c++11 -I. \
+    $SRC/bad_example_fuzzer.cc -o $OUT/bad_example_no_instrumentation \
+    -lFuzzingEngine ./libz.a
+
+
+# Testcase 4. Enable multiple sanitizers.
+################################################################################
+# Add UBSan to ASan or MSan build. Add ASan to UBSan build.
+EXTRA_SANITIZER="undefined"
+if [[ $SANITIZER = *undefined* ]]
+then
+  EXTRA_SANITIZER="address"
+fi
+
+export CFLAGS="-O1 -fsanitize=$SANITIZER,$EXTRA_SANITIZER -fsanitize-coverage=trace-pc-guard,trace-cmp"
+export CXXFLAGS="-O1 -fsanitize=$SANITIZER,$EXTRA_SANITIZER -fsanitize-coverage=trace-pc-guard,trace-cmp -stdlib=libc++"
+
+./configure
+make -j$(nproc) clean
+make -j$(nproc) all
+
+$CXX $CXXFLAGS -std=c++11 -I. \
+    $SRC/bad_example_fuzzer.cc -o $OUT/bad_example_mixed_sanitizers \
+    -lFuzzingEngine ./libz.a
