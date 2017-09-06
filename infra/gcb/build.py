fa78fa9963c4ef48da660c61d6d9416063080e0a
Max Moroz <mmoroz@chromium.org>
2017-09-06 16:38:59 -0700

    [infra] Temporary revert bad_build_check until most of the targets get fixed.
    Need to take a closer look at the following issues:
    - https://github.com/google/oss-fuzz/issues/815
    - https://github.com/google/oss-fuzz/issues/816
    - https://github.com/google/oss-fuzz/issues/817
    - https://github.com/google/oss-fuzz/issues/818
    - https://github.com/google/oss-fuzz/issues/819
    - https://github.com/google/oss-fuzz/issues/820
    - https://github.com/google/oss-fuzz/issues/821
    - https://github.com/google/oss-fuzz/issues/822
    - https://github.com/google/oss-fuzz/issues/823
    - https://github.com/google/oss-fuzz/issues/824
    - https://github.com/google/oss-fuzz/issues/825
    - https://github.com/google/oss-fuzz/issues/826
    - https://github.com/google/oss-fuzz/issues/827
    - https://github.com/google/oss-fuzz/issues/828
    - https://github.com/google/oss-fuzz/issues/829
    - https://github.com/google/oss-fuzz/issues/830

--- infra/gcb/build.py
+++ infra/gcb/build.py
@@ -185,9 +185,6 @@ def get_build_steps(project_yaml, dockerfile_path):
 
       env.append('OUT=' + out)
 
-      # To disable running of all fuzz targets while doing |test_all| step.
-      env.append('SKIP_TEST_TARGET_RUN=1')
-
       workdir = workdir_from_dockerfile(dockerfile_path)
       if not workdir:
         workdir = '/src'
@@ -208,17 +205,6 @@ def get_build_steps(project_yaml, dockerfile_path):
               # Container Builder doesn't pass --rm to docker run yet.
               'rm -r /out && cd /src && cd {1} && mkdir -p {0} && compile && rm -rf /work && rm -rf /src'.format(out, workdir),
             ],
-          },
-          # test binaries
-          {'name': 'gcr.io/oss-fuzz-base/base-runner',
-            'env': env,
-            'args': [
-              'bash',
-              '-c',
-              # Verify that fuzzers have been built properly and are not broken.
-              # TODO(mmoroz): raise a notification if not passing the tests.
-              'test_all'
-            ]
           },
           # zip binaries
           {'name': image,
