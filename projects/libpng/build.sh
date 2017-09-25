dcdd9afb14d39e960dfe7b3c6f3e3c16e21f9733
Glenn Randers-Pehrson <glennrp@gmail.com>
2017-09-25 05:17:32 -0400

    Use "-O0" in an attempt to prevent the compiler from optimizing out
    the core of the fuzzer as "dead code".

--- projects/libpng/build.sh
+++ projects/libpng/build.sh
@@ -37,7 +37,7 @@ make -j$(nproc) clean
 make -j$(nproc) libpng16.la
 
 # build libpng_read_fuzzer.
-$CXX $CXXFLAGS -std=c++11 -I. \
+$CXX $CXXFLAGS -O0 -std=c++11 -I. \
      $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer.cc \
      -o $OUT/libpng_read_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
