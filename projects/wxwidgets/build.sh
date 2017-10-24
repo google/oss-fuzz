ee358c11e4a62b1b186a5bc4a430890eef006343
VZ <vz-github@zeitlins.org>
2017-10-25 01:19:16 +0200

    Add wxWidgets project (#913)
    Add the project file and simple Dockerfile and the build script using
    the fuzzer source in the main wxWidgets repository itself.

---
+++ projects/wxwidgets/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eu
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# build project
+./configure --without-subdirs --disable-sys-libs --disable-gui
+make -j$(nproc) wxbase
+
+# build fuzzers
+$CXX $CXXFLAGS -o $OUT/zip ./tests/fuzz/zip.cpp \
+    -lFuzzingEngine `./wx-config --cxxflags --libs base`
+
+# and copy their corpora
+zip -j $OUT/zip_seed_corpus.zip $SRC/wxwidgets/tests/fuzz/corpus/zip/*
