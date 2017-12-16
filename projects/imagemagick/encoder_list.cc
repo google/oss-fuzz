7798cea767714493282a17e2521f9275cdbfc53f
Alex Gaynor <alex.gaynor@gmail.com>
2017-12-15 21:46:26 -0600

    formatting!

--- projects/imagemagick/encoder_list.cc
+++ projects/imagemagick/encoder_list.cc
@@ -3,16 +3,15 @@
 #include <Magick++/Blob.h>
 #include <Magick++/Image.h>
 
-
 extern "C" int main() {
-    size_t nFormats;
-    Magick::ExceptionInfo ex;
-    const Magick::MagickInfo **formats = GetMagickInfoList("*", &nFormats, &ex);
+  size_t nFormats;
+  Magick::ExceptionInfo ex;
+  const Magick::MagickInfo **formats = GetMagickInfoList("*", &nFormats, &ex);
 
-    for (size_t i = 0; i < nFormats; i++) {
-      const Magick::MagickInfo *format = formats[i];
-      if (format->encoder && format->name) {
-          std::cout << format->name << std::endl;
-      }
+  for (size_t i = 0; i < nFormats; i++) {
+    const Magick::MagickInfo *format = formats[i];
+    if (format->encoder && format->name) {
+      std::cout << format->name << std::endl;
     }
+  }
 }
