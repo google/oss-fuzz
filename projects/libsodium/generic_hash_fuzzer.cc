0131232fa089f7d9d7317ca9a776b081a7b31b75
Chris Wolfe <chriswwolfe@gmail.com>
2017-12-18 21:12:49 -0600

    initial commits

---
+++ projects/libsodium/generic_hash_fuzzer.cc
@@ -0,0 +1,19 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+
+#include <string>
+#include "sodium.h"
+
+using std::string;
+
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  if (sodium_init() == -1) {
+    return 1;
+  }
+
+  unsigned char hash[crypto_generichash_BYTES];
+  crypto_generichash(hash, sizeof hash,
+                     data, size,
+                     NULL, 0);
+  return 0;
+}
