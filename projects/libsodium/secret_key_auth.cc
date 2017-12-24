0758a6d0c668e977ad7c50ca49d99c078300d565
Chris Wolfe <chriswwolfe@gmail.com>
2017-12-24 09:43:12 -0600

    add secret key auth fuzzer

---
+++ projects/libsodium/secret_key_auth.cc
@@ -0,0 +1,23 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+
+#include <string>
+extern "C" {
+#include <sodium.h>
+}
+
+using std::string;
+
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  if (sodium_init() == -1) {
+    return 1;
+  }
+
+  unsigned char key[crypto_auth_KEYBYTES];
+  unsigned char mac[crypto_auth_BYTES];
+
+  crypto_auth_keygen(key);
+  crypto_auth(mac, data, size, key);
+
+  crypto_auth_verify(mac, data, size, key);
+}
