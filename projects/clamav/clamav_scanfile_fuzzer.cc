423689a4db112423e7fb323c8163d1f95735907f
Alex Gaynor <alex.gaynor@gmail.com>
2017-12-26 20:01:44 -0500

    Added a fuzzer for clamav
    Refs #402

---
+++ projects/clamav/clamav_scanfile_fuzzer.cc
@@ -0,0 +1,70 @@
+/*
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+*/
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <memory>
+
+#include <clamav.h>
+
+
+void clamav_message_callback(enum cl_msg severity, const char *fullmsg,
+                             const char *msg, void *context) {
+}
+
+class ClamAVState {
+public:
+    ClamAVState() {
+        // Silence all the log messages, none of them are meaningful.
+        cl_set_clcb_msg(clamav_message_callback);
+
+        cl_init(CL_INIT_DEFAULT);
+        engine = cl_engine_new();
+        cl_engine_compile(engine);
+    }
+
+    ~ClamAVState() {
+        cl_engine_free(engine);
+    }
+
+    struct cl_engine *engine;
+};
+
+// Global with static initializer to setup an engine so we don't need to do
+// that on each execution.
+ClamAVState kClamAVState;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    cl_fmap_t *clamav_data = cl_fmap_open_memory(data, size);
+
+    const char *virus_name = nullptr;
+    unsigned long scanned = 0;
+    cl_scanmap_callback(
+        clamav_data,
+        &virus_name,
+        &scanned,
+        kClamAVState.engine,
+        CL_SCAN_STDOPT,
+        nullptr
+    );
+
+    cl_fmap_close(clamav_data);
+
+    return 0;
+}
