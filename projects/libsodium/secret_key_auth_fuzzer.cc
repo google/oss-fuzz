afdc9d59d6349e59382a8ade9fe4cc9cb943a148
Chris Wolfe <chriswwolfe@gmail.com>
2017-12-30 20:16:58 -0600

    global initializer for secret_key_auth_fuzzer

--- projects/libsodium/secret_key_auth_fuzzer.cc
+++ projects/libsodium/secret_key_auth_fuzzer.cc
@@ -5,18 +5,21 @@ extern "C" {
 
 using std::string;
 
-extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
-  if (sodium_init() == -1) {
-    return 0;
-  }
-
+class SodiumState {
+public:
   unsigned char key[crypto_auth_KEYBYTES];
   unsigned char mac[crypto_auth_BYTES];
 
-  crypto_auth_keygen(key);
+  SodiumState() {
+    sodium_init();
+    crypto_auth_keygen(key);
+  }
+};
 
-  crypto_auth(mac, data, size, key);
-  crypto_auth_verify(mac, data, size, key);
+SodiumState state;
 
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  crypto_auth(state.mac, data, size, state.key);
+  crypto_auth_verify(state.mac, data, size, state.key);
   return 0;
 }
