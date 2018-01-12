3d710a8e91ceb51b6e3d232e48d6dc54c4e4e82a
Jonathan Rudenberg <jonathan@titanous.com>
2018-01-12 17:55:11 -0500

    Silence stderr spam

--- projects/mupdf/build.sh
+++ projects/mupdf/build.sh
@@ -22,7 +22,7 @@ $CXX $CXXFLAGS -std=c++11 -Iinclude \
     source/fuzz/pdf_fuzzer.cc -o $OUT/$fuzz_target \
     -lFuzzingEngine $WORK/libmupdf.a $WORK/libmupdfthird.a
 
-mv $SRC/*.zip $SRC/*.dict $OUT
+mv $SRC/{*.zip,*.dict,*.options} $OUT
 
 if [ ! -f "${OUT}/${fuzz_target}_seed_corpus.zip" ]; then
   echo "missing seed corpus"
@@ -31,5 +31,10 @@ fi
 
 if [ ! -f "${OUT}/${fuzz_target}.dict" ]; then
   echo "missing dictionary"
+  exit 1
+fi
+
+if [ ! -f "${OUT}/${fuzz_target}.options" ]; then
+  echo "missing options"
   exit 1
 fi
