25c2b85eebaf27d6e508d3ace524a238f01aff96
Chris Wolfe <chriswwolfe@gmail.com>
2018-01-12 16:26:31 -0600

    this is working, try to extract...

--- projects/libsodium/secretbox_easy_fuzzer.cc
+++ projects/libsodium/secretbox_easy_fuzzer.cc
@@ -1,10 +1,38 @@
 #include <assert.h>
 #include <sodium.h>
-#include <string.h>
+#include <stdint.h>
 
-#include "fake_random.h"
+// #include "fake_random.h"
+// extern struct randombytes_implementation fake_random;
 
-extern struct randombytes_implementation fake_random;
+static const char *
+fake_implementation_name(void) {
+  return "fake_random";
+}
+
+static uint32_t
+fake_randombytes(void) {
+  return 0;
+}
+
+static void
+fake_random_buffer(void * const buf, const size_t size) {
+  static const unsigned char constant_seed[randombytes_SEEDBYTES] = {   \
+    'N', 'O', 'T', 'E', 'S', 'T', 'D', 'A', 'T', 'A',                   \
+    'N', 'O', 'T', 'E', 'S', 'T', 'D', 'A', 'T', 'A',                   \
+    'N', 'O', 'T', 'E', 'S', 'T', 'D', 'A', 'T', 'A',                   \
+    'N', '0'};
+  randombytes_buf_deterministic(buf, size, constant_seed);
+}
+
+struct randombytes_implementation fake_random = {
+  .implementation_name = fake_implementation_name,
+  .random = fake_randombytes,
+  .stir = NULL,
+  .uniform = NULL,
+  .buf = fake_random_buffer,
+  .close = NULL
+};
 
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
   assert(randombytes_set_implementation(&fake_random) == 0);
