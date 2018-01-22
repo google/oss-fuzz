61cec32e267c5b1574b589c7b1eeab1b625831a8
Jonathan Rudenberg <jonathan@titanous.com>
2018-01-22 11:20:12 -0500

    [mupdf] Ensure ctm variable is initialized (#1102)

--- projects/mupdf/pdf_fuzzer.cc
+++ projects/mupdf/pdf_fuzzer.cc
@@ -25,7 +25,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   fz_register_document_handlers(ctx);
 
   fz_stream *stream = fz_open_memory(ctx, data, size);
-  fz_matrix ctm;
+  fz_matrix ctm = fz_identity;
   fz_pixmap *pix = NULL;
   fz_document *doc = NULL;
   fz_try(ctx) {
