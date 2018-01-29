d4c7cf3511b92e5af03fc6aea732ee3e624a867b
Chris Wolfe <chriswwolfe@gmail.com>
2018-01-28 20:15:29 -0600

    free the object; the fuzzer was responsible for the leak

--- projects/json-c/tokener_parse_ex_fuzzer.cc
+++ projects/json-c/tokener_parse_ex_fuzzer.cc
@@ -4,7 +4,11 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
   json_tokener *tok = json_tokener_new();
-  json_tokener_parse_ex(tok, data, size);
+  json_object *obj;
+
+  obj = json_tokener_parse_ex(tok, data, size);
+
+  json_object_put(obj);
   json_tokener_free(tok);
   return 0;
 }
