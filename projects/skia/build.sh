78b26af6fd413d53e977f804c80a24636fa33ede
Kevin Lubick <kjlubick@google.com>
2018-02-08 09:20:04 -0500

    move image_filter_deserialize to skia proper and rename a few things

--- projects/skia/build.sh
+++ projects/skia/build.sh
@@ -21,7 +21,7 @@ export CXXFLAGS="$CXXFLAGS -fno-sanitize=vptr"
 
 # This splits a space separated list into a quoted, comma separated list for gn.
 export CXXFLAGS_ARR=`echo $CXXFLAGS | sed -e "s/\s/\",\"/g"`
-$SRC/depot_tools/gn gen out/Fuzz\
+$SRC/depot_tools/gn gen out/Fuzz_mem_constraints\
     --args='cc="'$CC'"
     cxx="'$CXX'"
     is_debug=false
@@ -32,11 +32,24 @@ $SRC/depot_tools/gn gen out/Fuzz\
     skia_enable_gpu=false
     extra_ldflags=["-lFuzzingEngine", "'"$CXXFLAGS_ARR"'"]'
 
-$SRC/depot_tools/ninja -C out/Fuzz fuzz_region_deserialize image_filter_deserialize
+$SRC/depot_tools/gn gen out/Fuzz\
+    --args='cc="'$CC'"
+    cxx="'$CXX'"
+    is_debug=false
+    extra_cflags=["'"$CXXFLAGS_ARR"'","-DIS_FUZZING_WITH_LIBFUZZER",
+        "-Wno-zero-as-null-pointer-constant", "-Wno-unused-template", "-Wno-cast-qual"]
+    skia_use_system_freetype2=false
+    skia_use_fontconfig=false
+    skia_enable_gpu=false
+    extra_ldflags=["-lFuzzingEngine", "'"$CXXFLAGS_ARR"'"]'
 
-cp out/Fuzz/fuzz_region_deserialize $OUT/region_deserialize
+$SRC/depot_tools/ninja -C out/Fuzz_mem_constraints image_filter_deserialize
+
+$SRC/depot_tools/ninja -C out/Fuzz region_deserialize
+
+cp out/Fuzz/region_deserialize $OUT/region_deserialize
 cp ./region_deserialize.options $OUT/region_deserialize.options
 
-cp out/Fuzz/image_filter_deserialize $OUT/image_filter_deserialize
+cp out/Fuzz_mem_constraints/image_filter_deserialize $OUT/image_filter_deserialize
 cp ./image_filter_deserialize.options $OUT/image_filter_deserialize.options
 cp ./image_filter_deserialize_seed_corpus.zip $OUT/image_filter_deserialize_seed_corpus.zip
