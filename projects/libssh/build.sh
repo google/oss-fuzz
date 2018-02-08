f12183bc187fa414634c87884e62c8c7612b35cd
Oliver Chang <oliverchang@users.noreply.github.com>
2018-02-09 10:29:11 +1100

    Update build.sh

--- projects/libssh/build.sh
+++ projects/libssh/build.sh
@@ -23,6 +23,6 @@ cmake -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
 make "-j$(nproc)"
 
 $CXX $CXXFLAGS -std=c++11 -Iinclude/ \
-    "tests/fuszz/ssh_server_fuzzer.cpp" -o "$OUT/libssh_server_fuzzer" \
+    "tests/fuzz/ssh_server_fuzzer.cpp" -o "$OUT/libssh_server_fuzzer" \
     -lFuzzingEngine ./src/libssh.a -Wl,-Bstatic -lcrypto -lz -Wl,-Bdynamic
 popd
