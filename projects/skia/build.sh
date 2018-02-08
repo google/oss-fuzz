e80177d7561da6de4b46ec2f00137b9c93ee91e8
Kevin Lubick <kjlubick@google.com>
2018-02-08 12:03:22 -0500

    Add textblob_deserialize

--- projects/skia/build.sh
+++ projects/skia/build.sh
@@ -45,7 +45,7 @@ $SRC/depot_tools/gn gen out/Fuzz\
 
 $SRC/depot_tools/ninja -C out/Fuzz_mem_constraints image_filter_deserialize
 
-$SRC/depot_tools/ninja -C out/Fuzz region_deserialize region_set_path
+$SRC/depot_tools/ninja -C out/Fuzz region_deserialize region_set_path textblob_deserialize
 
 cp out/Fuzz/region_deserialize $OUT/region_deserialize
 cp ./region_deserialize.options $OUT/region_deserialize.options
@@ -53,6 +53,10 @@ cp ./region_deserialize.options $OUT/region_deserialize.options
 cp out/Fuzz/region_set_path $OUT/region_set_path
 cp ./region_set_path.options $OUT/region_set_path.options
 cp ./region_set_path_seed_corpus.zip $OUT/region_set_path_seed_corpus.zip
+
+cp out/Fuzz/textblob_deserialize $OUT/textblob_deserialize
+cp ./textblob_deserialize.options $OUT/textblob_deserialize.options
+cp ./textblob_deserialize_seed_corpus.zip $OUT/textblob_deserialize_seed_corpus.zip
 
 cp out/Fuzz_mem_constraints/image_filter_deserialize $OUT/image_filter_deserialize
 cp ./image_filter_deserialize.options $OUT/image_filter_deserialize.options
