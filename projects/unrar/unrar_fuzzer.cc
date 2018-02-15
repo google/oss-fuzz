6b5ff6182b883d1895d6f21cfc128b78c4f3d741
Varun Khaneja <aawc@users.noreply.github.com>
2018-02-15 10:30:23 -0800

    Discontinue in-memory fuzzing for unrar (#1169)

--- projects/unrar/unrar_fuzzer.cc
+++ projects/unrar/unrar_fuzzer.cc
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <unistd.h>
 
@@ -6,13 +8,19 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static const std::string filename = "temp.rar";
+  std::ofstream file(filename,
+                     std::ios::binary | std::ios::out | std::ios::trunc);
+  if (!file.is_open()) {
+    return 0;
+  }
+  file.write(reinterpret_cast<const char *>(data), size);
+  file.close();
 
   std::unique_ptr<CommandData> cmd_data(new CommandData);
   cmd_data->ParseArg(const_cast<wchar_t *>(L"-p"));
   cmd_data->ParseArg(const_cast<wchar_t *>(L"x"));
   cmd_data->ParseDone();
   std::wstring wide_filename(filename.begin(), filename.end());
-  cmd_data->SetArcInMem(const_cast<unsigned char *>(data), size);
   cmd_data->AddArcName(wide_filename.c_str());
 
   try {
@@ -21,5 +29,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   } catch (...) {
   }
 
+  unlink(filename.c_str());
   return 0;
 }
