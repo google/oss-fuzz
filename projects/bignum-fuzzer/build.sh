b62af16f522ad4c62c542511e163e760e0ed4e5c
Guido Vranken <guidovranken@gmail.com>
2018-03-09 14:27:24 +0100

    Build OpenSSL+Rust target

--- projects/bignum-fuzzer/build.sh
+++ projects/bignum-fuzzer/build.sh
@@ -3,7 +3,7 @@ if [[ $CFLAGS = *sanitize=memory* ]]
 then
   CFLAGS+=" -DOPENSSL_NO_ASM=1"
 fi
-./config $CFLAGS
+./config
 make -j$(nproc)
 
 # Build OpenSSL module
@@ -14,18 +14,31 @@ OPENSSL_INCLUDE_PATH=$SRC/openssl/include OPENSSL_LIBCRYPTO_A_PATH=$SRC/openssl/
 cd $SRC/bignum-fuzzer/modules/go
 make
 
-CXXFLAGS+=" -DBNFUZZ_FLAG_NO_NEGATIVE=1 -DBNFUZZ_FLAG_NUM_LEN=1200 -DBNFUZZ_FLAG_ALL_OPERATIONS=1"
+# Build Rust module
+cd $SRC/bignum-fuzzer/modules/rust
+make
 
-# Build fuzzer
+BASE_CXXFLAGS=$CXXFLAGS
+
+# Build OpenSSL/Go fuzzer
 cd $SRC/bignum-fuzzer
 ./config-modules.sh openssl go
+CXXFLAGS="$BASE_CXXFLAGS -DBNFUZZ_FLAG_NO_NEGATIVE=1 -DBNFUZZ_FLAG_NUM_LEN=1200 -DBNFUZZ_FLAG_ALL_OPERATIONS=1"
 LIBFUZZER_LINK="-lFuzzingEngine" make
 
-cd $SRC
-
-# Copy fuzzer to the designated location
+# Copy OpenSSL/Go fuzzer to the designated location
 cp $SRC/bignum-fuzzer/fuzzer $OUT/fuzzer_openssl_go_no_negative_num_len_1200_all_operations
 
+# Build OpenSSL/Rust fuzzer
+cd $SRC/bignum-fuzzer
+make clean
+./config-modules.sh openssl rust
+CXXFLAGS="$BASE_CXXFLAGS -DBNFUZZ_FLAG_NUM_LEN=1200 -DBNFUZZ_FLAG_ALL_OPERATIONS=1 -DBNFUZZ_FLAG_NUM_LOOPS=1"
+LIBFUZZER_LINK="-lFuzzingEngine" make
 
-# Copy seed corpus to the designated location
+# Copy OpenSSL/Rust fuzzer to the designated location
+cp $SRC/bignum-fuzzer/fuzzer $OUT/fuzzer_openssl_rust_num_len_1200_all_operations_num_loops_1
+
+# Copy seed corpora to the designated location
 cp $SRC/bignum-fuzzer/corpora/fuzzer_openssl_go_no_negative_num_len_1200_all_operations_seed_corpus.zip $OUT
+cp $SRC/bignum-fuzzer/corpora/fuzzer_openssl_rust_num_len_1200_all_operations_num_loops_1_seed_corpus.zip $OUT
