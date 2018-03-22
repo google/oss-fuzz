690dd9cdfb2e63b435f1394294e4400c14c1f8ab
Guido Vranken <guidovranken@users.noreply.github.com>
2018-03-22 16:01:35 +0100

    [bignum-fuzzer] Add OpenSSL/C++ Boost multiprecision target (#1262)
    * Add OpenSSL/C++-Boost target
    
    * Add OpenSSL/C++-Boost target
    
    * Add security@golang.org to CCs

--- projects/bignum-fuzzer/build.sh
+++ projects/bignum-fuzzer/build.sh
@@ -18,6 +18,10 @@ make
 cd $SRC/bignum-fuzzer/modules/rust
 make
 
+# Build C++-Boost module
+cd $SRC/bignum-fuzzer/modules/cpp_boost
+make
+
 BASE_CXXFLAGS=$CXXFLAGS
 
 # Build OpenSSL/Go fuzzer
@@ -39,6 +43,17 @@ LIBFUZZER_LINK="-lFuzzingEngine" make
 # Copy OpenSSL/Rust fuzzer to the designated location
 cp $SRC/bignum-fuzzer/fuzzer $OUT/fuzzer_openssl_rust_num_len_1200_all_operations_num_loops_1
 
+# Build OpenSSL/C++-Boost fuzzer
+cd $SRC/bignum-fuzzer
+make clean
+./config-modules.sh openssl cpp_boost
+CXXFLAGS="$BASE_CXXFLAGS -DBNFUZZ_FLAG_NUM_LEN=1200 -DBNFUZZ_FLAG_ALL_OPERATIONS=1 -DBNFUZZ_FLAG_NUM_LOOPS=1"
+LIBFUZZER_LINK="-lFuzzingEngine" make
+
+# Copy OpenSSL/C++-Boost fuzzer to the designated location
+cp $SRC/bignum-fuzzer/fuzzer $OUT/fuzzer_openssl_cpp_boost_num_len_1200_all_operations_num_loops_1
+
 # Copy seed corpora to the designated location
 cp $SRC/bignum-fuzzer/corpora/fuzzer_openssl_go_no_negative_num_len_1200_all_operations_seed_corpus.zip $OUT
 cp $SRC/bignum-fuzzer/corpora/fuzzer_openssl_rust_num_len_1200_all_operations_num_loops_1_seed_corpus.zip $OUT
+cp $SRC/bignum-fuzzer/corpora/fuzzer_openssl_cpp_boost_num_len_1200_all_operations_num_loops_1_seed_corpus.zip $OUT
