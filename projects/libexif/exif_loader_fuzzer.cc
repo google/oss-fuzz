f6995d12d447fdcc4fe11ccbfb695fc9d6d8fa04
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-04-02 16:31:57 -0500

    review feedback, be less confusing with Data and data vars

--- projects/libexif/exif_loader_fuzzer.cc
+++ projects/libexif/exif_loader_fuzzer.cc
@@ -12,20 +12,20 @@ void data_func(ExifContent *content, void *user_data) {
   exif_content_foreach_entry(content, content_func, NULL);
 }
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   ExifLoader *loader = exif_loader_new();
-  ExifData *data;
+  ExifData *exif_data;
   if (!loader) {
     return 0;
   }
-  exif_loader_write(loader, (unsigned char *)Data, Size);
-  data = exif_loader_get_data(loader);
-  if(!data) {
+  exif_loader_write(loader, const_cast<unsigned char*>(data), size);
+  exif_data = exif_loader_get_data(loader);
+  if(!exif_data) {
     exif_loader_unref(loader);
     return 0;
   }
-  exif_data_foreach_content(data, data_func, NULL);
+  exif_data_foreach_content(exif_data, data_func, NULL);
   exif_loader_unref(loader);
-  exif_data_unref(data);
+  exif_data_unref(exif_data);
   return 0;
 }
