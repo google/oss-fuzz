bdc43f48baf6ad02f572fb7906b49c3d6b09bfda
Chris Wolfe <chriswwolfe@gmail.com>
2018-04-08 20:05:34 -0500

    long line

--- projects/msgpack-c/unpack_fuzzer.cc
+++ projects/msgpack-c/unpack_fuzzer.cc
@@ -3,7 +3,8 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   try {
-    msgpack::object_handle result = msgpack::unpack(reinterpret_cast<const char *>(data), size);
+    msgpack::object_handle result = \
+      msgpack::unpack(reinterpret_cast<const char *>(data), size);
     msgpack::object obj = result.get();
   } catch (...) {
   }
