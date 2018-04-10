3f712f3a7c1110355950334f4d5c7f030cd0e76c
Chris Wolfe <chriswwolfe@gmail.com>
2018-04-09 21:22:16 -0500

    pack it too

--- projects/msgpack-c/unpack_fuzzer.cc
+++ projects/msgpack-c/unpack_fuzzer.cc
@@ -1,11 +1,13 @@
 #include <msgpack.hpp>
-#include <assert.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   try {
     msgpack::object_handle result = \
       msgpack::unpack(reinterpret_cast<const char *>(data), size);
     msgpack::object obj = result.get();
+
+    msgpack::sbuffer sbuf;
+    msgpack::pack(sbuf, obj);
   } catch (...) {
   }
   return 0;
