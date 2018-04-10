8290058be59bd6d4687a4b73c8544d6e72849e16
Chris Wolfe <chriswwolfe@gmail.com>
2018-04-10 08:36:47 -0500

    remove unneeded intermediate var

--- projects/msgpack-c/unpack_fuzzer.cc
+++ projects/msgpack-c/unpack_fuzzer.cc
@@ -4,10 +4,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   try {
     msgpack::object_handle result = \
       msgpack::unpack(reinterpret_cast<const char *>(data), size);
-    msgpack::object obj = result.get();
 
     msgpack::sbuffer sbuf;
-    msgpack::pack(sbuf, obj);
+    msgpack::pack(sbuf, result.get());
   } catch (...) {
   }
   return 0;
