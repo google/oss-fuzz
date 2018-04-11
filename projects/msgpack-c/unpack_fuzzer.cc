f6ad5238b8675671eef3b6c51a1cd87b399f6021
Chris Wolfe <chriswwolfe@gmail.com>
2018-04-10 21:42:46 -0500

    specify some limits to get around the ooms

--- projects/msgpack-c/unpack_fuzzer.cc
+++ projects/msgpack-c/unpack_fuzzer.cc
@@ -2,11 +2,13 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   try {
-    msgpack::object_handle result = \
-      msgpack::unpack(reinterpret_cast<const char *>(data), size);
-
+    msgpack::object_handle unpacked = msgpack::unpack(reinterpret_cast<const char *>(data),
+                                                      size,
+                                                      nullptr,
+                                                      nullptr,
+                                                      msgpack::unpack_limit(100, 100, 100, 1000));
     msgpack::sbuffer sbuf;
-    msgpack::pack(sbuf, result.get());
+    msgpack::pack(sbuf, unpacked.get());
   } catch (...) {
   }
   return 0;
