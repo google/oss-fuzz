af4734f8a39c2354a085216ef921b330469fc450
Paras Chetal <paras.chetal@gmail.com>
2018-04-29 15:29:30 +0530

    Remove buggy libqubes-rpc-filecopy for now

--- projects/qubes-os/build.sh
+++ projects/qubes-os/build.sh
@@ -15,21 +15,6 @@
 #
 ################################################################################
 
-cd $SRC/qubes-os/linux-utils/
-
-cd qrexec-lib
-
-$CC $CFLAGS -c ioall.c
-$CC $CFLAGS -c copy-file.c
-$CC $CFLAGS -c crc32.c
-$CC $CFLAGS -c pack.c
-$CC $CFLAGS -c unpack.c
-ar rcs libqubes-rpc-filecopy.a ioall.o copy-file.o crc32.o unpack.o pack.o
-
-$CXX $CXXFLAGS -o $OUT/libqubes-rpc-filecopy -I. -I./fuzzer fuzzer/fuzzer.cc -lFuzzingEngine libqubes-rpc-filecopy.a
-
-cp $SRC/*.options $OUT/
-
 cd $SRC/qubes-os/app-linux-input-proxy
  
 make -C fuzz
