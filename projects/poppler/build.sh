9c7eeb8cb5ca03df46bb9c33b3169e7fa4e42aec
Jonathan Rudenberg <jonathan@titanous.com>
2018-05-21 15:23:26 +0000

    cleanup

--- projects/poppler/build.sh
+++ projects/poppler/build.sh
@@ -15,26 +15,38 @@
 #
 ################################################################################
 
-pushd "$SRC/freetype2"
+pushd $SRC/freetype2
 ./autogen.sh
 ./configure --prefix="$WORK" --disable-shared PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
 make -j$(nproc)
 make install
-popd
-
-pushd poppler
-
-sed "s/poppler SHARED/poppler/" CMakeLists.txt
-sed '/option/ a option(BUILD_SHARED_LIBS "Build the shared library" ON)' CMakeLists.txt
 
 mkdir -p $WORK/poppler
 pushd $WORK/poppler
-cmake $SRC/poppler -DCMAKE_BUILD_TYPE=debug -DENABLE_DCTDECODER=none -DENABLE_LIBOPENJPEG=none -DBUILD_SHARED_LIBS=OFF -DENABLE_LIBPNG=OFF -DFONT_CONFIGURATION=generic -DFREETYPE_INCLUDE_DIRS=$WORK/include/freetype2 -DFREETYPE_LIBRARY=$WORK/lib
+cmake $SRC/poppler \
+  -DCMAKE_BUILD_TYPE=debug \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DFONT_CONFIGURATION=generic \
+  -DENABLE_DCTDECODER=none \
+  -DENABLE_LIBOPENJPEG=none \
+  -DENABLE_CMS=none \
+  -DENABLE_LIBPNG=OFF \
+  -DENABLE_ZLIB=OFF \
+  -DENABLE_LIBTIFF=OFF \
+  -DENABLE_LIBJPEG=OFF \
+  -DENABLE_GLIB=OFF \
+  -DENABLE_LIBCURL=OFF \
+  -DENABLE_QT5=OFF \
+  -DENABLE_UTILS=OFF \
+  -DWITH_Cairo=OFF \
+  -DWITH_NSS3=OFF \
+  -DFREETYPE_INCLUDE_DIRS=$WORK/include/freetype2 \
+  -DFREETYPE_LIBRARY=$WORK/lib
 make -j$(nproc) poppler poppler-cpp
-popd
 
 fuzz_target=pdf_fuzzer
 
+pushd $SRC/poppler
 $CXX $CXXFLAGS -std=c++11 -Icpp \
     fuzz/pdf_fuzzer.cc -o $OUT/$fuzz_target \
     -lFuzzingEngine $WORK/poppler/cpp/libpoppler-cpp.a $WORK/poppler/libpoppler.a $WORK/lib/libfreetype.a
