965c71ac1203042964018d623e16ac2b722337c7
Michael Niedermayer <michaelni@gmx.at>
2018-05-28 00:45:48 +0200

    Revert "Remove unneeded enable-ossfuzz flag, fixes #568"
    configure should no longer add fsanitize flags if there are already some
    provided by the user. Also CONFIG_OSSFUZZ is needed currently to modify
    the iterate API to prevent linking everything in. This should reduce
    disk space requirements back to "pre iterate".
    
    This reverts commit a83ce13c5466fb321b33c8b045412a44ae19d14b.

--- projects/ffmpeg/build.sh
+++ projects/ffmpeg/build.sh
@@ -164,6 +164,7 @@ PKG_CONFIG_PATH="$FFMPEG_DEPS_PATH/lib/pkgconfig" ./configure \
     --extra-ldflags="-L$FFMPEG_DEPS_PATH/lib" \
     --prefix="$FFMPEG_DEPS_PATH" \
     --pkg-config-flags="--static" \
+    --enable-ossfuzz \
     --libfuzzer=-lFuzzingEngine \
     --optflags=-O1 \
     --enable-gpl \
