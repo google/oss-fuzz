9b459a6c2da261d0d16ae539366851066130371f
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2018-05-31 14:53:25 +0200

    OpenvSwitch integration: Fixed issues copying dict and fuzzer opetions

--- projects/openvswitch/build.sh
+++ projects/openvswitch/build.sh
@@ -21,3 +21,4 @@ $CC $CFLAGS -c -g $SRC/target-flow-extract.c -I . -I lib/ -I include/ \
 $CXX $CXXFLAGS $SRC/target-flow-extract.o ./lib/.libs/libopenvswitch.a \
 	-lz -lssl -lcrypto -latomic -lFuzzingEngine \
 	-o $OUT/flow_extract_fuzzer
+cp $SRC/*.dict $SRC/*.options $OUT/
