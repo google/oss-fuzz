cccb5e9450dff3987178d0fad4c5d05bb895477b
Kostya Serebryany <kcc@google.com>
2018-05-31 20:06:07 -0700

    fix nestegg build.sh

--- projects/nestegg/build.sh
+++ projects/nestegg/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
 
-$CXX $CXXFLAGS -o $OUT/fuzz -I./include src/nestegg.c test/fuzz.cc -lFuzzingEngine
+$CC $CFLAGS -c -I./include src/nestegg.c
+$CXX $CXXFLAGS -o $OUT/fuzz -I./include nestegg.o test/fuzz.cc -lFuzzingEngine
+
 
 mkdir corpus/
-cp -R testdata/*.webm corpus/
-cp nestegg/test/media/*.webm corpus/
+cp -R ../testdata/*.webm corpus/
+cp test/media/*.webm corpus/
 zip -rj0 $OUT/fuzz_seed_corpus.zip corpus/*.webm
