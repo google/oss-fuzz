ea7d5938faa2c143e704d728862b785b5f0a4681
urvangjoshi <urvang@google.com>
2018-06-11 18:27:33 -0700

    [libaom] Add project (#1502)

---
+++ projects/libaom/av1_dec_fuzzer.cc
@@ -0,0 +1,68 @@
+// Fuzzing of AV1 decoder.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "aom/aom_decoder.h"
+#include "aom/aomdx.h"
+#include "aom_ports/mem_ops.h"
+#include "aom/common/ivfdec.h"
+
+// TODO(urvang): Create a 2nd fuzz target that defines DECODE_MODE_threaded.
+#define DECODE_MODE_serial
+
+static const char *const kIVFSignature = "DKIF";
+
+extern "C" void usage_exit(void) { exit(EXIT_FAILURE); }
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  FILE *const file = fmemopen((void *)data, size, "rb");
+  if (file == nullptr) {
+    return 0;
+  }
+
+  char header[32];
+  if (fread(header, 1, 32, file) != 32) {
+    return 0;
+  }
+  const AvxInterface *decoder = get_aom_decoder_by_name("av1");
+  if (decoder == nullptr) {
+    return 0;
+  }
+
+  aom_codec_ctx_t codec;
+
+#if defined(DECODE_MODE_serial)
+  const int threads = 1;
+#elif defined(DECODE_MODE_threaded)
+  const int threads = 16;
+#else
+#error define one of DECODE_MODE_(serial|threaded)
+#endif
+  aom_codec_dec_cfg_t cfg = {threads, 0, 0};
+  if (aom_codec_dec_init(&codec, decoder->codec_interface(), &cfg, 0)) {
+    aom_codec_destroy(&codec);
+    return 0;
+  }
+
+  int frame_in_cnt = 0;
+  int frame_out_cnt = 0;
+  uint8_t *buffer = nullptr;
+  size_t buffer_size = 0;
+  size_t frame_size = 0;
+  while (!ivf_read_frame(file, &buffer, &frame_size, &buffer_size, NULL)) {
+    const aom_codec_err_t err = aom_codec_decode(
+        &codec, buffer, static_cast<unsigned int>(frame_size), NULL);
+    ++frame_in_cnt;
+    aom_codec_iter_t iter = nullptr;
+    aom_image_t *img = nullptr;
+    while ((img = aom_codec_get_frame(&codec, &iter)) != nullptr) {
+      ++frame_out_cnt;
+    }
+  }
+
+  fclose(file);
+  free(buffer);
+  return 0;
+}
---
+++ projects/libaom/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -eu
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# Build libaom
+pushd $WORK
+rm -rf ./*
+cmake $SRC/aom -DCONFIG_PIC=1 -DCONFIG_SCALABILITY=0 -DCONFIG_LOWBITDEPTH=1 \
+  -DENABLE_EXAMPLES=0 -DENABLE_DOCS=0 -DCONFIG_UNIT_TESTS=0 \
+  -DCONFIG_SIZE_LIMIT=1 -DDECODE_HEIGHT_LIMIT=12288 -DDECODE_WIDTH_LIMIT=12288
+make -j$(nproc)
+popd
+
+# Build some libaom utils that are not part of the core lib.
+$CC $CFLAGS -std=c99 -c \
+  -I$SRC/aom \
+  -I$WORK \
+  $SRC/aom/common/ivfdec.c -o $WORK/ivfdec.o
+
+$CC $CFLAGS -std=c99 -c \
+  -I$SRC/aom \
+  -I$WORK \
+  $SRC/aom/common/tools_common.c -o $WORK/tools_common.o
+
+# build fuzzer
+fuzzer_name=av1_dec_fuzzer
+
+$CXX $CXXFLAGS -std=c++11 \
+  -I$SRC/aom \
+  -I$WORK \
+  -Wl,--start-group \
+  -lFuzzingEngine \
+  $SRC/${fuzzer_name}.cc -o $OUT/${fuzzer_name} \
+  $WORK/libaom.a $WORK/ivfdec.o $WORK/tools_common.o \
+  -Wl,--end-group
+
+# copy seed corpus.
+cp $SRC/dec_fuzzer_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
