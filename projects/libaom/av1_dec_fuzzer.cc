b8234bb1bd4bc50ded2c2117b39594b5aa17956f
Urvang Joshi <urvang@google.com>
2018-06-11 18:22:18 -0700

    Remove unnecessary ifs and fprintfs.

--- projects/libaom/av1_dec_fuzzer.cc
+++ projects/libaom/av1_dec_fuzzer.cc
@@ -19,31 +19,17 @@ extern "C" void usage_exit(void) { exit(EXIT_FAILURE); }
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   FILE *const file = fmemopen((void *)data, size, "rb");
   if (file == nullptr) {
-    fprintf(stderr, "Failed to get file stream.\n");
     return 0;
   }
 
   char header[32];
   if (fread(header, 1, 32, file) != 32) {
-    fprintf(stderr, "Failed to get file header.\n");
     return 0;
   }
-  if (memcmp(kIVFSignature, header, 4) != 0) {
-    fprintf(stderr, "Wrong IVF signature.\n");
-    // Do not terminate.
-  }
-  if (mem_get_le16(header + 4) != 0) {
-    fprintf(stderr, "Wrong IVF version.\n");
-    // Do not terminate.
-  }
-
   const AvxInterface *decoder = get_aom_decoder_by_name("av1");
   if (decoder == nullptr) {
-    fprintf(stderr, "Unknown input codec.\n");
     return 0;
   }
-  fprintf(stderr, "Using %s\n",
-          aom_codec_iface_name(decoder->codec_interface()));
 
   aom_codec_ctx_t codec;
 
@@ -56,7 +42,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 #endif
   aom_codec_dec_cfg_t cfg = {threads, 0, 0};
   if (aom_codec_dec_init(&codec, decoder->codec_interface(), &cfg, 0)) {
-    fprintf(stderr, "Failed to initialize decoder.\n");
     aom_codec_destroy(&codec);
     return 0;
   }
@@ -69,10 +54,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   while (!ivf_read_frame(file, &buffer, &frame_size, &buffer_size, NULL)) {
     const aom_codec_err_t err = aom_codec_decode(
         &codec, buffer, static_cast<unsigned int>(frame_size), NULL);
-    if (err != AOM_CODEC_OK) {
-      fprintf(stderr, "Failed to decode frame %d; error code %d.\n",
-              frame_in_cnt, err);
-    }
     ++frame_in_cnt;
     aom_codec_iter_t iter = nullptr;
     aom_image_t *img = nullptr;
@@ -80,11 +61,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
       ++frame_out_cnt;
     }
   }
-  fprintf(stderr, "Processed %d frames.\n", frame_out_cnt);
 
-  if (aom_codec_destroy(&codec)) {
-    fprintf(stderr, "Failed to destroy codec\n");
-  }
   fclose(file);
   free(buffer);
   return 0;
