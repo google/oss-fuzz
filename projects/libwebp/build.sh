15281937661708e9e3b28e34d61c64eaa4e42e32
pdknsk <pdknsk@gmail.com>
2018-06-13 19:28:13 +0200

    new target: (de)mux API

--- projects/libwebp/build.sh
+++ projects/libwebp/build.sh
@@ -21,6 +21,7 @@ WEBP_CFLAGS="$CFLAGS -DWEBP_MAX_IMAGE_SIZE=838860800" # 800MiB
 ./autogen.sh
 CFLAGS="$WEBP_CFLAGS" ./configure \
   --enable-libwebpdemux \
+  --enable-libwebpmux \
   --disable-shared \
   --disable-jpeg \
   --disable-tiff \
@@ -51,6 +52,16 @@ cp $SRC/fuzz_advanced_api.options $OUT
 $CC $CFLAGS -Isrc -c $SRC/fuzz_animation_api.c
 $CXX $CXXFLAGS -lFuzzingEngine \
   fuzz_animation_api.o -o $OUT/fuzz_animation_api \
-  src/demux/.libs/libwebpdemux.a src/.libs/libwebp.a
+  src/demux/.libs/libwebpdemux.a \
+  src/.libs/libwebp.a
 cp $SRC/fuzz_seed_corpus.zip $OUT/fuzz_animation_api_seed_corpus.zip
 cp $SRC/fuzz_animation_api.options $OUT
+
+# (De)mux API
+$CC $CFLAGS -Isrc -c $SRC/fuzz_demux_api.c
+$CXX $CXXFLAGS -lFuzzingEngine \
+  fuzz_demux_api.o -o $OUT/fuzz_demux_api \
+  src/demux/.libs/libwebpdemux.a src/mux/.libs/libwebpmux.a \
+  src/.libs/libwebp.a
+cp $SRC/fuzz_seed_corpus.zip $OUT/fuzz_demux_api_seed_corpus.zip
+cp $SRC/fuzz_demux_api.options $OUT
