5fa780a646a0eadf3270b5ebaf7e8df07522f814
Alexander Zaitsev <zamazan4ik@tut.by>
2018-06-17 21:06:09 +0300

    Added inih project

---
+++ projects/inih/parse_fuzzer.cc
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <string>
+#include "ini.h"
+
+int dumper(void* user, const char* section, const char* name,
+           const char* value)
+{
+    return 1;
+}
+
+void parse(const char* string) 
+{
+    int u = 100;
+    int e = ini_parse_string(string, dumper, &u);
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    std::string str((const char*)data, size);
+    parse(str.c_str());
+    return 0;
+}
