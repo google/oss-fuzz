2915d1d42e6f8111fd1f5091fb1fa93375b2391e
Eugene Kliuchnikov <eustas@google.com>
2018-06-18 13:44:29 +0200

    Update brotli build script
    Brotli fuzzer is being converted to C99 in google/brotli#686

--- projects/brotli/build.sh
+++ projects/brotli/build.sh
@@ -4,8 +4,8 @@ cmake . -DBUILD_TESTING=OFF
 make clean
 make -j$(nproc) brotlidec-static
 
-$CXX $CXXFLAGS -std=c++11 -I. \
-    c/fuzz/decode_fuzzer.cc -I./c/include -o $OUT/decode_fuzzer \
+$CXX $CXXFLAGS -std=c99 -I. \
+    c/fuzz/decode_fuzzer.c -I./c/include -o $OUT/decode_fuzzer \
     -lFuzzingEngine ./libbrotlidec-static.a ./libbrotlicommon-static.a
 
 cp java/org/brotli/integration/fuzz_data.zip $OUT/decode_fuzzer_seed_corpus.zip
