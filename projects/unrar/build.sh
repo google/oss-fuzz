d77fb57384a643cb24ef32dd1482da28d84eb08c
Varun Khaneja <ca.aawc@yahoo.com>
2018-06-21 13:39:05 -0700

    Add -DSILENT -DRAR_NOCRYPT defines when compiling unrar

--- projects/unrar/build.sh
+++ projects/unrar/build.sh
@@ -15,16 +15,18 @@
 #
 ################################################################################
 
+UNRAR_DEFINES="-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -DRAR_SMP -DRARDLL -DSILENT -DRAR_NOCRYPT"
+UNRAR_WNOS="-Wno-logical-op-parentheses -Wno-switch -Wno-dangling-else"
 UNRAR_SRC_DIR="$SRC/unrar"
 
 # build 'lib'. This builds libunrar.a and libunrar.so
 # -fPIC is required for successful compilation.
-make CXX=$CXX CXXFLAGS="$CXXFLAGS -fPIC" -C $UNRAR_SRC_DIR lib
+make CXX=$CXX CXXFLAGS="$CXXFLAGS -fPIC $UNRAR_DEFINES $UNRAR_WNOS" \
+  -C $UNRAR_SRC_DIR lib
 
 # remove the .so file so that the linker links unrar statically.
 rm -v $UNRAR_SRC_DIR/libunrar.so
 
 # build fuzzer
 $CXX $CXXFLAGS -I. $UNRAR_SRC_DIR/unrar_fuzzer.cc -o $OUT/unrar_fuzzer \
-     -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -DRAR_SMP -DRARDLL \
-     -lFuzzingEngine -L$UNRAR_SRC_DIR -lunrar
+     $UNRAR_DEFINES -lFuzzingEngine -L$UNRAR_SRC_DIR -lunrar
