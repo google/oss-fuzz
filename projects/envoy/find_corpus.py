4caf81088d9c8b1de16752c420d5c277abebe3b1
Anirudh M <m.anirudh18@gmail.com>
2018-07-12 18:47:41 +0000

    addressed comments
    Signed-off-by: Anirudh M <m.anirudh18@gmail.com>

--- projects/envoy/find_corpus.py
+++ projects/envoy/find_corpus.py
@@ -1,23 +1,23 @@
-#!/usr/bin/pxython
+#!/usr/bin/python
 
 import os
 import sys
 import re
 
-fuzzer_target = sys.argv[1].split("/")
-(directory_segments, fuzz_test) = (fuzzer_target[:-1], fuzzer_target[-1])
+fuzzer_target = sys.argv[1].split('/')
+directory_segments, fuzz_test = fuzzer_target[:-1], fuzzer_target[-1]
 directory = '/'.join(directory_segments)
 path = '../envoy/' + directory + '/BUILD'
 corpus_path = ""
 
 with open(path, 'r') as f:
   searchlines = f.readlines()
-  for (i, line) in enumerate(searchlines):
+  for i, line in enumerate(searchlines):
     if fuzz_test in line:
       for l in searchlines[i:]:
         if 'corpus =' in l:
           corpus_path = l
           break
 if not corpus_path:
-    raise Exception("No corpus path for the given fuzz target")
+  raise Exception("No corpus path for the given fuzz target")
 print re.findall(r'"([^"]*)"', corpus_path)[0]
