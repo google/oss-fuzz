37c3828c06f4dc543eda7f3cfb91a740b6d6d1e1
pdknsk <pdknsk@gmail.com>
2018-08-07 05:59:17 +0200

    new target: fuzz_dbus_message

--- projects/glib/build.sh
+++ projects/glib/build.sh
@@ -49,3 +49,15 @@ $CXX $CXXFLAGS -lFuzzingEngine \
   $BUILD/glib/libglib-2.0.a $BUILD/glib/libcharset/libcharset.a
 cp $SRC/fuzz.options $OUT/fuzz_variant_text.options
 cp $SRC/fuzz_variant_text.dict $OUT
+
+$CC $CFLAGS -I. -Iglib -Igmodule -I$BUILD -I$BUILD/glib \
+  -c $SRC/fuzz_dbus_message.c
+$CXX $CXXFLAGS -lFuzzingEngine \
+  fuzz_dbus_message.o -o $OUT/fuzz_dbus_message \
+  $BUILD/gio/libgio-2.0.a $BUILD/gmodule/libgmodule-2.0.a \
+  $BUILD/gobject/libgobject-2.0.a $BUILD/glib/libglib-2.0.a \
+  $BUILD/glib/libcharset/libcharset.a $BUILD/glib/pcre/libpcre.a \
+  $BUILD/gio/xdgmime/libxdgmime.a $BUILD/gio/inotify/libinotify.a \
+  $BUILD/subprojects/zlib*/libz.a $BUILD/subprojects/libffi/src/libffi.a \
+  -Bstatic -lresolv
+cp $SRC/fuzz.options $OUT/fuzz_dbus_message.options
