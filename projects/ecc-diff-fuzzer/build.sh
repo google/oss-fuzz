893b421e50904ff1715d14678ea49e7640e19b5c
Philippe Antoine <contact@catenacyber.fr>
2018-08-09 15:57:19 +0200

    Adds gcrypt to elliptic curve diff fuzzer
    And adds corpus to the project

--- projects/ecc-diff-fuzzer/build.sh
+++ projects/ecc-diff-fuzzer/build.sh
@@ -16,6 +16,22 @@
 ################################################################################
 
 # build projects
+#gcrypt
+(
+cd gcrypt
+tar -xvf ../libgpg-error-1.32.tar.bz2
+cd libgpg-error-1.32
+./configure --enable-static --disable-shared
+make
+make install
+cd ..
+./autogen.sh
+./configure --enable-static --disable-shared --disable-doc --enable-maintainer-mode
+#hack to have a different exported symbol than in openssl
+sed -i -e 's/poly1305_blocks/gcry_poly1305_blocks/' ./cipher/poly1305.c
+make
+)
+
 #mbedtls
 (
 cd mbedtls
@@ -35,15 +51,18 @@ make build_generated libcrypto.a
 cd libecc
 #required by libecc
 (export CFLAGS="$CFLAGS -fPIC"; make)
-echo $CFLAGS
 )
 
 #build fuzz target
 cd ecfuzzer
+zip -r fuzz_ec_seed_corpus.zip corpus/
+cp fuzz_ec_seed_corpus.zip $OUT/
 cp fuzz_ec.dict $OUT/
+
 $CC $CFLAGS -I. -c fuzz_ec.c -o fuzz_ec.o
 $CC $CFLAGS -I. -I../mbedtls/include -c modules/mbedtls.c -o mbedtls.o
 $CC $CFLAGS -I. -I../openssl/include -c modules/openssl.c -o openssl.o
 $CC $CFLAGS -DWITH_STDLIB -I. -I../libecc/src -c modules/libecc.c -o libecc.o
+$CC $CFLAGS -I. -I../gcrypt/src -c modules/gcrypt.c -o gcrypt.o
 
-$CXX $CXXFLAGS fuzz_ec.o mbedtls.o libecc.o openssl.o -o $OUT/fuzz_ec ../mbedtls/library/libmbedcrypto.a ../libecc/build/libec.a ../openssl/libcrypto.a -lFuzzingEngine
+$CXX $CXXFLAGS fuzz_ec.o mbedtls.o libecc.o openssl.o gcrypt.o -o $OUT/fuzz_ec ../mbedtls/library/libmbedcrypto.a ../libecc/build/libec.a ../openssl/libcrypto.a ../gcrypt/src/.libs/libgcrypt.a -lgpg-error -lFuzzingEngine
