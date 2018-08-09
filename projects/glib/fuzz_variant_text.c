c0e1e46bab68fb5e1fa655eb5e54201380801e34
pdknsk <pdknsk@gmail.com>
2018-08-09 16:52:25 +0200

    [glib] GLib: more targets (#1695)
    * better seed corpus for fuzz_key
    
    * new target: fuzz_variant_text
    
    * new target: fuzz_dbus_message
    
    * get ninja from pip
    
    * remove target: fuzz_markup
    
    * new target: fuzz_variant_binary

---
+++ projects/glib/fuzz_variant_text.c
@@ -0,0 +1,12 @@
+#include "glib/glib.h"
+#include <stdint.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  const gchar* gdata = (const gchar*)data;
+  g_autoptr(GVariant) variant =
+      g_variant_parse(NULL, gdata, gdata + size, NULL, NULL);
+  if (variant) { // g_autofree requires {}
+    g_autofree gchar* text = g_variant_print(variant, TRUE);
+  }
+  return 0;
+}
