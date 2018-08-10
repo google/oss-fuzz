7bb6714073ab83c26c5dd460eb9bdbf1fa562478
pdknsk <pdknsk@gmail.com>
2018-08-11 00:14:12 +0200

    [glib] GLib: remove names from dictionary (#1706)
    * consistency
    
    * remove names from dictionary

--- projects/glib/fuzz_variant_binary.c
+++ projects/glib/fuzz_variant_binary.c
@@ -4,9 +4,9 @@
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   g_autoptr(GVariant) variant = g_variant_new_from_data(
       G_VARIANT_TYPE_VARIANT, data, size, FALSE, NULL, NULL);
-  if (variant) {
-    g_variant_get_normal_form(variant);
-    g_variant_get_data(variant);
-  }
+  if (!variant)
+    return 0;
+  g_variant_get_normal_form(variant);
+  g_variant_get_data(variant);
   return 0;
 }
