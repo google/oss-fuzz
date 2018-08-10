5d558907bb9c8a02619a7a25a44b46488c16afc4
pdknsk <pdknsk@gmail.com>
2018-08-10 22:38:50 +0200

    consistency

--- projects/glib/fuzz_variant_text.c
+++ projects/glib/fuzz_variant_text.c
@@ -5,8 +5,8 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   const gchar* gdata = (const gchar*)data;
   g_autoptr(GVariant) variant =
       g_variant_parse(NULL, gdata, gdata + size, NULL, NULL);
-  if (variant) { // g_autofree requires {}
-    g_autofree gchar* text = g_variant_print(variant, TRUE);
-  }
+  if (!variant)
+    return 0;
+  g_autofree gchar* text = g_variant_print(variant, TRUE);
   return 0;
 }
