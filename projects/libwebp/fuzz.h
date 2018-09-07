bc56d409f95413919d5224feb49158e45fa80306
Yannis Guyon <yguyon@google.com>
2018-09-07 17:30:21 +0200

    Add new fuzz target for encoding and misc
    - Add fuzz_webp_enc_dec and adapt Dockerfile, build.sh
    - Lint existing targets
    - Add license headers
    - Increase fuzz.dict

--- projects/libwebp/fuzz.h
+++ projects/libwebp/fuzz.h
@@ -1,19 +1,34 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include <stdint.h>
 #include <stdlib.h>
 
 // Arbitrary limits to prevent OOM, timeout, or slow execution.
 //
 // The decoded image size, and for animations additionally the canvas size.
-static const size_t fuzz_px_limit = 1024 * 1024;
+static const size_t kFuzzPxLimit = 1024 * 1024;
 // Demuxed or decoded animation frames.
-static const int fuzz_frame_limit = 3;
+static const int kFuzzFrameLimit = 3;
 
 // Reads and sums (up to) 128 spread-out bytes.
-static uint8_t fuzz_hash(const uint8_t* data, size_t size) {
+uint8_t FuzzHash(const uint8_t* const data, size_t size) {
   uint8_t value = 0;
   size_t incr = size / 128;
   if (!incr) incr = 1;
-  for (size_t i = 0; i < size; i += incr)
-    value += data[i];
+  for (size_t i = 0; i < size; i += incr) value += data[i];
   return value;
 }
