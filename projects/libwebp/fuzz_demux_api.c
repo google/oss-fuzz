36605707f3c17985816d31c9756f9c20cb6ba024
YannisGuyon <7632072+YannisGuyon@users.noreply.github.com>
2018-09-13 00:38:48 +0200

    [libwebp] Add new fuzz target for encoding and misc (#1803)
    - Add fuzz_webp_enc_dec and adapt Dockerfile, build.sh
    - Lint existing targets
    - Add license headers
    - Increase fuzz.dict

--- projects/libwebp/fuzz_demux_api.c
+++ projects/libwebp/fuzz_demux_api.c
@@ -1,8 +1,24 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include "fuzz.h"
 #include "webp/mux.h"
 #include "webp/demux.h"
 
-int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+int LLVMFuzzerTestOneInput(const uint8_t* const data, size_t size) {
   WebPData webp_data;
   WebPDataInit(&webp_data);
   webp_data.size = size;
@@ -14,13 +30,12 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size & 1) {
     // Mux API
     WebPMux* mux = WebPMuxCreate(&webp_data, size & 2);
-    if (!mux)
-      return 0;
+    if (!mux) return 0;
 
     WebPData chunk;
     WebPMuxGetChunk(mux, "EXIF", &chunk);
     WebPMuxGetChunk(mux, "ICCP", &chunk);
-    WebPMuxGetChunk(mux, "FUZZ", &chunk); // unknown
+    WebPMuxGetChunk(mux, "FUZZ", &chunk);  // unknown
 
     uint32_t flags;
     WebPMuxGetFeatures(mux, &flags);
@@ -30,7 +45,7 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
     WebPMuxError status;
     WebPMuxFrameInfo info;
-    for (int i = 0; i < fuzz_frame_limit; i++) {
+    for (int i = 0; i < kFuzzFrameLimit; i++) {
       status = WebPMuxGetFrame(mux, i + 1, &info);
       if (status == WEBP_MUX_NOT_FOUND) {
         break;
@@ -52,24 +67,24 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       }
     } else {
       demux = WebPDemux(&webp_data);
-      if (!demux)
-        return 0;
+      if (!demux) return 0;
     }
 
     WebPChunkIterator chunk_iter;
-    if (WebPDemuxGetChunk(demux, "EXIF", 1, &chunk_iter))
+    if (WebPDemuxGetChunk(demux, "EXIF", 1, &chunk_iter)) {
       WebPDemuxNextChunk(&chunk_iter);
+    }
     WebPDemuxReleaseChunkIterator(&chunk_iter);
-    if (WebPDemuxGetChunk(demux, "ICCP", 0, &chunk_iter)) // 0 == last
+    if (WebPDemuxGetChunk(demux, "ICCP", 0, &chunk_iter)) {  // 0 == last
       WebPDemuxPrevChunk(&chunk_iter);
+    }
     WebPDemuxReleaseChunkIterator(&chunk_iter);
     // Skips FUZZ because the Demux API has no concept of (un)known chunks.
 
     WebPIterator iter;
     if (WebPDemuxGetFrame(demux, 1, &iter)) {
-      for (int i = 1; i < fuzz_frame_limit; i++) {
-        if (!WebPDemuxNextFrame(&iter))
-          break;
+      for (int i = 1; i < kFuzzFrameLimit; i++) {
+        if (!WebPDemuxNextFrame(&iter)) break;
       }
     }
 
