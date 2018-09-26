846ccc97cbe2420531146aece8953ac34fa09279
Bhargava Shastry <bshastry@sect.tu-berlin.de>
2018-09-26 15:26:14 +0200

    OvS: Make use of corp dir name

--- projects/openvswitch/build.sh
+++ projects/openvswitch/build.sh
@@ -28,6 +28,6 @@ do
        corp_name=$(basename $file _target)
        corp_dir=$SRC/ovs-fuzzing-corpus/${corp_name}_seed_corpus
        if [ -d ${corp_dir} ]; then
-           zip -j $OUT/${name}_seed_corpus.zip $SRC/ovs-fuzzing-corpus/${corp_name}_seed_corpus/*
+           zip -j $OUT/${name}_seed_corpus.zip ${corp_dir}/*
        fi
 done
