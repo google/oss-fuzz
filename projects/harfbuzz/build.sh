8d7039abee8e014f9c1f4b2011940ec4c47a7cd2
Behdad Esfahbod <behdad@behdad.org>
2018-10-11 22:50:04 -0400

    Update Harfbuzz settings (#1869)
    * [harfbuzz] Remove UBSan exemptions
    
    We are clean on all sanitizers now.
    
    * [harfbuz] Seed corpus from more fonts
    
    * [harfbuzz] Share seed corpus amongst the two sanitizers
    
    Is this really what those archives are for?  Or just for archival?
    
    * [harfbuzz] Remove hb-subset-get-codepoints-fuzzer
    
    Folded into hb-subset-fuzzer upstream.

--- projects/harfbuzz/build.sh
+++ projects/harfbuzz/build.sh
@@ -15,12 +15,6 @@
 #
 ################################################################################
 
-# Disable:
-# 1. UBSan vptr since target built with -fno-rtti.
-# 2. UBSan function to avoid crashes with void* cast crashes.
-export CFLAGS="$CFLAGS -fno-sanitize=function,vptr"
-export CXXFLAGS="$CXXFLAGS -fno-sanitize=function,vptr"
-
 # Build the library.
 ./autogen.sh
 ./configure
@@ -37,10 +31,6 @@ $CXX $CXXFLAGS -std=c++11 -Isrc \
     ./test/fuzzing/hb-subset-fuzzer.cc -o $OUT/hb-subset-fuzzer \
     -lFuzzingEngine ./src/.libs/libharfbuzz-subset-fuzzing.a ./src/.libs/libharfbuzz-fuzzing.a
 
-$CXX $CXXFLAGS -std=c++11 -Isrc \
-    ./test/fuzzing/hb-subset-get-codepoints-fuzzer.cc -o $OUT/hb-subset-get-codepoints-fuzzer \
-    -lFuzzingEngine ./src/.libs/libharfbuzz-subset-fuzzing.a ./src/.libs/libharfbuzz-fuzzing.a
-
 # Archive and copy to $OUT seed corpus if the build succeeded.
-zip -j -r $OUT/hb-shape-fuzzer_seed_corpus.zip $SRC/harfbuzz/test/shaping/data/in-house/fonts
-zip -j -r $OUT/hb-subset-fuzzer_seed_corpus.zip $SRC/harfbuzz/test/api/fonts
+zip -j -r $OUT/hb-shape-fuzzer_seed_corpus.zip $SRC/harfbuzz/test/shaping/data/in-house/fonts $SRC/harfbuzz/test/shaping/data/text-rendering-tests/fonts $SRC/harfbuzz/test/api/fonts
+cp $OUT/hb-shape-fuzzer_seed_corpus.zip $OUT/hb-subset-fuzzer_seed_corpus.zip
