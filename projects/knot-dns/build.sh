1b3eb9873f0e5a1e8a804a665151cc5d67b33221
Jonathan Foote <foote@fastly.com>
2018-12-10 03:35:45 +0000

    [knot-dns] use libnettle 3.4.1 to build gnutls

--- projects/knot-dns/build.sh
+++ projects/knot-dns/build.sh
@@ -39,7 +39,9 @@ if [[ $CFLAGS = *sanitize=memory* ]]; then
   NETTLE_CONFIGURE_FLAGS="--disable-assembler --disable-fat"
 fi
 
+
 cd $SRC/nettle
+git checkout tags/nettle_3.4.1_release_20181204
 bash .bootstrap
 ./configure --enable-mini-gmp --enable-static --disable-shared --disable-documentation --prefix=$DEPS_PATH $NETTLE_CONFIGURE_FLAGS
 ( make -j$(nproc) || make -j$(nproc) ) && make install
