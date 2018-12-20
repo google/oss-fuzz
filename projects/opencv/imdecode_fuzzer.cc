2fe7212f5171ddb8df7390b4094ef1cee33c4e74
Markus Kusano <kusano@google.com>
2018-12-19 19:16:03 -0500

    add TODO for trying different opencv image types.

--- projects/opencv/imdecode_fuzzer.cc
+++ projects/opencv/imdecode_fuzzer.cc
@@ -3,6 +3,7 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   std::vector<uint8_t> image_data = {data, data + size};
+  // TODO: Try other image types than CV_8UC1.
   cv::Mat data_matrix =
       cv::Mat(1, image_data.size(), CV_8UC1, image_data.data());
   try {
