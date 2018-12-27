96d51be0471348720ac1a0818b8be24fd68b183a
Tim King <taking@cs.nyu.edu>
2018-12-27 11:13:11 -0800

    [qpdf] Limiting the number of bytes the read_memory_fuzzer handles. (#2056)
    * [qpdf] Limiting the number of bytes the read_memory_fuzzer handles.
    
    * Raising the limit to 64 KiB.

--- projects/qpdf/qpdf_read_memory_fuzzer.cc
+++ projects/qpdf/qpdf_read_memory_fuzzer.cc
@@ -1,12 +1,13 @@
 #include "qpdf/qpdf-c.h"
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
-#include <string>
-#include <vector>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  constexpr size_t kMaxSize = 64 * 1024; // 64 KiB
+  size = std::min(size, kMaxSize);
   _qpdf_data* qpdf = qpdf_init();
   const char* buffer = reinterpret_cast<const char*>(data);
   qpdf_read_memory(qpdf, /*description=*/"", buffer, size, /*password=*/"");
