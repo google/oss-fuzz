bfdd2e227c2f609b988c6eb9144bd88863475135
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2019-02-08 17:24:16 -0800

    Allocate region once.

--- projects/unbound/parse_packet_fuzzer.c
+++ projects/unbound/parse_packet_fuzzer.c
@@ -3,15 +3,21 @@
 #include "util/fptr_wlist.h"
 #include "sldns/sbuffer.h"
 
+struct regional * region = NULL;
+
 int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len)
 {
-	struct regional *region = regional_create();
+	if (!region) {
+		region = regional_create();
+		if (!region) {
+			abort();
+		}
+}
 	sldns_buffer pktbuf;
 	sldns_buffer_init_frm_data(&pktbuf, (void*)buf, len);
 
 	struct msg_parse prs;
 	memset(&prs, 0, sizeof(prs));
 	parse_packet(&pktbuf, &prs, region);
-	regional_destroy(region);
 	return 0;
 }
