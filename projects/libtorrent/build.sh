d6b7a259c64370cfd454ff73082c9b12e4e7dd09
Paul Dreik <github@pauldreik.se>
2019-03-28 07:07:56 +0100

    add libtorrent

---
+++ projects/libtorrent/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eu
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# build project
+# e.g.
+# ./autogen.sh
+# ./configure
+# make -j$(nproc) all
+
+# build fuzzers
+# e.g.
+# $CXX $CXXFLAGS -std=c++11 -Iinclude \
+#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
+#     -lFuzzingEngine /path/to/library.a
+mkdir build
+cd build
+cmake -DBUILD_SHARED_LIBS=Off ..
+make
+cp fuzzers/fuzzer_* $OUT
+pushd $OUT
+for f in bdecode http_parser magnet ; do
+wget https://www.pauldreik.se/fuzzer_${f}_seed_corpus.zip
+done  
