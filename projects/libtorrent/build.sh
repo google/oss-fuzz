037d6e212b841a5c2c0b634ca0f5d69e9425f446
Paul Dreik <github@pauldreik.se>
2019-03-28 16:01:33 +0100

    remove templated text

--- projects/libtorrent/build.sh
+++ projects/libtorrent/build.sh
@@ -15,17 +15,6 @@
 #
 ################################################################################
 
-# build project
-# e.g.
-# ./autogen.sh
-# ./configure
-# make -j$(nproc) all
-
-# build fuzzers
-# e.g.
-# $CXX $CXXFLAGS -std=c++11 -Iinclude \
-#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
-#     -lFuzzingEngine /path/to/library.a
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=Off ..
