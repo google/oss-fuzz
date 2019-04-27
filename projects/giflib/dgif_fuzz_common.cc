098e74cbfae2f958f43d10a0adf886b78eb3bcec
Bhargava Shastry <bshas3@gmail.com>
2019-04-27 18:49:30 +0200

    Add new function that is a replica of Android JNI code

--- projects/giflib/dgif_fuzz_common.cc
+++ projects/giflib/dgif_fuzz_common.cc
@@ -58,3 +58,100 @@ int fuzz_dgif_extended(const uint8_t *Data, size_t Size)
 	free(gifData);
 	return 0;
 }
+
+static Color8888 gifColorToColor8888(const GifColorType& color) {
+	return ARGB_TO_COLOR8888(0xff, color.Red, color.Green, color.Blue);
+}
+
+static bool willBeCleared(const GraphicsControlBlock& gcb) {
+	return gcb.DisposalMode == DISPOSE_BACKGROUND || gcb.DisposalMode == DISPOSE_PREVIOUS;
+}
+
+static long getDelayMs(GraphicsControlBlock& gcb) {
+	return gcb.DelayTime * 10;
+}
+
+int fuzz_dgif_ala_android(const uint8_t *Data, size_t Size)
+{
+	GifFileType *GifFile;
+	int Error;
+	uint8_t *gifData = (uint8_t *)malloc(Size);
+	memcpy(gifData, Data, Size);
+	struct gifUserData gUData = {Size, gifData};
+
+	GifFile = DGifOpen((void *)&gUData, stub_input_reader, &Error);
+	if (GifFile == NULL) {
+		free(gifData);
+		return 0;
+	}
+
+	if(DGifSlurp(GifFile) != GIF_OK){
+		DGifCloseFile(GifFile, &Error);
+		free(gifData);
+		return 0;
+	}
+
+	long durationMs = 0;
+	int lastUnclearedFrame = -1;
+	bool *preservedFrames = new bool[GifFile->ImageCount];
+	int *restoringFrames = new int[GifFile->ImageCount];
+	int loopCount = 0;
+	Color8888 bgColor = 0;
+
+	GraphicsControlBlock gcb;
+	for (int i = 0; i < GifFile->ImageCount; i++) {
+		const SavedImage& image = GifFile->SavedImages[i];
+		// find the loop extension pair
+		for (int j = 0; (j + 1) < image.ExtensionBlockCount; j++) {
+			ExtensionBlock* eb1 = image.ExtensionBlocks + j;
+			ExtensionBlock* eb2 = image.ExtensionBlocks + j + 1;
+			if (eb1->Function == APPLICATION_EXT_FUNC_CODE
+			    // look for "NETSCAPE2.0" app extension
+			    && eb1->ByteCount == 11
+			    && !memcmp((const char*)(eb1->Bytes), "NETSCAPE2.0", 11)
+			    // verify extension contents and get loop count
+			    && eb2->Function == CONTINUE_EXT_FUNC_CODE
+			    && eb2->ByteCount == 3
+			    && eb2->Bytes[0] == 1) {
+				loopCount = (int)(eb2->Bytes[2] << 8) + (int)(eb2->Bytes[1]);
+			}
+		}
+		DGifSavedExtensionToGCB(GifFile, i, &gcb);
+		// timing
+		durationMs += getDelayMs(gcb);
+		// preserve logic
+		preservedFrames[i] = false;
+		restoringFrames[i] = -1;
+		if (gcb.DisposalMode == DISPOSE_PREVIOUS && lastUnclearedFrame >= 0) {
+			preservedFrames[lastUnclearedFrame] = true;
+			restoringFrames[i] = lastUnclearedFrame;
+		}
+		if (!willBeCleared(gcb)) {
+			lastUnclearedFrame = i;
+		}
+	}
+#if GIF_DEBUG
+	ALOGD("FrameSequence_gif created with size %d %d, frames %d dur %ld",
+            mGif->SWidth, mGif->SHeight, mGif->ImageCount, durationMs);
+    for (int i = 0; i < mGif->ImageCount; i++) {
+        DGifSavedExtensionToGCB(mGif, i, &gcb);
+        ALOGD("    Frame %d - must preserve %d, restore point %d, trans color %d",
+                i, mPreservedFrames[i], mRestoringFrames[i], gcb.TransparentColor);
+    }
+#endif
+	const ColorMapObject* cmap = GifFile->SColorMap;
+	if (cmap) {
+		// calculate bg color
+		GraphicsControlBlock gcb;
+		DGifSavedExtensionToGCB(GifFile, 0, &gcb);
+		if (gcb.TransparentColor == NO_TRANSPARENT_COLOR
+		    && GifFile->SBackGroundColor < cmap->ColorCount) {
+			bgColor = gifColorToColor8888(cmap->Colors[GifFile->SBackGroundColor]);
+		}
+	}
+	DGifCloseFile(GifFile, &Error);
+	free(gifData);
+	delete[] preservedFrames;
+	delete[] restoringFrames;
+	return 0;
+}
--- projects/giflib/dgif_fuzz_common.h
+++ projects/giflib/dgif_fuzz_common.h
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ARGB_TO_COLOR8888(a, r, g, b) \
+    ((a) << 24 | (b) << 16 | (g) << 8 | (r))
+
+typedef uint32_t Color8888;
+
 struct gifUserData {
 	size_t gifLen;
 	uint8_t *gifData;
 };
-
 
 int stub_input_reader (GifFileType *gifFileType, GifByteType *gifByteType, int len);
 int fuzz_dgif(const uint8_t *Data, size_t Size);
