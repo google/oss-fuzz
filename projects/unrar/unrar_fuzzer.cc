bd6ca4dc70f4dfc798e186cc677988066860db42
Varun Khaneja <ca.aawc@yahoo.com>
2019-05-01 14:12:01 -0700

    Re-enable unrar fuzzing by creating/clearing a temporary directory

--- projects/unrar/unrar_fuzzer.cc
+++ projects/unrar/unrar_fuzzer.cc
@@ -3,10 +3,21 @@
 #include <sstream>
 #include <string>
 #include <unistd.h>
+#include <filesystem>
 
 #include "rar.hpp"
 
+namespace fs = std::__fs::filesystem;
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // unrar likes to create files in the current directory.
+  // So, the following few lines created and 'cd' to a directory named 'o'
+  // in the current working directory.
+  fs::path original_path = fs::current_path();
+  fs::path out_path = original_path / "o";
+  bool created = fs::create_directory(out_path);
+  fs::current_path(out_path);
+
   static const std::string filename = "temp.rar";
   std::ofstream file(filename,
                      std::ios::binary | std::ios::out | std::ios::trunc);
@@ -30,5 +41,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   }
 
   unlink(filename.c_str());
+
+  // 'cd' back to the original directory and delete 'o' along with
+  // all its contents.
+  fs::current_path(original_path);
+  fs::remove_all(out_path);
   return 0;
 }
