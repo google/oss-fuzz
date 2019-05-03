609c9fea2eea393e83715b67fd3d56e01704fff2
arvidn <arvid@libtorrent.org>
2019-05-03 09:54:09 +0200

    update libtorrent build script to only build and expose large fuzz targets

--- projects/libtorrent/build.sh
+++ projects/libtorrent/build.sh
@@ -27,10 +27,10 @@ echo "CXXFLAGS=$CXXFLAGS"
 echo "using clang : ossfuzz : $CXX : <compileflags>\"$CXXFLAGS\" <linkflags>\"$CXXFLAGS\" <linkflags>\"${LIB_FUZZING_ENGINE}\" ;" >project-config.jam
 cat project-config.jam
 cd fuzzers
-b2 clang-ossfuzz -j$(nproc) crypto=openssl fuzz=external sanitize=off stage
+b2 clang-ossfuzz -j$(nproc) crypto=openssl fuzz=external sanitize=off stage-large
 cp fuzzers/* $OUT
 
-wget https://github.com/arvidn/libtorrent/releases/download/libtorrent_1_2_0/corpus.zip
+wget https://github.com/arvidn/libtorrent/releases/download/libtorrent_1_2_1/corpus.zip
 unzip -q corpus.zip
 cd corpus
 for f in *; do
