9e933ae1f3d5322f45bd00074490e397d12e176f
Jonathan Metzman <metzman@chromium.org>
2019-05-10 15:56:39 -0700

    add module docstring

--- infra/travis/travis_build.py
+++ infra/travis/travis_build.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 ################################################################################
+"""Build modified projects."""
 
 from __future__ import print_function
 
