7850119a75613d89cc39e8217f55d10c95076868
Vincent Ulitzsch <vincent@srlabs.de>
2019-05-23 00:07:52 +0200

    Fix bug that caused build failure

--- projects/giflib/build.sh
+++ projects/giflib/build.sh
@@ -15,7 +15,7 @@ $CXX $CFLAGS -Wall -c -I giflib-code dgif_target.cc -o dgif_target.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE -std=c++11  -I giflib-code dgif_fuzz_common.cc dgif_target.o  \
         -o $OUT/dgif_target giflib-code/libgif.a
 
-$CXX $CFLAGS -Wall -c -I giflib-code egif_target.cc -o egif_target.o
+$CXX $CXXFLAGS -Wall -c -I giflib-code egif_target.cc -o egif_target.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE -std=c++11  -I giflib-code egif_fuzz_common.cc egif_target.o  \
         -o $OUT/egif_target giflib-code/libgif.a
 
@@ -33,7 +33,7 @@ ProtoToGif.cpp \
 LPM/src/libfuzzer/libprotobuf-mutator-libfuzzer.a \
 LPM/src/libprotobuf-mutator.a \
 LPM/external.protobuf/lib/libprotobuf.a \
-        -o $OUT/dgif_protobuf_target -fsanitize=fuzzer giflib-code/libgif.a
+        -o $OUT/dgif_protobuf_target giflib-code/libgif.a
 
 
 # Place dict and config in OUT
