02a91a656ba27a9dc494d1ef9c6a5390b00b87ed
Vincent Ulitzsch <vincent.ulitzsch@live.de>
2019-05-24 14:17:06 +0200

    Extend giflib harness: Cover more functions and add protobuf specification (#2442)
    * Add protobuf target prototype
    
    * Generalized architecture and added message stubs for future enhancements
    
    * Added fine grained conversion of global/local color tables, LSD and image data
    
    * Added conversion of extensions
    
    * Flagged important TODOs
    
    * Address important todos and add converter for GraphicControlExtension
    
    * Minor bugfix: m_hasLct now reset for every image
    
    * Fixed minor bugs and added GCExt to plaintext chunk
    
    * Minor change to bug fix just for clarity
    
    * Refactoring and add fuzz_dgif_extended
    
    Refactor the code so that both targets import from dgif_fuzz_common.
    Also add fuzz_dgif_extended, which exercises more functionality
    of giflib
    
    * Added minor comment
    
    * Add new function that is a replica of Android JNI code
    
    * Minor fix
    
    * print errors (covers gif_err.c)
    
    * bug fix in printing and disable it
    
    * bug fix in printing error desc
    
    * Add version variants to header message
    
    * Add draw function from gif_font.c
    
    * Remove LIB_FUZZING_ENGINE from compilation step and add GifDrawText8x8 API to custom fuzz harness
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Remove unnecessary loop
    
    * Small bugfix: x is no longer unitialized
    
    * Change to consistent formatting
    
    * Fix bug that caused build failure

---
+++ projects/giflib/egif_fuzz_common.cc
@@ -0,0 +1,39 @@
+#include "egif_fuzz_common.h"
+
+//using namespace std;
+
+extern "C" void PrintGifError(int ErrorCode);
+
+int stub_output_writer(GifFileType *gifFileType, const uint8_t *buf, int len)
+{
+	struct gifUserData *gud = (struct gifUserData *)gifFileType->UserData;
+
+	if (gud == NULL || gud->gifData == NULL || len == 0)
+		return 0;
+
+	memcpy(gud->gifData, buf, len);
+	gud->gifData += len;
+	gud->gifLen += len;
+	return len;
+}
+
+int fuzz_egif(const uint8_t *Data, size_t Size)
+{
+	GifFileType *GifFile;
+	int Error;
+	uint8_t *gifData = (uint8_t *)malloc(Size);
+	memcpy(gifData, Data, Size);
+	struct gifUserData gUData = {Size, gifData};
+
+	GifFile = EGifOpen((void *)&gUData, stub_output_writer, &Error);
+	if (GifFile == NULL)
+	{
+		PrintGifError(GifFile->Error);
+		free(gifData);
+		return 0;
+	}
+
+	EGifCloseFile(GifFile, &Error);
+	free(gifData);
+	return 0;
+}
