ecf68cf70b9eeadd075c06eebc29979fb0586130
Max Moroz <mmoroz@chromium.org>
2019-05-31 20:47:11 -0700

    [njs] Null terminate the script string.

--- projects/njs/njs_process_script_fuzzer.c
+++ projects/njs/njs_process_script_fuzzer.c
@@ -674,8 +674,9 @@ lvlhsh_pool_free(void *pool, void *p, size_t size)
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size == 0) return 0;
 
-  char* input = malloc(size);
+  char* input = malloc(size + 1);
   memcpy(input, data, size);
+  input[size] = 0;
   nxt_str_t line = {size, input};
 
   njs_vm_t      *vm;
