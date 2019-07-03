86f5130543c27faee5eb38740dca2cbe15e5507c
Sean Kau <sean@kautown.com>
2019-07-03 09:56:40 -0700

    [ghostscript] Run ./autogen.sh instead of autoconf and add $ORIGIN to rpath (#2564)
    * Run ./autogen.sh instead of autoconf.
    * Add $ORIGIN to the rpath

--- projects/ghostscript/build.sh
+++ projects/ghostscript/build.sh
@@ -42,7 +42,7 @@ CUPS_LDFLAGS=$($CUPSCONFIG --ldflags)
 CUPS_LIBS=$($CUPSCONFIG --image --libs)
 export CXXFLAGS="$CXXFLAGS $CUPS_CFLAGS"
 
-autoconf
+./autogen.sh
 CPPFLAGS="${CPPFLAGS:-} $CUPS_CFLAGS" ./configure \
   --enable-freetype --enable-fontconfig \
   --enable-cups --with-ijs --with-jbig2dec \
@@ -52,6 +52,7 @@ make -j$(nproc) libgs
 $CXX $CXXFLAGS $CUPS_LDFLAGS -std=c++11 -I. \
     fuzz/gstoraster_fuzzer.cc \
     -o "$OUT/gstoraster_fuzzer" \
+    -Wl,-rpath='$ORIGIN' \
     $CUPS_LIBS \
     $LIB_FUZZING_ENGINE bin/gs.a
 
