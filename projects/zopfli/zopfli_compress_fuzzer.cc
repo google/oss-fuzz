44fd44443c0a27b0f825987f015d09aa66eff14f
Google-Autofuzz <security-tps@google.com>
2019-07-30 16:21:46 -0400

    removed unnecessary comments

--- projects/zopfli/zopfli_compress_fuzzer.cc
+++ projects/zopfli/zopfli_compress_fuzzer.cc
@@ -11,20 +11,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   ZopfliOptions options;
   ZopfliInitOptions(&options);
 
-/*  if (size == 0) {
-    return 0;
-  }*/
-
-/*  const ZopfliFormat format = static_cast<ZopfliFormat>(data[0] % 3);
-  data++;
-  size--;*/
   const ZopfliFormat format = stream.PickValueInArray(
     {ZOPFLI_FORMAT_GZIP, ZOPFLI_FORMAT_ZLIB, ZOPFLI_FORMAT_DEFLATE});
 
   unsigned char* outbuf = nullptr;
   size_t outsize = 0;
   std::string input = stream.ConsumeRemainingBytesAsString();
-  /*ZopfliCompress(&options, format, data, size, &outbuf, &outsize);*/
   ZopfliCompress(&options, format,
                  reinterpret_cast<const unsigned char*>(input.data()),
                  input.size(), &outbuf, &outsize);
