e0e0679e6a0ed886dc7f431c8fb2aa0aff95aab6
Google-Autofuzz <security-tps@google.com>
2019-07-30 10:15:26 -0400

    update zopfli files

--- projects/zopfli/zopfli_compress_fuzzer.cc
+++ projects/zopfli/zopfli_compress_fuzzer.cc
@@ -4,24 +4,30 @@
 #include <string>
 
 #include "zopfli.h"
+#include "FuzzedDataProvider.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-
+  FuzzedDataProvider stream(data, size);
   ZopfliOptions options;
   ZopfliInitOptions(&options);
 
-  if (size == 0) {
+/*  if (size == 0) {
     return 0;
-  }
+  }*/
 
-  const ZopfliFormat format = static_cast<ZopfliFormat>(data[0] % 3);
+/*  const ZopfliFormat format = static_cast<ZopfliFormat>(data[0] % 3);
   data++;
-  size--;
+  size--;*/
+  const ZopfliFormat format = stream.PickValueInArray(
+    {ZOPFLI_FORMAT_GZIP, ZOPFLI_FORMAT_ZLIB, ZOPFLI_FORMAT_DEFLATE});
 
   unsigned char* outbuf = nullptr;
   size_t outsize = 0;
-
-  ZopfliCompress(&options, format, data, size, &outbuf, &outsize);
+  std::string input = stream.ConsumeRemainingBytesAsString();
+  /*ZopfliCompress(&options, format, data, size, &outbuf, &outsize);*/
+  ZopfliCompress(&options, format,
+                 reinterpret_cast<const unsigned char*>(input.data()),
+                 input.size(), &outbuf, &outsize);
 
   if (outbuf != nullptr) {
     free(outbuf);
