9e1c72c74a46a9bf0a9e80d469e5a3f48995dbc0
Google-Autofuzz <security-tps@google.com>
2019-07-31 10:17:04 -0400

    pushed new version of target

--- projects/zopfli/zopfli_compress_fuzzer.cc
+++ projects/zopfli/zopfli_compress_fuzzer.cc
@@ -4,22 +4,23 @@
 #include <string>
 
 #include "zopfli.h"
-#include "FuzzedDataProvider.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  FuzzedDataProvider stream(data, size);
   ZopfliOptions options;
   ZopfliInitOptions(&options);
 
-  const ZopfliFormat format = stream.PickValueInArray(
-    {ZOPFLI_FORMAT_GZIP, ZOPFLI_FORMAT_ZLIB, ZOPFLI_FORMAT_DEFLATE});
+  if (size == 0 || size > 1024) {
+    return 0;
+  }
+
+  const ZopfliFormat format = static_cast<ZopfliFormat>(data[0] % 3);
+  data++;
+  size--;
 
   unsigned char* outbuf = nullptr;
   size_t outsize = 0;
-  std::string input = stream.ConsumeRemainingBytesAsString();
-  ZopfliCompress(&options, format,
-                 reinterpret_cast<const unsigned char*>(input.data()),
-                 input.size(), &outbuf, &outsize);
+
+  ZopfliCompress(&options, format, data, size, &outbuf, &outsize);
 
   if (outbuf != nullptr) {
     free(outbuf);
