018d80ed71ed2b13c8248ef63d503b144754944d
Oliver Chang <oliverchang@users.noreply.github.com>
2019-08-21 11:36:18 +1000

    Fix build links on builds status page. (#2732)
    Setting routeData does not work if there is no existing route set.
    
    Also fix deploy to not cache files.

--- infra/gcb/templates/deploy.sh
+++ infra/gcb/templates/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gsutil -m cp -r bower_components index.html src manifest.json gs://oss-fuzz-build-logs
+gsutil -h "Cache-Control:no-cache,max-age=0" -m cp -r bower_components index.html src manifest.json gs://oss-fuzz-build-logs
