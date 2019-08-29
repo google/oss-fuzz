8dc528011347d5bc9ab797307fe6cfac6b5f5fb1
Guido Vranken <guidovranken@gmail.com>
2019-08-29 21:14:52 +0200

    [cryptofuzz] Disable libsodium to fix build

--- projects/cryptofuzz/build.sh
+++ projects/cryptofuzz/build.sh
@@ -144,22 +144,22 @@ then
 fi
 
 ##############################################################################
-if [[ $CFLAGS != *sanitize=memory* ]]
-then
-    # Compile libsodium (with assembly)
-    cd $SRC/libsodium
-    autoreconf -ivf
-    ./configure
-    make -j$(nproc) >/dev/null 2>&1
-
-    export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_LIBSODIUM"
-    export LIBSODIUM_A_PATH="$SRC/libsodium/src/libsodium/.libs/libsodium.a"
-    export LIBSODIUM_INCLUDE_PATH="$SRC/libsodium/src/libsodium/include"
-
-    # Compile Cryptofuzz libsodium (with assembly) module
-    cd $SRC/cryptofuzz/modules/libsodium
-    make -B
-fi
+#if [[ $CFLAGS != *sanitize=memory* ]]
+#then
+#    # Compile libsodium (with assembly)
+#    cd $SRC/libsodium
+#    autoreconf -ivf
+#    ./configure
+#    make -j$(nproc) >/dev/null 2>&1
+#
+#    export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_LIBSODIUM"
+#    export LIBSODIUM_A_PATH="$SRC/libsodium/src/libsodium/.libs/libsodium.a"
+#    export LIBSODIUM_INCLUDE_PATH="$SRC/libsodium/src/libsodium/include"
+#
+#    # Compile Cryptofuzz libsodium (with assembly) module
+#    cd $SRC/cryptofuzz/modules/libsodium
+#    make -B
+#fi
 
 if [[ $CFLAGS != *sanitize=memory* && $CFLAGS != *-m32* ]]
 then
