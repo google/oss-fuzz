0cc73f6c0a9ad35099d5f5e8d24c71201ae3a21d
Bhargava Shastry <bshas3@gmail.com>
2019-09-20 10:51:06 +0200

    libxml2: Move fuzzers to own repo

--- projects/libxml2/build.sh
+++ projects/libxml2/build.sh
@@ -21,14 +21,57 @@
 make -j$(nproc) clean
 make -j$(nproc) all
 
-seed_corpus_temp_file="$OUT/xml_seed_corpus.zip"
-zip -r $seed_corpus_temp_file $SRC/libxml2/test
+# create_seed_corpus name extension
+function create_seed_corpus() {
+    # Sub-directories of libxml2 to look for seed inputs
+    local xml_test_dirs=(./doc ./test ./test/XPath/tests ./result/XPath/tests)
+    # Wildcard corpus is used for fuzzers that do not
+    # process a file of a known extension.
+    if [[ $2 == "wildcard" ]]; then
+        find ${xml_test_dirs[@]} -type f | xargs zip $OUT/$1.zip;
+    else
+        find ${xml_test_dirs[@]} -type f -name "*.$2" | xargs zip $OUT/$1.zip;
+    fi
+}
 
-for fuzzer in libxml2_xml_read_memory_fuzzer libxml2_xml_reader_for_file_fuzzer; do
+function create_seed_corpus_for_all_known_extensions()
+{
+    local extensions=(xml html xsd rng wildcard)
+    for ext in ${extensions[@]}; do
+        create_seed_corpus ${ext}_seed_corpus ${ext}
+    done
+}
+
+create_seed_corpus_for_all_known_extensions
+
+ADD_FUZZERS_DIR=libxml2_fuzzers
+
+for fuzzer in libxml2_xml_read_memory_fuzzer libxml2_xml_reader_for_file_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_regexp_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_html_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_html_pushparse_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_xml_pushparse_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_relax_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_uri_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_schema_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_xpath_fuzzer \
+    $ADD_FUZZERS_DIR/libxml2_schema_validate_fuzzer; do
+  fuzzer_name=$(basename $fuzzer)
   $CXX $CXXFLAGS -std=c++11 -Iinclude/ \
-      $SRC/$fuzzer.cc -o $OUT/$fuzzer \
+      $SRC/$fuzzer.cc -o $OUT/${fuzzer_name} \
       $LIB_FUZZING_ENGINE .libs/libxml2.a
 
-  cp $SRC/*.dict $OUT/$fuzzer.dict
-  cp $seed_corpus_temp_file $OUT/${fuzzer}_seed_corpus.zip
+  if [[ $fuzzer == *"libxml2_xml"* ]]; then
+	  cp $SRC/xml.dict $OUT/${fuzzer_name}.dict
+      cp $OUT/xml_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
+  elif [[ $fuzzer == *"libxml2_html"* ]]; then
+	  cp $SRC/html_tags.dict $OUT/${fuzzer_name}.dict
+	  cp $OUT/html_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
+  elif [[ $fuzzer == *"libxml2_schema"* ]]; then
+	  cp $OUT/xsd_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
+  elif [[ $fuzzer == *"libxml2_relax"* ]]; then
+	  cp $OUT/rng_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
+  else
+      cp $OUT/wildcard_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
+  fi
 done
