c5b0fe9e05d2d189c3af23a52f4115c1b084fc51
Shane F. Carr <sffc@google.com>
2019-10-11 16:40:45 -0700

    Add project myanmar-tools (#2929)
    * Add project myanmar-tools
    
    * Tweaks to myanmar tools build.sh
    
    * Fix travis build error
    
    * Fix Craig Cornelius email address
    
    * Set CMAKE_{C,CXX} flags
    
    * Write verbose output in cmake
    
    * Disable uninteresting UBSan sanitizer based on this target

---
+++ projects/myanmar-tools/build.sh
@@ -0,0 +1,32 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd $SRC/myanmar-tools/clients/cpp
+mkdir build
+cd build
+cmake -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
+    -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
+make all
+
+# Note: don't use the myanmartools_fuzz CMake target directly because we want
+# to link with LIB_FUZZING_ENGINE instead of the default fuzzer.
+# Copy the .so file to $OUT as well as the executable.
+mkdir -p $OUT/lib
+cp libmyanmartools.so $OUT/lib
+$CXX $CXXFLAGS -std=c++11 -I../public -L$OUT/lib \
+    -Wl,-rpath $OUT/lib -lmyanmartools \
+    -o $OUT/zawgyi_detector_fuzz_target \
+    ../zawgyi_detector_fuzz_target.cpp \
+    $LIB_FUZZING_ENGINE
