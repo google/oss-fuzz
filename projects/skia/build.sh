ca2f85608afbd76d20f4c48ac4415f73c4be0365
Kevin Lubick <kjlubick@google.com>
2019-10-21 08:54:33 -0400

    copy

--- projects/skia/build.sh
+++ projects/skia/build.sh
@@ -102,7 +102,8 @@ $SRC/depot_tools/ninja -C out/Fuzz region_deserialize region_set_path \
                                    textblob_deserialize skjson \
                                    api_null_canvas api_image_filter api_pathop \
                                    api_polyutils android_codec image_decode_incremental \
-                                   sksl2glsl sksl2spirv sksl2metal sksl2pipeline
+                                   sksl2glsl sksl2spirv sksl2metal sksl2pipeline \
+                                   api_skdescriptor
 
 $SRC/depot_tools/ninja -C out/Fuzz_mem_constraints image_filter_deserialize \
                                                    api_raster_n32_canvas \
@@ -225,3 +226,7 @@ cp ./sksl_seed_corpus.zip $OUT/sksl2metal_seed_corpus.zip
 cp out/Fuzz/sksl2pipeline $OUT/sksl2pipeline
 cp ./sksl2pipeline.options $OUT/sksl2pipeline.options
 cp ./sksl_seed_corpus.zip $OUT/sksl2pipeline_seed_corpus.zip
+
+cp out/Fuzz/api_skdescriptor $OUT/api_skdescriptor
+cp ./api_skdescriptor.options $OUT/api_skdescriptor.options
+cp ./api_skdescriptor_seed_corpus.zip $OUT/api_skdescriptor_seed_corpus.zip
