14bd043a8c4abe7d425de0acb72efdee100f7d7e
Andreas Schneider <asn@cryptomilk.org>
2019-11-09 16:08:42 +0100

    [libssh] Fix building libssh as a static library (#3020)

--- projects/libssh/build.sh
+++ projects/libssh/build.sh
@@ -19,7 +19,7 @@ mkdir -p build
 pushd build
 cmake -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
     -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-    -DWITH_STATIC_LIB=ON ..
+    -DBUILD_SHARED_LIBS=OFF ..
 make "-j$(nproc)"
 
 $CXX $CXXFLAGS -std=c++11 -I$SRC/libssh/include/ \
