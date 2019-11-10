16ddb85aafd645739aa6ec192210fb088c21f552
Guido Vranken <guidovranken@users.noreply.github.com>
2019-11-10 01:26:12 +0100

    [cryptofuzz] ARM mbed crypto (#3021)

--- projects/cryptofuzz/build.sh
+++ projects/cryptofuzz/build.sh
@@ -90,6 +90,27 @@ then
 fi
 
 ##############################################################################
+# Compile mbed crypto
+cd $SRC/mbed-crypto/
+scripts/config.pl set MBEDTLS_PLATFORM_MEMORY
+if [[ $CFLAGS == *sanitize=memory* ]]
+then
+    scripts/config.pl unset MBEDTLS_HAVE_ASM
+    scripts/config.pl unset MBEDTLS_PADLOCK_C
+    scripts/config.pl unset MBEDTLS_AESNI_C
+fi
+mkdir build/
+cd build/
+cmake .. -DENABLE_PROGRAMS=0 -DENABLE_TESTING=0
+make -j$(nproc) >/dev/null 2>&1
+export MBEDTLS_LIBMBEDCRYPTO_A_PATH="$SRC/mbed-crypto/build/library/libmbedcrypto.a"
+export MBEDTLS_INCLUDE_PATH="$SRC/mbed-crypto/include"
+export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_MBEDTLS"
+# Compile Cryptofuzz mbed crypto module
+cd $SRC/cryptofuzz/modules/mbedtls
+make -B
+
+##############################################################################
 # Compile wolfCrypt
 cd $SRC/wolfssl
 autoreconf -ivf
