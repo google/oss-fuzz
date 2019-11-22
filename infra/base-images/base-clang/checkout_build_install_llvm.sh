476c00a7890d43671d798ab7c0b4bfabccbb5642
Jonathan Metzman <metzman@chromium.org>
2019-11-22 11:54:47 -0800

    Use git to get clang source code

--- infra/base-images/base-clang/checkout_build_install_llvm.sh
+++ infra/base-images/base-clang/checkout_build_install_llvm.sh
@@ -23,11 +23,7 @@ CHECKOUT_RETRIES=10
 function clone_with_retries {
   REPOSITORY=$1
   LOCAL_PATH=$2
-  COMMIT=$3
   CHECKOUT_RETURN_CODE=1
-  if [ -d $LOCAL_PATH ]; then
-    return 0
-  fi
 
   # Disable exit on error since we might encounter some failures while retrying.
   set +e
@@ -51,18 +47,21 @@ cd $SRC/chromium_tools
 git clone https://chromium.googlesource.com/chromium/src/tools/clang
 cd clang
 
-OUR_LLVM_REVISION=  # For manual bumping (must be a git SHA)
-FORCE_OUR_REVISION=0  # To allow for manual downgrades.
+LLVM_SRC=$SRC/llvm-project
 
+OUR_LLVM_REVISION=e84b7a5fe230e42b8e6fe451369874a773bf1867  # For manual bumping.
+FORCE_OUR_REVISION=0  # To allow for manual downgrades.
 LLVM_REVISION=$(grep -Po "CLANG_REVISION = '\K[a-f0-9]+(?=')" scripts/update.py)
-IS_OUR_REVISION_ANCESTOR=$(git merge-base --is-ancestor $OUR_LLVM_REVISION $LLVM_REVISION)
-if [ $FORCE_OUR_REVISION  && OUR_REVISION_ANCESTOR] ; then
+
+clone_with_retries https://github.com/llvm/llvm-project.git $LLVM_SRC
+
+IS_OUR_REVISION_ANCESTOR=$(git -C $LLVM_SRC merge-base --is-ancestor $OUR_LLVM_REVISION $LLVM_REVISION)
+if [ $FORCE_OUR_REVISION  ] || [ $IS_OUR_REVISION_ANCESTOR ] ; then
   LLVM_REVISION=$OUR_LLVM_REVISION
 fi
-echo "Using LLVM revision: $LLVM_REVISION"
 
-cd $SRC && clone_with_retries https://github.com/llvm/llvm-project.git llvm-project
-git -C $LOCAL_PATH checkout $LLVM_REVISION
+git -C $LLVM_SRC checkout $LLVM_REVISION
+echo "Using LLVM revision: $LLVM_REVISION"
 
 # Build & install. We build clang in two stages because gcc can't build a
 # static version of libcxxabi
@@ -86,7 +85,8 @@ esac
 cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLIBCXXABI_ENABLE_SHARED=OFF \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
-      $SRC/llvm-project/llvm
+      -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi;compiler-rt;clang" \
+      $LLVM_SRC/llvm
 ninja
 
 cd $WORK/llvm-stage2
@@ -95,7 +95,8 @@ export CXX=$WORK/llvm-stage1/bin/clang++
 cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLIBCXXABI_ENABLE_SHARED=OFF \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
-      $SRC/llvm-project/llvm
+      -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi;compiler-rt;clang" \
+      $LLVM_SRC/llvm
 ninja
 ninja install
 rm -rf $WORK/llvm-stage1 $WORK/llvm-stage2
@@ -108,7 +109,8 @@ cmake -G "Ninja" \
       -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_FLAGS="-m32" -DCMAKE_CXX_FLAGS="-m32" \
       -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
-      $SRC/llvm-project
+      -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi;compiler-rt;clang" \
+      $LLVM_SRC/llvm
 
 ninja cxx
 ninja install-cxx
@@ -128,16 +130,15 @@ cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
       -DCMAKE_CXX_FLAGS="-fsanitize-blacklist=$WORK/msan/blacklist.txt" \
-      $SRC/llvm-project/llvm
+      -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi;compiler-rt;clang" \
+      $LLVM_SRC/llvm
 ninja cxx
 ninja install-cxx
 rm -rf $WORK/msan
 
-# Pull trunk libfuzzer.
-cd $SRC && svn co https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
-
+cp -r $LLVM_SRC/compiler-rt/lib/fuzzer $SRC/libfuzzer
 # Cleanup
-rm -rf $SRC/llvm-project
+rm -rf $LLVM_SRC
 rm -rf $SRC/chromium_tools
 apt-get remove --purge -y $LLVM_DEP_PACKAGES
 apt-get autoremove -y
