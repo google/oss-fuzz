5668cc422c2c92d38a370545d3591039fb5bb8d4
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2019-11-22 13:21:29 -0800

    Install clang using git (#3060)

--- infra/base-images/base-clang/checkout_build_install_llvm.sh
+++ infra/base-images/base-clang/checkout_build_install_llvm.sh
@@ -15,12 +15,12 @@
 #
 ################################################################################
 
-LLVM_DEP_PACKAGES="build-essential make cmake ninja-build git subversion python2.7 g++-multilib"
+LLVM_DEP_PACKAGES="build-essential make cmake ninja-build git python2.7 g++-multilib"
 apt-get install -y $LLVM_DEP_PACKAGES
 
 # Checkout
 CHECKOUT_RETRIES=10
-function checkout_with_retries {
+function clone_with_retries {
   REPOSITORY=$1
   LOCAL_PATH=$2
   CHECKOUT_RETURN_CODE=1
@@ -29,7 +29,7 @@ function checkout_with_retries {
   set +e
   for i in $(seq 1 $CHECKOUT_RETRIES); do
     rm -rf $LOCAL_PATH
-    svn co $REPOSITORY $LOCAL_PATH
+    git clone $REPOSITORY $LOCAL_PATH
     CHECKOUT_RETURN_CODE=$?
     if [ $CHECKOUT_RETURN_CODE -eq 0 ]; then
       break
@@ -47,21 +47,25 @@ cd $SRC/chromium_tools
 git clone https://chromium.googlesource.com/chromium/src/tools/clang
 cd clang
 
-OUR_LLVM_REVISION=359254  # For manual bumping.
+LLVM_SRC=$SRC/llvm-project
+OUR_LLVM_REVISION=e84b7a5fe230e42b8e6fe451369874a773bf1867  # For manual bumping.
 FORCE_OUR_REVISION=0  # To allow for manual downgrades.
-LLVM_REVISION=$(grep -Po "CLANG_SVN_REVISION = '\K\d+(?=')" scripts/update.py)
+LLVM_REVISION=$(grep -Po "CLANG_REVISION = '\K[a-f0-9]+(?=')" scripts/update.py)
 
-if [ $OUR_LLVM_REVISION -gt $LLVM_REVISION ] || [ $FORCE_OUR_REVISION -ne 0 ]; then
+clone_with_retries https://github.com/llvm/llvm-project.git $LLVM_SRC
+
+set +e
+IS_OUR_REVISION_ANCESTOR=$(git -C $LLVM_SRC merge-base --is-ancestor $OUR_LLVM_REVISION $LLVM_REVISION)
+set -e
+
+# Use our revision if specified or if our revision is a later revision than
+# Chrome's.
+if [ ! $IS_OUR_REVISION_ANCESTOR  ] || [ $FORCE_OUR_REVISION ] ; then
   LLVM_REVISION=$OUR_LLVM_REVISION
 fi
 
+git -C $LLVM_SRC checkout $LLVM_REVISION
 echo "Using LLVM revision: $LLVM_REVISION"
-
-cd $SRC && checkout_with_retries https://llvm.org/svn/llvm-project/llvm/trunk@$LLVM_REVISION llvm
-cd $SRC/llvm/tools && checkout_with_retries https://llvm.org/svn/llvm-project/cfe/trunk@$LLVM_REVISION clang
-cd $SRC/llvm/projects && checkout_with_retries https://llvm.org/svn/llvm-project/compiler-rt/trunk@$LLVM_REVISION compiler-rt
-cd $SRC/llvm/projects && checkout_with_retries https://llvm.org/svn/llvm-project/libcxx/trunk@$LLVM_REVISION libcxx
-cd $SRC/llvm/projects && checkout_with_retries https://llvm.org/svn/llvm-project/libcxxabi/trunk@$LLVM_REVISION libcxxabi
 
 # Build & install. We build clang in two stages because gcc can't build a
 # static version of libcxxabi
@@ -82,10 +86,13 @@ case $(uname -m) in
         exit 1
         ;;
 esac
+
+PROJECTS_TO_BUILD="libcxx;libcxxabi;compiler-rt;clang"
 cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLIBCXXABI_ENABLE_SHARED=OFF \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
-      $SRC/llvm
+      -DLLVM_ENABLE_PROJECTS=$PROJECTS_TO_BUILD \
+      $LLVM_SRC/llvm
 ninja
 
 cd $WORK/llvm-stage2
@@ -94,7 +101,8 @@ export CXX=$WORK/llvm-stage1/bin/clang++
 cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLIBCXXABI_ENABLE_SHARED=OFF \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
-      $SRC/llvm
+      -DLLVM_ENABLE_PROJECTS=$PROJECTS_TO_BUILD \
+      $LLVM_SRC/llvm
 ninja
 ninja install
 rm -rf $WORK/llvm-stage1 $WORK/llvm-stage2
@@ -107,7 +115,8 @@ cmake -G "Ninja" \
       -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_FLAGS="-m32" -DCMAKE_CXX_FLAGS="-m32" \
       -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
-      $SRC/llvm
+      -DLLVM_ENABLE_PROJECTS=$PROJECTS_TO_BUILD \
+      $LLVM_SRC/llvm
 
 ninja cxx
 ninja install-cxx
@@ -127,16 +136,15 @@ cmake -G "Ninja" \
       -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="$TARGET_TO_BUILD" \
       -DCMAKE_CXX_FLAGS="-fsanitize-blacklist=$WORK/msan/blacklist.txt" \
-      $SRC/llvm
+      -DLLVM_ENABLE_PROJECTS=$PROJECTS_TO_BUILD \
+      $LLVM_SRC/llvm
 ninja cxx
 ninja install-cxx
 rm -rf $WORK/msan
 
-# Pull trunk libfuzzer.
-cd $SRC && svn co https://llvm.org/svn/llvm-project/compiler-rt/trunk/lib/fuzzer libfuzzer
-
+cp -r $LLVM_SRC/compiler-rt/lib/fuzzer $SRC/libfuzzer
 # Cleanup
-rm -rf $SRC/llvm
+rm -rf $LLVM_SRC
 rm -rf $SRC/chromium_tools
 apt-get remove --purge -y $LLVM_DEP_PACKAGES
 apt-get autoremove -y
