db51e84fc0db72c648f1d3618ce93544029e06e6
Nikita Popov <nikita.ppv@gmail.com>
2019-12-14 14:46:40 +0100

    [php] Disable object-size sanitizer (#3128)
    PHP uses a union which is only allocated to the size of the used
    union member, which is apparently incompatible with the object-size
    sanitizer. This is really hard for us to fix, so instead disable
    the sanitizer.

--- projects/php/build.sh
+++ projects/php/build.sh
@@ -24,6 +24,10 @@ popd
 export ONIG_CFLAGS="-I$PWD/oniguruma/src"
 export ONIG_LIBS="-L$PWD/oniguruma/src/.libs -l:libonig.a"
 
+# PHP's zend_function union is incompatible with the object-size sanitizer
+export CFLAGS="$CFLAGS -fno-sanitize=object-size"
+export CXXFLAGS="$CXXFLAGS -fno-sanitize=object-size"
+
 # build project
 ./buildconf
 ./configure \
