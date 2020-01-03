a0f96ce1d03de0941a33daa548e705795f4b3012
Leo Neat <leosneat@gmail.com>
2020-01-03 15:15:43 -0800

    Infra for CI Fuzz script

---
+++ infra/ci_fuzz.py
@@ -0,0 +1,56 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module used by CI tools in order to interact with fuzzers.
+This module helps CI tools do the following
+  1. build fuzzers
+  2. run fuzzers
+Eventually it will be used to help CI tools determine which fuzzers to run.
+"""
+
+import argparse
+
+def main():
+  """Connects Fuzzers with CI tools."""
+  parser = argparse.ArgumentParser(
+      description='Help CI tools manage specific fuzzers')
+
+  parser.add_subparsers(dest='command')
+  build_fuzzer_parser = subparsers.add_parser('build_fuzzers', help='Build fuzzers')
+  build_fuzzer_parser.add_argument('project_name')
+
+  run_fuzzer_parser = subparsers.add_parser('run_fuzzer', help='Run a specific projects fuzzers')
+  run_fuzzer_parser.add_argument('project_name')
+  run_fuzzer_parser.add_argument('fuzzer_name')
+
+  if args.command == 'build_fuzzers':
+    return build_fuzzers(args)
+  elif args.command == 'run_fuzzer':
+    return run_fuzzer(args)
+  else:
+    print('Invalid argument option, use  "build_fuzzers" or "run_fuzzer"'')
+    return 1
+
+
+def build_fuzzers(args):
+  """Builds all of the fuzzers for a specific OSS-Fuzz project."""
+  return 0
+
+
+def run_fuzzers(args):
+  """Runs a specific fuzzer for a specific OSS-Fuzz project."""
+  return 0
+
+
+if __name__ == '__main__':
+  main()
