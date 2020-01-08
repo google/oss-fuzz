cc257b78f9c7a8ca6bb8f1367e62d19e3e6fe0f3
Leo Neat <leosneat@gmail.com>
2020-01-08 11:04:12 -0800

    detect_repo style updates

--- infra/base-images/base-builder/detect_repo.py
+++ infra/base-images/base-builder/detect_repo.py
@@ -42,11 +42,10 @@ def main():
       help='The location of the oss-fuzz projects source directory',
       required=True)
   parser.add_argument(
-      '--repo_name', help='the name of the github repo', required=False)
+      '--repo_name', help='the name of the github repo')
   parser.add_argument(
       '--example_commit',
-      help='A commit SHA refrencing the projects main repo',
-      required=False)
+      help='A commit SHA refrencing the projects main repo')
 
   args = parser.parse_args()
   if not args.repo_name and not args.example_commit:
@@ -91,15 +90,12 @@ def check_for_repo_name(repo_path, repo_name):
     repo_name: The name of the target git repo
 
   """
-  # Check if valid git repo.
-  print("Repo path: " + repo_path)
   if not os.path.exists(os.path.join(repo_path, '.git')):
     return False
 
   out, _ = execute(['git', 'config', '--get', 'remote.origin.url'],
                    location=repo_path)
   out = out.split('/')[-1].replace('.git', '').rstrip()
-  print("Repo possability: " + out)
   return out == repo_name
 
 
