ca92b7539326d87b8831dbabe5b5a3cdb3988841
Leo Neat <leosneat@gmail.com>
2020-01-09 11:43:58 -0800

    Entrypoint.py back to name over url

--- infra/cifuzz/actions/entrypoint.py
+++ infra/cifuzz/actions/entrypoint.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,29 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Builds a specific OSS-Fuzz project's fuzzers at a specific commit.
-"""
-import argparse
+"""Builds a specific OSS-Fuzz project's fuzzers at a specific commit."""
+
 import os
 import subprocess
 
-GITHUB_URL = 'https://github.com/'
 
 def main():
   """Finds the commit SHA where an error was initally introduced."""
   project_name = os.environ['OSS_FUZZ_PROJECT_NAME']
-  repo_url = GITHUB_URL + os.environ['GITHUB_REPOSITORY']
+  repo_name = os.environ['GITHUB_REPOSITORY'].rsplit('/',1)[-1]
   commit_sha = os.environ['GITHUB_SHA']
-  branch_name = os.environ['GITHUB_REF']
-  print("Building fuzzers\nproject: %s\nrepo url: %s\nbranch: %s\nCOMMIT: %s" % (project_name, repo_url ,branch_name, commit_sha))
-  command = ['python3', '/src/oss-fuzz/infra/ci_fuzz.py', 'build_fuzzers', project_name, repo_url , commit_sha]
+  print('Building fuzzers\nproject: %s\nrepo name: %s\nbranch: %s\ncommit: %s' % (project_name, repo_name, commit_sha))
+  command = ['python3', '/src/oss-fuzz/infra/ci_fuzz.py', 'build_fuzzers', project_name, repo_name , commit_sha]
   print('Running command: %s' % command)
   if subprocess.check_call(command):
     return 1
   command = ['python3', '/src/oss-fuzz/infra/cifuzz.py', 'run_fuzzers', project_name]
-  print('Running command: %s' % command)
-  print(subprocess.check_output(command))
-
+  if subprocess.check_call(command):
+    return 1
   return 0
 
 
