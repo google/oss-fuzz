b8fc0b3c9cb2805f7e90434dc4c56d021bee0c69
Leo Neat <leosneat@gmail.com>
2020-02-18 08:04:17 -0800

    Olivers comments

--- infra/cifuzz/fuzz_target.py
+++ infra/cifuzz/fuzz_target.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """A module to handle running a fuzz target for a specified amount of time."""
-import io
 import logging
 import os
 import posixpath
