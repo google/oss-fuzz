53361b4f6739d1721edc4c8ee9e3344e2338a365
Robert Loehning <robert.loehning@qt.io>
2020-02-27 21:09:39 +0100

    [qt] Provide corpus for beginlayout

--- projects/qt/build.sh
+++ projects/qt/build.sh
@@ -32,6 +32,7 @@ make -j$(nproc)
 # prepare corpus files
 zip -j $WORK/html $SRC/qtqa/fuzzing/testcases/html/*
 zip -j $WORK/markdown $SRC/qtqa/fuzzing/testcases/markdown/*
+zip -j $WORK/text $SRC/qtqa/fuzzing/testcases/text/* /usr/share/afl/testcases/others/text/*
 zip -j $WORK/xml $SRC/qtqa/fuzzing/testcases/xml/* /usr/share/afl/testcases/others/xml/*
 
 # build fuzzers
@@ -79,4 +80,4 @@ build_fuzzer() {
 build_fuzzer "old" "qtbase" "corelib/serialization/qxmlstream/qxmlstreamreader/readnext/readnext.pro" "xml" "/usr/share/afl/testcases/_extras/xml.dict"
 # build_fuzzer "new" "qtbase" "gui/text/qtextdocument/sethtml/sethtml.pro" "html" "/usr/share/afl/testcases/_extras/html_tags.dict"
 build_fuzzer "old" "qtbase" "gui/text/qtextdocument/setmarkdown/setmarkdown.pro" "markdown"
-build_fuzzer "old" "qtbase" "gui/text/qtextlayout/beginlayout/beginlayout.pro"
+build_fuzzer "old" "qtbase" "gui/text/qtextlayout/beginlayout/beginlayout.pro" "text"
