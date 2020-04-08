bc308a7a32677b839dd67c2f06539d7b42bca843
Abhishek Arya <inferno@chromium.org>
2020-04-07 19:26:24 -0700

    Add honggfuzz build in travis. (#3589)
    * Add honggfuzz build in travis.
    * Skip check_build for honggfuzz, only build.

--- infra/travis/travis_build.py
+++ infra/travis/travis_build.py
@@ -25,7 +25,7 @@ import subprocess
 import yaml
 
 DEFAULT_ARCHITECTURES = ['x86_64']
-DEFAULT_ENGINES = ['afl', 'libfuzzer']
+DEFAULT_ENGINES = ['afl', 'honggfuzz', 'libfuzzer']
 DEFAULT_SANITIZERS = ['address', 'undefined']
 
 
@@ -125,7 +125,10 @@ def build_project(project):
 
   print('Building project', project)
   build_fuzzers(project, engine, sanitizer, architecture)
-  if engine != 'none':
+
+  # TODO(https://github.com/google/oss-fuzz/issues/3592): Re-enable after
+  # Honggfuzz is supported in check_build.
+  if engine not in ['none', 'honggfuzz']:
     check_build(project, engine, sanitizer, architecture)
 
 
