70cab1c4efb39bb8a2b0d4f07fd9ca4bcd7a087d
davkor <david@adalogics.com>
2020-04-13 19:09:51 +0100

    Added initial integration of Lexbor.

---
+++ projects/lexbor/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+cd lexbor
+mkdir build && cd build
+cmake .. -DLEXBOR_BUILD_SEPARATELY=ON
+make
+
+$CC $CFLAGS -I/src/lexbor/source -Wall -pedantic -pipe -std=c99 -fPIC -c  /srx/lexbor/test/fuzzers/lexbor/encoding/decode.c
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE -stdlib=libc++ -Wall -pedantic -pipe -std=c99 -fPIC decode.o  /src/lexbor/build/liblexbor_static.a -Wl,-rpath,/src/lexbor/build -o $OUT/fuzz_decode
