415822f7c5015f31bac77038f56f547d79da9162
Kunal Tyagi <tyagi.kunal@live.com>
2020-04-15 08:59:53 +0900

    Adding license and correcting some errors

--- projects/pcl/build.sh
+++ projects/pcl/build.sh
@@ -1,5 +1,27 @@
-mkdir build && cd build
+#! /usr/bin/env sh
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+mkdir -p build && cd build
 cmake \
-  -DCMAKE_CXX_FLAGS="-Wall -Wextra -Og -g -Wno-cpp ${CXX_FLAGS_EXTRA} ${SANITIZERS}" \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=${OUT} \
   $@ \
-  ..
+  ${SRC}
+
+make -j${nproc}
