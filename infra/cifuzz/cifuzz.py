dac83d7b8e886b9d1102ecc3219906e48ca716db
Jonathan Metzman <metzman@chromium.org>
2020-04-28 13:37:52 -0700

    Revert "[CIFuzz] Support ALLOWED_BROKEN_TARGETS_PERCENTAGE"
    This reverts commit 6f1a7f79115a949c8954ccd76bca1baa66e686f6.

--- infra/cifuzz/cifuzz.py
+++ infra/cifuzz/cifuzz.py
@@ -255,7 +255,6 @@ def check_fuzzer_build(out_dir, sanitizer='address'):
   Returns:
     True if fuzzers are correct.
   """
-
   if not os.path.exists(out_dir):
     logging.error('Invalid out directory: %s.', out_dir)
     return False
@@ -267,14 +266,6 @@ def check_fuzzer_build(out_dir, sanitizer='address'):
       '--cap-add', 'SYS_PTRACE', '-e', 'FUZZING_ENGINE=' + DEFAULT_ENGINE, '-e',
       'SANITIZER=' + sanitizer, '-e', 'ARCHITECTURE=' + DEFAULT_ARCHITECTURE
   ]
-
-  allowed_broken_targets_percentage = os.getenv(
-      'ALLOWED_BROKEN_TARGETS_PERCENTAGE')
-  if allowed_broken_targets_percentage is not None:
-    set_env_var_arg = ('ALLOWED_BROKEN_TARGETS_PERCENTAGE=' +
-                       allowed_broken_targets_percentage)
-    command += ['-e', set_env_var_arg]
-
   container = utils.get_container_name()
   if container:
     command += ['-e', 'OUT=' + out_dir, '--volumes-from', container]
