df83eaec34fb878abc8796fce12b8ed1b8247313
Will Scott <willscott@gmail.com>
2020-04-29 20:18:41 -0700

    split backends into separate binaries for libfuzzer address sanitizer

--- projects/ipfs/build.sh
+++ projects/ipfs/build.sh
@@ -16,18 +16,31 @@
 ################################################################################
 
 cd $GOPATH/src/github.com/ipfs/go-datastore/fuzz
-DS_PROVIDERS="github.com/ipfs/go-ds-badger,github.com/ipfs/go-ds-badger2" go generate
 
-# vendor dependencies since go-fuzz doesn't play nicely with go mod.
-go mod vendor
-mkdir .gopath
-cd .gopath
-ln -s ../vendor src
-cd ..
+function compile_ds_fuzzer {
+  fuzzer=$1
 
-fuzzer="ipfs_ds_fuzzer"
-# Compile and instrument all Go files relevant to this fuzz target.
-GO111MODULE=off GOPATH=$PWD/.gopath go-fuzz -o $fuzzer.a .
+  if [ $# == 2 ]; then
+    rm provider* || true
+    DS_PROVIDERS="$2" go generate
+  fi
 
-# Link Go code ($fuzzer.a) with fuzzing engine to produce fuzz target binary.
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
+  # vendor dependencies since go-fuzz doesn't play nicely with go mod.
+  rm -rf vendor .gopath || true
+  go mod vendor
+  mkdir .gopath
+  cd .gopath
+  ln -s ../vendor src
+  cd ..
+
+  # Compile and instrument all Go files relevant to this fuzz target.
+  GO111MODULE=off GOPATH=$PWD/.gopath go-fuzz -o $fuzzer.a .
+
+  # Link Go code ($fuzzer.a) with fuzzing engine to produce fuzz target binary.
+  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
+}
+
+compile_ds_fuzzer ipfs_ds_flatfs
+compile_ds_fuzzer ipfs_ds_badger "github.com/ipfs/go-ds-badger"
+compile_ds_fuzzer ipfs_ds_badger2 "github.com/ipfs/go-ds-badger2"
+
