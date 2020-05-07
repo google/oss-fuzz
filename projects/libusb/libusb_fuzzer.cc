eba5ed440fad7814c6a0e94e10ecc71ffda94803
Google-Autofuzz <security-tps@google.com>
2020-05-07 15:26:07 -0400

    modified the fuzzer for initial integration

--- projects/libusb/libusb_fuzzer.cc
+++ projects/libusb/libusb_fuzzer.cc
@@ -20,23 +20,8 @@
 #include "libusb/libusb.h"
 #include "libusb/libusbi.h"
 
-/*static unsigned char *data_;
-static size_t size_;*/
-
-/*int fuzzer_get_active_config_descriptor(struct libusb_device *device,
-                                        unsigned char *buffer, size_t len,
-                                        int *host_endian) {
-  buffer = data_;
-  return std::min(len, size_);
-}
-
-const struct usbi_os_backend usbi_backend = {
-    .get_active_config_descriptor = fuzzer_get_active_config_descriptor,
-};*/
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-/*  data_ = (unsigned char *)data;
-  size_ = size;*/
+  struct libusb_transfer *transfer;
   FuzzedDataProvider stream(data, size);
   uint8_t bmRequestType = stream.ConsumeIntegral<uint8_t>();
   uint8_t bRequest = stream.ConsumeIntegral<uint8_t>();
@@ -44,22 +29,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   uint16_t wIndex = stream.ConsumeIntegral<uint16_t>();
   uint16_t wLength = stream.ConsumeIntegral<uint16_t>();
   std::vector<char> data_ = stream.ConsumeRemainingBytes<char>();
+  unsigned char* buffer = reinterpret_cast<unsigned char*>(data_.data());
 
-  libusb_fill_control_setup(reinterpret_cast<unsigned char*>(data_.data()),
+  transfer = libusb_alloc_transfer(0);
+  if (!transfer) {
+    return LIBUSB_ERROR_NO_MEM;
+  }
+
+  if (!buffer) {
+    libusb_free_transfer(transfer);
+    return LIBUSB_ERROR_NO_MEM;
+  }
+
+  libusb_fill_control_setup(buffer,
     bmRequestType, bRequest, wValue, wIndex, wLength);
 
-/*  int r;
-  libusb_device_handle *dev_handle;
-
-  r = libusb_get_string_descriptor(nullptr, 0, 0, buffer, sizeof(buffer));
-
-  if (r < 4){
-    return LIBUSB_ERROR_IO;
-  }*/
-
-/*  struct libusb_config_descriptor *config = nullptr;
-  libusb_get_active_config_descriptor(nullptr, &config);
-  libusb_free_config_descriptor(config);*/
-
+  libusb_free_transfer(transfer);
   return 0;
 }
