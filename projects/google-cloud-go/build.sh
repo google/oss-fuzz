c1c0db44ce3d11b9f4ee12bf37460c4cf60e9214
Adam Korczynski <adam@adalogics.com>
2020-05-15 19:18:59 +0100

    [google-cloud-go] Initial integration

---
+++ projects/google-cloud-go/build.sh
@@ -0,0 +1,33 @@
+#/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+function compile_fuzzer {
+  path=$1
+  function=$2
+  fuzzer=$3
+
+  go-fuzz -func $function -o $fuzzer.a $path
+
+  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
+}
+
+mkdir sample_cloud_project && cd sample_cloud_project
+echo -e 'import "cloud.google.com/go/spanner"'>>sample.go
+go get cloud.google.com/go/spanner
+
+mkdir $GOPATH/src/cloud.google.com/go/spanner/fuzz
+cp $SRC/fuzz.go $GOPATH/src/cloud.google.com/go/spanner/fuzz/
+compile_fuzzer cloud.google.com/go/spanner/fuzz Fuzz fuzz
