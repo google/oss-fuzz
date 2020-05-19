8c2792a28a07f6cb2d5ae2ee269ef6c4d6342135
Adam Korczynski <adam@adalogics.com>
2020-05-19 12:49:50 +0100

    [Minify] Initial integration

---
+++ projects/minify/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+function compile_fuzzer {
+  path=$1
+  function=$2
+  fuzzer=$3
+
+  go-fuzz -func $function -o $fuzzer.a $path
+
+  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
+}
+
+git clone https://github.com/tdewolff/fuzz
+cp -rf $SRC/fuzz/minify $GOPATH/src/github.com/tdewolff/minify/fuzz
+
+
+ls $GOPATH/src/github.com/tdewolff/minify/fuzz | while read target
+do
+    echo $target
+    compile_fuzzer github.com/tdewolff/minify/fuzz/$target Fuzz fuzz_$target
+done
