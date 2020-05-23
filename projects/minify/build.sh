d96459aa05fd9bb8112beaad4dea462eee61e22c
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2020-05-23 23:47:26 +0100

    [minify] Moved build script upstream (#3867)

--- projects/minify/build.sh
+++ projects/minify/build.sh
@@ -14,18 +14,4 @@
 # limitations under the License.
 #
 ################################################################################
-function compile_fuzzer {
-  path=$1
-  function=$2
-  fuzzer=$3
-
-  go-fuzz -func $function -o $fuzzer.a $path
-
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
-}
-
-find $GOPATH/src/github.com/tdewolff/minify/tests/*/ -maxdepth 1 -type d | while read target
-do
-    fuzz_target=`echo $target | cut -d'/' -f 9`
-    compile_fuzzer github.com/tdewolff/minify/tests/$fuzz_target Fuzz $fuzz_target-fuzzer
-done
+$GOPATH/src/github.com/tdewolff/minify/tests/oss-fuzz-build.sh
