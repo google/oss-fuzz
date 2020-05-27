3ea705f8e88ab4c35301cb3f1af4acf30c48bb8c
Google AutoFuzz Team <security-tps@google.com>
2020-05-27 19:45:59 -0400

    Update fuzzer

--- projects/libgd/gd_image_string_fuzzer.cc
+++ projects/libgd/gd_image_string_fuzzer.cc
@@ -31,25 +31,25 @@
 #include "src/gdfontl.h"*/
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  FuzzedDataProvider stream(data, size);
-  const uint8_t slate_width = stream.ConsumeIntegral<uint8_t>();
-  const uint8_t slate_height = stream.ConsumeIntegral<uint8_t>();
-  gdImagePtr slate_image = gdImageCreateTrueColor(slate_width, slate_height);
-  if (slate_image == nullptr) {
+    FuzzedDataProvider stream(data, size);
+    const uint8_t slate_width = stream.ConsumeIntegral<uint8_t>();
+    const uint8_t slate_height = stream.ConsumeIntegral<uint8_t>();
+    gdImagePtr slate_image = gdImageCreateTrueColor(slate_width, slate_height);
+    if (slate_image == nullptr) {
+      return 0;
+    }
+
+    const int x_position = stream.ConsumeIntegral<int>();
+    const int y_position = stream.ConsumeIntegral<int>();
+    const int text_color = stream.ConsumeIntegral<int>();
+    const gdFontPtr font_ptr = stream.PickValueInArray(
+        {gdFontGetGiant(), gdFontGetLarge(), gdFontGetMediumBold(),
+        gdFontGetSmall(), gdFontGetTiny()});
+    const std::string text = stream.ConsumeRemainingBytesAsString();
+
+    gdImageString(slate_image, font_ptr, x_position, y_position,
+                  reinterpret_cast<uint8_t*>(const_cast<char*>(text.c_str())),
+                  text_color);
+    gdImageDestroy(slate_image);
     return 0;
-  }
-
-  const int x_position = stream.ConsumeIntegral<int>();
-  const int y_position = stream.ConsumeIntegral<int>();
-  const int text_color = stream.ConsumeIntegral<int>();
-  const gdFontPtr font_ptr = stream.PickValueInArray(
-      {gdFontGetGiant(), gdFontGetLarge(), gdFontGetMediumBold(),
-       gdFontGetSmall(), gdFontGetTiny()});
-  const std::string text = stream.ConsumeRemainingBytesAsString();
-
-  gdImageString(slate_image, font_ptr, x_position, y_position,
-                reinterpret_cast<uint8_t*>(const_cast<char*>(text.c_str())),
-                text_color);
-  gdImageDestroy(slate_image);
-  return 0;
 }
