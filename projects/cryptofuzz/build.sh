46e8a18cceee6461c7f3406ba539aa70dc42aa44
Guido Vranken <guidovranken@gmail.com>
2020-05-29 20:48:17 +0200

    [cryptofuzz] wolfCrypt: Build with support for AES-CFB, AES-OFB

--- projects/cryptofuzz/build.sh
+++ projects/cryptofuzz/build.sh
@@ -326,7 +326,7 @@ fi
 cd $SRC/wolfssl
 autoreconf -ivf
 
-export WOLFCRYPT_CONFIGURE_PARAMS="--enable-static --enable-md2 --enable-md4 --enable-ripemd --enable-blake2 --enable-blake2s --enable-pwdbased --enable-scrypt --enable-hkdf --enable-cmac --enable-arc4 --enable-camellia --enable-rabbit --enable-aesccm --enable-aesctr --enable-hc128 --enable-xts --enable-des3 --enable-idea --enable-x963kdf --enable-harden"
+export WOLFCRYPT_CONFIGURE_PARAMS="--enable-static --enable-md2 --enable-md4 --enable-ripemd --enable-blake2 --enable-blake2s --enable-pwdbased --enable-scrypt --enable-hkdf --enable-cmac --enable-arc4 --enable-camellia --enable-rabbit --enable-aesccm --enable-aesctr --enable-hc128 --enable-xts --enable-des3 --enable-idea --enable-x963kdf --enable-harden --enable-aescfb --enable-aesofb"
 
 if [[ $CFLAGS = *sanitize=memory* ]]
 then
