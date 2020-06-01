e3e1174635306690fb2e543869265b2f6394a05c
DavidKorczynski <david@adalogics.com>
2020-06-01 15:18:46 +0100

    [haproxy] Fix build failures. (#3884)
    * Updated the haproxy fuzzers to build again.
    
    * The frame decoder needs additional updates since we need to call init_h2 to initialise a memory pool. Disabling this for now as this is a larger change in the code base and will fix up in the coming week.
    
    * Fixed hpack decode.
    
    * Updated the yaml since we dont want memory sanitizer.

--- projects/haproxy/fuzz_hpack_decode.c
+++ projects/haproxy/fuzz_hpack_decode.c
@@ -23,9 +23,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 #include <common/chunk.h>
 #include <common/hpack-dec.h>
 #include <common/mini-clist.h>
+#define HPACK_STANDALONE
 
 #define MAX_RQ_SIZE 65536
 #define MAX_HDR_NUM 1000
@@ -36,9 +38,7 @@ uint8_t buf[MAX_RQ_SIZE];
 char trash_buf[MAX_RQ_SIZE];
 char tmp_buf[MAX_RQ_SIZE];
 
-struct buffer trash = { .area = trash_buf, .data = 0, .size = sizeof(trash_buf) };
 struct buffer tmp   = { .area = tmp_buf,   .data = 0, .size = sizeof(tmp_buf)   };
-
 
 /* Empty function we dont need - we just need a callback */
 void debug_hexdump(FILE *out, const char *pfx, const char *buf,
@@ -54,21 +54,27 @@ void debug_hexdump(FILE *out, const char *pfx, const char *buf,
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
         char *new_str = (char *)malloc(size+1);
         struct hpack_dht *dht;
+        struct pool_head pool;
         int dht_size = 4096;
         if (new_str == NULL){
                 return 0;
         }
         memcpy(new_str, data, size);
         new_str[size] = '\0';
-        struct http_hdr list[MAX_HDR_NUM];      
+        struct http_hdr list[MAX_HDR_NUM];
 
-        dht = hpack_dht_alloc(dht_size);
-        hpack_decode_frame(dht, new_str, size, list,sizeof(list)/sizeof(list[0]), &tmp);
+        pool.size = dht_size;
+        pool_head_hpack_tbl = &pool;
+        dht = hpack_dht_alloc();
+
         if (dht != NULL)
         {
-            free(dht);
+            hpack_decode_frame(dht, new_str, size, list,sizeof(list)/sizeof(list[0]), &tmp);
+            if (dht != NULL)
+            {
+                free(dht);
+            }
         }
-
         free(new_str);
         return 0;
 }
