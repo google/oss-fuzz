03a9986e2f24ad5505a09e26b98b13ecea70f4c6
Jamie Pinheiro <jamiepinheiro@gmail.com>
2020-06-02 11:49:20 -0400

    Add size check

--- projects/libraw/libraw_fuzzer.cc
+++ projects/libraw/libraw_fuzzer.cc
@@ -1,3 +1,18 @@
+/*  Copyright 2020 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -6,6 +21,11 @@
 #include <libraw.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // RAW files less than 10mb
+  if (size > 10000000) {
+    return 0;
+  }
+
   LibRaw lib_raw;
 
   int err = lib_raw.open_buffer(
