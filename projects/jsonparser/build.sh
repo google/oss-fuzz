943dbe6ab8dc0506ea769f90496cc868980da3c6
AdamKorcz <adam@adalogics.com>
2020-06-04 10:33:37 +0100

    Updated build script

--- projects/jsonparser/build.sh
+++ projects/jsonparser/build.sh
@@ -15,14 +15,4 @@
 #
 ################################################################################
 
-function compile_fuzzer {
-  path=$1
-  function=$2
-  fuzzer=$3
-
-  go-fuzz -func $function -o $fuzzer.a $path
-
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
-}
-
-compile_fuzzer github.com/buger/jsonparser FuzzParseString fuzz
+$GOPATH/src/github.com/buger/jsonparser/oss-fuzz-build.sh
