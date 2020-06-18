9d589a7e86b9a5b68f8b06306fc510cca95ff296
asraa <asraa@google.com>
2020-06-18 11:20:41 -0400

    Add arthur and use less memory (#4001)
    Signed-off-by: Asra Ali <asraa@google.com>

--- projects/envoy/build.sh
+++ projects/envoy/build.sh
@@ -72,6 +72,7 @@ done
 
 # Build driverless libraries.
 bazel build --verbose_failures --dynamic_mode=off --spawn_strategy=standalone \
+  --discard_analysis_cache --notrack_incremental_state --nokeep_state_after_build \
   --genrule_strategy=standalone --strip=never \
   --copt=-fno-sanitize=vptr --linkopt=-fno-sanitize=vptr \
   --define tcmalloc=disabled --define signal_trace=disabled \
