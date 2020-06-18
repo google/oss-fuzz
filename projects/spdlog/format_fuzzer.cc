1e7f14a81d36818a252b10b18682d4c8bf62a0e7
Yunshu Ouyang <ouyangyunshu@google.com>
2020-06-18 08:44:44 +0200

    [spdlog] Added new fuzzers, modifed existing ones

--- projects/spdlog/format_fuzzer.cc
+++ projects/spdlog/format_fuzzer.cc
@@ -18,6 +18,25 @@
 
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/pattern_formatter.h"
+
+
+std::string my_formatter_txt = "custom-flag";
+
+class my_formatter_flag : public spdlog::custom_flag_formatter
+{
+  
+public:
+    void format(const spdlog::details::log_msg &, const std::tm &, spdlog::memory_buf_t &dest) override
+    {
+        dest.append(my_formatter_txt.data(), my_formatter_txt.data() + my_formatter_txt.size());
+    }
+
+    std::unique_ptr<custom_flag_formatter> clone() const override
+    {
+        return spdlog::details::make_unique<my_formatter_flag>();
+    }
+};
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static std::shared_ptr<spdlog::logger> my_logger;
@@ -34,23 +53,18 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   FuzzedDataProvider stream(data, size);
 
   const unsigned long size_arg = stream.ConsumeIntegral<unsigned long>();
-  const unsigned long int_arg = stream.ConsumeIntegral<unsigned long>();
-  const std::string string_arg = stream.ConsumeRandomLengthString(size);
+  const unsigned long  int_arg = stream.ConsumeIntegral<unsigned long>();
+  const char flag = (char)(stream.ConsumeIntegral<unsigned char>());
+  const std::string pattern = stream.ConsumeRandomLengthString();
+  my_formatter_txt = stream.ConsumeRandomLengthString();
+  const std::string string_arg = stream.ConsumeRandomLengthString();
   const std::string format_string = stream.ConsumeRemainingBytesAsString();
-
+  
+  using spdlog::details::make_unique;
+  auto formatter = make_unique<spdlog::pattern_formatter>();
+  formatter->add_flag<my_formatter_flag>(flag).set_pattern(pattern);
+  spdlog::set_formatter(std::move(formatter));
+  
   spdlog::info(format_string.c_str(), size_arg, int_arg, string_arg);
-  spdlog::trace(format_string.c_str(), size_arg, int_arg, string_arg);
-  spdlog::debug(format_string.c_str(), size_arg, int_arg, string_arg);
-  spdlog::error(format_string.c_str(), size_arg, int_arg, string_arg);
-  spdlog::warn(format_string.c_str(), size_arg, int_arg, string_arg);
-  spdlog::critical(format_string.c_str(), size_arg, int_arg, string_arg);
-
-  SPDLOG_INFO(format_string.c_str(), size_arg, int_arg, string_arg);
-  SPDLOG_TRACE(format_string.c_str(), size_arg, int_arg, string_arg);
-  SPDLOG_DEBUG(format_string.c_str(), size_arg, int_arg, string_arg);
-  SPDLOG_ERROR(format_string.c_str(), size_arg, int_arg, string_arg);
-  SPDLOG_WARN(format_string.c_str(), size_arg, int_arg, string_arg);
-  SPDLOG_CRITICAL(format_string.c_str(), size_arg, int_arg, string_arg);
-
   return 0;
 }
