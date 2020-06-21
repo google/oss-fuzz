63868cba907a461b9cc3ba1372744f4c44088821
werdnaH <32367543+werdnaH@users.noreply.github.com>
2020-06-21 23:35:58 +0000

    [skia] Remove Options Files with Only max_len Flag (#3987)
    * [skia] add svg fuzzer
    
    * [skia] removed options file
    
    * [skia] removed options file
    
    * [skia] removed all options files with only max_len
    
    * delete options
    
    * fixed cp statements
    
    Co-authored-by: Zepeng Hu <zepenghu@google.com>

--- projects/skia/build.sh
+++ projects/skia/build.sh
@@ -118,30 +118,23 @@ $SRC/depot_tools/ninja -C out/Fuzz_mem_constraints image_filter_deserialize \
                                                    api_mock_gpu_canvas
 
 cp out/Fuzz/region_deserialize $OUT/region_deserialize
-cp ./region_deserialize.options $OUT/region_deserialize.options
 
 cp out/Fuzz/region_set_path $OUT/region_set_path
-cp ./region_set_path.options $OUT/region_set_path.options
 cp ./region_set_path_seed_corpus.zip $OUT/region_set_path_seed_corpus.zip
 
 cp out/Fuzz/textblob_deserialize $OUT/textblob_deserialize
-cp ./textblob_deserialize.options $OUT/textblob_deserialize.options
 cp ./textblob_deserialize_seed_corpus.zip $OUT/textblob_deserialize_seed_corpus.zip
 
 cp out/Fuzz/path_deserialize $OUT/path_deserialize
-cp ./path_deserialize.options $OUT/path_deserialize.options
 cp ./path_deserialize_seed_corpus.zip $OUT/path_deserialize_seed_corpus.zip
 
 cp out/Fuzz/image_decode $OUT/image_decode
-cp ./image_decode.options $OUT/image_decode.options
 cp ./image_decode_seed_corpus.zip $OUT/image_decode_seed_corpus.zip
 
 cp out/Fuzz/animated_image_decode $OUT/animated_image_decode
-cp ./animated_image_decode.options $OUT/animated_image_decode.options
 cp ./animated_image_decode_seed_corpus.zip $OUT/animated_image_decode_seed_corpus.zip
 
 cp out/Fuzz_mem_constraints/image_filter_deserialize $OUT/image_filter_deserialize
-cp ./image_filter_deserialize.options $OUT/image_filter_deserialize.options
 cp ./image_filter_deserialize_seed_corpus.zip $OUT/image_filter_deserialize_seed_corpus.zip
 
 # Only create the width version of image_filter_deserialize if building with
@@ -156,31 +149,24 @@ then
 fi
 
 cp out/Fuzz/api_draw_functions $OUT/api_draw_functions
-cp ./api_draw_functions.options $OUT/api_draw_functions.options
 cp ./api_draw_functions_seed_corpus.zip $OUT/api_draw_functions_seed_corpus.zip
 
 cp out/Fuzz/api_gradients $OUT/api_gradients
-cp ./api_gradients.options $OUT/api_gradients.options
 cp ./api_gradients_seed_corpus.zip $OUT/api_gradients_seed_corpus.zip
 
 cp out/Fuzz/api_path_measure $OUT/api_path_measure
-cp ./api_path_measure.options $OUT/api_path_measure.options
 cp ./api_path_measure_seed_corpus.zip $OUT/api_path_measure_seed_corpus.zip
 
 cp out/Fuzz/api_pathop $OUT/api_pathop
-cp ./api_pathop.options $OUT/api_pathop.options
 cp ./api_pathop_seed_corpus.zip $OUT/api_pathop_seed_corpus.zip
 
 cp out/Fuzz/png_encoder $OUT/png_encoder
-cp ./encoder.options $OUT/png_encoder.options
 cp ./encoder_seed_corpus.zip $OUT/png_encoder_seed_corpus.zip
 
 cp out/Fuzz/jpeg_encoder $OUT/jpeg_encoder
-cp ./encoder.options $OUT/jpeg_encoder.options
 cp ./encoder_seed_corpus.zip $OUT/jpeg_encoder_seed_corpus.zip
 
 cp out/Fuzz/webp_encoder $OUT/webp_encoder
-cp ./encoder.options $OUT/webp_encoder.options
 cp ./encoder_seed_corpus.zip $OUT/webp_encoder_seed_corpus.zip
 
 cp out/Fuzz/skottie_json $OUT/skottie_json
@@ -191,57 +177,44 @@ cp json.dict $OUT/skjson.dict
 cp ./skjson_seed_corpus.zip $OUT/skjson_seed_corpus.zip
 
 cp out/Fuzz_mem_constraints/api_mock_gpu_canvas $OUT/api_mock_gpu_canvas
-cp ./api_mock_gpu_canvas.options $OUT/api_mock_gpu_canvas.options
 cp ./canvas_seed_corpus.zip $OUT/api_mock_gpu_canvas_seed_corpus.zip
 
 cp out/Fuzz_mem_constraints/api_raster_n32_canvas $OUT/api_raster_n32_canvas
-cp ./api_raster_n32_canvas.options $OUT/api_raster_n32_canvas.options
 cp ./canvas_seed_corpus.zip $OUT/api_raster_n32_canvas_seed_corpus.zip
 
 cp out/Fuzz/api_image_filter $OUT/api_image_filter
-cp ./api_image_filter.options $OUT/api_image_filter.options
 cp ./api_image_filter_seed_corpus.zip $OUT/api_image_filter_seed_corpus.zip
 
 cp out/Fuzz/api_null_canvas $OUT/api_null_canvas
-cp ./api_null_canvas.options $OUT/api_null_canvas.options
 cp ./canvas_seed_corpus.zip $OUT/api_null_canvas_seed_corpus.zip
 
 cp out/Fuzz/api_polyutils $OUT/api_polyutils
-cp ./api_polyutils.options $OUT/api_polyutils.options
 cp ./api_polyutils_seed_corpus.zip $OUT/api_polyutils_seed_corpus.zip
 
 # These 2 can use the same corpus as the (non animated) image_decode.
 cp out/Fuzz/android_codec $OUT/android_codec
-cp ./android_codec.options $OUT/android_codec.options
 cp ./image_decode_seed_corpus.zip $OUT/android_codec_seed_corpus.zip.
 
 cp out/Fuzz/image_decode_incremental $OUT/image_decode_incremental
-cp ./image_decode_incremental.options $OUT/image_decode_incremental.options
 cp ./image_decode_seed_corpus.zip $OUT/image_decode_incremental_seed_corpus.zip
 
 cp out/Fuzz/sksl2glsl $OUT/sksl2glsl
-cp ./sksl2glsl.options $OUT/sksl2glsl.options
 cp ./sksl_seed_corpus.zip $OUT/sksl2glsl_seed_corpus.zip
 
 cp out/Fuzz/sksl2spirv $OUT/sksl2spirv
-cp ./sksl2spirv.options $OUT/sksl2spirv.options
 cp ./sksl_seed_corpus.zip $OUT/sksl2spirv_seed_corpus.zip
 
 cp out/Fuzz/sksl2metal $OUT/sksl2metal
-cp ./sksl2metal.options $OUT/sksl2metal.options
 cp ./sksl_seed_corpus.zip $OUT/sksl2metal_seed_corpus.zip
 
 cp out/Fuzz/sksl2pipeline $OUT/sksl2pipeline
-cp ./sksl2pipeline.options $OUT/sksl2pipeline.options
 cp ./sksl_seed_corpus.zip $OUT/sksl2pipeline_seed_corpus.zip
 
 cp out/Fuzz/api_skdescriptor $OUT/api_skdescriptor
-cp ./api_skdescriptor.options $OUT/api_skdescriptor.options
 cp ./api_skdescriptor_seed_corpus.zip $OUT/api_skdescriptor_seed_corpus.zip
 
 # Don't have any examples of an SkDescriptor atm, so some random bytes is all we have.
 cp out/Fuzz/skdescriptor_deserialize $OUT/skdescriptor_deserialize
-cp ./skdescriptor_deserialize.options $OUT/skdescriptor_deserialize.options
 cp ./api_skdescriptor_seed_corpus.zip $OUT/skdescriptor_deserialize_seed_corpus.zip
 
 cp out/Fuzz/svg_dom $OUT/svg_dom
