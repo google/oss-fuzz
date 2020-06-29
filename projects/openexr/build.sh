75358ec27bde4c37867d5cb77f958ec96f62b356
Michael Jezierny <mtjz@google.com>
2020-06-29 13:55:02 -0700

    Add a working skeleton for OpenEXR fuzzers.

---
+++ projects/openexr/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd $WORK/
+cmake $SRC/openexr \
+  -DPYILMBASE_ENABLE=OFF \
+  -DBUILD_TESTING=OFF \
+  -DBUILD_SHARED_LIBS=OFF
+make -j$(nproc)
+
+OPENEXR_INCLUDES=(
+  "-I $SRC/openexr/OpenEXR/IlmImf"
+  "-I $SRC/openexr/IlmBase/Imath"
+  "-I $SRC/openexr/IlmBase/Iex"
+  "-I $SRC/openexr/IlmBase/Half"
+  "-I $WORK/OpenEXR/config"
+  "-I $WORK/IlmBase/config"
+)
+
+OPENEXR_LIBS=(
+  "/work/OpenEXR/IlmImf/libIlmImf-2_5.a"
+  "/work/IlmBase/Iex/libIex-2_5.a"
+  "/work/IlmBase/Half/libHalf-2_5.a"
+  "/work/IlmBase/IlmThread/libIlmThread-2_5.a"
+  "/work/IlmBase/Imath/libImath-2_5.a"
+)
+
+$CXX $CXXFLAGS -std=c++11 -pthread \
+  ${OPENEXR_INCLUDES[@]} \
+  $SRC/openexr_fuzz_tiles.cc \
+  $LIB_FUZZING_ENGINE \
+  ${OPENEXR_LIBS[@]} \
+  -lz \
+  -o $OUT/openexr_fuzz_tiles
