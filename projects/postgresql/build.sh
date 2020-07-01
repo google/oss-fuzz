0924667567e15f5665e7aaf169578b9320ea8100
Yunshu Ouyang <ouyangyunshu@google.com>
2020-07-01 15:46:11 +0200

    Renamed fix.diff

--- projects/postgresql/build.sh
+++ projects/postgresql/build.sh
@@ -14,20 +14,9 @@
 # limitations under the License.
 #
 ################################################################################
-git apply ../fix.diff
+git apply ../add_fuzzers.diff
 mkdir bld
 cd bld
-
-# export ASAN_OPTIONS=detect_leaks=0
-
-# Limit max length of data blobs and sql queries to prevent irrelevant OOMs.
-# Also limit max memory page count to avoid creating large databases.
-# export CFLAGS="$CFLAGS -DSQLITE_MAX_LENGTH=128000000 \
-#                -DSQLITE_MAX_SQL_LENGTH=128000000 \
-#                -DSQLITE_MAX_MEMORY=25000000 \
-#                -DSQLITE_PRINTF_PRECISION_LIMIT=1048576 \
-#                -DSQLITE_DEBUG=1 \
-#                -DSQLITE_MAX_PAGE_COUNT=16384"             
 
 ../configure --prefix /usr/local/
 make -j$(nproc)
