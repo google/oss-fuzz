8c9aa923bbddd5e844e8308d090f5685541c4188
Ebrahim Byagowi <ebrahim@gnu.org>
2020-07-08 00:52:48 +0430

    [harfbuzz] workaround single quotes in fuzzer_ldflags

--- projects/harfbuzz/build.sh
+++ projects/harfbuzz/build.sh
@@ -28,7 +28,7 @@ rm -rf $build
 mkdir -p $build
 
 # Build the library.
-meson --default-library=static -Dfuzzer_ldflags=$LIB_FUZZING_ENGINE \
+meson --default-library=static -Dfuzzer_ldflags="$(echo $LIB_FUZZING_ENGINE)" \
       $build \
    || (cat build/meson-logs/meson-log.txt && false)
 
