4ac0c3eb3c28c88ecd4ce7784ec9b7706ffc535a
Randy <randy408@protonmail.com>
2020-07-15 13:33:40 +0200

    [stb] run upstreamed build script

--- projects/stb/build.sh
+++ projects/stb/build.sh
@@ -14,28 +14,5 @@
 # limitations under the License.
 #
 ################################################################################
-
-# remove this line when this change is upstreamed
-sed '2d' $SRC/stb/tests/stb_png_read_fuzzer.cpp > $SRC/stb/tests/stbi_read_fuzzer.c
-
-$CXX $CXXFLAGS -std=c++11 -I. -DSTBI_ONLY_PNG  \
-    $SRC/stb/tests/stbi_read_fuzzer.c \
-    -o $OUT/stb_png_read_fuzzer $LIB_FUZZING_ENGINE
-
-$CXX $CXXFLAGS -std=c++11 -I. \
-    $SRC/stb/tests/stbi_read_fuzzer.c \
-    -o $OUT/stbi_read_fuzzer $LIB_FUZZING_ENGINE
-
-find $SRC/stb/tests/pngsuite -name "*.png" | \
-     xargs zip $OUT/stb_png_read_fuzzer_seed_corpus.zip
-
-cp $SRC/stb/tests/stb_png.dict $OUT/stb_png_read_fuzzer.dict
-
-tar xvzf $SRC/stb/jpg.tar.gz --directory $SRC/stb/tests
-tar xvzf $SRC/stb/gif.tar.gz --directory $SRC/stb/tests
-
-find $SRC/stb/tests -name "*.png" -o -name "*.jpg" -o -name ".gif" | \
-     xargs zip $OUT/stbi_read_fuzzer_seed_corpus.zip
-
-echo "" >> $SRC/stb/tests/gif.dict
-cat $SRC/stb/tests/gif.dict $SRC/stb/tests/stb_png.dict > $OUT/stbi_read_fuzzer.dict
+# Run the OSS-Fuzz script in the project
+$SRC/stb/tests/ossfuzz.sh
