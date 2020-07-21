293bbe37a70bd502710542a46614b6545af8e841
Ravi Jotwani <rjotwani@google.com>
2020-07-21 11:31:51 -0700

    added license to exrheader_fuzzer

--- projects/openexr/openexr_exrheader_fuzzer.cc
+++ projects/openexr/openexr_exrheader_fuzzer.cc
@@ -12,13 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-//-----------------------------------------------------------------------------
-//
-//	Utility program to print an image file's header
-//
-//-----------------------------------------------------------------------------
-
 #include "ImfNamespace.h"
 #include <ImfMultiPartInputFile.h>
 #include <ImfBoxAttribute.h>
