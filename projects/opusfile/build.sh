3cc6a786319095be13e6f770c91445580a7842ae
Ravi Jotwani <rjotwani@google.com>
2020-08-03 10:36:14 -0700

    [opusfile] Initial integration

---
+++ projects/opusfile/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+./autogen.sh
+./configure --prefix=$SRC
+make -j$(nproc)
+
+for fuzzer in $(find $SRC -name '*_fuzzer.cc'); do
+  fuzzer_basename=$(basename -s .cc $fuzzer)
+  $CXX $CXXFLAGS -std=c++11 \
+      -I $SRC -I /usr/include/opus -I /usr/include/ogg \
+      $fuzzer $LIB_FUZZING_ENGINE -o $OUT/$fuzzer_basename \
+      $SRC/opusfile/.libs/libopusfile.a -lopus -logg
+done
