2756b8d57b73a044c708e4f2c7daeb536638bdbf
Ravi Jotwani <rjotwani@google.com>
2020-08-03 10:39:01 -0700

    add license header

--- projects/opusfile/opusfile_fuzzer.cc
+++ projects/opusfile/opusfile_fuzzer.cc
@@ -1,5 +1,16 @@
-// Opusfile fuzzing wrapper to help with automated fuzz testing. It's based on
-// examples/opusfile_example.c.
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <errno.h>
 #include <stdint.h>
@@ -10,6 +21,8 @@
 #include "opusfile/config.h"
 #include "opusfile/include/opusfile.h"
 
+// Opusfile fuzzing wrapper to help with automated fuzz testing. It's based on
+// https://github.com/xiph/opusfile/blob/master/examples/opusfile_example.c
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   int ret, tmp;
   OggOpusFile *of = op_open_memory(data, size, &ret);
