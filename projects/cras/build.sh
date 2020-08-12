b7f94d18ceb9e1234816efa686795f7928d770a4
Chih-Yang Hsia <b92paul@gmail.com>
2020-08-13 02:59:22 +0800

    [CRAS] Fix build error and build new fuzzer
    - Fix build error by installing 1.8.x gtest and using `make install` to
      install gtest pc file for pkg-config
    - Build and add new fuzzer `cras_hfp_slc`
    
    BUG=oss-fuzz:24744

--- projects/cras/build.sh
+++ projects/cras/build.sh
@@ -8,17 +8,24 @@
 
 cd ${SRC}/adhd/cras
 ./git_prepare.sh
-./configure --disable-dbus --disable-webrtc-apm
-cargo build --release --manifest-path $SRC/adhd/cras/src/server/rust/Cargo.toml && \
-  cp $SRC/adhd/cras/src/server/rust/target/release/libcras_rust.a /usr/local/lib
+./configure
 make -j$(nproc)
+cp ${SRC}/adhd/cras/src/server/rust/target/release/libcras_rust.a /usr/local/lib
 
+CRAS_FUZZERS="rclient_message cras_hfp_slc"
+
+for fuzzer in ${CRAS_FUZZERS};
+do
 $CXX $CXXFLAGS $FUZZER_LDFLAGS \
-  ${SRC}/adhd/cras/src/fuzz/rclient_message.cc -o ${OUT}/rclient_message \
+  ${SRC}/adhd/cras/src/fuzz/${fuzzer}.cc -o ${OUT}/${fuzzer} \
   -I ${SRC}/adhd/cras/src/server \
   -I ${SRC}/adhd/cras/src/common \
+  $(pkg-config --cflags dbus-1) \
   ${SRC}/adhd/cras/src/.libs/libcrasserver.a \
-  -lcras_rust -lpthread -lrt -ludev -ldl -lm \
+  -lcras_rust -lpthread -lrt -ludev -ldl -lm -lsystemd \
   $LIB_FUZZING_ENGINE \
-  -Wl,-Bstatic -liniparser -lasound -lspeexdsp -Wl,-Bdynamic
+  -Wl,-Bstatic -liniparser -lasound -lspeexdsp -ldbus-1 -lsbc -Wl,-Bdynamic
+done
+
 zip -j ${OUT}/rclient_message_corpus.zip ${SRC}/adhd/cras/src/fuzz/corpus/*
+cp "${SRC}/adhd/cras/src/fuzz/cras_hfp_slc.dict" "${OUT}/cras_hfp_slc.dict"
