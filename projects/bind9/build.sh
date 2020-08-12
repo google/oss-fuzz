697d0d2835c9c47eb5a84fcb0b587a7ee7ebe7a6
Ondřej Surý <ondrej@sury.org>
2020-08-12 21:21:42 +0200

    [bind9] Add bind9 as new proposed oss-fuzz target (#4304)
    * Add bind9 as new proposed oss-fuzz target
    
    * Add basic Dockerfile and build.sh
    
    * Change the auto_cc addresses to @isc.org
    
    * fix the copyright
    
    * fix the copyright
    
    Co-authored-by: Max Moroz <mmoroz@chromium.org>

---
+++ projects/bind9/build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -eu
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# build the project
+autoreconf -fi
+./configure --disable-shared --enable-static --enable-developer --without-cmocka --without-zlib --disable-linux-caps --prefix="$WORK"
+make -j"$(nproc)" all
+
+LIBISC_CFLAGS="-Ilib/isc/unix/include -Ilib/isc/pthreads/include -Ilib/isc/include"
+LIBDNS_CFLAGS="-Ilib/dns/include"
+LIBISC_LIBS="libltdl/.libs/libltdlc.a lib/isc/.libs/libisc.a -Wl,-Bstatic -lcrypto -luv -Wl,-Bdynamic"
+LIBDNS_LIBS="lib/dns/.libs/libdns.a -Wl,-Bstatic -lcrypto -Wl,-Bdynamic"
+
+for fuzzer in fuzz/*.c; do
+    output=$(basename "${fuzzer%.c}")
+    [ "$output" = "main" ] && continue
+    (cd fuzz && make "${output}.o")
+    ${CXX} ${CXXFLAGS} \
+	   -o "${OUT}/${output}_fuzzer" \
+	   "fuzz/${output}.o" \
+	   -include config.h \
+	   $LIBISC_CFLAGS $LIBDNS_CFLAGS \
+	   $LIBDNS_LIBS $LIBISC_LIBS $LIB_FUZZING_ENGINE
+    zip -j "${OUT}/${output}_seed_corpus.zip" "fuzz/${output}.in/"*
+done
