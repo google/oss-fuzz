1cea2f40e037af195ceac6d563fb8dbe05074fd6
Yunshu Ouyang <ouyangyunshu@google.com>
2020-08-25 08:56:45 +0200

    Improved style

--- projects/nginx/fuzz/wrappers.c
+++ projects/nginx/fuzz/wrappers.c
@@ -11,31 +11,24 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#include <sys/types.h>
+//
+////////////////////////////////////////////////////////////////////////////////
 #include <sys/socket.h>
+#include <sys/types.h>
 
-
-int __wrap_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+int __wrap_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
   return 0;
 }
 
-
-int __wrap_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+int __wrap_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
   return 0;
 }
 
+int __wrap_shutdown(int socket, int how) { return 0; }
 
-int __wrap_shutdown(int socket, int how){
-  return 0;
-}
+ssize_t __wrap_listen(int fd, void *buf, size_t bytes) { return 0; }
 
-
-ssize_t __wrap_listen(int fd, void* buf, size_t bytes) {
-  return 0;
-}
-
-
-int __wrap_setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen) {
+int __wrap_setsockopt(int fd, int level, int optname, const void *optval,
+                      socklen_t optlen) {
   return 0;
 }
