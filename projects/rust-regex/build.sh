20f7ead61021eb399b5dc468461b40fb2c9ae3d9
davkor <david@adalogics.com>
2020-10-12 10:40:02 +0100

    Removed whitespace.

--- projects/rust-regex/build.sh
+++ projects/rust-regex/build.sh
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 ################################################################################
-
 cd $SRC/regex
 cargo fuzz build -O 
 cp fuzz/target/x86_64-unknown-linux-gnu/release/fuzz_regex_match $OUT/
