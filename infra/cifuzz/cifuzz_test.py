cfa142eb0189d0b42b56aaeb550f4882df67cc0e
Jonathan Metzman <metzman@chromium.org>
2020-10-20 08:25:21 -0700

    fix

--- infra/cifuzz/cifuzz_test.py
+++ infra/cifuzz/cifuzz_test.py
@@ -331,7 +331,7 @@ class GetFilesCoveredByTargetTest(unittest.TestCase):
   example_cov_json = 'example_curl_cov.json'
   example_fuzzer_cov_json = 'example_curl_fuzzer_cov.json'
   example_fuzzer = 'curl_fuzzer'
-  example_curl_file_list = 'example_curl_file_list'
+  example_curl_file_list = 'example_curl_file_list.json'
 
   def setUp(self):
     with open(os.path.join(TEST_FILES_PATH, self.example_cov_json),
@@ -350,8 +350,8 @@ class GetFilesCoveredByTargetTest(unittest.TestCase):
       file_list = cifuzz.get_files_covered_by_target(
           self.proj_cov_report_example, self.example_fuzzer, '/src/curl')
 
-    with open(os.path.join(TEST_FILES_PATH, 'example_curl_file_list'),
-              'rb') as file_handle:
+    example_curl_file_list = os.path.join(TEST_FILES_PATH, 'example_curl_file_list')
+    with open(example_curl_file_list) as file_handle:
       true_files_list = json.load(file_handle)
     self.assertCountEqual(file_list, true_files_list)
 
