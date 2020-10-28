ec798a694c6f0216273d74770f420e743fcad6ef
Kim Kulling <kimkulling@users.noreply.github.com>
2020-10-28 20:50:57 +0100

    Build zlib explicitly

--- projects/assimp/build.sh
+++ projects/assimp/build.sh
@@ -16,10 +16,11 @@
 ################################################################################
 
 # generate build env and build assimp
-cmake CMakeLists.txt -G "Ninja" -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_ZLIB=ON -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_SAMPLES=OFF
+cmake CMakeLists.txt -G "Ninja" -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_SAMPLES=OFF
 cmake --build .
 
 # build the fuzzer
 $CXX $CXXFLAGS -std=c++11 -I$SRC/assimp/include \
 	fuzz/assimp_fuzzer.cc -o $OUT/assimp_fuzzer \
-	$LIB_FUZZING_ENGINE $SRC/assimp/lib/libassimp.a
+	$LIB_FUZZING_ENGINE $SRC/assimp/lib/libassimp.a \
+	$SRC/assimp/lib/libzlibstatic.a
