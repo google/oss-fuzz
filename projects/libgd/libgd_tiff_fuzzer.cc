e281c0189d311d5f15207a4a0cc46331a1dbc9cf
Hardik Shah <hardik05@gmail.com>
2020-10-29 10:03:34 +0530

    Update libgd_tiff_fuzzer.cc

--- projects/libgd/libgd_tiff_fuzzer.cc
+++ projects/libgd/libgd_tiff_fuzzer.cc
@@ -1,3 +1,4 @@
+/*
 # Copyright 2018 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +14,7 @@
 # limitations under the License.
 #
 ################################################################################
-
+*/
 #include "gd.h"
 #include<stdint.h>
 
