b897a1f71341b2f35115cba31f3755a593f27b45
Philippe Antoine <contact@catenacyber.fr>
2020-11-12 21:49:39 +0100

    Performance profile for golang projects

--- infra/base-images/base-builder/ossfuzz_coverage_runner.go
+++ infra/base-images/base-builder/ossfuzz_coverage_runner.go
@@ -17,6 +17,7 @@ package mypackagebeingfuzzed
 import (
 	"io/ioutil"
 	"os"
+	"runtime/pprof"
 	"testing"
 )
 
@@ -37,6 +38,15 @@ func TestFuzzCorpus(t *testing.T) {
 			t.Error("Fuzz panicked in "+filename, r)
 		}
 	}()
+	profname := os.Getenv("FUZZ_PROFILE_NAME")
+	if profname != "" {
+		f, err := os.Create(profname + ".cpu.prof")
+		if err != nil {
+			t.Logf("error creating profile file %s\n", err)
+		} else {
+			_ = pprof.StartCPUProfile(f)
+		}
+	}
 	for i := range infos {
 		filename = dir + infos[i].Name()
 		data, err := ioutil.ReadFile(filename)
@@ -44,5 +54,16 @@ func TestFuzzCorpus(t *testing.T) {
 			t.Error("Failed to read corpus file", err)
 		}
 		FuzzFunction(data)
+	}
+	if profname != "" {
+		pprof.StopCPUProfile()
+		f, err := os.Create(profname + ".heap.prof")
+		if err != nil {
+			t.Logf("error creating heap profile file %s\n", err)
+		}
+		if err = pprof.WriteHeapProfile(f); err != nil {
+			t.Logf("error writing heap profile file %s\n", err)
+		}
+		f.Close()
 	}
 }
