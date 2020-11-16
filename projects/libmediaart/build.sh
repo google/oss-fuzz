584b39b193d6d052872009a0f6a7bfc5803ae9d7
Ceyhun Alp <ceyhunalp@google.com>
2020-11-16 12:46:59 +0000

    Building fuzz targets individually - ld cannot find libgcc_s

--- projects/libmediaart/build.sh
+++ projects/libmediaart/build.sh
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-1.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,17 +20,25 @@ BUILD=$WORK/build
 rm -rf $BUILD
 mkdir -p $BUILD
 
-export CFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION"
-export CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -stdlib=libc++"
-export LD_LIBRARY_PATH="$BUILD/libmediaart"
+pushd $SRC/gdk-pixbuf
+meson $BUILD/gdk-pixbuf -Ddefault_library=static
+ninja -C $BUILD/gdk-pixbuf gdk-pixbuf/libgdk_pixbuf-2.0.a
 
-meson $BUILD
-#ninja -C $BUILD
+pushd $SRC/libmediaart
+meson $BUILD/libmediaart -Ddefault_library=static -Ddisable_tests=true
+ninja -C $BUILD/libmediaart libmediaart/libmediaart-2.0.a
 
 fuzzers=$(find $SRC/libmediaart/fuzzing/ -name "*_fuzzer.c")
 for f in $fuzzers; do
   fuzzer_name=$(basename $f .c)
-  ninja -C $BUILD fuzzing/$fuzzer_name
+  $CC $CFLAGS -c -std=c99 \
+    -I. -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include \
+    $f -o $WORK/${fuzzer_name}.o
+  $CXX $CXXFLAGS $WORK/${fuzzer_name}.o -o $OUT/${fuzzer_name} \
+    $BUILD/libmediaart/libmediaart/libmediaart-2.0.a \
+    $BUILD/gdk-pixbuf/gdk-pixbuf/libgdk_pixbuf-2.0.a \
+    $LIB_FUZZING_ENGINE \
+    -Wl,-Bstatic -lgio-2.0 -lgobject-2.0 -lglib-2.0
 done
 
 mv $BUILD/fuzzing/*_fuzzer $OUT/
