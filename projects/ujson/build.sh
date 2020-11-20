5e3f47dba6323aef4a85796a188ddf4fc0e445e2
Abhishek Arya <inferno@chromium.org>
2020-11-19 22:41:12 -0800

    Fix ujson python fuzzing. (#4667)
    * Fix ujson python fuzzing.
    
    * Fix.
    
    * Improve integration.
    
    * Remove preload, stick with wrapper with /bin/sh.
    
    * Update build.sh

--- projects/ujson/build.sh
+++ projects/ujson/build.sh
@@ -15,10 +15,18 @@
 #
 ################################################################################
 
-# Build and install the project package.
+# Build and install project (using current CFLAGS, CXXFLAGS).
 pip3 install .
 
 # Build fuzzers in $OUT.
 for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
-  pyinstaller --distpath $OUT --onefile $fuzzer
+  fuzzer_basename=$(basename -s .py $fuzzer)
+  fuzzer_package=${fuzzer_basename}.pkg
+  pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
+
+  # Create execution wrapper.
+  echo "#/bin/sh
+# LLVMFuzzerTestOneInput for fuzzer detection.
+LD_PRELOAD=\$(dirname "\$0")/libclang_rt.asan-x86_64.so \$(dirname "\$0")/$fuzzer_package \$@" > $OUT/$fuzzer_basename
+  chmod u+x $OUT/$fuzzer_basename
 done
