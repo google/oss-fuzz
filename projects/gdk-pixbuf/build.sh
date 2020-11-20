c2ab3c44e995c01153255327298251aa4ab4a2fe
Ceyhun Alp <ceyhunalp@google.com>
2020-11-20 21:07:35 +0000

    Build check passes - TODO: dict and corpus

--- projects/gdk-pixbuf/build.sh
+++ projects/gdk-pixbuf/build.sh
@@ -14,25 +14,62 @@
 # limitations under the License.
 #
 ################################################################################
+PREFIX=$WORK/prefix
+mkdir -p $PREFIX
+
+export PKG_CONFIG="`which pkg-config` --static"
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+export PATH=$PREFIX/bin:$PATH
 
 BUILD=$WORK/build
 
+rm -rf $WORK/*
 rm -rf $BUILD
 mkdir -p $BUILD
 
-meson $BUILD/gdk-pixbuf -Ddefault_library=static
-ninja -C $BUILD/gdk-pixbuf gdk-pixbuf/libgdk_pixbuf-2.0.a
+pushd $WORK
+tar xvJf $SRC/glib-2.64.2.tar.xz
+cd glib-2.64.2
+meson \
+    --prefix=$PREFIX \
+    --libdir=lib \
+    --default-library=static \
+    -Db_lundef=false \
+    -Doss_fuzz=enabled \
+    -Dlibmount=disabled \
+    -Dinternal_pcre=true \
+    _builddir
+ninja -C _builddir
+ninja -C _builddir install
+popd
+
+pushd $SRC/gdk-pixbuf
+meson \
+    --prefix=$PREFIX \
+    --libdir=lib \
+    --default-library=static \
+    -Dintrospection=disabled \
+    -Dbuiltin_loaders='all' \
+    _builddir
+ninja -C _builddir
+ninja -C _builddir install
+popd
+
+PREDEPS_LDFLAGS="-Wl,-Bdynamic -ldl -lm -lc -pthread -lrt -lpthread"
+DEPS="gmodule-2.0 glib-2.0 gio-2.0 gobject-2.0 gdk-pixbuf-2.0"
+BUILD_CFLAGS="$CFLAGS `pkg-config --static --cflags $DEPS`"
+BUILD_LDFLAGS="-Wl,-static `pkg-config --static --libs $DEPS`"
 
 fuzzers=$(find $SRC/gdk-pixbuf/fuzzing/ -name "*_fuzzer.c")
 for f in $fuzzers; do
   fuzzer_name=$(basename $f .c)
-  $CC $CFLAGS -c -std=c99 \
-    -I. -I$BUILD/gdk-pixbuf -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include \
-    $f -o $WORK/${fuzzer_name}.o
-  $CXX $CXXFLAGS $WORK/${fuzzer_name}.o -o $OUT/${fuzzer_name} \
-    $BUILD/gdk-pixbuf/gdk-pixbuf/libgdk_pixbuf-2.0.a \
+  $CC $CFLAGS $BUILD_CFLAGS -c $f -o $WORK/${fuzzer_name}.o
+  $CXX $CXXFLAGS \
+    $WORK/${fuzzer_name}.o -o $OUT/${fuzzer_name} \
+    $PREDEPS_LDFLAGS \
+    $BUILD_LDFLAGS \
     $LIB_FUZZING_ENGINE \
-    -Wl,-Bstatic -lgio-2.0 -lgobject-2.0 -lglib-2.0
+    -Wl,-Bdynamic
+  #ln -sf $OUT/libmediaart_seed_corpus.zip $OUT/${fuzzer_name}_seed_corpus.zip
+  #ln -sf $OUT/libmediaart_ogg.dict $OUT/${fuzzer_name}.dict
 done
-
-mv $BUILD/fuzzing/*_fuzzer $OUT/
