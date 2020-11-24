0c34a2b6eafe993d8d4dcb47ce764e2f5b5f8e17
David Cook <divergentdave@gmail.com>
2020-11-23 23:02:38 -0600

    Style fixes, compiler_wrapper

--- infra/base-images/base-sanitizer-libs-builder/compiler_wrapper_test.py
+++ infra/base-images/base-sanitizer-libs-builder/compiler_wrapper_test.py
@@ -8,35 +8,36 @@ import compiler_wrapper
 
 
 class CompilerWrapperTest(unittest.TestCase):
+  """Tests for compiler_wrapper."""
 
-  def testFilterZDefs(self):
-    self.assertListEqual(
-        ['arg'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z,defs']))
+  def test_filter_z_defs(self):
+    """Reference tests for RemoveZDefs."""
+    self.assertListEqual(['arg'],
+                         compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z,defs']))
 
-    self.assertListEqual(
-        ['arg'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,--no-undefined']))
+    self.assertListEqual(['arg'],
+                         compiler_wrapper.RemoveZDefs(
+                             ['arg', '-Wl,--no-undefined']))
 
-    self.assertListEqual(
-        ['arg', '-Wl,-z,relro'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z,relro']))
+    self.assertListEqual(['arg', '-Wl,-z,relro'],
+                         compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z,relro']))
 
-    self.assertListEqual(
-        ['arg', '-Wl,-soname,lib.so.1,-z,relro'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,-soname,lib.so.1,-z,defs,-z,relro']))
+    self.assertListEqual(['arg', '-Wl,-soname,lib.so.1,-z,relro'],
+                         compiler_wrapper.RemoveZDefs(
+                             ['arg', '-Wl,-soname,lib.so.1,-z,defs,-z,relro']))
 
-    self.assertListEqual(
-        ['arg', '-Wl,-z,relro'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z,relro,-z,defs']))
+    self.assertListEqual(['arg', '-Wl,-z,relro'],
+                         compiler_wrapper.RemoveZDefs(
+                             ['arg', '-Wl,-z,relro,-z,defs']))
 
-    self.assertListEqual(
-        ['arg'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z', '-Wl,defs']))
+    self.assertListEqual(['arg'],
+                         compiler_wrapper.RemoveZDefs(
+                             ['arg', '-Wl,-z', '-Wl,defs']))
 
-    self.assertListEqual(
-        ['arg', 'arg2'],
-        compiler_wrapper.RemoveZDefs(['arg', '-Wl,-z', 'arg2', '-Wl,--no-undefined']))
+    self.assertListEqual(['arg', 'arg2'],
+                         compiler_wrapper.RemoveZDefs(
+                             ['arg', '-Wl,-z', 'arg2', '-Wl,--no-undefined']))
+
 
 if __name__ == '__main__':
   unittest.main()
