f043b86d7fbc594ca294dca6b51e18a590af1107
Abhishek Arya <inferno@chromium.org>
2020-11-30 10:42:25 -0800

    Leak detection is not expected to work with python targets, disable. (#4751)
    Disable leak detection in python target execution wrapper since it
    is not expected to work. Also, updated the documentation for python
    -only code fuzzers to not use LD_PRELOAD.

--- projects/ujson/build.sh
+++ projects/ujson/build.sh
@@ -27,6 +27,8 @@ for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
   # Create execution wrapper.
   echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
-LD_PRELOAD=\$(dirname "\$0")/libclang_rt.asan-x86_64.so \$(dirname "\$0")/$fuzzer_package \$@" > $OUT/$fuzzer_basename
+LD_PRELOAD=\$(dirname "\$0")/libclang_rt.asan-x86_64.so \
+ASAN_OPTIONS=\$ASAN_OPTIONS:detect_leaks=0 \
+\$(dirname "\$0")/$fuzzer_package \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
 done
