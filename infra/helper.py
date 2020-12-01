98f7ab9a93b35fe7c6797a7f2f6842b06a735773
Jakub Jelen <jjelen@redhat.com>
2020-12-01 13:36:42 +0100

    helper: Make sure directories are created before they are passed to container engine (podman)

--- infra/helper.py
+++ infra/helper.py
@@ -277,18 +277,27 @@ def get_dockerfile_path(project_name):
 
 
 def _get_corpus_dir(project_name=''):
-  """Returns path to /corpus directory for the given project (if specified)."""
-  return os.path.join(BUILD_DIR, 'corpus', project_name)
+  """Creates and returns path to /corpus directory for the given project (if specified)."""
+  directory = os.path.join(BUILD_DIR, 'corpus', project_name)
+  os.makedirs(directory, exist_ok=True)
+
+  return directory
 
 
 def _get_output_dir(project_name=''):
-  """Returns path to /out directory for the given project (if specified)."""
-  return os.path.join(BUILD_DIR, 'out', project_name)
+  """Creates and returns path to /out directory for the given project (if specified)."""
+  directory = os.path.join(BUILD_DIR, 'out', project_name)
+  os.makedirs(directory, exist_ok=True)
+
+  return directory
 
 
 def _get_work_dir(project_name=''):
-  """Returns path to /work directory for the given project (if specified)."""
-  return os.path.join(BUILD_DIR, 'work', project_name)
+  """Creates and returns path to /work directory for the given project (if specified)."""
+  directory = os.path.join(BUILD_DIR, 'work', project_name)
+  os.makedirs(directory, exist_ok=True)
+
+  return directory
 
 
 def _get_project_language(project_name):
