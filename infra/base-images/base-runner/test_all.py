08c36e6f16083439755ba441b5b1cebd8be6b73d
Jonathan Metzman <metzman@chromium.org>
2020-12-02 16:27:04 -0800

    Address comments

--- infra/base-images/base-runner/test_all.py
+++ infra/base-images/base-runner/test_all.py
@@ -45,9 +45,10 @@ def move_directory_contents(src_directory, dst_directory):
     shutil.move(src_path, dst_directory)
 
 
-def find_fuzz_targets(directory):
+def find_fuzz_targets(directory, fuzzing_language):
   """Returns paths to fuzz targets in |directory|."""
-  fuzzing_language = os.getenv('FUZZING_LANGUAGE')
+  # TODO(https://github.com/google/oss-fuzz/issues/4585): Use libClusterFuzz for
+  # this.
   fuzz_targets = []
   for filename in os.listdir(directory):
     path = os.path.join(directory, filename)
@@ -58,10 +59,10 @@ def find_fuzz_targets(directory):
     if not os.stat(path).st_mode & EXECUTABLE:
       continue
     with open(path, 'rb') as file_handle:
-      binary = file_handle.read()
-      if b'LLVMFuzzerTestOneInput' not in binary:
+      binary_contents = file_handle.read()
+      if b'LLVMFuzzerTestOneInput' not in binary_contents:
         continue
-      if fuzzing_language != 'python' and b'ELF' not in binary:
+      if fuzzing_language != 'python' and b'ELF' not in binary_contents:
         continue
     fuzz_targets.append(path)
   return fuzz_targets
@@ -105,23 +106,47 @@ def has_ignored_targets(out_dir):
       return True
   return False
 
-
-def test_all(pool, out, initial_out):
-  """Do bad_build_check on all fuzz targets."""
-  # TODO(metzman): Refactor so that we can conver test_one to python.
+@contextlib.contextmanager
+def move_out_to_tmp_dir():
+  """Context manager that moves OUT to TMP_FUZZER_DIR. This is useful for
+  catching hardcoding. Note that this sets the environment variable OUT and
+  therefore must be run before multiprocessing.Pool is created."""
   # Use a fake OUT directory to catch path hardcoding that breaks on
   # ClusterFuzz.
-  recreate_directory(TMP_FUZZER_DIR)
+  out = os.getenv('OUT')
+  initial_out = out
+  out = TMP_FUZZER_DIR
+  # Set this so that run_fuzzer which is called by bad_build_check works
+  # properly.
+  os.environ['OUT'] = out
+  # We move the contents of the directory because we can't move the
+  # directory itself because it is a mount.
+  move_directory_contents(initial_out, out)
   try:
-    # Set this so that run_fuzzer which is called by bad_build_check works
-    # properly.
-    move_directory_contents(initial_out, out)
-    fuzz_targets = find_fuzz_targets(out)
-    bad_build_results = pool.map(do_bad_build_check, fuzz_targets)
+    yield
   finally:
-    move_directory_contents(out, initial_out)
-    out = initial_out
+    move_directory_contents(initial_out, out)
 
+
+def test_all_outside_out(
+    fuzzing_language, allowed_broken_targets_percentage):
+  """Wrapper around test_all that changes OUT, passes it a
+  multiprocessing.Pool and returns the result or False on failure."""
+  with move_out_to_tmp_dir():
+    return test_all(
+        out, fuzzing_language, allowed_broken_targets_percentage)
+  finally:
+
+  return False
+
+
+def test_all(out, fuzzing_language, allowed_broken_targets_percentage):
+  """Do bad_build_check on all fuzz targets."""
+  # TODO(metzman): Refactor so that we can convert test_one to python.
+  recreate_directory(TMP_FUZZER_DIR)
+  fuzz_targets = find_fuzz_targets(out)
+  pool = multiprocessing.Pool()
+  bad_build_results = pool.map(do_bad_build_check, fuzz_targets)
   broken_targets = get_broken_fuzz_targets(bad_build_results, fuzz_targets)
   broken_targets_count = len(broken_targets)
   if not broken_targets_count:
@@ -132,10 +157,9 @@ def test_all(pool, out, initial_out):
   broken_targets_percentage = 100 * broken_targets_count / total_targets_count
   for broken_target, result in broken_targets:
     print(broken_target)
+    # Use write because we can't print binary strings.
     sys.stdout.buffer.write(result.stdout + result.stderr + b'\n')
 
-  allowed_broken_targets_percentage = int(
-      os.getenv('ALLOWED_BROKEN_TARGETS_PERCENTAGE', '10'))
   if broken_targets_percentage > allowed_broken_targets_percentage:
     print('ERROR: {broken_targets_percentage}% of fuzz targets seem to be '
           'broken. See the list above for a detailed information.'.format(
@@ -151,23 +175,21 @@ def test_all(pool, out, initial_out):
   return True
 
 
-def change_out_to_tmp_fuzzer_dir():
-  """Changes $OUT to TMP_FUZZER_DIR and returns the initial value of $OUT and
-  the new value of $OUT."""
-  out = os.getenv('OUT')
-  initial_out = out
-  out = TMP_FUZZER_DIR
-  os.environ['OUT'] = TMP_FUZZER_DIR
-  return out, initial_out
+def get_allowed_broken_targets_percentage():
+  """Returns the value of the environment value
+  'ALLOWED_BROKEN_TARGETS_PERCENTAGE' as an int or returns a reasonable
+  default."""
+  return int(os.getenv('ALLOWED_BROKEN_TARGETS_PERCENTAGE', '10'))
 
 
 def main():
   """Does bad_build_check on all fuzz targets in parallel. Returns 0 on success.
   Returns 1 on failure."""
   # Set these environment variables here so that stdout
-  out, initial_out = change_out_to_tmp_fuzzer_dir()
-  pool = multiprocessing.Pool()
-  if not test_all(pool, out, initial_out):
+  fuzzing_language = os.getenv('FUZZING_LANGUAGE')
+  allowed_broken_targets_percentage = get_allowed_broken_targets_percentage()
+  if not test_all_outside_out(
+      fuzzing_language, allowed_broken_targets_percentage):
     return 1
   return 0
 
