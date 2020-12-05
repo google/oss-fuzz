google/oss-fuzz

commit 6fc050ec9ec29534ad079c83ef4bc2f709c083df

Clean up MemorySanitizer library warnings (#4694)
* Update infra/base-images/all.sh

Add build of base-sanitizer-libs-builder and msan-libs-builder to this
shell script.

* msan: Don't warn on un-instrumented standard libs

These libraries do not need to be built with instrumentation, because
MemorySanitizer includes interceptors for them.

* Fix indentation

* Add missing docstrings

* Fix unused variable

* Fix invalid names

* Install python-apt on CI

* Revert "Install python-apt on CI"

This reverts commit d3da49cf90f138674c1a8bcbdca2f0dfe6c385d1.

* Install and use python-apt in system directory

* Revert "Install and use python-apt in system directory"

This reverts commit e0ede101fbdfbf82da5f32d0a206da247e6dde98.

* Build python-apt from source

* Check out correct version of python-apt

* Fix octal literals

* More indentation fixes

* Add more missing docstrings

* Change variable names of opened files

* Remove unused import

* Ignore lints about package.Package API

* Fix or ignore remaining invalid names

* Fix apparent typo in compiler_wrapper_test.py

-z should precede a keyword, not a long option

* Fix use of xrange

* Style fixes, compiler_wrapper

* Fix apparent error in compiler_wrapper_test.py

Similar to the previous error, the test case would pass "-z
--no-undefined" to the linker. "-z" only has an effect when it is
followed by a keyword, otherwise ld ignores it and prints a warning
message. In this test case, "-z" and "--no-undefined" were passed in two
separate "-Wl," compiler arguments, but they reflect a common issue.

* Add missing license header

* Rename more functions

* Better name for global variable

* Rename methods of Package

* Rename functions in msan_builder.py

* Fix invalid variable names

* Fix useless-object-inheritance

* pylint: Fixes for Package and its subclasses

* Remove unused imports

* Indentation fixes

infra/base-images/base-sanitizer-libs-builder/msan_build.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 ################################################################################
-
+"""Script to build libraries with MemorySanitizer instrumentation."""
 from __future__ import print_function
 import argparse
 import imp
@@ -43,12 +43,30 @@ INJECTED_ARGS = [
     '-fno-omit-frame-pointer',
 ]
 
+C_OR_CXX_DEPS = [
+    'libc++1',
+    'libc6',
+    'libc++abi1',
+    'libgcc1',
+    'libstdc++6',
+]
+
+BLACKLISTED_PACKAGES = [
+    'libcapnp-0.5.3',  # fails to compile on newer clang.
+    'libllvm5.0',
+    'libmircore1',
+    'libmircommon7',
+    'libmirclient9',
+    'libmirprotobuf3',
+    'multiarch-support',
+]
+
 
 class MSanBuildException(Exception):
   """Base exception."""
 
 
-def GetTrackOriginsFlag():
+def get_track_origins_flag():
   """Get the track origins flag."""
   if os.getenv('MSAN_NO_TRACK_ORIGINS'):
     return TRACK_ORIGINS_ARG + '0'
@@ -56,11 +74,12 @@ def GetTrackOriginsFlag():
   return TRACK_ORIGINS_ARG + '2'
 
 
-def GetInjectedFlags():
-  return INJECTED_ARGS + [GetTrackOriginsFlag()]
+def get_injected_flags():
+  """Get the additional command line arguments."""
+  return INJECTED_ARGS + [get_track_origins_flag()]
 
 
-def SetUpEnvironment(work_dir):
+def set_up_environment(work_dir):
   """Set up build environment."""
   env = {}
   env['REAL_CLANG_PATH'] = subprocess.check_output(['which', 'clang']).strip()
@@ -71,9 +90,11 @@ def SetUpEnvironment(work_dir):
   bin_dir = os.path.join(work_dir, 'bin')
   os.mkdir(bin_dir)
 
-  dpkg_host_architecture = wrapper_utils.DpkgHostArchitecture()
+  dpkg_host_architecture = wrapper_utils.dpkg_host_architecture()
   wrapper_utils.CreateSymlinks(
-      compiler_wrapper_path, bin_dir, [
+      compiler_wrapper_path,
+      bin_dir,
+      [
           'clang',
           'clang++',
           # Not all build rules respect $CC/$CXX, so make additional symlinks.
@@ -88,54 +109,48 @@ def SetUpEnvironment(work_dir):
   env['CC'] = os.path.join(bin_dir, 'clang')
   env['CXX'] = os.path.join(bin_dir, 'clang++')
 
-  MSAN_OPTIONS = ' '.join(GetInjectedFlags())
+  msan_options = ' '.join(get_injected_flags())
 
   # We don't use nostrip because some build rules incorrectly break when it is
   # passed. Instead we install our own no-op strip binaries.
   env['DEB_BUILD_OPTIONS'] = ('nocheck parallel=%d' %
                               multiprocessing.cpu_count())
-  env['DEB_CFLAGS_APPEND'] = MSAN_OPTIONS
-  env['DEB_CXXFLAGS_APPEND'] = MSAN_OPTIONS + ' -stdlib=libc++'
-  env['DEB_CPPFLAGS_APPEND'] = MSAN_OPTIONS
-  env['DEB_LDFLAGS_APPEND'] = MSAN_OPTIONS
+  env['DEB_CFLAGS_APPEND'] = msan_options
+  env['DEB_CXXFLAGS_APPEND'] = msan_options + ' -stdlib=libc++'
+  env['DEB_CPPFLAGS_APPEND'] = msan_options
+  env['DEB_LDFLAGS_APPEND'] = msan_options
   env['DPKG_GENSYMBOLS_CHECK_LEVEL'] = '0'
 
   # debian/rules can set DPKG_GENSYMBOLS_CHECK_LEVEL explicitly, so override it.
-  gen_symbols_wrapper = (
-        '#!/bin/sh\n'
-        'export DPKG_GENSYMBOLS_CHECK_LEVEL=0\n'
-        '/usr/bin/dpkg-gensymbols "$@"\n')
+  gen_symbols_wrapper = ('#!/bin/sh\n'
+                         'export DPKG_GENSYMBOLS_CHECK_LEVEL=0\n'
+                         '/usr/bin/dpkg-gensymbols "$@"\n')
 
-  wrapper_utils.InstallWrapper(bin_dir, 'dpkg-gensymbols',
-                               gen_symbols_wrapper)
+  wrapper_utils.install_wrapper(bin_dir, 'dpkg-gensymbols', gen_symbols_wrapper)
 
   # Install no-op strip binaries.
-  no_op_strip = ('#!/bin/sh\n'
+  no_op_strip = ('#!/bin/sh\n' \
                  'exit 0\n')
-  wrapper_utils.InstallWrapper(
-      bin_dir, 'strip', no_op_strip,
-      [dpkg_host_architecture + '-strip'])
+  wrapper_utils.install_wrapper(bin_dir, 'strip', no_op_strip,
+                                [dpkg_host_architecture + '-strip'])
 
   env['PATH'] = bin_dir + ':' + os.environ['PATH']
 
   # nocheck doesn't disable override_dh_auto_test. So we have this hack to try
   # to disable "make check" or "make test" invocations.
-  make_wrapper = (
-      '#!/bin/bash\n'
-      'if [ "$1" = "test" ] || [ "$1" = "check" ]; then\n'
-      '  exit 0\n'
-      'fi\n'
-      '/usr/bin/make "$@"\n')
-  wrapper_utils.InstallWrapper(bin_dir, 'make',
-                               make_wrapper)
+  make_wrapper = ('#!/bin/bash\n'
+                  'if [ "$1" = "test" ] || [ "$1" = "check" ]; then\n'
+                  '  exit 0\n'
+                  'fi\n'
+                  '/usr/bin/make "$@"\n')
+  wrapper_utils.install_wrapper(bin_dir, 'make', make_wrapper)
 
   # Prevent entire build from failing because of bugs/uninstrumented in tools
   # that are part of the build.
   msan_log_dir = os.path.join(work_dir, 'msan')
   os.mkdir(msan_log_dir)
   msan_log_path = os.path.join(msan_log_dir, 'log')
-  env['MSAN_OPTIONS'] = (
-      'halt_on_error=0:exitcode=0:report_umrs=0:log_path=' + msan_log_path)
+  env['MSAN_OPTIONS'] = 'halt_on_error=0:exitcode=0:report_umrs=0:log_path=' + msan_log_path
 
   # Increase maximum stack size to prevent tests from failing.
   limit = 128 * 1024 * 1024
@@ -143,7 +158,7 @@ def SetUpEnvironment(work_dir):
   return env
 
 
-def FindPackageDebs(package_name, work_directory):
+def find_package_debs(package_name, work_directory):
   """Find package debs."""
   deb_paths = []
   cache = apt.Cache()
@@ -176,7 +191,7 @@ def FindPackageDebs(package_name, work_directory):
   return deb_paths
 
 
-def ExtractLibraries(deb_paths, work_directory, output_directory):
+def extract_libraries(deb_paths, work_directory, output_directory):
   """Extract libraries from .deb packages."""
   extract_directory = os.path.join(work_directory, 'extracted')
   if os.path.exists(extract_directory):
@@ -187,7 +202,6 @@ def ExtractLibraries(deb_paths, work_directory, output_directory):
   for deb_path in deb_paths:
     subprocess.check_call(['dpkg-deb', '-x', deb_path, extract_directory])
 
-  extracted = []
   for root, _, filenames in os.walk(extract_directory):
     if 'libx32' in root or 'lib32' in root:
       continue
@@ -206,8 +220,8 @@ def ExtractLibraries(deb_paths, work_directory, output_directory):
         os.makedirs(target_dir)
 
       target_file_path = os.path.join(output_directory, rel_file_path)
-      extracted.append(target_file_path)
-        
+      yield target_file_path
+
       if os.path.lexists(target_file_path):
         os.remove(target_file_path)
 
@@ -215,17 +229,16 @@ def ExtractLibraries(deb_paths, work_directory, output_directory):
         link_path = os.readlink(file_path)
         if os.path.isabs(link_path):
           # Make absolute links relative.
-          link_path = os.path.relpath(
-              link_path, os.path.join('/', rel_directory))
+          link_path = os.path.relpath(link_path,
+                                      os.path.join('/', rel_directory))
 
         os.symlink(link_path, target_file_path)
       else:
         shutil.copy2(file_path, target_file_path)
 
-  return extracted
 
-
-def GetPackage(package_name):
+def get_package(package_name):
+  """Factory for Package objects."""
   apt_cache = apt.Cache()
   version = apt_cache[package_name].candidate
   source_name = version.source_name
@@ -241,11 +254,11 @@ def GetPackage(package_name):
   return module.Package(version)
 
 
-def PatchRpath(path, output_directory):
+def patch_rpath(path, output_directory):
   """Patch rpath to be relative to $ORIGIN."""
   try:
-    rpaths = subprocess.check_output(
-        ['patchelf', '--print-rpath', path]).strip()
+    rpaths = subprocess.check_output(['patchelf', '--print-rpath',
+                                      path]).strip()
   except subprocess.CalledProcessError:
     return
 
@@ -262,37 +275,17 @@ def PatchRpath(path, output_directory):
       processed_rpath.append(rpath)
       continue
 
-    processed_rpath.append(os.path.join(
-        '$ORIGIN',
-        os.path.relpath(rpath, rel_directory)))
+    processed_rpath.append(
+        os.path.join('$ORIGIN', os.path.relpath(rpath, rel_directory)))
 
   processed_rpath = ':'.join(processed_rpath)
   print('Patching rpath for', path, 'to', processed_rpath)
   subprocess.check_call(
-      ['patchelf', '--force-rpath', '--set-rpath',
-       processed_rpath, path])
+      ['patchelf', '--force-rpath', '--set-rpath', processed_rpath, path])
 
 
-def _CollectDependencies(apt_cache, pkg, cache, dependencies):
+def _collect_dependencies(apt_cache, pkg, cache, dependencies):
   """Collect dependencies that need to be built."""
-  C_OR_CXX_DEPS = [
-      'libc++1',
-      'libc6',
-      'libc++abi1',
-      'libgcc1',
-      'libstdc++6',
-  ]
-
-  BLACKLISTED_PACKAGES = [
-      'libcapnp-0.5.3',  # fails to compile on newer clang.
-      'libllvm5.0',
-      'libmircore1',
-      'libmircommon7',
-      'libmirclient9',
-      'libmirprotobuf3',
-      'multiarch-support',
-  ]
-
   if pkg.name in BLACKLISTED_PACKAGES:
     return False
 
@@ -309,8 +302,9 @@ def _CollectDependencies(apt_cache, pkg, cache, dependencies):
     if dependency.name in cache:
       is_c_or_cxx |= cache[dependency.name]
     else:
-      is_c_or_cxx |= _CollectDependencies(apt_cache, apt_cache[dependency.name],
-                                          cache, dependencies)
+      is_c_or_cxx |= _collect_dependencies(apt_cache,
+                                           apt_cache[dependency.name], cache,
+                                           dependencies)
   if is_c_or_cxx:
     dependencies.append(pkg.name)
 
@@ -318,20 +312,24 @@ def _CollectDependencies(apt_cache, pkg, cache, dependencies):
   return is_c_or_cxx
 
 
-def GetBuildList(package_name):
+def get_build_list(package_name):
   """Get list of packages that need to be built including dependencies."""
   apt_cache = apt.Cache()
   pkg = apt_cache[package_name]
 
   dependencies = []
-  _CollectDependencies(apt_cache, pkg, {}, dependencies)
+  _collect_dependencies(apt_cache, pkg, {}, dependencies)
   return dependencies
 
 
-class MSanBuilder(object):
+class MSanBuilder:
   """MSan builder."""
 
-  def __init__(self, debug=False, log_path=None, work_dir=None, no_track_origins=False):
+  def __init__(self,
+               debug=False,
+               log_path=None,
+               work_dir=None,
+               no_track_origins=False):
     self.debug = debug
     self.log_path = log_path
     self.work_dir = work_dir
@@ -346,7 +344,7 @@ class MSanBuilder(object):
       shutil.rmtree(self.work_dir, ignore_errors=True)
 
     os.makedirs(self.work_dir)
-    self.env = SetUpEnvironment(self.work_dir)
+    self.env = set_up_environment(self.work_dir)
 
     if self.debug and self.log_path:
       self.env['WRAPPER_DEBUG_LOG_PATH'] = self.log_path
@@ -360,16 +358,16 @@ class MSanBuilder(object):
     if not self.debug:
       shutil.rmtree(self.work_dir, ignore_errors=True)
 
-  def Build(self, package_name, output_directory, create_subdirs=False):
+  def build(self, package_name, output_directory, create_subdirs=False):
     """Build the package and write results into the output directory."""
-    deb_paths = FindPackageDebs(package_name, self.work_dir)
+    deb_paths = find_package_debs(package_name, self.work_dir)
     if deb_paths:
       print('Source package already built for', package_name)
     else:
-      pkg = GetPackage(package_name)
+      pkg = get_package(package_name)
 
-      pkg.InstallBuildDeps()
-      source_directory = pkg.DownloadSource(self.work_dir)
+      pkg.install_build_deps()
+      source_directory = pkg.download_source(self.work_dir)
       print('Source downloaded to', source_directory)
 
       # custom bin directory for custom build scripts to write wrappers.
@@ -378,10 +376,10 @@ class MSanBuilder(object):
       env = self.env.copy()
       env['PATH'] = custom_bin_dir + ':' + env['PATH']
 
-      pkg.Build(source_directory, env, custom_bin_dir)
+      pkg.build(source_directory, env, custom_bin_dir)
       shutil.rmtree(custom_bin_dir, ignore_errors=True)
 
-      deb_paths = FindPackageDebs(package_name, self.work_dir)
+      deb_paths = find_package_debs(package_name, self.work_dir)
 
     if not deb_paths:
       raise MSanBuildException('Failed to find .deb packages.')
@@ -393,22 +391,25 @@ class MSanBuilder(object):
     else:
       extract_directory = output_directory
 
-    extracted_paths = ExtractLibraries(deb_paths, self.work_dir,
-                                       extract_directory)
+    extracted_paths = list(
+        extract_libraries(deb_paths, self.work_dir, extract_directory))
     for extracted_path in extracted_paths:
       if not os.path.islink(extracted_path):
-        PatchRpath(extracted_path, extract_directory)
+        patch_rpath(extracted_path, extract_directory)
 
 
 def main():
+  """Builds packages with MemorySanitizer instrumentation."""
   parser = argparse.ArgumentParser('msan_build.py', description='MSan builder.')
   parser.add_argument('package_names', nargs='+', help='Name of the packages.')
   parser.add_argument('output_dir', help='Output directory.')
-  parser.add_argument('--create-subdirs', action='store_true',
+  parser.add_argument('--create-subdirs',
+                      action='store_true',
                       help=('Create subdirectories in the output '
                             'directory for each package.'))
   parser.add_argument('--work-dir', help='Work directory.')
-  parser.add_argument('--no-build-deps', action='store_true',
+  parser.add_argument('--no-build-deps',
+                      action='store_true',
                       help='Don\'t build dependencies.')
   parser.add_argument('--debug', action='store_true', help='Enable debug mode.')
   parser.add_argument('--log-path', help='Log path for debugging.')
@@ -431,7 +432,7 @@ def main():
 
     # Get list of packages to build, including all dependencies.
     for package_name in args.package_names:
-      for dep in GetBuildList(package_name):
+      for dep in get_build_list(package_name):
         if dep in all_packages:
           continue
 
@@ -445,11 +446,12 @@ def main():
   for package_name in package_names:
     print('\t', package_name)
 
-  with MSanBuilder(debug=args.debug, log_path=args.log_path,
+  with MSanBuilder(debug=args.debug,
+                   log_path=args.log_path,
                    work_dir=args.work_dir,
                    no_track_origins=args.no_track_origins) as builder:
     for package_name in package_names:
-      builder.Build(package_name, args.output_dir, args.create_subdirs)
+      builder.build(package_name, args.output_dir, args.create_subdirs)
 
 
 if __name__ == '__main__':

infra/base-images/base-sanitizer-libs-builder/packages/gnutls28.py
@@ -14,24 +14,22 @@
 # limitations under the License.
 #
 ################################################################################
-
-import os
-import shutil
-
+"""Custom options for gnutls28."""
 import package
 import wrapper_utils
 
 
-class Package(package.Package):
+class Package(package.Package):  # pylint: disable=too-few-public-methods
   """gnutls28 package."""
 
   def __init__(self, apt_version):
     super(Package, self).__init__('gnutls28', apt_version)
 
-  def PreBuild(self, source_directory, env, custom_bin_dir):
+  def pre_build(self, _source_directory, _env, custom_bin_dir):  # pylint: disable=no-self-use
+    """Pre-build configuration for gnutls28."""
     configure_wrapper = (
         '#!/bin/bash\n'
         '/usr/bin/dh_auto_configure "$@" --disable-hardware-acceleration')
 
-    wrapper_utils.InstallWrapper(
-        custom_bin_dir, 'dh_auto_configure', configure_wrapper)
+    wrapper_utils.InstallWrapper(custom_bin_dir, 'dh_auto_configure',
+                                 configure_wrapper)

infra/base-images/base-sanitizer-libs-builder/packages/libgcrypt20.py
@@ -14,24 +14,21 @@
 # limitations under the License.
 #
 ################################################################################
-
-import os
-import shutil
-
+"""Custom options for libgcrypt20."""
 import package
 import wrapper_utils
 
 
-class Package(package.Package):
+class Package(package.Package):  # pylint: disable=too-few-public-methods
   """libgcrypt20 package."""
 
   def __init__(self, apt_version):
     super(Package, self).__init__('libgcrypt20', apt_version)
 
-  def PreBuild(self, source_directory, env, custom_bin_dir):
-    configure_wrapper = (
-        '#!/bin/bash\n'
-        '/usr/bin/dh_auto_configure "$@" --disable-asm')
+  def pre_build(self, _source_directory, _env, custom_bin_dir):  # pylint: disable=no-self-use
+    """Pre-build configuration for libgcrypt20."""
+    configure_wrapper = ('#!/bin/bash\n'
+                         '/usr/bin/dh_auto_configure "$@" --disable-asm')
 
-    wrapper_utils.InstallWrapper(
-        custom_bin_dir, 'dh_auto_configure', configure_wrapper)
+    wrapper_utils.InstallWrapper(custom_bin_dir, 'dh_auto_configure',
+                                 configure_wrapper)

infra/base-images/base-sanitizer-libs-builder/packages/systemd.py
@@ -14,29 +14,27 @@
 # limitations under the License.
 #
 ################################################################################
-
+"""Custom options for systemd."""
 from __future__ import print_function
-import glob
-import os
-import subprocess
 
 import package
 import wrapper_utils
 
 
-class Package(package.Package):
+class Package(package.Package):  # pylint: disable=too-few-public-methods
   """systemd package."""
 
   def __init__(self, apt_version):
     super(Package, self).__init__('systemd', apt_version)
 
-  def PreBuild(self, source_directory, env, custom_bin_dir):
+  def pre_build(self, _source_directory, _env, custom_bin_dir):  # pylint: disable=no-self-use
+    """Pre-build installation of a wrapper script for systemd."""
     # Hide msan symbols from nm. the systemd build system uses this to find
     # undefined symbols and errors out if it does.
-    nm_wrapper = (
-        '#!/bin/bash\n'
-        '/usr/bin/nm "$@" | grep -E -v "U (__msan|memset)"\n'
-        'exit ${PIPESTATUS[0]}\n')
+    nm_wrapper = ('#!/bin/bash\n'
+                  '/usr/bin/nm "$@" | grep -E -v "U (__msan|memset)"\n'
+                  'exit ${PIPESTATUS[0]}\n')
 
-    wrapper_utils.InstallWrapper(custom_bin_dir, 'nm', nm_wrapper,
-                                 [wrapper_utils.DpkgHostArchitecture() + '-nm'])
+    wrapper_utils.InstallWrapper(
+        custom_bin_dir, 'nm', nm_wrapper,
+        [wrapper_utils.dpkg_host_architecture() + '-nm'])

.github/workflows/infra_tests.yml
@@ -25,6 +25,13 @@ jobs:
           sudo env "PATH=$PATH" pip install -r infra/ci/requirements.txt
           sudo env "PATH=$PATH" pip install -r infra/build/functions/requirements.txt
 
+      - name: Install python-apt
+        run: |
+          git clone https://salsa.debian.org/apt-team/python-apt.git -b 1.6.5ubuntu0.3 --depth 1 /tmp/python-apt
+          cd /tmp/python-apt
+          sudo env "PATH=$PATH" apt build-dep ./
+          sudo env "PATH=$PATH" python setup.py install
+
       - uses: GoogleCloudPlatform/github-actions/setup-gcloud@master
         with:
           version: '298.0.0'

.pylintrc
@@ -90,7 +90,6 @@ disable=print-statement,
         reduce-builtin,
         standarderror-builtin,
         unicode-builtin,
-        xrange-builtin,
         coerce-method,
         delslice-method,
         getslice-method,

infra/base-images/all.sh
@@ -20,3 +20,5 @@ docker build -t gcr.io/oss-fuzz-base/base-clang "$@" infra/base-images/base-clan
 docker build -t gcr.io/oss-fuzz-base/base-builder -t gcr.io/oss-fuzz/base-libfuzzer "$@" infra/base-images/base-builder
 docker build -t gcr.io/oss-fuzz-base/base-runner "$@" infra/base-images/base-runner
 docker build -t gcr.io/oss-fuzz-base/base-runner-debug "$@" infra/base-images/base-runner-debug
+docker build -t gcr.io/oss-fuzz-base/base-sanitizer-libs-builder "$@" infra/base-images/base-sanitizer-libs-builder
+docker build -t gcr.io/oss-fuzz-base/msan-libs-builder "$@" infra/base-images/msan-libs-builder

infra/base-images/base-sanitizer-libs-builder/compiler_wrapper.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 ################################################################################
-
+"""Wrapper script to call clang or GCC with modified arguments."""
 from __future__ import print_function
 import os
 import subprocess
@@ -26,23 +26,23 @@ GCC_ONLY_ARGS = [
     '-aux-info',
 ]
 
+M32_BIT_ARGS = [
+    '-m32',
+    '-mx32',
+]
 
-def InvokedAsGcc():
+
+def invoked_as_gcc():
   """Return whether or not we're pretending to be GCC."""
   return sys.argv[0].endswith('gcc') or sys.argv[0].endswith('g++')
 
 
-def Is32Bit(args):
+def is_32_bit(args):
   """Return whether or not we're 32-bit."""
-  M32_BIT_ARGS = [
-      '-m32',
-      '-mx32',
-  ]
-
   return any(arg in M32_BIT_ARGS for arg in args)
 
 
-def FilterWlArg(arg):
+def filter_wl_arg(arg):
   """Remove -z,defs and equivalents from a single -Wl option."""
   parts = arg.split(',')[1:]
 
@@ -55,7 +55,7 @@ def FilterWlArg(arg):
 
     if part == '--no-undefined':
       continue
-      
+
     filtered.append(part)
 
   if filtered:
@@ -65,29 +65,29 @@ def FilterWlArg(arg):
   return None
 
 
-def _RemoveLastMatching(l, find):
-  for i in xrange(len(l) - 1, -1, -1):
-    if l[i] == find:
-      del l[i]
+def _remove_last_matching(args, find):
+  for i in range(len(args) - 1, -1, -1):
+    if args[i] == find:
+      del args[i]
       return
 
   raise IndexError('Not found')
 
 
-def RemoveZDefs(args):
+def remove_z_defs(args):
   """Remove unsupported -Wl,-z,defs linker option."""
   filtered = []
 
   for arg in args:
     if arg == '-Wl,defs':
-      _RemoveLastMatching(filtered, '-Wl,-z')
+      _remove_last_matching(filtered, '-Wl,-z')
       continue
 
     if arg == '-Wl,--no-undefined':
       continue
 
     if arg.startswith('-Wl,'):
-      arg = FilterWlArg(arg)
+      arg = filter_wl_arg(arg)
       if not arg:
         continue
 
@@ -96,11 +96,11 @@ def RemoveZDefs(args):
   return filtered
 
 
-def GetCompilerArgs(args, is_cxx):
+def get_compiler_args(args, is_cxx):
   """Generate compiler args."""
   compiler_args = args[1:]
 
-  if Is32Bit(args):
+  if is_32_bit(args):
     # 32 bit builds not supported.
     compiler_args.extend([
         '-fno-sanitize=memory',
@@ -109,7 +109,7 @@ def GetCompilerArgs(args, is_cxx):
 
     return compiler_args
 
-  compiler_args = RemoveZDefs(compiler_args)
+  compiler_args = remove_z_defs(compiler_args)
   compiler_args.extend([
       # FORTIFY_SOURCE is not supported by sanitizers.
       '-U_FORTIFY_SOURCE',
@@ -122,15 +122,15 @@ def GetCompilerArgs(args, is_cxx):
       '-fno-lto',
   ])
 
-  if InvokedAsGcc():
+  if invoked_as_gcc():
     compiler_args.extend([
-      # For better compatibility with flags passed via -Wa,...
-      '-fno-integrated-as',
+        # For better compatibility with flags passed via -Wa,...
+        '-fno-integrated-as',
     ])
 
   if '-fsanitize=memory' not in args:
     # If MSan flags weren't added for some reason, add them here.
-    compiler_args.extend(msan_build.GetInjectedFlags())
+    compiler_args.extend(msan_build.get_injected_flags())
 
   if is_cxx:
     compiler_args.append('-stdlib=libc++')
@@ -138,35 +138,36 @@ def GetCompilerArgs(args, is_cxx):
   return compiler_args
 
 
-def FindRealClang():
+def find_real_clang():
   """Return path to real clang."""
   return os.environ['REAL_CLANG_PATH']
 
 
-def FallbackToGcc(args):
+def fallback_to_gcc(args):
   """Check whether if we should fall back to GCC."""
-  if not InvokedAsGcc():
+  if not invoked_as_gcc():
     return False
 
   return any(arg in GCC_ONLY_ARGS for arg in args[1:])
 
 
 def main(args):
-  if FallbackToGcc(args):
-    sys.exit(subprocess.call(['/usr/bin/' + os.path.basename(args[0])] +
-                             args[1:]))
+  """Modify arguments and call the real compiler."""
+  if fallback_to_gcc(args):
+    sys.exit(
+        subprocess.call(['/usr/bin/' + os.path.basename(args[0])] + args[1:]))
 
   is_cxx = args[0].endswith('++')
-  real_clang = FindRealClang()
+  real_clang = find_real_clang()
 
   if is_cxx:
     real_clang += '++'
 
-  args = [real_clang] + GetCompilerArgs(args, is_cxx)
+  args = [real_clang] + get_compiler_args(args, is_cxx)
   debug_log_path = os.getenv('WRAPPER_DEBUG_LOG_PATH')
   if debug_log_path:
-    with open(debug_log_path, 'a') as f:
-      f.write(str(args) + '\n')
+    with open(debug_log_path, 'a') as log_file:
+      log_file.write(str(args) + '\n')
 
   sys.exit(subprocess.call(args))
 

commit 27f2ae905a12bf0b73ce6f847971a29a887afcc0

Fix msan build breakage.
Regression from
https://github.com/google/oss-fuzz/pull/4694

infra/base-images/base-sanitizer-libs-builder/msan_build.py
@@ -91,7 +91,7 @@ def set_up_environment(work_dir):
   os.mkdir(bin_dir)
 
   dpkg_host_architecture = wrapper_utils.dpkg_host_architecture()
-  wrapper_utils.CreateSymlinks(
+  wrapper_utils.create_symlinks(
       compiler_wrapper_path,
       bin_dir,
       [
@@ -150,7 +150,8 @@ def set_up_environment(work_dir):
   msan_log_dir = os.path.join(work_dir, 'msan')
   os.mkdir(msan_log_dir)
   msan_log_path = os.path.join(msan_log_dir, 'log')
-  env['MSAN_OPTIONS'] = 'halt_on_error=0:exitcode=0:report_umrs=0:log_path=' + msan_log_path
+  env['MSAN_OPTIONS'] = ('halt_on_error=0:exitcode=0:report_umrs=0:log_path=' +
+                         msan_log_path)
 
   # Increase maximum stack size to prevent tests from failing.
   limit = 128 * 1024 * 1024

infra/base-images/base-sanitizer-libs-builder/packages/gnutls28.py
@@ -31,5 +31,5 @@ class Package(package.Package):  # pylint: disable=too-few-public-methods
         '#!/bin/bash\n'
         '/usr/bin/dh_auto_configure "$@" --disable-hardware-acceleration')
 
-    wrapper_utils.InstallWrapper(custom_bin_dir, 'dh_auto_configure',
-                                 configure_wrapper)
+    wrapper_utils.install_wrapper(custom_bin_dir, 'dh_auto_configure',
+                                  configure_wrapper)

infra/base-images/base-sanitizer-libs-builder/packages/libgcrypt20.py
@@ -30,5 +30,5 @@ class Package(package.Package):  # pylint: disable=too-few-public-methods
     configure_wrapper = ('#!/bin/bash\n'
                          '/usr/bin/dh_auto_configure "$@" --disable-asm')
 
-    wrapper_utils.InstallWrapper(custom_bin_dir, 'dh_auto_configure',
-                                 configure_wrapper)
+    wrapper_utils.install_wrapper(custom_bin_dir, 'dh_auto_configure',
+                                  configure_wrapper)

infra/base-images/base-sanitizer-libs-builder/packages/systemd.py
@@ -35,6 +35,6 @@ class Package(package.Package):  # pylint: disable=too-few-public-methods
                   '/usr/bin/nm "$@" | grep -E -v "U (__msan|memset)"\n'
                   'exit ${PIPESTATUS[0]}\n')
 
-    wrapper_utils.InstallWrapper(
+    wrapper_utils.install_wrapper(
         custom_bin_dir, 'nm', nm_wrapper,
         [wrapper_utils.dpkg_host_architecture() + '-nm'])