301ed831be24f8f8b6d2a7b7dc482db18e71c338
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2020-12-08 09:05:48 -0800

    Use print for printing stacktrace instead of using log. (#4799)
    Use print for printing stacktrace instead of using log.
    This makes stacktrace more legible.
    Fixes https://github.com/google/oss-fuzz/issues/4649

--- infra/cifuzz/cifuzz.py
+++ infra/cifuzz/cifuzz.py
@@ -427,8 +427,8 @@ def run_fuzzers(  # pylint: disable=too-many-arguments,too-many-locals
     if not testcase or not stacktrace:
       logging.info('Fuzzer %s, finished running.', target.target_name)
     else:
-      logging.info(b'Fuzzer %s, detected error: %s.', target.target_name,
-                   stacktrace)
+      utils.binary_print(b'Fuzzer %s, detected error: %s.' %
+                         (target.target_name.encode(), stacktrace))
       shutil.move(testcase, os.path.join(artifacts_dir, 'test_case'))
       parse_fuzzer_output(stacktrace, artifacts_dir)
       return True, True
