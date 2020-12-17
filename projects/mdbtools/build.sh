f4102552f4eba3e0ad029c755a5534fe54214938
Evan Miller <emmiller@gmail.com>
2020-12-17 15:09:48 -0500

    Fix copyright notice and placement of shebang

--- projects/mdbtools/build.sh
+++ projects/mdbtools/build.sh
@@ -1,4 +1,6 @@
-# Copyright 2020 Evan Miller
+#!/bin/bash -eu
+
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +15,6 @@
 # limitations under the License.
 #
 ################################################################################
-
-#!/bin/bash -eu
 
 autoreconf -f -i
 ./configure --enable-static --disable-man --disable-glib
