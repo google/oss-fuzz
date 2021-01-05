22ebedbd675ff1d148e988f57e38063fbf6a2246
davkor <david@adalogics.com>
2021-01-05 12:09:23 +0000

    tmp commit, for minor introspection.

--- projects/pyyaml/build.sh
+++ projects/pyyaml/build.sh
@@ -32,3 +32,5 @@ ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm
 \$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
 done
+
+ls -la $OUT/
