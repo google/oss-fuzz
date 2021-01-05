167374b434e74db18f063c9d0db0ebf4241a943b
davkor <david@adalogics.com>
2021-01-05 11:56:22 +0000

    pyyaml: updated project to finalise integration.

--- projects/pyyaml/build.sh
+++ projects/pyyaml/build.sh
@@ -16,7 +16,6 @@
 ################################################################################
 cd pyyaml
 python3 ./setup.py --without-libyaml install
-#pip3 install .
 
 # Build fuzzers in $OUT.
 for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
@@ -25,13 +24,11 @@ for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
   pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
 
   # Create execution wrapper.
-  # The documentation specifies we need to have LD_PRELOAD set (https://google.github.io/oss-fuzz/getting-started/new-project-guide/python-lang/#buildsh)
-  # as follows:
-  #LD_PRELOAD=\$(dirname "\$0")/libclang_rt.asan-x86_64.so \$(dirname "\$0")/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  # However, in my experience this is only needed when fuzzing native libraries, and,
-  # in fact causes an error to occur with fuzzing pure python modules.
-  echo "#/bin/sh
+  echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
-\$(dirname "\$0")/$fuzzer_package \$@" > $OUT/$fuzzer_basename
+this_dir=\$(dirname \"\$0\")
+LD_PRELOAD=\$this_dir/sanitizer_with_fuzzer.so \
+ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
+\$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
 done
