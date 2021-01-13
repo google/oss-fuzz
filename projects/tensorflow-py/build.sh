8b96748b315afa271876a177621ffb7c77e059c2
Amit Patankar <amitpatankar@google.com>
2021-01-13 10:25:19 -0800

    Resolving comments by inferno-chromium.

--- projects/tensorflow-py/build.sh
+++ projects/tensorflow-py/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2020 Google Inc.
+# Copyright 2021 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,28 +15,18 @@
 #
 ################################################################################
 
-# Build and install project (using current CFLAGS, CXXFLAGS). This is required
-# for projects with C extensions so that they're built with the proper flags.
 python3 -m pip install tensorflow
 
 # Rename to avoid the following: https://github.com/tensorflow/tensorflow/issues/40182
 mv $SRC/tensorflow/tensorflow $SRC/tensorflow/tensorflow_src
 # Build fuzzers into $OUT. These could be detected in other ways.
+
 for fuzzer in $(find $SRC -name '*_fuzz.py'); do
   fuzzer_basename=$(basename -s .py $fuzzer)
   fuzzer_package=${fuzzer_basename}.pkg
-  # To avoid issues with Python version conflicts, or changes in environment
-  # over time on the OSS-Fuzz bots, we use pyinstaller to create a standalone
-  # package. Though not necessarily required for reproducing issues, this is
-  # required to keep fuzzers working properly in OSS-Fuzz.
+
   pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
-  # Create execution wrapper. Atheris requires that certain libraries are
-  # preloaded, so this is also done here to ensure compatibility and simplify
-  # test case reproduction. Since this helper script is what OSS-Fuzz will
-  # actually execute, it is also always required.
-  # NOTE: If you are fuzzing python-only code and do not have native C/C++
-  # extensions, then remove the LD_PRELOAD line below as preloading sanitizer
-  # library is not required and can lead to unexpected startup crashes.
+
   echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
