3ea96cd2e5b99fe71b534c7a4812244229278b08
Kim Kulling <kimkulling@users.noreply.github.com>
2021-01-16 11:30:57 +0100

    Update build.sh

--- projects/assimp/build.sh
+++ projects/assimp/build.sh
@@ -24,4 +24,4 @@ echo $SRC/assimp/lib
 # build the fuzzer
 $CXX $CXXFLAGS -std=c++11 -I$SRC/assimp/include \
 	fuzz/assimp_fuzzer.cc -o $OUT/assimp_fuzzer \
-	$LIB_FUZZING_ENGINE $SRC/assimp/lib/libassimp.a $SRC/assimp/lib/libIrrXML.a $SRC/assimp/lib/libzlibstatic.a
+	$LIB_FUZZING_ENGINE $SRC/assimp/lib/libassimp.a $SRC/assimp/lib/libzlibstatic.a
