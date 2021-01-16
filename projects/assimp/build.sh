8456d7f7de5c84b025e6113cbeaed61379c194a6
Kim Kulling <kimkulling@users.noreply.github.com>
2021-01-16 16:26:17 +0100

    Update build.sh

--- projects/assimp/build.sh
+++ projects/assimp/build.sh
@@ -18,9 +18,10 @@
 # generate build env and build assimp
 cmake CMakeLists.txt -G "Ninja" -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_ZLIB=ON -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_SAMPLES=OFF
 cmake --build .
+echo Configuration
 echo $LIB_FUZZING_ENGINE
 echo $SRC/assimp/lib
-#ls $SRC/assimp/lib
+
 # build the fuzzer
 $CXX $CXXFLAGS -std=c++11 -I$SRC/assimp/include \
 	fuzz/assimp_fuzzer.cc -o $OUT/assimp_fuzzer \
