23afd6d6a1dd7220f8c1a012c09d6f853ed83d44
Guido Vranken <guidovranken@gmail.com>
2021-01-18 09:56:14 +0100

    [relic] Add Botan for comparing output against

--- projects/relic/build.sh
+++ projects/relic/build.sh
@@ -29,6 +29,7 @@ cp -R boost/ /usr/include/
 # Prevent Boost compilation error with -std=c++17
 export CXXFLAGS="$CXXFLAGS -D_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR"
 
+# Build Relic
 cd $SRC/relic/
 mkdir build/
 cd build/
@@ -38,14 +39,33 @@ cd ../..
 export RELIC_PATH=$(realpath relic)
 export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_RELIC"
 
+# Build Botan
+cd $SRC/botan
+if [[ $CFLAGS != *-m32* ]]
+then
+    ./configure.py --cc-bin=$CXX --cc-abi-flags="$CXXFLAGS" --disable-shared --disable-modules=locking_allocator --build-targets=static --without-documentation
+else
+    ./configure.py --cpu=x86_32 --cc-bin=$CXX --cc-abi-flags="$CXXFLAGS" --disable-shared --disable-modules=locking_allocator --build-targets=static --without-documentation
+fi
+make -j$(nproc)
+
+export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_BOTAN"
+export LIBBOTAN_A_PATH="$SRC/botan/libbotan-3.a"
+export BOTAN_INCLUDE_PATH="$SRC/botan/build/include"
+
+# Build Cryptofuzz
 cd $SRC/cryptofuzz
 python gen_repository.py
 rm extra_options.h
 echo -n '"' >>extra_options.h
 echo -n '--force-module=relic ' >>extra_options.h
-echo -n '--operations=BignumCalc ' >>extra_options.h
+echo -n '--operations=BignumCalc,ECC_PrivateToPublic,ECDSA_Sign,ECDSA_Verify ' >>extra_options.h
+echo -n '--curves=secp256k1,secp256r1 ' >>extra_options.h
+echo -n '--digests=NULL ' >>extra_options.h
 echo -n '"' >>extra_options.h
 cd modules/relic/
+make -B -j$(nproc)
+cd ../botan/
 make -B -j$(nproc)
 cd ../../
 make -B -j$(nproc)
