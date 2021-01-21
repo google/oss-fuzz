3b23be29db9e9f02eae4e48132a1efeaebaaac83
Oliver Chang <ochang@google.com>
2021-01-21 13:42:48 +1100

    Fix build functions tests when invoked from unittest.TestSuite.

--- infra/presubmit.py
+++ infra/presubmit.py
@@ -28,9 +28,6 @@ _SRC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEST_BLOCKLIST = [
     # Test errors with error: "ModuleNotFoundError: No module named 'apt'".
     re.compile(r'.*\/infra\/base-images\/base-sanitizer-libs-builder'),
-    # TODO(https://github.com/google/oss-fuzz/issues/5025): Reenable these
-    # tests.
-    re.compile(r'.*\/infra\/build(\/?).*'),
 ]
 
 
