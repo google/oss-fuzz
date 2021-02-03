17dae277d821472eaad57f2764a3b5266627471f
davkor <david@adalogics.com>
2021-02-03 11:39:00 +0000

    Updated imageio set up following upstream merges.

--- projects/imageio/build.sh
+++ projects/imageio/build.sh
@@ -18,7 +18,7 @@
 python3 setup.py build install
 
 # Build fuzzers in $OUT.
-for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
+for fuzzer in $(find . -name 'fuzz_*.py'); do
   fuzzer_basename=$(basename -s .py $fuzzer)
   fuzzer_package=${fuzzer_basename}.pkg
   pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
@@ -27,7 +27,6 @@ for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
   echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
-LD_PRELOAD=\$this_dir/sanitizer_with_fuzzer.so \
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
 \$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
