982672921c3ef973db462b014b241756e9a37cbd
vanhauser-thc <vh@thc.org>
2021-02-06 22:32:57 +0000

    fixes

--- projects/libreoffice/build.sh
+++ projects/libreoffice/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -eu
 
-# afl++ workaround, this also randomly enables CMPLOG lite
+# afl++ workaround, as afl-clang-fast does not work. yet.
 test -n "$AFL_MAP_SIZE" && {
   export CC=clang
   export CXX=clang++
   AFL_FLAGS="-fsanitize-coverage=trace-pc-guard"
-  rm -f $WORK/afl_cmplog
-  # 40% chance for cmplog lite
-  test $(($RANDOM % 10)) -lt 4 && {
-    AFL_FLAGS="$AFL_FLAGS,trace-cmp"
-    touch $WORK/afl_cmplog
-  }
+  #Future: enable 40% chance for cmplog lite
+  #rm -f $WORK/afl_cmplog
+  #test $(($RANDOM % 10)) -lt 4 && {
+  #  AFL_FLAGS="$AFL_FLAGS,trace-cmp"
+  #  touch $WORK/afl_cmplog
+  #}
   export CFLAGS="$AFL_FLAGS $CFLAGS"
   export CXXFLAGS="$AFL_FLAGS $CXXFLAGS"
   rm -f $LIB_FUZZING_ENGINE
