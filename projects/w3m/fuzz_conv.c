2cab1d3cbb1f4b8cf7ebf250f38002a102f3916e
davkor <david@adalogics.com>
2021-02-12 19:44:55 +0000

    Simplified fuzzer.

--- projects/w3m/fuzz_conv.c
+++ projects/w3m/fuzz_conv.c
@@ -18,56 +18,51 @@ limitations under the License.
 #include <stdlib.h>
 #include "wc.h"
 
+char *get_null_terminated(const uint8_t *data, size_t size) {
+    char *new_str = (char *)malloc(size+1);
+    if (new_str == NULL){
+            return NULL;
+    }
+    memcpy(new_str, data, size);
+    new_str[size] = '\0';
+    return new_str;
+}
+
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
-        if (size < 30) {
-                return 0;
-        }
-
-        char *new_str = (char *)malloc(10);
-        if (new_str == NULL){
-                return 0;
-        }
-        memcpy(new_str, data, 9);
-        new_str[9] = '\0';
-        data += 10;
-        size -= 10;
-
-        char *new_str2 = (char *)malloc(10);
-        if (new_str2 == NULL){
-                return 0;
-        }
-        memcpy(new_str2, data, 9);
-        new_str2[9] = '\0';
-        data += 10;
-        size -= 10;
-
-        /* Insert fuzzer contents here */
-        wc_ces old, from, to;
-        from = wc_guess_charset_short(new_str,0);
-        to = wc_guess_charset_short(new_str2, 0);
-
-        char filename[256];
-        sprintf(filename, "/tmp/libfuzzer.%d", getpid());
-
-        FILE *fp = fopen(filename, "wb");
-        if (!fp)
-                return 0;
-        fwrite(data, size, 1, fp);
-        fclose(fp);
-
-        Str s;// = Strnew();
-
-         FILE *f = fopen(filename, "r");
-        s = Strfgetall(f);
-        wc_Str_conv_with_detect(s, &from, from, to);
-        if (s != NULL) {
-                Strfree(s);
-        }
-
-        unlink(filename);
-        /* - end of fuzzer contents  - */
-
-        free(new_str);
-        free(new_str2);
+    if (size < 30) {
         return 0;
+    }
+
+    char *new_str1 = get_null_terminated(data, 20);
+    data += 20; size -= 20;
+
+    char *new_str2 = get_null_terminated(data, size);
+    //data += 10; size -= 10;
+
+    wc_ces old, from, to;
+    from = wc_guess_charset_short(new_str1,0);
+    to = wc_guess_charset_short(new_str2, 0);
+
+    char filename[256];
+    sprintf(filename, "/tmp/libfuzzer.%d", getpid());
+
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) {
+            return 0;
+    }
+    fwrite(data, size, 1, fp);
+    fclose(fp);
+
+    FILE *f = fopen(filename, "r");
+    Str s = Strfgetall(f);
+    wc_Str_conv_with_detect(s, &from, from, to);
+    if (s != NULL) {
+            Strfree(s);
+    }
+
+    unlink(filename);
+
+    free(new_str1);
+    free(new_str2);
+    return 0;
 }
