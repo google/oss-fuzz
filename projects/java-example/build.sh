bd8d5e8fc138f195b4255702ecf526ba48695a2b
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2021-02-18 14:28:47 +0100

    [jazzer] Add java-example project
    The java-example project does not pull in any external dependencies, but
    rather consists of three basic Java fuzzers that hit a synthetic bug
    after a few seconds. The project can be used to verify that ClusterFuzz
    correctly handles JVM fuzz targets.
    
    * ExampleFuzzer fails with an assertion.
    * ExampleValueProfileFuzzer fails with an exception after a few seconds,
      but only if running in value profile mode.
    * ExampleFuzzerNative fails with an ASan report.

---
+++ projects/java-example/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -eu
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# Build native library.
+JVM_INCLUDES="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
+$CXX $CXXFLAGS $JVM_INCLUDES -fPIC -shared \
+    ExampleFuzzerNative.cpp -o $OUT/libnative.so
+
+BUILD_CLASSPATH=$JAZZER_API_PATH
+
+# All class files lie in the same directory as the fuzzer at runtime.
+RUNTIME_CLASSPATH=.
+
+for fuzzer in $(find $SRC -name '*Fuzzer.java' -or -name '*FuzzerNative.java'); do
+  fuzzer_basename=$(basename -s .java $fuzzer)
+  javac -cp $BUILD_CLASSPATH $fuzzer
+  cp $SRC/$fuzzer_basename.class $OUT/
+
+  if [[ $fuzzer_basename == *FuzzerNative ]]; then
+    driver=jazzer_driver_asan
+  else
+    driver=jazzer_driver
+  fi
+
+  # Create execution wrapper.
+  echo "#!/bin/sh
+# LLVMFuzzerTestOneInput for fuzzer detection.
+this_dir=\$(dirname \"\$0\")
+LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":. \
+ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
+\$this_dir/$driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
+                   --cp=$RUNTIME_CLASSPATH \
+                   --target_class=$fuzzer_basename \
+                   --jvm_args=\"-Xmx2048m\" \
+                   \$@" > $OUT/$fuzzer_basename
+  chmod u+x $OUT/$fuzzer_basename
+done
