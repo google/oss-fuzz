a5bf6a009e6c1a6a3f85d2837c6e75638da0c477
davkor <david@adalogics.com>
2021-02-22 20:47:11 +0000

    Fix fuzzer name.

--- projects/libiec61850/build.sh
+++ projects/libiec61850/build.sh
@@ -20,7 +20,7 @@ mkdir build && cd build
 cmake ../
 make
 
-$CC $CFLAGS $LIB_FUZZING_ENGINE ../fuzz/fuzz_decode.c -o $OUT/fuzz_decode ./src/libiec61850.a \
+$CC $CFLAGS $LIB_FUZZING_ENGINE ../fuzz/fuzz_mms_decode.c -o $OUT/fuzz_mms_decode ./src/libiec61850.a \
 	-I../src/iec61850/inc -I../src/mms/inc -I../src/common/inc \
 	-I../hal/inc -I../src/logging ./hal/libhal.a
 
