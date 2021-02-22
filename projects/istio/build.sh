6b2f3145f82c28695859a7b15ea07c5dd5c2375a
AdamKorcz <adam@adalogics.com>
2021-02-22 12:16:57 +0000

    Disable coverage build

--- projects/istio/build.sh
+++ projects/istio/build.sh
@@ -15,6 +15,9 @@
 #
 ################################################################################
 
+if [[ $SANITIZER = *coverage* ]]; then
+	exit 0
+fi
 
 compile_go_fuzzer ./tests/fuzz FuzzParseInputs fuzz_parse_inputs
 compile_go_fuzzer ./tests/fuzz FuzzParseAndBuildSchema fuzz_parse_and_build_schema
