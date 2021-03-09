727b85166edf8d49bb6397194965eec9cdc13571
Philippe Antoine <contact@catenacyber.fr>
2021-03-09 11:51:08 +0100

    Fixup target try catch

--- projects/cel-cpp/fuzz_parse.cc
+++ projects/cel-cpp/fuzz_parse.cc
@@ -22,9 +22,13 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     std::string str (reinterpret_cast<const char*>(data), size);
-    auto parse_status = google::api::expr::parser::Parse(str, "fuzzinput", MAX_RECURSION);
-    if (!parse_status.ok()) {
-        parse_status.status().message();
+    try {
+        auto parse_status = google::api::expr::parser::Parse(str, "fuzzinput", MAX_RECURSION);
+        if (!parse_status.ok()) {
+            parse_status.status().message();
+        }
+    } catch (const std::exception& e) {
+        return 0;
     }
     return 0;
 }
