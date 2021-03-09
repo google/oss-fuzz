ecc76dd7c1a68bdb1289d80be577676bbb26d63f
davkor <david@adalogics.com>
2021-03-09 11:04:47 +0000

    rustls: initial integration.

---
+++ projects/rustls/persist.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate rustls;
+
+use rustls::internal::msgs::persist;
+use rustls::internal::msgs::codec::{Reader, Codec};
+
+fn try_type<T>(data: &[u8]) where T: Codec {
+    let mut rdr = Reader::init(data);
+    T::read(&mut rdr);
+}
+
+fuzz_target!(|data: &[u8]| {
+    //try_type::<persist::ClientSessionValue>(data);
+    //try_type::<persist::ServerSessionValue>(data);
+});
