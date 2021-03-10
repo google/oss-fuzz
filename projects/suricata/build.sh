5fface26b8235c4ba6d67b1a12e837b42ff117f4
Philippe Antoine <contact@catenacyber.fr>
2021-03-10 11:29:04 +0100

    Use more RUSTFLAGS for Suricata
    Taken from libra and cargo fuzz

--- projects/suricata/build.sh
+++ projects/suricata/build.sh
@@ -45,6 +45,7 @@ make install
 cd ..
 
 export CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
+export RUSTFLAGS="$RUSTFLAGS -Cpasses=sancov -Cllvm-args=-sanitizer-coverage-level=4 -Cllvm-args=-sanitizer-coverage-trace-compares -Cllvm-args=-sanitizer-coverage-inline-8bit-counters -Cllvm-args=-sanitizer-coverage-trace-geps -Cllvm-args=-sanitizer-coverage-prune-blocks=0 -Cllvm-args=-sanitizer-coverage-pc-table -Clink-dead-code -Cllvm-args=-sanitizer-coverage-stack-depth"
 
 #we did not put libhtp there before so that cifuzz does not remove it
 mv libhtp suricata/
