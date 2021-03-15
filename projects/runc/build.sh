9beedb0693922f34bad7c16b67b38a8ca7a8e5fc
AdamKorcz <adam@adalogics.com>
2021-03-15 15:30:35 +0000

    Revert

--- projects/runc/build.sh
+++ projects/runc/build.sh
@@ -15,6 +15,4 @@
 #
 ################################################################################
 
-compile_go_fuzzer ./libcontainer/system FuzzUIDMap id_map_fuzzer linux
-compile_go_fuzzer ./libcontainer/user FuzzUser user_fuzzer
-compile_go_fuzzer ./libcontainer/configs FuzzUnmarshalJSON configs_fuzzer
+$SRC/runc/tests/fuzzing/oss_fuzz_build.sh
