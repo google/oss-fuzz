62435bd64911d38b4fb900b3cc14624bf1ec078d
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2021-03-22 14:46:07 +0100

    [json-sanitizer] Fix dictionaries and add corpora

--- projects/json-sanitizer/build.sh
+++ projects/json-sanitizer/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2021 Google Inc.
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 ################################################################################
+
+# Move seed corpus and dictionary.
+mv $SRC/{*.zip,*.dict} $OUT
 
 # Build the json-sanitizer jar.
 CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate \
