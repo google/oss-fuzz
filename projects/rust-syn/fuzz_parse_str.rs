b8e0df9e4271c386ef8cd789fa9f8b38bc0a5720
Philippe Antoine <contact@catenacyber.fr>
2021-03-24 08:46:38 +0100

    Limit size for input

--- projects/rust-syn/fuzz_parse_str.rs
+++ projects/rust-syn/fuzz_parse_str.rs
@@ -3,6 +3,9 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(sd) = std::str::from_utf8(data) {
-        syn::parse_str::<syn::Expr>(sd);
+        // limit size cf https://github.com/dtolnay/syn/issues/901
+        if sd.len() < 128 {
+            syn::parse_str::<syn::Expr>(sd);
+        }
     }
 });
