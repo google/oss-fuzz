eb35289fb7dee8ed84f22c7604f4f779c6a489b1
davkor <david@adalogics.com>
2021-03-31 13:42:16 +0100

    phaspmap: retab.

--- projects/phashmap/phashmap_fuzz.cc
+++ projects/phashmap/phashmap_fuzz.cc
@@ -24,43 +24,43 @@ void serialise_test(const uint8_t *data, size_t size) {
     FuzzedDataProvider fuzzed_data(data, size);
     const int num_items = fuzzed_data.ConsumeIntegral<int16_t>();
 
-	for (int i=0; i < num_items; ++i)  {
-		table.insert(typename phmap::flat_hash_map<unsigned int, int>::value_type(
-													fuzzed_data.ConsumeIntegral<uint32_t>(), 
-													fuzzed_data.ConsumeIntegral<int32_t>()));
-	}
+    for (int i=0; i < num_items; ++i)  {
+        table.insert(typename phmap::flat_hash_map<unsigned int, int>::value_type(
+                    fuzzed_data.ConsumeIntegral<uint32_t>(), 
+                    fuzzed_data.ConsumeIntegral<int32_t>()));
+    }
 
     phmap::BinaryOutputArchive ar_out("/dump.data");
     table.dump(ar_out);
 
-	//MapType table_in;
-	phmap::flat_hash_map<unsigned int, int> table_in;
-	phmap::BinaryInputArchive ar_in("/dump.data");
+    //MapType table_in;
+    phmap::flat_hash_map<unsigned int, int> table_in;
+    phmap::BinaryInputArchive ar_in("/dump.data");
     table_in.load(ar_in);
 
-	if(table == table_in) {
-		unlink("/dump.data");
-		return;
-	}
-	unlink("/dump.data");
+    if(table == table_in) {
+        unlink("/dump.data");
+        return;
+    }
+    unlink("/dump.data");
 }
 
 void
 test_assignments(const uint8_t *data, size_t size) {
-	phmap::flat_hash_map<std::string, std::string> email;
-	FuzzedDataProvider fuzzed_data(data, size);
-	const int num_items = fuzzed_data.ConsumeIntegral<int16_t>();
-	for (int i=0; i < num_items; ++i) {
-			phmap::flat_hash_map<std::string, std::string>::value_type(
-											fuzzed_data.ConsumeRandomLengthString(), 
-											fuzzed_data.ConsumeRandomLengthString());
-	}
-	// Iterate through all of the items.
-	for (const auto& n: email) {}
+    phmap::flat_hash_map<std::string, std::string> email;
+    FuzzedDataProvider fuzzed_data(data, size);
+    const int num_items = fuzzed_data.ConsumeIntegral<int16_t>();
+    for (int i=0; i < num_items; ++i) {
+            phmap::flat_hash_map<std::string, std::string>::value_type(
+                    fuzzed_data.ConsumeRandomLengthString(), 
+                    fuzzed_data.ConsumeRandomLengthString());
+    }
+    // Iterate through all of the items.
+    for (const auto& n: email) {}
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-	serialise_test(data, size);
-	test_assignments(data, size);
-	return 0;
+    serialise_test(data, size);
+    test_assignments(data, size);
+    return 0;
 }
