008c7ec1f548755f2e97d5c0ed7e163a42ad32dd
Stefan Bucur <281483+stefanbucur@users.noreply.github.com>
2021-04-02 23:01:49 -0400

    Switch the cel-cpp project to the new Bazel build script. This reduces the build.sh file to a single line. (#5549)

--- projects/cel-cpp/build.sh
+++ projects/cel-cpp/build.sh
@@ -15,18 +15,4 @@
 #
 ################################################################################
 
-declare -r QUERY='
-    let all_fuzz_tests = attr(tags, "fuzz-test", "//...") in
-    $all_fuzz_tests - attr(tags, "no-oss-fuzz", $all_fuzz_tests)
-'
-
-declare -r PACKAGE_SUFFIX="_oss_fuzz"
-declare -r OSS_FUZZ_TESTS="$(bazel query "${QUERY}" | sed "s/$/${PACKAGE_SUFFIX}/")"
-
-bazel build -c opt --config=oss-fuzz --linkopt=-lc++ \
-    --action_env=CC="${CC}" --action_env=CXX="${CXX}" \
-    ${OSS_FUZZ_TESTS[*]}
-
-for oss_fuzz_archive in $(find bazel-bin/ -name "*${PACKAGE_SUFFIX}.tar"); do
-    tar -xvf "${oss_fuzz_archive}" -C "${OUT}"
-done
+bazel_build_fuzz_tests
