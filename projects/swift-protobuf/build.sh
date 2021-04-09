296d22adfed974900b123dc2ec2abc37ea35bdb9
Philippe Antoine <contact@catenacyber.fr>
2021-04-09 13:13:37 +0200

    symbolizer

--- projects/swift-protobuf/build.sh
+++ projects/swift-protobuf/build.sh
@@ -15,6 +15,13 @@
 #
 ################################################################################
 
+(
+cd llvm-project
+git apply ../llvmsymbol.diff
+cmake -G "Ninja" -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLIBCXXABI_ENABLE_SHARED=OFF -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_BUILD_TESTS=OFF -DLLVM_INCLUDE_TESTS=OFF llvm
+ninja -j$(nproc) llvm-symbolizer
+cp bin/llvm-symbolizer $OUT/
+)
 
 # build project
 mkdir swift-protobuf-fuzz
@@ -25,5 +32,5 @@ cp $SRC/Package.swift Package.swift
 cp ../swift-protobuf/Tests/SwiftProtobufTests/unittest.pb.swift Sources/swift-protobuf-fuzz/
 cp ../swift-protobuf/Tests/SwiftProtobufTests/unittest_import.pb.swift Sources/swift-protobuf-fuzz/
 cp ../swift-protobuf/Tests/SwiftProtobufTests/unittest_import_public.pb.swift Sources/swift-protobuf-fuzz/
-swift build -c debug -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library -Xswiftc -static-stdlib 
+swift build -c debug -Xswiftc -sanitize=address,fuzzer -Xswiftc -parse-as-library -Xswiftc -static-stdlib -Xswiftc -use-ld=/usr/bin/ld --static-swift-stdlib --sanitize=address
 cp .build/x86_64-unknown-linux-gnu/debug/swift-protobuf-fuzz $OUT/
