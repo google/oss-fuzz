e12f6a966cc6d016ab4c08402deca4399b89a41c
Cuong Manh Le <cuong@orijtech.com>
2021-04-19 22:12:54 +0700

    Use cosmos-sdk oss fuzz build script

--- projects/cosmos-sdk/build.sh
+++ projects/cosmos-sdk/build.sh
@@ -15,6 +15,4 @@
 #
 ################################################################################
 
-export FUZZ_ROOT="github.com/cosmos/cosmos-sdk"
-
-compile_go_fuzzer "$FUZZ_ROOT"/fuzz/types/dec/setstring Fuzz fuzz_types_dec_setstring fuzz
+bash -x ./fuzz/oss-fuzz-build.sh
