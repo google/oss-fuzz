ea83ce8e4b6e39b776e87211bfada519d0351dad
Jonathan Metzman <metzman@chromium.org>
2021-04-21 09:28:06 -0700

    Revert "[CIFuzz] End fuzzing docker processes properly (#5473)"
    This reverts commit c9b3d057b0e66e4e239ee2a10bae9da4fdbfd4f6.
    This commit causes CIFuzz to hang.

--- infra/cifuzz/docker.py
+++ infra/cifuzz/docker.py
@@ -12,13 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Module for dealing with docker."""
-import logging
 import os
-import subprocess
 import sys
-import tempfile
-
-import process_utils
 
 # pylint: disable=wrong-import-position,import-error
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -41,63 +36,3 @@ def delete_images(images):
   command = ['docker', 'rmi', '-f'] + images
   utils.execute(command)
   utils.execute(['docker', 'builder', 'prune', '-f'])
-
-
-def stop_docker_container(container_id, wait_time=1):
-  """Stops the docker container, |container_id|. Returns True on success."""
-  result = subprocess.run(
-      ['docker', 'stop', container_id, '-t',
-       str(wait_time)], check=False)
-  return result.returncode == 0
-
-
-def _handle_timed_out_container_process(process, cid_filename):
-  """Stops the docker container |process| (and child processes) that has a
-  container id in |cid_filename|. Returns stdout and stderr of |process|. This
-  function is a helper for run_container_command and should only be invoked by
-  it. Returns None for each if we can't get stdout and stderr."""
-  # Be cautious here. We probably aren't doing anything essential for CIFuzz to
-  # function. So try extra hard not to throw uncaught exceptions.
-  try:
-    with open(cid_filename, 'r') as cid_file_handle:
-      container_id = cid_file_handle.read()
-  except FileNotFoundError:
-    logging.error('cid_file not found.')
-    return None, None
-
-  if not stop_docker_container(container_id):
-    logging.error('Failed to stop docker container: %s', container_id)
-    return None, None
-
-  # Use a timeout so we don't wait forever.
-  try:
-    return process.communicate(timeout=5)
-  except subprocess.TimeoutExpired:
-    logging.error('Docker container still running after stop attempt.')
-    return None, None
-
-
-def run_container_command(command_arguments, timeout=None):
-  """Runs |command_arguments| as a "docker run" command. Returns ProcessResult.
-  Stops the command if timeout is reached."""
-  command = ['docker', 'run', '--rm', '--privileged']
-  timed_out = False
-  with tempfile.TemporaryDirectory() as temp_dir:
-    # Use temp dir instead of file because docker complains if file exists
-    # already.
-    cid_file_path = os.path.join(temp_dir, 'cidfile')
-    command.extend(['--cidfile', cid_file_path])
-    command.extend(command_arguments)
-    logging.info('Running command: %s', ' '.join(command))
-    process = subprocess.Popen(command,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    try:
-      stdout, stderr = process.communicate(timeout=timeout)
-    except subprocess.TimeoutExpired:
-      logging.warning('Command timed out: %s', ' '.join(command))
-      stdout, stderr = _handle_timed_out_container_process(
-          process, cid_file_path)
-      timed_out = True
-
-    return process_utils.ProcessResult(process, stdout, stderr, timed_out)
