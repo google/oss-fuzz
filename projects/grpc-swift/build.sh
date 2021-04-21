39043e43d2f02909b84f842127e79a1b4a36da13
Philippe Antoine <contact@catenacyber.fr>
2021-04-21 17:35:32 +0200

    More multiline to avoid horizontal scrolling

--- projects/grpc-swift/build.sh
+++ projects/grpc-swift/build.sh
@@ -19,7 +19,16 @@
 (
 cd $SRC/llvm-project
 git apply ../llvmsymbol.diff
-cmake -G "Ninja" -DLIBCXX_ENABLE_SHARED=OFF -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON -DLIBCXXABI_ENABLE_SHARED=OFF -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_BUILD_TESTS=OFF -DLLVM_INCLUDE_TESTS=OFF llvm
+cmake -G "Ninja" \
+    -DLIBCXX_ENABLE_SHARED=OFF \
+    -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
+    -DLIBCXXABI_ENABLE_SHARED=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_TARGETS_TO_BUILD=X86 \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DLLVM_BUILD_TESTS=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF llvm
 ninja -j$(nproc) llvm-symbolizer
 cp bin/llvm-symbolizer $OUT/
 )
@@ -34,12 +43,21 @@ cp -r ../Sources/Examples/Echo Sources/
 cp $SRC/Package.swift Package.swift
 # Maybe we should have a helper script to set $SWIFT_FLAGS
 # for instance about -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION in -Xcc
-swift build -c debug -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library -Xswiftc -static-stdlib -Xswiftc -use-ld=/usr/bin/ld --static-swift-stdlib  --sanitize=address -Xcc="-fsanitize=fuzzer-no-link,address" -Xcxx="-fsanitize=fuzzer-no-link,address"
+swift build -c debug -Xswiftc -sanitize=fuzzer,address \
+    -Xswiftc -parse-as-library -Xswiftc -static-stdlib \
+    -Xswiftc -use-ld=/usr/bin/ld --static-swift-stdlib \
+    --sanitize=address -Xcc="-fsanitize=fuzzer-no-link,address" \
+    -Xcxx="-fsanitize=fuzzer-no-link,address"
+
 (
 cd .build/debug/
 find . -maxdepth 1 -type f -name "*fuzz" -executable | while read i; do cp $i $OUT/"$i"-debug; done
 )
-swift build -c release -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library -Xswiftc -static-stdlib -Xswiftc -use-ld=/usr/bin/ld --static-swift-stdlib  --sanitize=address -Xcc="-fsanitize=fuzzer-no-link,address" -Xcxx="-fsanitize=fuzzer-no-link,address"
+swift build -c release -Xswiftc -sanitize=fuzzer,address \
+    -Xswiftc -parse-as-library -Xswiftc -static-stdlib \
+    -Xswiftc -use-ld=/usr/bin/ld --static-swift-stdlib \
+    --sanitize=address -Xcc="-fsanitize=fuzzer-no-link,address" \
+    -Xcxx="-fsanitize=fuzzer-no-link,address"
 (
 cd .build/release/
 find . -maxdepth 1 -type f -name "*fuzz" -executable | while read i; do cp $i $OUT/"$i"-release; done
