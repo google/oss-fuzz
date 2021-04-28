8a7ff9aa7597755a498f9bbd243286498597d837
AdamKorcz <adam@adalogics.com>
2021-04-28 22:39:05 +0100

    Removed null-termination

--- projects/gravity/fuzzer.c
+++ projects/gravity/fuzzer.c
@@ -25,17 +25,9 @@ static void report_error (gravity_vm *vm, error_type_t error_type,
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
-	char *source_code = (char *)malloc(size+1);
-	if (source_code == NULL){
-		return 0;
-	}
-	memcpy(source_code, data, size);
-	source_code[size] = '\0';
-	
 	gravity_delegate_t delegate = {.error_callback = report_error};    
 	gravity_compiler_t *compiler = gravity_compiler_create(&delegate);
-	gravity_closure_t *closure = gravity_compiler_run(compiler, source_code, strlen(source_code), 0, true, true);
-	free(source_code);
+	gravity_closure_t *closure = gravity_compiler_run(compiler, data, size, 0, true, true);
 	gravity_compiler_free(compiler);
 	return 0;
 }
