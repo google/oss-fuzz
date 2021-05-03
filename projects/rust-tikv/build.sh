8c22276f4120af14641d8f593e9648fe1b999d4e
Philippe Antoine <contact@catenacyber.fr>
2021-05-03 16:26:49 +0200

    Adds rust project tikv

---
+++ projects/rust-tikv/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd fuzz
+../target/debug/fuzz list-targets | while read t; do
+    if [ "$FUZZING_ENGINE" = honggfuzz ]; then
+        CFLAGS="" CXXFLAGS="" ../target/debug/fuzz build $FUZZING_ENGINE ${t}
+    fi
+    if [ "$FUZZING_ENGINE" = afl ]; then
+        CC=clang CXX=clang++ CFLAGS="" CXXFLAGS="" RUSTFLAGS="" ../target/debug/fuzz build $FUZZING_ENGINE ${t}
+    fi
+    if [ "$FUZZING_ENGINE" = libfuzzer ]; then
+        ../target/debug/fuzz build $FUZZING_ENGINE ${t}
+    fi
+    find ../target/ -name ${t} -type f | while read i; do cp $i $OUT/; done
+done
