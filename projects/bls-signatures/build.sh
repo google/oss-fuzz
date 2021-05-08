6c30fc23abbad3a43748692f3d51431165b5125f
Guido Vranken <guidovranken@gmail.com>
2021-05-08 13:53:36 +0200

    [bls-signatures] Enable more operations

--- projects/bls-signatures/build.sh
+++ projects/bls-signatures/build.sh
@@ -49,6 +49,7 @@ echo -n "BLS_IsG1OnCurve," >>extra_options.h
 echo -n "BLS_IsG2OnCurve," >>extra_options.h
 echo -n "BLS_Pairing," >>extra_options.h
 echo -n "BLS_PrivateToPublic," >>extra_options.h
+echo -n "BLS_PrivateToPublic_G2," >>extra_options.h
 echo -n "BLS_Sign," >>extra_options.h
 echo -n "BLS_Verify," >>extra_options.h
 echo -n "BLS_Compress_G1," >>extra_options.h
@@ -57,8 +58,12 @@ echo -n "BLS_Decompress_G1," >>extra_options.h
 echo -n "BLS_Decompress_G2," >>extra_options.h
 echo -n "BLS_G1_Add," >>extra_options.h
 echo -n "BLS_G1_Mul," >>extra_options.h
+echo -n "BLS_G1_IsEq," >>extra_options.h
+echo -n "BLS_G1_Neg," >>extra_options.h
 echo -n "BLS_G2_Add," >>extra_options.h
 echo -n "BLS_G2_Mul," >>extra_options.h
+echo -n "BLS_G2_IsEq," >>extra_options.h
+echo -n "BLS_G2_Neg," >>extra_options.h
 echo -n "BignumCalc_Mod_BLS12_381_P," >>extra_options.h
 echo -n "BignumCalc_Mod_BLS12_381_R," >>extra_options.h
 echo -n "KDF_HKDF," >>extra_options.h
