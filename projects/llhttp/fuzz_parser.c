5efe71935ef96f3b5e88153b8af12bd712e4ba58
davkor <david@adalogics.com>
2021-05-28 19:35:28 +0100

    fix up project yaml.

--- projects/llhttp/fuzz_parser.c
+++ projects/llhttp/fuzz_parser.c
@@ -14,8 +14,6 @@ limitations under the License.
 #include <string.h>
 #include "llhttp.h"
 
-llhttp_t parser;
-llhttp_settings_t settings;
 
 int handle_on_message_complete(llhttp_t* arg) {
 	return 0;
@@ -23,13 +21,41 @@ int handle_on_message_complete(llhttp_t* arg) {
 
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    llhttp_t parser;
+    llhttp_settings_t settings;
+    llhttp_type_t http_type;
+
+    /* We need four bytes to determine variable parameters */
+    if (size < 4) {
+        return 0;
+    }
+
+    int headers = (data[0] & 0x01) == 1;
+    int chunked_length = (data[1] & 0x01) == 1;
+    int keep_alive = (data[2] & 0x01) == 1;
+    if (data[0] % 3 == 0) {
+        http_type = HTTP_BOTH;
+    } 
+    else if (data[0] % 3 == 1) {
+        http_type = HTTP_REQUEST;
+    }
+    else {
+        http_type = HTTP_RESPONSE;
+    }
+    data += 4; size -= 4;
+
 	/* Initialize user callbacks and settings */
 	llhttp_settings_init(&settings);
 
 	/* Set user callback */
 	settings.on_message_complete = handle_on_message_complete;
-	llhttp_init(&parser, HTTP_BOTH, &settings);
+
+	llhttp_init(&parser, http_type, &settings);
+    llhttp_set_lenient_headers(&parser, headers);
+    llhttp_set_lenient_chunked_length(&parser, chunked_length);
+    llhttp_set_lenient_keep_alive(&parser, keep_alive);
+
 	llhttp_execute(&parser, data, size);
 
-	return 1;
+	return 0;
 }
