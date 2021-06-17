35566fbc571a076f89c45977bd9d262720cf0145
Jonathan Metzman <metzman@chromium.org>
2021-06-17 14:31:50 -0700

    fix

--- infra/cifuzz/generate_coverage_report.py
+++ infra/cifuzz/generate_coverage_report.py
@@ -46,5 +46,6 @@ def generate_coverage_report(fuzz_target_paths, out_dir, clusterfuzz_deployment,
   run_coverage_command(out_dir, config)
   os.system('ls -lh ' + os.path.join(out_dir, "report"))
   os.system('chmod -R 777 ' + os.path.join(out_dir, "report"))
-  shutil.copytree(os.path.join(out_dir, 'report'),
-                  os.path.join(out_dir, 'artifacts', 'report'))
+  os.system(f'zip -r {os.path.join(out_dir, "artifacts", "report.zip")} {os.path.join(out_dir, "report")}')
+  # shutil.copytree(os.path.join(out_dir, 'report'),
+  #                 os.path.join(out_dir, 'artifacts', 'report'))
