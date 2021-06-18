082e1a94a9dfcfbfee54170cf1b86b3bb2a99b16
davkor <david@adalogics.com>
2021-06-18 10:32:13 +0100

    remove verbose logging.

--- projects/jsonnet/build.sh
+++ projects/jsonnet/build.sh
@@ -17,9 +17,9 @@
 
 mkdir jsonnet/build
 pushd jsonnet/build
-cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
+cmake -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
   -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" ..
-make -j$(nproc) V=1
+make -j$(nproc)
 popd
 
 INSTALL_DIR="$SRC/jsonnet"
