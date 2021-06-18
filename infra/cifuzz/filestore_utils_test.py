572f8d6a5d1c3d2c6ee1ef0e8b57d72a83497d9b
Jonathan Metzman <metzman@chromium.org>
2021-06-18 06:47:00 -0700

    Fix tests

--- infra/cifuzz/filestore_utils_test.py
+++ infra/cifuzz/filestore_utils_test.py
@@ -28,21 +28,21 @@ class GetFilestoreTest(unittest.TestCase):
   """Tests for get_filestore."""
 
   @parameterized.parameterized.expand([
-      (config_utils.BaseConfig.Platform.EXTERNAL_GENERIC_GITHUB,
+      (config_utils.BaseConfig.Platform.EXTERNAL_GITHUB,
        github_actions.GithubActionsFilestore)
   ])
   def test_get_filestore(self, platform, filestore_cls):
     """Tests that get_filestore returns the right filestore given a certain
     platform."""
-    with mock.patch('config_utils.BaseConfig.platform', return_value=platform):
-      run_config = test_helpers.create_run_config()
-      filestore_impl = filestore_utils.get_filestore(run_config)
-      self.assertIsInstance(filestore_impl, filestore_cls)
+    run_config = test_helpers.create_run_config(build_integration_path='/',
+                                                is_github=True)
+    filestore_impl = filestore_utils.get_filestore(run_config)
+    self.assertIsInstance(filestore_impl, filestore_cls)
 
   def test_get_filestore_unsupported_platform(self):
     """Tests that get_filestore exceptions given a platform it doesn't
     support."""
     with mock.patch('config_utils.BaseConfig.platform', return_value='other'):
       run_config = test_helpers.create_run_config()
-      with self.assertRaises(filestore.FilestoreException):
+      with self.assertRaises(filestore.FilestoreError):
         filestore_utils.get_filestore(run_config)
