77211e836cbc4e261e52a481d5700e251ae7a0bf
Abhishek Arya <inferno@chromium.org>
2021-06-25 12:46:09 -0700

    Update update_build_status.py

--- infra/build/functions/update_build_status.py
+++ infra/build/functions/update_build_status.py
@@ -34,7 +34,7 @@ from datastore_entities import Project
 BADGE_DIR = 'badge_images'
 BADGE_IMAGE_TYPES = {'svg': 'image/svg+xml', 'png': 'image/png'}
 DESTINATION_BADGE_DIR = 'badges'
-MAX_BUILD_LOGS = 5
+MAX_BUILD_LOGS = 4
 
 STATUS_BUCKET = 'oss-fuzz-build-logs'
 
@@ -174,7 +174,7 @@ def update_build_status(build_tag, status_filename):
     project = get_build_history(project_build.build_ids)
     project['name'] = project_build.project
     print('Processing project', project['name'])
-    time.sleep(3)  # Avoid rate limits.
+    time.sleep(2)  # Avoid rate limits.
     return project
 
   with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
