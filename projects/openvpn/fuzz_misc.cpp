42c045fe275bcad44b90451740f660e85eaa31e3
davkor <david@adalogics.com>
2021-06-25 16:43:12 +0100

    Improve state of openvpn fuzzing, the proxy fuzzer in particular.

--- projects/openvpn/fuzz_misc.cpp
+++ projects/openvpn/fuzz_misc.cpp
@@ -9,7 +9,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <fuzzer/FuzzedDataProvider.h>
+
+#include "fuzz.h"
 
 extern "C" {
 #include "config.h"
