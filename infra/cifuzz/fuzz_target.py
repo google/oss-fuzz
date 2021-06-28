0a9f87dc83b7be6f3adc109612cd6d875e4c44c7
Jonathan Metzman <metzman@chromium.org>
2021-06-28 11:39:47 -0700

    try again

--- infra/cifuzz/fuzz_target.py
+++ infra/cifuzz/fuzz_target.py
@@ -163,7 +163,7 @@ class FuzzTarget:  # pylint: disable=too-many-instance-attributes
     # Crash was discovered.
     logging.info('Fuzzer %s, ended before timeout.', self.target_name)
     testcase = get_testcase(stderr)
-    assert not os.system(f'echo "FIND"; find -name {os.path.basename(testcase)}')
+    assert not os.system(f'echo "FIND"; find / -name {os.path.basename(testcase)}; echo "find / -name {os.path.basename(testcase)}"')
     if not testcase:
       logging.error(b'No testcase found in stacktrace: %s.', stderr)
       return FuzzResult(None, None, self.latest_corpus_path)
