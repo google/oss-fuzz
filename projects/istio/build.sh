a47195f75450d50c24a8c482890728ff3e38acfc
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2021-06-30 10:33:08 +0100

    istio: Move build script upstream (#5980)

--- projects/istio/build.sh
+++ projects/istio/build.sh
@@ -15,6 +15,5 @@
 #
 ################################################################################
 
+$SRC/istio/tests/fuzz/oss_fuzz_build.sh
 
-compile_go_fuzzer ./tests/fuzz FuzzParseInputs fuzz_parse_inputs
-compile_go_fuzzer ./tests/fuzz FuzzParseAndBuildSchema fuzz_parse_and_build_schema
