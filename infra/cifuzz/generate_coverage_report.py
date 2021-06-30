16b139123a7a48826f3709ada909d74fd9d5a33e
Jonathan Metzman <metzman@chromium.org>
2021-06-30 12:12:26 -0700

    tmp

--- infra/cifuzz/generate_coverage_report.py
+++ infra/cifuzz/generate_coverage_report.py
@@ -24,6 +24,7 @@ def run_coverage_command(workspace, config):
                                                    config.language)
   docker_args += [
       '-e', 'COVERAGE_EXTRA_ARGS=', '-e', 'HTTP_PORT=', '-t',
+      '-e', f'COVERAGE_OUTPUT_DIR={self.workspace.coverage_report}'
       docker.BASE_RUNNER_TAG, 'coverage'
   ]
   return helper.docker_run(docker_args)
