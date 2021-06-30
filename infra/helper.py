81c2e7bded5c349fa9925b05ef34f1b855dbc937
Asra Ali <asraa@google.com>
2021-06-30 14:23:42 -0400

    fix python format
    Signed-off-by: Asra Ali <asraa@google.com>

--- infra/helper.py
+++ infra/helper.py
@@ -36,7 +36,7 @@ BASE_IMAGES = [
     'gcr.io/oss-fuzz-base/base-image',
     'gcr.io/oss-fuzz-base/base-clang',
     'gcr.io/oss-fuzz-base/base-builder',
-#    'gcr.io/oss-fuzz-base/base-builder-new',
+    'gcr.io/oss-fuzz-base/base-builder-new',
     'gcr.io/oss-fuzz-base/base-runner',
     'gcr.io/oss-fuzz-base/base-runner-debug',
     'gcr.io/oss-fuzz-base/base-sanitizer-libs-builder',
