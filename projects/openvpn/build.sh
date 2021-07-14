209a0ee3340565ce5d199673b22b01bacd142451
DavidKorczynski <david@adalogics.com>
2021-07-14 11:21:30 +0100

    refine openvpn build script. (#6038)

--- projects/openvpn/build.sh
+++ projects/openvpn/build.sh
@@ -15,36 +15,43 @@
 #
 ################################################################################
 
+BASE=${SRC}/openvpn/src/openvpn
+
+apply_sed_changes() {
+  sed -i 's/read(/fuzz_read(/g' ${BASE}/console_systemd.c
+  sed -i 's/fgets(/fuzz_fgets(/g' ${BASE}/console_builtin.c
+  sed -i 's/fgets(/fuzz_fgets(/g' ${BASE}/misc.c
+  sed -i 's/#include "forward.h"/#include "fuzz_header.h"\n#include "forward.h"/g' ${BASE}/proxy.c
+  sed -i 's/select(/fuzz_select(/g' ${BASE}/proxy.c
+  sed -i 's/send(/fuzz_send(/g' ${BASE}/proxy.c
+  sed -i 's/recv(/fuzz_recv(/g' ${BASE}/proxy.c
+  sed -i 's/isatty/fuzz_isatty/g' ${BASE}/console_builtin.c
+
+  sed -i 's/fopen/fuzz_fopen/g' ${BASE}/console_builtin.c
+  sed -i 's/fclose/fuzz_fclose/g' ${BASE}/console_builtin.c
+
+  sed -i 's/sendto/fuzz_sendto/g' ${BASE}/socket.h
+  sed -i 's/#include "misc.h"/#include "misc.h"\nextern size_t fuzz_sendto(int sockfd, void *buf, size_t len, int flags, struct sockaddr *dest_addr, socklen_t addrlen);/g' ${BASE}/socket.h
+
+  sed -i 's/fp = (flags/fp = stdout;\n\/\//g' ${BASE}/error.c
+
+  sed -i 's/crypto_msg(M_FATAL/crypto_msg(M_WARN/g' ${BASE}/crypto_openssl.c
+  sed -i 's/msg(M_FATAL, \"Cipher/return;msg(M_FATAL, \"Cipher/g' ${BASE}/crypto.c
+  sed -i 's/msg(M_FATAL/msg(M_WARN/g' ${BASE}/crypto.c
+
+  sed -i 's/= write/= fuzz_write/g' ${BASE}/packet_id.c
+}
+
 # Changes in the code so we can fuzz it.
 git apply $SRC/crypto_patch.txt
 
-echo "" >> $SRC/openvpn/src/openvpn/openvpn.c
-echo "ssize_t fuzz_get_random_data(void *buf, size_t len) { return 0; }" >> $SRC/openvpn/src/openvpn/fake_fuzz_header.h
-echo "int fuzz_success;" >> $SRC/openvpn/src/openvpn/fake_fuzz_header.h
-echo "#include \"fake_fuzz_header.h\"" >> $SRC/openvpn/src/openvpn/openvpn.c
+echo "" >> ${BASE}/openvpn.c
+echo "#include \"fake_fuzz_header.h\"" >> ${BASE}/openvpn.c
+echo "ssize_t fuzz_get_random_data(void *buf, size_t len) { return 0; }" >> ${BASE}/fake_fuzz_header.h
+echo "int fuzz_success;" >> ${BASE}/fake_fuzz_header.h
 
-sed -i 's/read(/fuzz_read(/g' ./src/openvpn/console_systemd.c
-sed -i 's/fgets(/fuzz_fgets(/g' ./src/openvpn/console_builtin.c
-sed -i 's/fgets(/fuzz_fgets(/g' ./src/openvpn/misc.c
-sed -i 's/#include "forward.h"/#include "fuzz_header.h"\n#include "forward.h"/g' ./src/openvpn/proxy.c
-sed -i 's/select(/fuzz_select(/g' ./src/openvpn/proxy.c
-sed -i 's/send(/fuzz_send(/g' ./src/openvpn/proxy.c
-sed -i 's/recv(/fuzz_recv(/g' ./src/openvpn/proxy.c
-sed -i 's/isatty/fuzz_isatty/g' ./src/openvpn/console_builtin.c
-
-sed -i 's/fopen/fuzz_fopen/g' ./src/openvpn/console_builtin.c
-sed -i 's/fclose/fuzz_fclose/g' ./src/openvpn/console_builtin.c
-
-sed -i 's/sendto/fuzz_sendto/g' ./src/openvpn/socket.h
-sed -i 's/#include "misc.h"/#include "misc.h"\nextern size_t fuzz_sendto(int sockfd, void *buf, size_t len, int flags, struct sockaddr *dest_addr, socklen_t addrlen);/g' ./src/openvpn/socket.h
-
-sed -i 's/fp = (flags/fp = stdout;\n\/\//g' ./src/openvpn/error.c
-
-sed -i 's/crypto_msg(M_FATAL/crypto_msg(M_WARN/g' ./src/openvpn/crypto_openssl.c
-sed -i 's/msg(M_FATAL, \"Cipher/return;msg(M_FATAL, \"Cipher/g' ./src/openvpn/crypto.c
-sed -i 's/msg(M_FATAL/msg(M_WARN/g' ./src/openvpn/crypto.c
-
-sed -i 's/= write/= fuzz_write/g' ./src/openvpn/packet_id.c
+# Apply hooking changes
+apply_sed_changes
 
 # Copy corpuses out
 zip -r $OUT/fuzz_verify_cert_seed_corpus.zip $SRC/boringssl/fuzz/cert_corpus
@@ -63,13 +70,13 @@ ar r libopenvpn.a *.o
 $CXX $CXXFLAGS -g -c $SRC/fuzz_randomizer.cpp -o $SRC/fuzz_randomizer.o
 
 # Compile the fuzzers
-for fuzzname in fuzz_dhcp fuzz_misc fuzz_base64 fuzz_proxy fuzz_buffer fuzz_route fuzz_packet_id fuzz_mroute fuzz_list fuzz_verify_cert fuzz_forward fuzz_crypto; do
-    $CC -DHAVE_CONFIG_H -I. -I../.. -I../../include  -I../../include -I../../src/compat \
-      -DPLUGIN_LIBDIR=\"/usr/local/lib/openvpn/plugins\"  -Wall -std=c99 $CFLAGS \
-      -c $SRC/${fuzzname}.c -o $SRC/${fuzzname}.o
+for fuzzname in dhcp misc base64 proxy buffer route packet_id mroute list verify_cert forward crypto; do
+    $CC -DHAVE_CONFIG_H -I. -I../.. -I../../include -I../../src/compat \
+      -DPLUGIN_LIBDIR=\"/usr/local/lib/openvpn/plugins\" -std=c99 $CFLAGS \
+      -c $SRC/fuzz_${fuzzname}.c -o $SRC/fuzz_${fuzzname}.o
 
     # Link with CXX
-    $CXX ${CXXFLAGS} ${LIB_FUZZING_ENGINE} $SRC/${fuzzname}.o -o $OUT/${fuzzname} $SRC/fuzz_randomizer.o \
+    $CXX ${CXXFLAGS} ${LIB_FUZZING_ENGINE} $SRC/fuzz_${fuzzname}.o -o $OUT/fuzz_${fuzzname} $SRC/fuzz_randomizer.o \
         libopenvpn.a ../../src/compat/.libs/libcompat.a /usr/lib/x86_64-linux-gnu/libnsl.a \
         /usr/lib/x86_64-linux-gnu/libresolv.a /usr/lib/x86_64-linux-gnu/liblzo2.a \
         -lssl -lcrypto -ldl
