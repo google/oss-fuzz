8a8db3a106eb724097b7d055518be27f56907cfb
David Korczynski <david@adalogics.com>
2021-07-16 16:37:59 +0100

    net-snmp: two new fuzzers and request access to project

--- projects/net-snmp/build.sh
+++ projects/net-snmp/build.sh
@@ -21,15 +21,11 @@
 make
 
 # build fuzzers (remember to link statically)
-$CC $CFLAGS -c -Iinclude $SRC/snmp_pdu_parse_fuzzer.c -o $WORK/snmp_pdu_parse_fuzzer.o
-$CXX $CXXFLAGS $WORK/snmp_pdu_parse_fuzzer.o \
-      $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
-      -Wl,-Bstatic -lcrypto -Wl,-Bdynamic -lm \
-      -o $OUT/snmp_pdu_parse_fuzzer
-
-$CC $CFLAGS -c -Iinclude -Iagent/mibgroup/agentx $SRC/agentx_parse_fuzzer.c -o $WORK/agentx_parse_fuzzer.o
-$CXX $CXXFLAGS $WORK/agentx_parse_fuzzer.o \
-      $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
-      agent/.libs/libnetsnmpagent.a \
-      -Wl,-Bstatic -lcrypto -Wl,-Bdynamic -lm \
-      -o $OUT/agentx_parse_fuzzer
+for fuzzname in snmp_pdu_parse_fuzzer snmp_parse_fuzzer snmp_scoped_pdu_parse_fuzzer agentx_parse_fuzzer; do
+  $CC $CFLAGS -c -Iinclude -Iagent/mibgroup/agentx $SRC/${fuzzname}.c -o $WORK/${fuzzname}.o
+  $CXX $CXXFLAGS $WORK/${fuzzname}.o \
+        $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
+        agent/.libs/libnetsnmpagent.a \
+        -Wl,-Bstatic -lcrypto -Wl,-Bdynamic -lm \
+        -o $OUT/${fuzzname}
+done
