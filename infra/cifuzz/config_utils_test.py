df4c8d65c289d207ca44bf5a9b0f1cb5e8f398ed
Oliver Chang <ochang@google.com>
2021-07-16 16:15:42 +1000

    fix tests

--- infra/cifuzz/config_utils_test.py
+++ infra/cifuzz/config_utils_test.py
@@ -73,11 +73,24 @@ class BuildFuzzersConfigTest(unittest.TestCase):
     config = self._create_config()
     self.assertEqual(config.base_ref, expected_base_ref)
 
-  def test_keep_unaffected_defaults_to_false(self):
-    """Tests that keep_unaffected_fuzz_targets defaults to false."""
+  def test_keep_unaffected_defaults_to_true(self):
+    """Tests that keep_unaffected_fuzz_targets defaults to true."""
     config = self._create_config()
+    self.assertTrue(config.keep_unaffected_fuzz_targets)
+
+  def test_keep_unaffected_defaults_to_true_when_pr(self):
+    """Tests that keep_unaffected_fuzz_targets defaults to false when from a
+    pr."""
+    config = self._create_config()
+    config.pr_ref = 'pr_ref'
     self.assertFalse(config.keep_unaffected_fuzz_targets)
 
+  def test_keep_unaffected_defaults_to_true_when_push(self):
+    """Tests that keep_unaffected_fuzz_targets defaults to false when from a
+    push."""
+    config = self._create_config()
+    config.base_commit = 'commit'
+    self.assertFalse(config.keep_unaffected_fuzz_targets)
 
 class RunFuzzersConfigTest(unittest.TestCase):
   """Tests for RunFuzzersConfig."""
