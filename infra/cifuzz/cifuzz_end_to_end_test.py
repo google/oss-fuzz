68c57764c55a0649b9ddace68fef6bf44a506e04
Jonathan Metzman <metzman@chromium.org>
2021-07-29 08:00:25 -0700

    [cifuzz] Add basic end-to-end tests.
    This test doesn't really do diffing or affected fuzzers properly.
    Nor does it check the workspace for existence of certain things, nor
    does check the filesystem for proof things happened. It is still
    a WIP.

---
+++ infra/cifuzz/cifuzz_end_to_end_test.py
@@ -0,0 +1,40 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""End-to-End tests for CIFuzz."""
+import os
+import tempfile
+import unittest
+
+
+import run_cifuzz
+
+
+CIFUZZ_DIR = os.path.dirname(os.path.abspath(__file__))
+EXTERNAL_PROJECT_PATH = os.path.join('CIFUZZ_DIR', 'external-project')
+
+@unittest.skipIf(not os.getenv('END_TO_END_TESTS'),
+                 'END_TO_END_TESTS=1 not set')
+class EndToEndTest(unittest.TestCase):
+  """End-to-End tests for CIFuzz."""
+  def test_simple(self):
+    """Simple end-to-end test using run_cifuzz.main()."""
+    os.environ['REPOSITORY'] = 'external-project'
+    os.environ['PROJECT_SRC_PATH'] = EXTERNAL_PROJECT_PATH
+    os.environ['BUILD_INTEGRATION_PATH'] = 'cifuzz-build-integration'
+    with tempfile.TemporaryDirectory() as temp_dir:
+      os.environ['WORKSPACE'] = temp_dir
+      # This test is weird because running the fuzzer results in a crash which
+      # main returns 0 on.
+      # TODO(metzman): Verify the crash, affected fuzzers and other things.
+      self.assertEqual(run_cifuzz.main(), 0)
