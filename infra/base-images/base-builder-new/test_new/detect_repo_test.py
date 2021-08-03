a8f820cbb89b84a772b1a10bcbc57861dd3cbc6c
Asra Ali <asraa@google.com>
2021-08-03 16:12:53 -0400

    fix import
    Signed-off-by: Asra Ali <asraa@google.com>

--- infra/base-images/base-builder-new/test_new/detect_repo_test.py
+++ infra/base-images/base-builder-new/test_new/detect_repo_test.py
@@ -30,9 +30,9 @@ import detect_repo
 # Appending to path for access to repo_manager module.
 # pylint: disable=wrong-import-position
 sys.path.append(
-  os.path.dirname(
-      os.path.dirname(
-          os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 import repo_manager
 import test_repos
 # pylint: enable=wrong-import-position
