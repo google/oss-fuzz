9a08fd7cf2a98b5f6845d28c4e36184f14ff93e7
Jonathan Metzman <metzman@chromium.org>
2021-08-03 17:46:51 -0700

    fix

--- infra/build_specified_commit.py
+++ infra/build_specified_commit.py
@@ -147,7 +147,7 @@ def copy_src_from_docker(project_name, host_dir):
 @retry.wrap(_IMAGE_BUILD_TRIES, 2)
 def _build_image_with_retries(project_name):
   """Build image with retries."""
-  return helper.build_image_impl(project_name)
+  return helper.build_image_impl(helper.Project(project_name))
 
 
 def get_required_post_checkout_steps(dockerfile_path):
