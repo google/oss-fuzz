006471ea7c02ba54ba9752b89ab6ee0158b782e9
Abhishek Arya <inferno@chromium.org>
2021-08-31 19:00:48 -0700

    Fix go coverage build regression.

--- projects/protoreflect/build.sh
+++ projects/protoreflect/build.sh
@@ -17,3 +17,4 @@
 
 compile_go_fuzzer github.com/jhump/protoreflect/desc/protoparse FuzzProtoParse fuzz_protoparse
 #compile_go_fuzzer github.com/jhump/protoreflect/proto_decoder Fuzz fuzz_dynamic
+
