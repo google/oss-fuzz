ab6233dee9fe051e037f95da9127a138a4d34025
Guido Vranken <guidovranken@users.noreply.github.com>
2021-09-12 05:44:11 +0200

    [bls-signatures] Fix build (#6424)

--- projects/bls-signatures/build.sh
+++ projects/bls-signatures/build.sh
@@ -145,10 +145,13 @@ then
     fi
     cmake .. -DBUILD_BLS_PYTHON_BINDINGS=0 -DBUILD_BLS_TESTS=0 -DBUILD_BLS_BENCHMARKS=0 -DARCH=$CHIA_ARCH
     make -j$(nproc)
-    export CHIA_BLS_LIBBLS_A_PATH=$(realpath libbls.a)
+    export CHIA_BLS_LIBBLS_A_PATH=$(realpath src/libbls.a)
+    export CHIA_BLS_LIBRELIC_S_A_PATH=$(realpath _deps/relic-build/lib/librelic_s.a)
+    export CHIA_BLS_LIBSODIUM_A_PATH=$(realpath _deps/sodium-build/libsodium.a)
     export CHIA_BLS_INCLUDE_PATH=$(realpath ../src/)
     export CHIA_BLS_RELIC_INCLUDE_PATH_1=$(realpath _deps/relic-build/include/)
     export CHIA_BLS_RELIC_INCLUDE_PATH_2=$(realpath _deps/relic-src/include/)
+    export LINK_FLAGS="$LINK_FLAGS -lgmp"
     export CXXFLAGS="$CXXFLAGS -DCRYPTOFUZZ_CHIA_BLS"
 fi
 
