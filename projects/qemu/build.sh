9a9a3cea1a8406453a598abc6b9eb9dc27a67030
MarcoFalke <falke.marco@gmail.com>
2021-09-17 16:02:17 +0200

    qemu: Simplify build script (#6468)

--- projects/qemu/build.sh
+++ projects/qemu/build.sh
@@ -17,16 +17,4 @@
 
 pip3 install meson
 
-# Version of glib in Ubuntu Xenial is too old
-pushd $SRC/glib-2.67.6
-meson _build \
-    -Doss_fuzz=enabled \
-    -Dinternal_pcre=true \
-    --default-library=static \
-    -Db_lundef=false \
-    -Dlibmount=disabled
-ninja -C _build
-ninja -C _build install
-popd
-
 ./scripts/oss-fuzz/build.sh
