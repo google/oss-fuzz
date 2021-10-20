6eca9f77fd48ae34b339628b974a4fe86e116ca5
DavidKorczynski <david@adalogics.com>
2021-10-20 15:27:49 +0100

    dng_sdk: improve fuzzers (#6621)
    * dng_sdk: improve fuzzer
    
    * dng_sdk: add a validate fuzzer that is fixed so we can use seeds easily
    
    * fix build
    
    * dng_sdk: rename output validate fuzzers

--- projects/dng_sdk/build.sh
+++ projects/dng_sdk/build.sh
@@ -31,14 +31,26 @@ $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $SRC/dng_stage_fuzzer.cpp -o $OUT/dng_stage_f
 
 sed -i 's/main/main2/g' $SRC/dng_sdk/source/dng_validate.cpp
 sed -i 's/printf ("Val/\/\//g' $SRC/dng_sdk/source/dng_validate.cpp
-cat $SRC/dng_sdk/source/dng_validate.cpp $SRC/dng_validate_fuzzer.cpp >> $SRC/dng_validate_fuzzer1.cpp
+sed -i 's/static//g' $SRC/dng_sdk/source/dng_validate.cpp
+
+cat $SRC/dng_sdk/source/dng_validate.cpp $SRC/dng_validate_fuzzer.cpp >> $SRC/dng_validate_fuzzer.tmp
+mv $SRC/dng_validate_fuzzer.tmp $SRC/dng_validate_fuzzer.cpp
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE -DqDNGValidateTarget \
   $SRC/dng_sdk/source/dng_globals.cpp \
-  $SRC/dng_validate_fuzzer1.cpp \
-  -o $OUT/dng_validate_fuzzer1 \
+  $SRC/dng_validate_fuzzer.cpp \
+  -o $OUT/dng_validate_fuzzer \
+  ./libdns_sdk.a -I./ -l:libjpeg.a -lz
+
+cat $SRC/dng_sdk/source/dng_validate.cpp $SRC/dng_fixed_validate_fuzzer.cpp >> $SRC/dng_fixed_validate_fuzzer.tmp
+mv $SRC/dng_fixed_validate_fuzzer.tmp $SRC/dng_fixed_validate_fuzzer.cpp
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE -DqDNGValidateTarget \
+  $SRC/dng_sdk/source/dng_globals.cpp \
+  $SRC/dng_fixed_validate_fuzzer.cpp \
+  -o $OUT/dng_fixed_validate_fuzzer \
   ./libdns_sdk.a -I./ -l:libjpeg.a -lz
 
 # move seeds
 cd ../fuzzer/seeds/CVE_2020_9589
 zip -q $OUT/dng_parser_fuzzer_seed_corpus.zip *.dng
 zip -q $OUT/dng_stage_fuzzer_seed_corpus.zip *.dng
+zip -q $OUT/dng_fixed_validate_fuzzer_seed_corpus.zip *.dng
