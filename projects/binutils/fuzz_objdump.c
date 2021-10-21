bf02a45c354356d88f9458b5f8e955e0fc8c76d9
David Korczynski <david@adalogics.com>
2021-10-21 21:07:12 +0100

    binutils: bit more cleanup

--- projects/binutils/fuzz_objdump.c
+++ projects/binutils/fuzz_objdump.c
@@ -23,12 +23,10 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
   char filename[256];
   sprintf(filename, "/tmp/libfuzzer.%d", getpid());
-
   FILE *fp = fopen(filename, "wb");
   if (!fp) {
     return 0;
   }
-
   fwrite(data, size, 1, fp);
   fclose(fp);
 
