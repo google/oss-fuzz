6785052906ccaeaaaa7d51290326ab59eea8e6d8
Oliver Chang <ochang@google.com>
2021-11-01 14:27:13 +1100

    CFLite: Logging fixes.
    Don't output stacktrace again in detected bug log message.
    
    Disable Python buffering to avoid log interleaving issues.

--- infra/cifuzz/fuzz_target.py
+++ infra/cifuzz/fuzz_target.py
@@ -177,8 +177,7 @@ class FuzzTarget:  # pylint: disable=too-many-instance-attributes
 
       # Only report first crash.
       crash = result.crashes[0]
-      logging.info('Fuzzer: %s. Detected bug:\n%s', self.target_name,
-                   crash.stacktrace)
+      logging.info('Fuzzer: %s. Detected bug.', self.target_name)
 
       if self.is_crash_reportable(crash.input_path):
         # We found a bug in the fuzz target and we will report it.
