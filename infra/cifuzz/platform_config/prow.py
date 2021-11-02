663431560057c263a665ed2c66156ee0a4ac9923
Mitchel Herman <58443548+mpherman2@users.noreply.github.com>
2021-11-02 13:14:56 -0700

    Add Prow PlatformConfig (#6732)

--- infra/cifuzz/platform_config/prow.py
+++ infra/cifuzz/platform_config/prow.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Module for getting the configuration CIFuzz needs to run on prow."""
+import logging
+import os
+
 import platform_config
 
 # pylint: disable=too-few-public-methods
@@ -19,4 +22,33 @@ import platform_config
 
 class PlatformConfig(platform_config.BasePlatformConfig):
   """CI environment for Prow."""
-  # TODO(mpherman2) Fill this out.
+
+  @property
+  def project_src_path(self):
+    """Returns the manually checked out path of the project's source if
+    specified or the current directory if not. Prow will run ClusterfuzzLite
+    at the directory head for the repo."""
+    project_src_path = os.getenv('PROJECT_SRC_PATH', os.getcwd())
+    logging.debug('PROJECT_SRC_PATH: %s.', project_src_path)
+    return project_src_path
+
+  @property
+  def workspace(self):
+    """Returns the workspace."""
+    # Let Prow user override workspace, but default to using artifacts dir
+    return os.getenv('WORKSPACE', os.getenv('ARTIFACTS'))
+
+  @property
+  def base_ref(self):
+    """Returns the base branch to diff against (pr fuzzing)."""
+    return os.getenv('PULL_BASE_REF')
+
+  @property
+  def project_repo_name(self):
+    """Returns the project repo name."""
+    return os.getenv('REPO_NAME')
+
+  @property
+  def base_commit(self):
+    """Returns the base commit to diff against (commit fuzzing)."""
+    return os.getenv('PULL_BASE_SHA')
