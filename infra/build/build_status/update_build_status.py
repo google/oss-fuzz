560f5274a2bfee109836cd6f475e2a13fe15a7bb
Navid Emamdoost <navid.emamdoost@gmail.com>
2021-11-04 16:23:03 -0700

    fix copyright year

--- infra/build/build_status/update_build_status.py
+++ infra/build/build_status/update_build_status.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google Inc.
+# Copyright 2021 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -291,5 +291,5 @@ def main():
   update_badges()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
