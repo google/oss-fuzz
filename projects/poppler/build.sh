baa79e1e6d67fc015d8fd85e96f438bc70e12aa8
tsdgeos <aacid@kde.org>
2021-11-07 22:41:41 +0100

    poppler: Fix build (#6793)

--- projects/poppler/build.sh
+++ projects/poppler/build.sh
@@ -68,6 +68,8 @@ if [ "$SANITIZER" != "memory" ]; then
     popd
 
     pushd $SRC/glib-2.70.0
+    # remove once there's a released glib that contains https://gitlab.gnome.org/GNOME/glib/-/merge_requests/2324
+    sed -i s#https://ftp.pcre.org/pub/pcre/pcre-8.37.tar.bz2#https://sourceforge.net/projects/pcre/files/pcre/8.37/pcre-8.37.tar.bz2# subprojects/libpcre.wrap
     meson \
         --prefix=$PREFIX \
         --libdir=lib \
