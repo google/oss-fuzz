bcfd36c3a1ae94f90a48909e41d16c7a50eb117b
DavidKorczynski <david@adalogics.com>
2021-11-18 16:03:30 +0000

    rustls: fix coverage build (#6852)
    * rustls: fix coverage build
    
    * rustls: copy over the py script

--- projects/rustls/rustc.py
+++ projects/rustls/rustc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,22 +13,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder-rust
-RUN apt-get update && apt-get install -y make autoconf automake libtool curl cmake python llvm-dev libclang-dev clang
-RUN apt-get --yes update \
-   && apt-get install --no-install-recommends --yes \
-   libssl-dev \
-   pkg-config \
-   python \
-   && apt-get clean \
-   && rm --recursive --force /var/lib/apt/lists/*
+import sys
+import subprocess
 
-RUN git clone https://github.com/ctz/rustls
-
-WORKDIR $SRC
-
-COPY build.sh $SRC/
-COPY *.py $SRC/
+#Disable coverage for troubling crates.
+sys.argv[0] = "rustc"
+if "tokio_util" in sys.argv or "hyper" in sys.argv:
+    try:
+        sys.argv.remove("-Zinstrument-coverage")
+    except:
+        pass
+    print(sys.argv)
+subprocess.call(sys.argv)
