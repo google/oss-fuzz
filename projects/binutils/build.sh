c09129596db799a8269dfeefbde17d423f506d76
David Korczynski <david@adalogics.com>
2021-11-24 16:16:33 +0000

    binutils: no longer restrict afl build

--- projects/binutils/build.sh
+++ projects/binutils/build.sh
@@ -15,10 +15,6 @@
 #
 ################################################################################
 
-export AFL_LLVM_INSTRUMENT=CLASSIC,CTX-2
-export AFL_ENABLE_CMPLOG=0
-export AFL_LAF_CHANCE=30
-
 # build project
 if [ "$SANITIZER" = undefined ]; then
     export CFLAGS="$CFLAGS -fno-sanitize=unsigned-integer-overflow"
