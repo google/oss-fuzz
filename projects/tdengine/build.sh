ff13be0c11188814bdc5a248b30c59903bbb003c
David Korczynski <david@adalogics.com>
2021-12-01 14:21:06 +0000

    tdengine: fix cov

--- projects/tdengine/build.sh
+++ projects/tdengine/build.sh
@@ -24,6 +24,10 @@ sed -i 's/-Werror//g' ./cmake/define.inc
 mkdir debug && cd debug
 export LDFLAGS="${CXXFLAGS}"
 
+if [[ $SANITIZER = *coverage* ]]; then
+  ln -f -s /usr/bin/gold /usr/bin/ld
+fi
+
 cmake -DBUILD_HTTP=true ..
 cmake --build .
 
