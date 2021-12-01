0bed7945b38dfd4173bc4faa6ed59fc1c4c2815f
DavidKorczynski <david@adalogics.com>
2021-12-01 23:01:01 +0000

    unbound: fix new fuzzer names (#6842)
    * unbound: fix new fuzzer names
    
    * unbound: fix license headers

--- projects/unbound/fuzz_1.c
+++ projects/unbound/fuzz_1.c
@@ -1,3 +1,15 @@
+/* Copyright 2021 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 /*
  * unbound-fuzzme.c - parse a packet provided on stdin (for fuzzing).
  *
@@ -29,7 +41,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len) {
   if (parse_packet(pkt, &msg, reg) != LDNS_RCODE_NOERROR) {    
     goto out;
   }
-  if (parse_extract_edns(&msg, &edns, reg) != LDNS_RCODE_NOERROR) {
+  if (parse_extract_edns_from_response_msg(&msg, &edns, reg) != LDNS_RCODE_NOERROR) {
     goto out;
   }
 
