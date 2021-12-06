79e5dc3d77cd64d04031c7054d85bdc7961bef27
MarcoFalke <falke.marco@gmail.com>
2021-12-06 16:24:41 +0100

    [base-clang] Change OUR_LLVM_REVISION (#6970)
    * [base-clang] Change OUR_LLVM_REVISION
    
    * llvmorg-14-init-8033-gabb2a91b

--- infra/base-images/base-clang/checkout_build_install_llvm.sh
+++ infra/base-images/base-clang/checkout_build_install_llvm.sh
@@ -70,7 +70,7 @@ cd clang
 LLVM_SRC=$SRC/llvm-project
 
 # For manual bumping.
-OUR_LLVM_REVISION==llvmorg-14-init-11072-gb1bc627e
+OUR_LLVM_REVISION=llvmorg-14-init-8033-gabb2a91b
 
 # To allow for manual downgrades. Set to 0 to use Chrome's clang version (i.e.
 # *not* force a manual downgrade). Set to 1 to force a manual downgrade.
