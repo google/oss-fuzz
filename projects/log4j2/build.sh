ab37bdf8cec4f7ca895e89446a85b577cdb05598
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2021-12-15 19:44:51 +0100

    Attempt to fix Maven build

--- projects/log4j2/build.sh
+++ projects/log4j2/build.sh
@@ -15,14 +15,15 @@
 #
 ################################################################################
 
-MAVEN_ARGS="-DskipTests -Djavac.src.version=15 -Djavac.target.version=15 --no-transfer-progress"
-$MVN package $MAVEN_ARGS
+MAVEN_ARGS="-DskipTests --no-transfer-progress"
+$MVN package -pl log4j-api,log4j-plugins,log4j-core $MAVEN_ARGS
 CURRENT_VERSION=$($MVN org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate \
  -Dexpression=project.version -q -DforceStdout)
 cp "log4j-core/target/log4j-core-$CURRENT_VERSION.jar" $OUT/log4j-core.jar
 cp "log4j-api/target/log4j-api-$CURRENT_VERSION.jar" $OUT/log4j-api.jar
+cp "log4j-plugins/target/log4j-plugins-$CURRENT_VERSION.jar" $OUT/log4j-plugins.jar
 
-ALL_JARS="log4j-core.jar log4j-api.jar"
+ALL_JARS="log4j-core.jar log4j-api.jar log4j-plugins.jar"
 
 # The classpath at build-time includes the project jars in $OUT as well as the
 # Jazzer API. Additionally, include $OUT itself to pick up
