70b05bb7a84fb3915441dabb461c22629517a04b
David Korczynski <david@adalogics.com>
2021-12-28 21:55:38 +0000

    radare2: fix build

--- projects/radare2/build.sh
+++ projects/radare2/build.sh
@@ -2,12 +2,16 @@
 
 export USERCC=$CC
 export HOST_CC=$CC
+export NOLTO=1
 
-sys/static.sh
+sed 's/gcc-ar/llvm-ar/g' -i sys/static.sh
+sys/static.sh || true
 cp -r r2-static $OUT/
 
 cp -r ../radare2-fuzz/targets .
 export RADARE2_STATIC_BUILD=$OUT/r2-static
+
+export CXXFLAGS="${CXXFLAGS} -I ${RADARE2_STATIC_BUILD}/usr/include/libr/sdb"
 
 cd targets 
 make
