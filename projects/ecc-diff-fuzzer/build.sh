e776c0597ba395e07f9f533ca5f4bb3c9b3d1ab7
Philippe Antoine <contact@catenacyber.fr>
2022-01-05 15:29:04 +0100

    ecc-diff-fuzzer: updates gmp version

--- projects/ecc-diff-fuzzer/build.sh
+++ projects/ecc-diff-fuzzer/build.sh
@@ -19,8 +19,8 @@
 #nettle
 (
 cd nettle
-tar -xvf ../gmp-6.1.2.tar.bz2
-cd gmp-6.1.2
+tar -xvf ../gmp-6.2.1.tar.bz2
+cd gmp-6.2.1
 #do not use assembly instructions as we do not know if they will be available on the machine who will run the fuzzer
 #we could do instead --enable-fat
 ./configure --disable-shared --disable-assembly
