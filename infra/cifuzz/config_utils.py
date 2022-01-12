6a13aa75ccd474434eced707e18d8b73c20fc1dd
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2022-01-12 13:10:03 -0500

    [CFL] minimize crashes (#7120)
    * cifuzz: minimize crashes
    
    * lnt
    
    * fix typo

--- infra/cifuzz/config_utils.py
+++ infra/cifuzz/config_utils.py
@@ -198,6 +198,10 @@ class RunFuzzersConfig(BaseConfig):
     self.report_unreproducible_crashes = environment.get_bool(
         'REPORT_UNREPRODUCIBLE_CRASHES', False)
 
+    self.minimize_crashes = environment.get_bool('MINIMIZE_CRASHES', False)
+    if self.mode == 'batch':
+      logging.warning(
+          'Minimizing crashes reduces fuzzing time in batch fuzzing.')
     self.report_timeouts = environment.get_bool('REPORT_TIMEOUTS', False)
     self.report_ooms = environment.get_bool('REPORT_OOMS', True)
     self.upload_all_crashes = environment.get_bool('UPLOAD_ALL_CRASHES', False)
