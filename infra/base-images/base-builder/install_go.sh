5306b4148eb9e666e4f1b80f313d59fadfc3e129
Alan32Liu <donggeliu@google.com>
2022-01-27 09:29:35 +1100

    Integrating CodeIntelligenceTesting Go

--- infra/base-images/base-builder/install_go.sh
+++ infra/base-images/base-builder/install_go.sh
@@ -15,14 +15,54 @@
 #
 ################################################################################
 
-cd /tmp
-curl -O https://storage.googleapis.com/golang/getgo/installer_linux
-chmod +x ./installer_linux
-SHELL="bash" ./installer_linux
-rm -rf ./installer_linux
+# Install Go1.4 to bootstrap
+GOBOOTSTRAP="/usr/local/go1.4-bootstrap"
+mkdir "$GOBOOTSTRAP"
+curl -L https://dl.google.com/go/go1.4-bootstrap-20171003.tar.gz | tar -xzf - --directory="$GOBOOTSTRAP" --strip=1
+cd "$GOBOOTSTRAP/src"
+CGO_ENABLED=0
+#go version  # Confirm Go is not installed
+./make.bash
+export PATH=$PATH:"$GOBOOTSTRAP/bin"
+go version  # go version go1.4-bootstrap-20170531 linux/amd64
 
-echo 'Set "GOPATH=/root/go"'
-echo 'Set "PATH=$PATH:/root/.go/bin:$GOPATH/bin"'
+# Install CodeIntelligenceTesting Go
+cd "/usr/local/"
+git clone https://github.com/CodeIntelligenceTesting/go.git go-exp
+cd "go-exp/src"
+git checkout dev.libfuzzer.18
+# Disable tests, which fails
+sed -i '/^exec .* tool dist test -rebuild "$@"/ s/./#&/' run.bash
+./all.bash
+GOEXPPATH="/usr/local/go-exp"
+export PATH="$GOEXPPATH/bin":$PATH
+mkdir -p "/root/go/bin"
+ln -s "$GOEXPPATH/bin/go" "/root/go/bin/go"
+ln -s "$GOEXPPATH/bin/gofmt" "/root/go/bin/gofmt"
+go version  # Verify Go version
 
 go get -u github.com/mdempsky/go114-fuzz-build
-ln -s $GOPATH/bin/go114-fuzz-build $GOPATH/bin/go-fuzz
+ln -s "$GOEXPPATH/bin/go114-fuzz-build" "/root/go/bin/go-fuzz"
+
+# Cleanup
+rm -rf $GOBOOTSTRAP
+
+echo 'Set "GOPATH=/root/go"'
+echo 'Set "PATH=/root/go/bin:$PATH:/root/.go/bin"'
+
+#GOPATH="/root/go"
+#echo 'export PATH="/root/go/bin":$PATH:"/root/.go/bin"' >> ~/.bashrc
+#source ~/.bashrc
+
+
+#cd /tmp
+#curl -O https://storage.googleapis.com/golang/getgo/installer_linux
+#chmod +x ./installer_linux
+#SHELL="bash" ./installer_linux
+#rm -rf ./installer_linux
+
+#echo 'Set "GOPATH=/root/go"'
+#echo 'Set "PATH=$PATH:/root/.go/bin:$GOPATH/bin"'
+
+#go get -u github.com/mdempsky/go114-fuzz-build
+#ln -s $GOPATH/bin/go114-fuzz-build $GOPATH/bin/go-fuzz
