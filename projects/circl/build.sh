d2f3bf6e310e97665d6830a2a6c455f7cbfd8431
Guido Vranken <guidovranken@gmail.com>
2022-02-14 11:00:13 +0100

    [circl] Enable more operations

--- projects/circl/build.sh
+++ projects/circl/build.sh
@@ -43,9 +43,12 @@ echo -n "BLS_G1_Add," >>extra_options.h
 echo -n "BLS_G1_Mul," >>extra_options.h
 echo -n "BLS_G1_Neg," >>extra_options.h
 echo -n "BLS_IsG1OnCurve," >>extra_options.h
+echo -n "BLS_HashToG1," >>extra_options.h
 echo -n "BLS_G2_Add," >>extra_options.h
 echo -n "BLS_G2_Mul," >>extra_options.h
 echo -n "BLS_G2_Neg," >>extra_options.h
+echo -n "BLS_IsG2OnCurve," >>extra_options.h
+echo -n "BLS_HashToG2," >>extra_options.h
 echo -n "BLS_Compress_G1," >>extra_options.h
 echo -n "BLS_Decompress_G1," >>extra_options.h
 echo -n "BLS_Pairing," >>extra_options.h
