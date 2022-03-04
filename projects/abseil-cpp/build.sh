712e9d9e2594a4fbe157d9569b89401c207eedb6
Navid Emamdoost <navid.emamdoost@gmail.com>
2022-03-03 19:07:06 -0800

    Changing bazel flags extraction

--- projects/abseil-cpp/build.sh
+++ projects/abseil-cpp/build.sh
@@ -14,21 +14,7 @@
 #
 ################################################################################
 
-readonly EXTRA_BAZEL_FLAGS="$(
-for f in ${CFLAGS}; do
-  echo "--conlyopt=${f}" "--linkopt=${f}"
-done
-for f in ${CXXFLAGS}; do
-  echo "--cxxopt=${f}" "--linkopt=${f}"
-done
-
-if [ "$SANITIZER" = "undefined" ]
-then
-  # Bazel uses clang to link binary, which does not link clang_rt ubsan library for C++ automatically.
-  # See issue: https://github.com/bazelbuild/bazel/issues/8777
-  echo "--linkopt=\"$(find $(llvm-config --libdir) -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1)\""
-fi
-)"
+readonly EXTRA_BAZEL_FLAGS="$(source collect_bazel_extra_flags)"
 
 declare FUZZ_TARGETS=("string_escape_fuzzer" "string_utilities_fuzzer")
 
