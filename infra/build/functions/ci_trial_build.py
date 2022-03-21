830da8e7b37b5d14c837fca0878851e250cfc62a
Jonathan Metzman <metzman@chromium.org>
2022-03-21 13:52:38 -0700

    fmt

--- infra/build/functions/ci_trial_build.py
+++ infra/build/functions/ci_trial_build.py
@@ -7,12 +7,14 @@ import trial_build
 
 TRIGGER_COMMAND = '/gcbrun '
 
+
 def get_comments(pull_request_number):
   url = ('https://api.github.com/repos/google/oss-fuzz/pulls/'
          f'{pull_request_number}/comments')
   # !!! Does this handle pagination?
   request = requests.get(url)
   return request.json()
+
 
 def get_latest_gcbrun_command(comments):
   for comment in reversed(comments):
@@ -26,6 +28,7 @@ def get_latest_gcbrun_command(comments):
     return body[len(TRIGGER_COMMAND):]
   return None
 
+
 def exec_command_from_github(pull_request_number):
   comments = get_comments(pull_request_number)
   command = get_latest_gcbrun_command(comments)
@@ -35,10 +38,12 @@ def exec_command_from_github(pull_request_number):
     return None
   return trial_build.trial_build_main(command)
 
+
 def main():
   logging.basicConfig(level=logging.INFO)
   pull_request_number = os.environ['PULL_REQUEST_NUMBER']
   return 0 if exec_command_from_github(pull_request_number) else 1
+
 
 if __name__ == '__main__':
   main()
