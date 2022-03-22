899e4c870e214c013c15f3a7a0087319a705e847
Navid Emamdoost <navid.emamdoost@gmail.com>
2022-03-22 16:33:41 -0700

    Replace use of sed with docker tag

--- infra/build/functions/base_images.py
+++ infra/build/functions/base_images.py
@@ -78,15 +78,18 @@ def _get_introspector_base_images_steps(images, tag_prefix=TAG_PREFIX):
       'name': 'gcr.io/cloud-builders/git',
   }]
 
-  steps.extend([{
+  steps.append({
+      'name': 'gcr.io/cloud-builders/docker',
+      'args': ['pull', 'gcr.io/oss-fuzz-base/base-clang:introspector'],
+  })
+  steps.append({
       'name':
-          'gcr.io/oss-fuzz-base/base-runner',
+          'gcr.io/cloud-builders/docker',
       'args': [
-          'bash', '-c',
-          (f'sed -i s/base-clang.*/base-clang:{INTROSPECTOR_TAG}/g'
-           ' oss-fuzz/infra/base-images/base-builder/Dockerfile')
-      ]
-  }])
+          'tag', 'gcr.io/oss-fuzz-base/base-clang:introspector',
+          'gcr.io/oss-fuzz-base/base-clang:latest'
+      ],
+  })
 
   for base_image in images:
     image = tag_prefix + base_image
