8258f48dd2c591d12396f4029cc3d66f82acaaeb
Jonathan Metzman <metzman@chromium.org>
2022-03-24 10:54:19 -0700

    fix

--- infra/ci/build.py
+++ infra/ci/build.py
@@ -124,15 +124,18 @@ def should_build_coverage(project_yaml):
 
 def flatten_options(option_list):
   """Generator that flattens |option_list| (a list of sanitizers, architectures
-  or fuzzing engines) by yielding each element in the list that isn't a
-  dictionary. For elements that are dictionaries, the sole key is yielded."""
+  or fuzzing engines) by returning each element in the list that isn't a
+  dictionary. For elements that are dictionaries, the sole key is returned."""
+  result = []
   for option in option_list:
     if isinstance(option, dict):
       keys = list(option.keys())
       assert len(keys) == 1
-      yield keys[0]
+      result.append(keys[0])
       continue
-    yield option
+    result.append(option)
+  print(result)
+  return result
 
 
 def should_build(project_yaml):
@@ -263,10 +266,10 @@ def main():
   """Build modified projects or canary project."""
   os.environ['OSS_FUZZ_CI'] = '1'
   infra_changed = is_infra_changed()
-  # if infra_changed:
-  #   print('Pulling and building base images first.')
-  #   if build_base_images():
-  #     return 1
+  if infra_changed:
+    print('Pulling and building base images first.')
+    if build_base_images():
+      return 1
 
   result = build_modified_projects()
   if result == BuildModifiedProjectsResult.BUILD_FAIL:
