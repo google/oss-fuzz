45b4b857795b86f5473d310ade942075ab102f3a
Jakub Sztandera <kubuxu@protonmail.ch>
2022-03-26 22:46:14 +0100

    [filecoin-ref-fvm] Initial integration
    This commit adds initial integration of the ref-fvm, which is a new coponent
    for Lotus (which is already part of OSS-Fuzz) written in Rust (vs Lotus
    in Go).
    
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

---
+++ projects/filecoin-ref-fvm/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -eu
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd "$SRC"
+
+declare -a PROJECTS=(amt hamt)
+declare -A PROJECT_PATHS=(
+	[amt]="ref-fvm/ipld/amt/fuzz"
+	[hamt]="ref-fvm/ipld/hamt/fuzz"
+)
+
+export CARGO_TARGET_DIR="$SRC/target"
+FUZZ_TARGET_OUTPUT_DIR="$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/release"
+
+
+for project in "${PROJECTS[@]}"; do
+	pushd "${PROJECT_PATHS[$project]}"
+
+	cargo +nightly fuzz build -O --debug-assertions
+
+	for f in fuzz_targets/*.rs; do
+		FUZZ_TARGET_NAME=$(basename "${f%.*}")
+		FUZZ_OUT_NAME="${project}_${FUZZ_TARGET_NAME}"
+
+		cp "$FUZZ_TARGET_OUTPUT_DIR/$FUZZ_TARGET_NAME" "$OUT/$FUZZ_OUT_NAME"
+		CORPUS_LOC="$SRC/corpus/corpus/$FUZZ_OUT_NAME"
+		if [ -d "$CORPUS_LOC" ]; then
+    		zip -j "$OUT/${FUZZ_OUT_NAME}_seed_corpus.zip" "$CORPUS_LOC/"*
+		fi
+	done
+
+	popd
+done
+
+exit 0
