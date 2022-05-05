97ad471f06204756e9d1b2cf9b727d90fe93bda4
DavidKorczynski <david@adalogics.com>
2022-05-05 12:18:32 +0100

    pyxdg: extend fuzzer and add corpus+dict (#7674)
    * pyxdg: extend fuzzer and add corpus+dict
    
    The extension is mainly to force xml checking before its passed into the
    pyxdg library. The problem I had was any code in xml would not be
    instrumented, so coverage could not be used to explore correct xml
    files. I did a minor guard to use lxml instead to check xml validity, as
    this could be instrumented proper. Gains are significant.
    
    * try to fix fuzzer not part of pyxdg
    
    * nit
    
    * nit
    
    * fix build failure

--- projects/pyxdg/fuzz_menu.py
+++ projects/pyxdg/fuzz_menu.py
@@ -18,18 +18,42 @@ import sys
 import atheris
 
 with atheris.instrument_imports(enable_loader_override=False):
+  from lxml import etree as et
+  import xml.etree.ElementTree as etree
+
+  #import etree
   import xdg.Menu
   from xdg.Exceptions import ParsingError
 
 @atheris.instrument_func
+def verify_xml(data):
+  try:
+    root = et.XML(data)
+    if root != None:
+      et.indent(root)
+      et.ElementTree(root)
+      return True
+  except et.XMLSyntaxError:
+    return False
+
+@atheris.instrument_func
 def TestOneInput(input_bytes):
+  # This is a slight hack to verify xml with code
+  # that gets instrumented. For some reason I was
+  # unable to instrument xml itself.
+  if not verify_xml(input_bytes):
+    return
+
   # We need to make the file an absolute path
   testfile_path = os.path.join(os.getcwd(), "test.menu")
   with open(testfile_path, "wb") as f:
     f.write(input_bytes)
+
   try:
     xdg.Menu.parse(filename = testfile_path)
   except ParsingError:
+    None
+  except RecursionError: # Just catch it because it's not that interesting.
     None
   os.remove(testfile_path)
 
