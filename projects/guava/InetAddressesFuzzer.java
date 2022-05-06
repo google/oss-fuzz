a9d7c011c20c7676b9ff3366419ffe69a818ea66
psy <patrice.salathe@code-intelligence.com>
2022-05-06 15:18:45 +0200

    Initial integration

---
+++ projects/guava/InetAddressesFuzzer.java
@@ -0,0 +1,28 @@
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueHigh;
+
+import com.google.common.net.*;
+import java.lang.IllegalArgumentException;
+import java.net.InetAddress;
+
+public class InetAddressesFuzzer {
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+	    String value = data.consumeRemainingAsString();
+        InetAddress addr;
+
+        try {
+            InetAddresses.isInetAddress(value);
+            InetAddresses.isUriInetAddress(value);
+            InetAddresses.isMappedIPv4Address(value);
+            InetAddresses.forUriString(value);
+            InetAddresses.isMappedIPv4Address(value);
+
+            addr = InetAddresses.forString(value);
+            InetAddresses.toUriString(addr);
+            InetAddresses.toAddrString(addr);
+            InetAddresses.increment(addr);
+            InetAddresses.decrement(addr);
+
+        } catch (IllegalArgumentException e) { }
+	} 
+}
---
+++ projects/guava/MediaTypeFuzzer.java
@@ -0,0 +1,17 @@
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueHigh;
+
+import com.google.common.net.*;
+import java.lang.IllegalArgumentException;
+
+public class MediaTypeFuzzer {
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+	    String value = data.consumeRemainingAsString();
+
+        try { 
+            MediaType.create(value, value);
+            MediaType.parse(value).type();
+            MediaType.create(value, value).withParameter(value, value);
+        } catch (IllegalArgumentException e) { }
+	} 
+}
