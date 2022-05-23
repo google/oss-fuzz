6e0713360e045861584c120c5e3b9b9e1c783160
psy <patrice.salathe@code-intelligence.com>
2022-05-23 11:55:41 +0200

    Initial integration rebasing

--- projects/spring-boot-actuator/SanitizerFuzzer.java
+++ projects/spring-boot-actuator/SanitizerFuzzer.java
@@ -1,5 +1,9 @@
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+<<<<<<< HEAD
 import com.code_intelligence.jazzer.api.FuzzerSecurityIssueMedium;
+=======
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueHigh;
+>>>>>>> 0d4261be (Initial integration)
 import org.springframework.boot.actuate.endpoint.Sanitizer;
 
 public class SanitizerFuzzer {
@@ -7,15 +11,24 @@ public class SanitizerFuzzer {
         String key = data.consumeString(50);
         String value = data.consumeRemainingAsString();
 
+<<<<<<< HEAD
         if (value.isEmpty() || key.isEmpty()) {
+=======
+        if (value.isEmpty()) {
+>>>>>>> 0d4261be (Initial integration)
             return;
         }
 
         Sanitizer sanitizer = new Sanitizer();
         sanitizer.keysToSanitize(key);
         String result = (String) sanitizer.sanitize(key, value);
+<<<<<<< HEAD
         if (!result.equals("******")) {
             throw new FuzzerSecurityIssueMedium("Value not sanitized. key: " + key + " value:" + value + " result:" + result);
+=======
+        if (result != "******") {
+            throw new FuzzerSecurityIssueHigh("Value not sanitized. key: " + key + " value:" + value + " result:" + result);
+>>>>>>> 0d4261be (Initial integration)
         }
     } 
 }
