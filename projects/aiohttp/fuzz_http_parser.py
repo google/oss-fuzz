1401a15df40b8db29b88afd36799be813d54dad5
David Korczynski <david@adalogics.com>
2022-05-25 13:23:24 +0100

    aiohttp: enable analysis of Pure python code
    This is in addition to the existing fuzzers that analyse compiled code.

--- projects/aiohttp/fuzz_http_parser.py
+++ projects/aiohttp/fuzz_http_parser.py
@@ -13,6 +13,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+AIOHTTP_VAL=0
+os.environ["AIOHTTP_NO_EXTENSIONS"] = str(AIOHTTP_VAL)
+
 import sys
 import atheris
 
@@ -30,6 +35,7 @@ def TestOneInput(data):
     h_p = aiohttp.http_parser.HttpRequestParser(pr, loop, 32768)
     try:
         h_p.feed_data(data)
+        h_p.feed_eof()
     except aiohttp.http_exceptions.HttpProcessingError:
         None
 
