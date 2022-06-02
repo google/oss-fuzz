3c1ae025ce35ffb5737e60a92b77ca229176fa11
Alan32Liu <donggeliu@google.com>
2022-06-02 18:52:36 +1000

    clang-format

--- infra/experimental/sanitizers/ExecSan/execSan.cpp
+++ infra/experimental/sanitizers/ExecSan/execSan.cpp
@@ -17,45 +17,46 @@
 
 /* C standard library */
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
-#include <signal.h>
 
 /* POSIX */
-#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/user.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 /* Linux */
-#include <syscall.h>
 #include <sys/ptrace.h>
+#include <syscall.h>
 
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <map>
-#include <vector>
 #include <set>
 #include <sstream>
-#include <iostream>
+#include <string>
+#include <vector>
 
 #define DEBUG_LOGS 0
 
 #if DEBUG_LOGS
-#define debug_log(...) \
-  do { \
-    fprintf(stderr, __VA_ARGS__); fflush(stdout); \
-    fputc('\n', stderr); \
+#define debug_log(...)            \
+  do {                            \
+    fprintf(stderr, __VA_ARGS__); \
+    fflush(stdout);               \
+    fputc('\n', stderr);          \
   } while (0)
 #else
 #define debug_log(...)
 #endif
 
-#define fatal_log(...) \
-  do { \
+#define fatal_log(...)            \
+  do {                            \
     fprintf(stderr, __VA_ARGS__); \
-    fputc('\n', stderr); \
-    exit(EXIT_FAILURE); \
+    fputc('\n', stderr);          \
+    exit(EXIT_FAILURE);           \
   } while (0)
 
 // The magic string that we'll use to detect full control over the command
@@ -76,63 +77,53 @@ constexpr int kErrorMessageLength = 100;
 
 // Syntax error messages of each shell.
 const std::map<std::string, std::set<std::string>> kShellSyntaxErrors = {
-  {
-    "sh",
-    {
-      ": command not found",  // General
-      ": syntax error",       // Unfinished " or ' or ` or if, leading | or ;
-      ": missing"             // Unfinished [
-      ": event not found",    // ! leads large numbers
-      ": No such file",       // Leading < or /
-    }
-  },
-  {
-    "bash",
-    {
-      ": command not found",  // General
-      ": syntax error",       // Unfinished " or ' or ` or if, leading | or ;
-      ": missing"             // Unfinished [
-      ": event not found",    // ! leads large numbers
-      ": No such file",       // Leading < or /
-    }
-  },
-  {
-    "csh",
-    {
-      ": Command not found",        // General
-      "Unmatched",                  // Unfinished " or ' or `, leading ;
-      ": Missing",                  // Unfinished {
-      "Too many",                   // Unfinished (
-      "No match",                   // Leading [
-      "Invalid null command",       // Leading | or < or >
-      "Missing name for redirect"   // Single < or >
-      ": No match",                 // Leading ? or [ or *
-      "Modifier failed",            // Leading ^
-      ": No such job",              // Leading %
-      ": Undefined variable",       // Containing $
-      ": Event not found",          // ! leads large numbers
-    }
-  },
-  {
-    "dash",
-    {
-      ": not found",     // General
-      ": Syntax error",  // Unfinished " or ' or ` or if, leading | or ; or &
-      ": missing"        // Unfinished [
-      ": No such file",  // Leading <
-    }
-  },
-  {
-    "zsh",
-    {
-      ": command not found",               // General
-      ": syntax error",                    // Unfinished " or ' or `
-      " expected"                          // Unfinished [
-      ": no such file or directory",       // Leading < or /
-      ": parse error",                     // Leading |, or &
-      ": no such user or named directory", // Leading ~
-    }
-  },
+    {"sh",
+     {
+         ": command not found",  // General
+         ": syntax error",       // Unfinished " or ' or ` or if, leading | or ;
+         ": missing"             // Unfinished [
+         ": event not found",    // ! leads large numbers
+         ": No such file",       // Leading < or /
+     }},
+    {"bash",
+     {
+         ": command not found",  // General
+         ": syntax error",       // Unfinished " or ' or ` or if, leading | or ;
+         ": missing"             // Unfinished [
+         ": event not found",    // ! leads large numbers
+         ": No such file",       // Leading < or /
+     }},
+    {"csh",
+     {
+         ": Command not found",       // General
+         "Unmatched",                 // Unfinished " or ' or `, leading ;
+         ": Missing",                 // Unfinished {
+         "Too many",                  // Unfinished (
+         "No match",                  // Leading [
+         "Invalid null command",      // Leading | or < or >
+         "Missing name for redirect"  // Single < or >
+         ": No match",                // Leading ? or [ or *
+         "Modifier failed",           // Leading ^
+         ": No such job",             // Leading %
+         ": Undefined variable",      // Containing $
+         ": Event not found",         // ! leads large numbers
+     }},
+    {"dash",
+     {
+         ": not found",     // General
+         ": Syntax error",  // Unfinished " or ' or ` or if, leading | or ; or &
+         ": missing"        // Unfinished [
+         ": No such file",  // Leading <
+     }},
+    {"zsh",
+     {
+         ": command not found",                // General
+         ": syntax error",                     // Unfinished " or ' or `
+         " expected"                           // Unfinished [
+         ": no such file or directory",        // Leading < or /
+         ": parse error",                      // Leading |, or &
+         ": no such user or named directory",  // Leading ~
+     }},
 };
 
 // Shells used by Processes.
@@ -159,7 +150,8 @@ pid_t run_child(char **argv) {
   return pid;
 }
 
-std::vector<std::byte> read_memory(pid_t pid, unsigned long long address, size_t size) {
+std::vector<std::byte> read_memory(pid_t pid, unsigned long long address,
+                                   size_t size) {
   std::vector<std::byte> memory;
 
   for (size_t i = 0; i < size; i += sizeof(long)) {
@@ -168,8 +160,8 @@ std::vector<std::byte> read_memory(pid_t pid, unsigned long long address, size_t
       return memory;
     }
 
-    std::byte *word_bytes = reinterpret_cast<std::byte*>(&word);
-    memory.insert(memory.end(), word_bytes, word_bytes+sizeof(long));
+    std::byte *word_bytes = reinterpret_cast<std::byte *>(&word);
+    memory.insert(memory.end(), word_bytes, word_bytes + sizeof(long));
   }
 
   return memory;
@@ -182,8 +174,8 @@ std::string read_string(pid_t pid, unsigned long reg, unsigned long length) {
     return "";
   }
 
-  std::string content(reinterpret_cast<char*>(
-        memory.data()), std::min(memory.size(), length));
+  std::string content(reinterpret_cast<char *>(memory.data()),
+                      std::min(memory.size(), length));
   return content;
 }
 
@@ -221,9 +213,11 @@ std::string get_pathname(pid_t pid, const user_regs_struct &regs) {
 
 std::string match_shell(std::string binary_pathname) {
   // Identify the name of the shell used in the pathname.
-  for (auto it = kShellSyntaxErrors.begin(); it != kShellSyntaxErrors.end(); ++it) {
+  for (auto it = kShellSyntaxErrors.begin(); it != kShellSyntaxErrors.end();
+       ++it) {
     std::string known_shell = it->first;
-    std::string binary_name = binary_pathname.substr(binary_pathname.find_last_of("/") + 1, known_shell.length());
+    std::string binary_name = binary_pathname.substr(
+        binary_pathname.find_last_of("/") + 1, known_shell.length());
     if (binary_name == known_shell) {
       debug_log("Matched %s\n", binary_name.c_str());
       return known_shell;
@@ -243,14 +237,16 @@ std::string get_shell(pid_t pid, const user_regs_struct &regs) {
 
 void match_error_pattern(std::string buffer, std::string shell) {
   auto error_patterns = kShellSyntaxErrors.at(shell);
-  for(auto it = error_patterns.begin(); it != error_patterns.end(); ++it) {
+  for (auto it = error_patterns.begin(); it != error_patterns.end(); ++it) {
     debug_log("Pattern : %s\n", it->c_str());
     debug_log("Found at: %lu\n", buffer.find(*it));
-    if(buffer.find(*it) != std::string::npos) {
+    if (buffer.find(*it) != std::string::npos) {
       buffer = buffer.substr(0, buffer.find("\n"));
-      printf("--- Found a sign of shell corruption ---\n"
-             "%s\n"
-             "----------------------------------------\n", buffer.c_str());
+      printf(
+          "--- Found a sign of shell corruption ---\n"
+          "%s\n"
+          "----------------------------------------\n",
+          buffer.c_str());
       report_bug(kCorruptionError);
     }
   }
@@ -289,7 +285,7 @@ void trace(std::map<pid_t, Tracee> pids) {
       debug_log("finished waiting %d", pid);
 
       if (WIFEXITED(status) || WIFSIGNALED(status)) {
-        debug_log("%d exited", pid); 
+        debug_log("%d exited", pid);
         it = pids.erase(it);
         // Remove pid from the watchlist when it exits
         g_shell_pids.erase(pid);
@@ -297,7 +293,8 @@ void trace(std::map<pid_t, Tracee> pids) {
       }
 
       // ptrace sets 0x80 for syscalls (with PTRACE_O_TRACESYSGOOD set).
-      bool is_syscall = WIFSTOPPED(status) && WSTOPSIG(status) == (SIGTRAP | 0x80);
+      bool is_syscall =
+          WIFSTOPPED(status) && WSTOPSIG(status) == (SIGTRAP | 0x80);
       int sig = 0;
       if (!is_syscall) {
         // Handle generic signal.
@@ -310,10 +307,10 @@ void trace(std::map<pid_t, Tracee> pids) {
         debug_log("forwarding signal %d to %d", sig, pid);
       }
 
-      if (WIFSTOPPED(status) && 
-          (status>>8 == (SIGTRAP | (PTRACE_EVENT_CLONE<<8)) ||
-           status>>8 == (SIGTRAP | (PTRACE_EVENT_FORK<<8)) ||
-           status>>8 == (SIGTRAP | (PTRACE_EVENT_VFORK<<8)))) {
+      if (WIFSTOPPED(status) &&
+          (status >> 8 == (SIGTRAP | (PTRACE_EVENT_CLONE << 8)) ||
+           status >> 8 == (SIGTRAP | (PTRACE_EVENT_FORK << 8)) ||
+           status >> 8 == (SIGTRAP | (PTRACE_EVENT_VFORK << 8)))) {
         long new_pid;
         if (ptrace(PTRACE_GETEVENTMSG, pid, 0, &new_pid) == -1) {
           debug_log("ptrace(PTRACE_GETEVENTMSG, %d): %s", pid, strerror(errno));
@@ -340,8 +337,10 @@ void trace(std::map<pid_t, Tracee> pids) {
             }
           }
 
-          if (regs.orig_rax == __NR_write && g_shell_pids.find(pid) != g_shell_pids.end()) {
-            debug_log("Inspecting the `write` buffer of shell process %d.", pid);
+          if (regs.orig_rax == __NR_write &&
+              g_shell_pids.find(pid) != g_shell_pids.end()) {
+            debug_log("Inspecting the `write` buffer of shell process %d.",
+                      pid);
             inspect_for_corruption(pid, regs);
           }
         }
@@ -382,11 +381,8 @@ int main(int argc, char **argv) {
 
   pid_t pid = run_child(argv + 1);
 
-  long options = 
-    PTRACE_O_TRACESYSGOOD
-    | PTRACE_O_TRACEFORK
-    | PTRACE_O_TRACEVFORK
-    | PTRACE_O_TRACECLONE;
+  long options = PTRACE_O_TRACESYSGOOD | PTRACE_O_TRACEFORK |
+                 PTRACE_O_TRACEVFORK | PTRACE_O_TRACECLONE;
 
   if (ptrace(PTRACE_SEIZE, pid, nullptr, options) == -1) {
     fatal_log("ptrace(PTRACE_SEIZE): %s", strerror(errno));
