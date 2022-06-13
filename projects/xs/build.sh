e06c4e46d68257672d61d95a435109b12093946a
Ari Rubinstein <ari@agoric.com>
2022-06-13 12:38:35 -0700

    XS: implement json_parse target, and wrap fuzzing binaries with a wrapper binary to ignore leaks

--- projects/xs/build.sh
+++ projects/xs/build.sh
@@ -17,9 +17,36 @@
 export MODDABLE=$PWD
 export ASAN_OPTIONS="detect_leaks=0"
 
+FUZZ_TARGETS=(
+  xst
+  xst_jsonparse
+)
+
+# Build a wrapper binary for each target to set environment variables.
+for FUZZ_TARGET in ${FUZZ_TARGETS[@]}
+do
+  $CC $CFLAGS -O0 \
+    -DFUZZ_TARGET=$FUZZ_TARGET \
+    $SRC/target.c -o $OUT/$FUZZ_TARGET
+done
+
+# Stash actual binaries in subdirectory so they aren't picked up by target discovery
+mkdir -p $OUT/real
+
+# build main target
 cd "$MODDABLE/xs/makefiles/lin"
 FUZZING=1 OSSFUZZ=1 make debug
 
 cd "$MODDABLE"
-cp ./build/bin/lin/debug/xst $OUT/
+cp ./build/bin/lin/debug/xst $OUT/real/xst
 cp $SRC/xst.options $OUT/
+
+# build jsonparse target
+cd "$MODDABLE/xs/makefiles/lin"
+make -f xst.mk clean
+FUZZING=1 OSSFUZZ=1 OSSFUZZ_JSONPARSE=1 make debug
+
+cd "$MODDABLE"
+cp ./build/bin/lin/debug/xst $OUT/real/xst_jsonparse
+
+cp $SRC/xst.options $OUT/xst_jsonparse.options
