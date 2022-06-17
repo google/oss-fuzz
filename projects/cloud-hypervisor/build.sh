844b78403823d73e51ceffd96dc4197f513e6387
Venkata Krishna Yaswanth Maturi <yashwanthmaturi@gmail.com>
2022-06-17 10:31:41 -0700

    fix issue

--- projects/cloud-hypervisor/build.sh
+++ projects/cloud-hypervisor/build.sh
@@ -1,5 +1,5 @@
 cd $SRC/cloud-hypervisor
 cargo fuzz build -O
-cp cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/block $OUT/
-cp cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/qcow $OUT/
-cp cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/vhdx $OUT/
+cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/block $OUT/
+cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/qcow $OUT/
+cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/vhdx $OUT/
