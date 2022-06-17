345072e521b3351794a5d1349484a5c4040f2ae0
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-06-17 17:22:05 +0200

    grpc-gateway: refactor (#7888)

--- projects/grpc-gateway/build.sh
+++ projects/grpc-gateway/build.sh
@@ -15,10 +15,4 @@
 #
 ################################################################################
 
-if [ "$SANITIZER" = "coverage" ]
-then
-	git clone https://github.com/grpc-ecosystem/grpc-gateway.git
-	compile_go_fuzzer github.com/grpc-ecosystem/grpc-gateway/v2/internal/httprule Fuzz fuzz gofuzz
-else
-	compile_go_fuzzer github.com/grpc-ecosystem/grpc-gateway/v2/internal/httprule Fuzz fuzz gofuzz
-fi
+compile_go_fuzzer github.com/grpc-ecosystem/grpc-gateway/v2/internal/httprule Fuzz fuzz gofuzz
