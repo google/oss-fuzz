19b50047c014421dc5d0be763eb9aa66209adfb4
Arthur Chan <arthur.chan@adalogics.com>
2022-07-05 17:07:27 +0100

    Retry: initial integration Create retry fuzzer

---
+++ projects/retry/fuzz_retry.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import atheris
+import sys
+import random
+with atheris.instrument_imports():
+     from retry import *
+
+def error_method():
+    if random.randint(1,4) == 1:
+        raise ValueError
+    else:
+       return
+
+def TestInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+
+    choice = fdp.ConsumeIntInRange(1,4)
+
+    retry_call(error_method,logger=None)
+    retry_call(
+        error_method,
+        exceptions=ValueError,
+        tries=fdp.ConsumeIntInRange(-1,100),
+        delay=fdp.ConsumeIntInRange(1,10),
+        logger=None
+    )
+
+    @retry(logger=None)
+    def wrapper_one():
+        error_method()
+
+    @retry(
+        ValueError,
+        tries=fdp.ConsumeIntInRange(-1,100),
+        delay=fdp.ConsumeIntInRange(1,10),
+        logger=None
+    )
+    def wrapper_two():
+        error_method()
+
+    wrapper_one()
+    wrapper_two()
+
+def main():
+    atheris.Setup(sys.argv, TestInput, enable_python_coverage=True)
+    atheris.Fuzz()
+
+if __name__ == "__main__":
+    main()
