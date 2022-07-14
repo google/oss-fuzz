60c7249568de77ac0b2a10801975879b483892d7
David Korczynski <david@adalogics.com>
2022-07-14 18:46:47 +0100

    nit

--- projects/ghostscript/gs_device_tiffsep1_fuzzer.cc
+++ projects/ghostscript/gs_device_tiffsep1_fuzzer.cc
@@ -12,6 +12,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
 #include "gs_fuzzlib.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
