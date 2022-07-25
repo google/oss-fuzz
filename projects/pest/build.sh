9d17ea0cc768ccf99972e45ea4e4352d33dccdc8
Venkata Krishna Yaswanth Maturi <yashwanthmaturi@gmail.com>
2022-07-24 20:44:12 -0400

    fix build pest

--- projects/pest/build.sh
+++ projects/pest/build.sh
@@ -13,10 +13,10 @@
 # limitations under the License.
 #
 ################################################################################
-cd $SRC/pest/meta
-cargo bootstrap && RUSTFLAGS="-Znew-llvm-pass-manager=no" cargo +nightly fuzz build
-cd $SRC/pest/grammars
-cargo bootstrap && RUSTFLAGS="-Znew-llvm-pass-manager=no" cargo +nightly fuzz build
+cd $SRC/pest/meta/fuzz
+RUSTFLAGS="-Znew-llvm-pass-manager=no" cargo +nightly fuzz build
+cd $SRC/pest/grammars/fuzz
+RUSTFLAGS="-Znew-llvm-pass-manager=no" cargo +nightly fuzz build
 cp $SRC/pest/meta/fuzz/target/x86_64-unknown-linux-gnu/release/parser $OUT/
 cp $SRC/pest/grammars/fuzz/target/x86_64-unknown-linux-gnu/release/toml $OUT/
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/json $OUT/
