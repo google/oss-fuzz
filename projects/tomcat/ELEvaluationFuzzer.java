c1e8d523db9d36c9a861cc5c12f5ccbe7beac120
henryrneh <henryharry93@gmail.com>
2022-07-27 20:00:18 +0200

    Update project.yaml and add fuzz targets

--- projects/tomcat/ELEvaluationFuzzer.java
+++ projects/tomcat/ELEvaluationFuzzer.java
@@ -16,11 +16,30 @@
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.code_intelligence.jazzer.api.FuzzerSecurityIssueHigh;
-import org.apache.tomcat.util.http.parser.HttpParser;
 
-public class HttpParserFuzzer {
+import org.apache.el.*;
+
+import jakarta.el.ELException;
+import jakarta.el.ValueExpression;
+
+import org.apache.el.lang.ELSupport;
+import org.apache.jasper.el.ELContextImpl;
+
+public class ELEvaluationFuzzer {
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
-        String input = data.consumeRemainingAsString();
-        HttpParser.unquote(input);
+        String str = data.consumeRemainingAsString();
+
+        try {
+            evaluateExpression(str);
+        } catch (ELException | IllegalArgumentException | ArithmeticException e) {
+        }
+    }
+
+    public static String evaluateExpression(String expression) {
+        ExpressionFactoryImpl exprFactory = new ExpressionFactoryImpl();
+        ELContextImpl ctx = new ELContextImpl(exprFactory);
+        ctx.setFunctionMapper(new TesterFunctions.FMapper());
+        ValueExpression ve = exprFactory.createValueExpression(ctx, expression, String.class);
+        return (String) ve.getValue(ctx);
     }
 }
