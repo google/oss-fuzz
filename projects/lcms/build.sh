bbd6966bd2996d7e48606b1eb55d189afeaeea0e
David Korczynski <david@adalogics.com>
2022-07-29 13:23:10 +0100

    lcms: extend fuzzing suite
    Add a fuzzer that target various output formats when transforming. There
    are more oppournities to extend this further but will let this fuzzer
    run for a while to see how it goes.

--- projects/lcms/build.sh
+++ projects/lcms/build.sh
@@ -20,7 +20,13 @@
 make -j$(nproc) all
 
 # build your fuzzer(s)
-FUZZERS="cmsIT8_load_fuzzer cms_transform_fuzzer cms_overwrite_transform_fuzzer cms_transform_all_fuzzer cms_profile_fuzzer"
+FUZZERS="cmsIT8_load_fuzzer            \
+        cms_transform_fuzzer           \
+        cms_overwrite_transform_fuzzer \
+        cms_transform_all_fuzzer       \
+        cms_profile_fuzzer             \
+        cms_transform_extended_fuzzer"
+
 for F in $FUZZERS; do
     $CC $CFLAGS -c -Iinclude \
         $SRC/$F.c -o $SRC/$F.o
@@ -31,5 +37,6 @@ done
 
 cp $SRC/icc.dict $SRC/*.options $OUT/
 cp $SRC/icc.dict $OUT/cms_transform_all_fuzzer.dict
+cp $SRC/icc.dict $OUT/cms_transform_extended_fuzzer.dict
 cp $SRC/seed_corpus.zip $OUT/cms_transform_fuzzer_seed_corpus.zip
 cp $SRC/seed_corpus.zip $OUT/cms_profile_fuzzer_seed_corpus.zip
