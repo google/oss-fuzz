f8e3d618d8b4b3d379ae611ff54a4005876c9958
AdamKorcz <adam@adalogics.com>
2022-08-06 18:13:34 +0100

    tendermint: fix broken build
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/tendermint/build.sh
+++ projects/tendermint/build.sh
@@ -14,5 +14,8 @@
 # limitations under the License.
 #
 ################################################################################
-
-bash -x ./test/fuzz/oss-fuzz-build.sh
+compile_go_fuzzer github.com/tendermint/tendermint/test/fuzz/mempool/v0 Fuzz mempool_v0_fuzzer
+compile_go_fuzzer github.com/tendermint/tendermint/test/fuzz/mempool/v1 Fuzz mempool_v1_fuzzer
+compile_go_fuzzer github.com/tendermint/tendermint/test/fuzz/p2p/addrbook Fuzz p2p_addrbook_fuzzer
+compile_go_fuzzer github.com/tendermint/tendermint/test/fuzz/p2p/pex Fuzz p2p_pex_fuzzer
+compile_go_fuzzer github.com/tendermint/tendermint/test/fuzz/rpc/jsonrpc/server Fuzz rpc_jsonrpc_server_fuzzer
