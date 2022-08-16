dbe164f83f2322383cd4e8804bb16e2bbe334944
Zied Aouini <aouinizied@gmail.com>
2022-08-16 18:48:07 +0200

    Fix missing requirements.

--- projects/nfstream/build.sh
+++ projects/nfstream/build.sh
@@ -18,7 +18,7 @@ python3 -m pip install -U --upgrade --force-reinstall pyinstaller
 python3 -m pip install -U -r dev_requirements.txt
 python3 -m pip install -U --upgrade --force-reinstall cffi
 python3 prepare.py
-python3 -m pip3 install -U  .
+python3 -m pip install -U  .
 
 for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
   fuzzer_basename=$(basename -s .py $fuzzer)
