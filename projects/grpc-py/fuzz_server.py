9951c7e4591d0c517d3a02844b32d5e577f100fd
David Korczynski <david@adalogics.com>
2022-08-22 11:35:22 +0100

    add doc

--- projects/grpc-py/fuzz_server.py
+++ projects/grpc-py/fuzz_server.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Fuzz grpc server using the Greeter example"""
 
 import sys
 import time
