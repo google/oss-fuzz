a834fd2883059ae56729f9046e0d0bc4c552dbe5
David Korczynski <david@adalogics.com>
2022-08-24 12:33:48 +0100

    pyodbc: initial integration

---
+++ projects/pyodbc/fake_odbc_driver.c
@@ -0,0 +1,35 @@
+/* Copyright 2022 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+int SQLAllocHandle(long a1, long a2, long a3){
+        return 0;
+}
+
+int SQLDriverConnect(long a1, long a2, long a3, long a4, long a5, long a6, long a7, long a8) {
+        return 0;
+}
+
+int SQLSetConnectAttr(long a1, long a2, long a3, long a4) {
+        return 0;
+}
+
+int SQLExecDirectW(long a1, long a2, long a3) {
+        return 0;
+}
+
+int SQLRowCount(long a1, long a2) {
+        return 0;
+}
+
+int SQLNumResultCols(long a1, long a2) {
+        return 0;
+}
