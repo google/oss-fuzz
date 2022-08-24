75f19e2cac0f3d22d808e134a522d1c44e48e771
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-08-24 23:56:42 +0100

    golang: add fuzzer (#8351)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/golang/build.sh
+++ projects/golang/build.sh
@@ -93,6 +93,13 @@ compile_native_go_fuzzer tarPackage FuzzReader fuzz_std_lib_tar_reader
 cd $SRC && git clone https://github.com/AdamKorcz/instrumentation
 cd instrumentation
 go run main.go $SRC/go/src/archive/tar
+go run main.go $SRC/net
+
+cp $SRC/h2c_fuzzer.go $SRC/net/http2/h2c/
+cd $SRC/net/http2/h2c
+go mod tidy -e -go=1.16 && go mod tidy -e -go=1.17
+compile_go_fuzzer . FuzzH2c fuzz_h2c
+mv $SRC/fuzz_h2c.options $OUT/
 
 cd $SRC/go/src/archive/tar
 cp $SRC/fuzz_tar_reader.go ./
