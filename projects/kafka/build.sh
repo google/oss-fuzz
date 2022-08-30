4aea064ab4e8e103ced4bad0c5c977a140549d2f
Arthur Chan <arthur.chan@adalogics.com>
2022-08-30 15:07:52 +0100

    Fix project initialization

--- projects/kafka/build.sh
+++ projects/kafka/build.sh
@@ -16,7 +16,6 @@
 ################################################################################
 
 # Build and install project (using current CFLAGS, CXXFLAGS).
-pip3 install --upgrade pip
 pip3 install .
 
 for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
