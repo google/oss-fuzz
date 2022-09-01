f3e341ce94fcc7b19a83498fdc0ae8126b5fc800
0x34d <ajsinghyadav00@gmail.com>
2022-09-01 15:14:18 +0530

    fix linker error
    Signed-off-by: 0x34d <ajsinghyadav00@gmail.com>

--- projects/mapserver/build.sh
+++ projects/mapserver/build.sh
@@ -28,11 +28,9 @@ popd
 cd $SRC/MapServer
 mkdir build
 cd build
-
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_STATIC=ON \
 -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" \
--DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CFLAGS" \
--DCMAKE_EXE_LINKER_FLAGS="$CFLAGS" -DCMAKE_SHARED_LINKER_FLAGS="$CFLAGS" \
+-DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
 -DWITH_PROTOBUFC=0 -DWITH_FRIBIDI=0 -DWITH_HARFBUZZ=0 -DWITH_CAIRO=0 -DWITH_FCGI=0 \
 -DWITH_GEOS=0 -DWITH_POSTGIS=0 -DWITH_GIF=0 ../
 make -j$(nproc)
@@ -45,29 +43,13 @@ $CC $CFLAGS -Wall -Wextra -I. -I/usr/include/gdal/. -DPROJ_VERSION_MAJOR=6 -c sh
 
 $CXX $CFLAGS $LIB_FUZZING_ENGINE mapfuzzer.o -o mapfuzzer \
 -L. -lmapserver_static -lgdal \
-/usr/lib/x86_64-linux-gnu/libpng.a \
-/usr/lib/x86_64-linux-gnu/libjpeg.a \
-/usr/lib/x86_64-linux-gnu/libproj.a \
-/usr/lib/x86_64-linux-gnu/libsqlite3.a \
-/usr/lib/x86_64-linux-gnu/libfreetype.a \
-/lib/x86_64-linux-gnu/libxml2.a \
-/lib/x86_64-linux-gnu/libicuuc.a \
-/lib/x86_64-linux-gnu/libicudata.a \
-/usr/lib/x86_64-linux-gnu/libz.a \
-/lib/x86_64-linux-gnu/liblzma.so.5 
+-l:libpng.a -l:libjpeg.a -l:libfreetype.a -l:libproj.a -l:libxml2.a -l:libz.a \
+-l:libicuuc.a -l:libicudata.a -l:libsqlite3.a -l:liblzma.so.5 -lc++
 
 $CXX $CFLAGS $LIB_FUZZING_ENGINE shapefuzzer.o -o shapefuzzer \
 -L. -lmapserver_static -lgdal \
-/usr/lib/x86_64-linux-gnu/libpng.a \
-/usr/lib/x86_64-linux-gnu/libjpeg.a \
-/usr/lib/x86_64-linux-gnu/libproj.a \
-/usr/lib/x86_64-linux-gnu/libsqlite3.a \
-/usr/lib/x86_64-linux-gnu/libfreetype.a \
-/lib/x86_64-linux-gnu/libxml2.a \
-/lib/x86_64-linux-gnu/libicuuc.a \
-/lib/x86_64-linux-gnu/libicudata.a \
-/usr/lib/x86_64-linux-gnu/libz.a \
-/lib/x86_64-linux-gnu/liblzma.so.5 
+-l:libpng.a -l:libjpeg.a -l:libfreetype.a -l:libproj.a -l:libxml2.a -l:libz.a \
+-l:libicuuc.a -l:libicudata.a -l:libsqlite3.a -l:liblzma.so.5 -lc++
 
 #SetUp
 cp mapfuzzer $OUT/mapfuzzer
