d7e8a6cee035936fc30163fc20742abd80d6d1a5
Isaac Brodsky <isaac@isaacbrodsky.com>
2022-09-04 16:03:21 -0700

    Working build

--- projects/h3/build.sh
+++ projects/h3/build.sh
@@ -18,5 +18,24 @@
 mkdir build
 cd build
 cmake ..
-make -j$(nproc) fuzzers
-cp bin/fuzzer* $OUT
+make -j$(nproc) h3
+
+H3_BASE=/src/h3/
+
+for fuzzer in $(find $H3_BASE/src/apps/fuzzers -name '*.c'); do
+  fuzzer_basename=$(basename -s .c $fuzzer)
+  # H3_USE_LIBFUZZER is needed so that H3 does not try to build its own
+  # implementation of `main`
+  $CC $CFLAGS -DH3_PREFIX="" \
+    -DH3_USE_LIBFUZZER=1 \
+    -I$H3_BASE/src/apps/applib/include \
+    -I$H3_BASE/src/h3lib/include \
+    -I$H3_BASE/build/src/h3lib/include \
+    -o $fuzzer_basename.o \
+    -c $fuzzer
+
+  $CC $CFLAGS $LIB_FUZZING_ENGINE -rdynamic \
+    $fuzzer_basename.o \
+    -o $OUT/$fuzzer_basename \
+    lib/libh3.a
+done
