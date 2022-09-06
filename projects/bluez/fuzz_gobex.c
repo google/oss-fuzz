3cdfe6a592d5c73e04c58e2783f2521d04746208
David Korczynski <david@adalogics.com>
2022-09-06 10:39:26 +0100

    bluez: extend fuzzing

--- projects/bluez/fuzz_gobex.c
+++ projects/bluez/fuzz_gobex.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Google LLC
+/* Copyright 2022 Google LLC
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -9,32 +9,26 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <config.h>
 
+#include <config.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <syslog.h>
 
-#include "bluetooth.h"
-#include "sdp.h"
-#include "sdp_lib.h"
+#include "gobex/gobex.h"
+#include "gobex/gobex-packet.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-
-  /* Target sdp_extract_pdu */
-  int scanned = 0;
-  sdp_record_t *out = NULL;
-  openlog("fuzz_sdp", LOG_PERROR, LOG_LOCAL0);
-  out = sdp_extract_pdu(data, size, &scanned);
-  if (out) {
-    sdp_record_t *copy = sdp_copy_record(out);
-    if (copy) {
-      sdp_record_free(copy);
-    }
-    sdp_record_free(out);
+  uint8_t buf[255];
+  GObexPacket *pkt;
+  GError *err = NULL;
+  pkt = g_obex_packet_decode(data, size, 0, G_OBEX_DATA_REF, &err);
+  if (pkt != NULL) {
+    /* Anything that decodes must encode */
+    g_obex_packet_encode(pkt, buf, sizeof(buf));
+    g_obex_packet_free(pkt);
   }
-  closelog();
 
   return 0;
 }
