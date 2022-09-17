51aa9d67cb3cb8b9f0bd93870bf25cbfc8874ec2
David Korczynski <david@adalogics.com>
2022-09-17 15:47:18 +0100

    add reference to issue

--- projects/pysan/fuzz_ldap.py
+++ projects/pysan/fuzz_ldap.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Targets: https://github.com/python-ldap/python-ldap/security/advisories/GHSA-r8wq-qrxc-hmcm"""
 
 import os
 import sys
