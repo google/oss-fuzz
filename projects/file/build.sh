244684cf5493d2dc03b3b528fcd392781047f610
Julien Voisin <jvoisin@users.noreply.github.com>
2022-09-23 22:07:03 +0200

    Add a file-base fuzzer for libmagic (#8542)
    Some libmagic features (like ELF parsing) are only working against files
    on disk.
    
    Co-authored-by: Julien Voisin <jvoisin@google.com>

--- projects/file/build.sh
+++ projects/file/build.sh
@@ -25,6 +25,9 @@ $CXX $CXXFLAGS -std=c++11 -Isrc/ \
 $CXX $CXXFLAGS -std=c++11 -Isrc/ \
      $SRC/magic_fuzzer_loaddb.cc -o $OUT/magic_fuzzer_loaddb \
      $LIB_FUZZING_ENGINE ./src/.libs/libmagic.a -l:libz.a -l:liblz4.a -l:libbz2.a -l:liblzma.a -l:libzstd.a
+$CXX $CXXFLAGS -std=c++11 -Isrc/ \
+     $SRC/magic_fuzzer_fd.cc -o $OUT/magic_fuzzer_loaddb \
+     $LIB_FUZZING_ENGINE ./src/.libs/libmagic.a -l:libz.a -l:liblz4.a -l:libbz2.a -l:liblzma.a -l:libzstd.a
 
 cp ./magic/magic.mgc $OUT/
 
