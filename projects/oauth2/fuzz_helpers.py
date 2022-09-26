68b05e8c031ec453c39b1d6125a712ebc4310fe5
Arthur Chan <arthur.chan@adalogics.com>
2022-09-26 17:08:17 +0100

    Fix missing error handing in Issue 49568 49591 49966 50022 50309 51419

--- projects/oauth2/fuzz_helpers.py
+++ projects/oauth2/fuzz_helpers.py
@@ -15,44 +15,65 @@
 
 import atheris
 import sys
+import binascii
 with atheris.instrument_imports():
     import oauth2client._helpers as helpers
 
 def TestInput(data):
     fdp = atheris.FuzzedDataProvider(data)
 
-    helpers.scopes_to_string([
-        fdp.ConsumeString(20),
-        fdp.ConsumeString(20)
-    ])
-    helpers.scopes_to_string(fdp.ConsumeString(20))
+    expected_error_list = [
+        'URL-encoded content contains a repeated value:',
+	'Invalid IPv6 URL',
+        'Is a symbolic link.',
+        'Is a directory',
+        'Incorrect padding',
+        'codec can\'t decode byte',
+        'codec can\'t encode character',
+        'could not be converted to'
+    ]
 
-    helpers.string_to_scopes(fdp.ConsumeString(100))
+    try:
+        helpers.scopes_to_string([
+            fdp.ConsumeString(20),
+            fdp.ConsumeString(20)
+        ])
+        helpers.scopes_to_string(fdp.ConsumeString(20))
 
-    helpers.parse_unique_urlencoded(fdp.ConsumeString(100))
+        helpers.string_to_scopes(fdp.ConsumeString(100))
 
-    helpers.update_query_params(
-        fdp.ConsumeString(100),{
-            fdp.ConsumeString(10):fdp.ConsumeString(20),
-            fdp.ConsumeString(10):fdp.ConsumeString(20),
-            fdp.ConsumeString(10):fdp.ConsumeString(20)
-    })
+        helpers.parse_unique_urlencoded(fdp.ConsumeString(100))
 
-    helpers._add_query_parameter(
-        fdp.ConsumeString(100),
-        fdp.ConsumeString(10),
-        fdp.ConsumeString(20)
-    )
+        helpers.update_query_params(
+            fdp.ConsumeString(100),{
+                fdp.ConsumeString(10):fdp.ConsumeString(20),
+                fdp.ConsumeString(10):fdp.ConsumeString(20),
+                fdp.ConsumeString(10):fdp.ConsumeString(20)
+        })
 
-    helpers.validate_file(fdp.ConsumeString(100))
+        helpers._add_query_parameter(
+            fdp.ConsumeString(100),
+            fdp.ConsumeString(10),
+            fdp.ConsumeString(20)
+        )
 
-    helpers._json_encode(fdp.ConsumeString(100))
+        helpers.validate_file(fdp.ConsumeString(100))
 
-    helpers._to_bytes(fdp.ConsumeString(100))
-    helpers._from_bytes(fdp.ConsumeBytes(100))
+        helpers._json_encode(fdp.ConsumeString(100))
 
-    helpers._urlsafe_b64encode(fdp.ConsumeString(100))
-    helpers._urlsafe_b64decode(fdp.ConsumeString(100))
+        helpers._to_bytes(fdp.ConsumeString(100))
+        helpers._from_bytes(fdp.ConsumeBytes(100))
+
+        helpers._urlsafe_b64encode(fdp.ConsumeString(100))
+        helpers._urlsafe_b64decode(fdp.ConsumeString(100))
+    except (ValueError, UnicodeDecodeError, IOError, binascii.Error) as e:
+        expected = False
+        for expected_error in expected_error_list:
+            if expected_error in str(e):
+                    expected = True
+                    break
+        if not expected:
+            raise e
 
 def main():
     atheris.Setup(sys.argv, TestInput, enable_python_coverage=True)
