da1cb5485bd2ef54b2a4ccb024b7ccda8f700d4d
Arthur Chan <arthur.chan@adalogics.com>
2022-09-30 13:27:49 +0100

    Fix build script

--- projects/git/build.sh
+++ projects/git/build.sh
@@ -21,6 +21,7 @@ make -j$(nproc) CC=$CC CXX=$CXX CFLAGS="$CFLAGS" \
   LIB_FUZZING_ENGINE="common-main.o $LIB_FUZZING_ENGINE" fuzz-all
 
 FUZZERS="fuzz-pack-headers fuzz-pack-idx fuzz-commit-graph"
+FUZZERS="$FUZZERS fuzz-cmd-status fuzz-cmd-diff fuzz-cmd-version"
 FUZZERS="$FUZZERS fuzz-command"
 
 # copy fuzzers
