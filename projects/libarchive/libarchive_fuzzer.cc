957bd9c96664b009455c991fbbae2edf0acc2baa
Julien Voisin <jvoisin@google.com>
2022-10-06 14:15:51 +0200

    Simplify a bit libarchive's fuzzer

--- projects/libarchive/libarchive_fuzzer.cc
+++ projects/libarchive/libarchive_fuzzer.cc
@@ -19,43 +19,30 @@
 
 #include "archive.h"
 
-struct Buffer {
-  const uint8_t *buf;
-  size_t len;
-};
-
-ssize_t reader_callback(struct archive *a, void *client_data,
-                        const void **block) {
-  Buffer *buffer = reinterpret_cast<Buffer *>(client_data);
-  *block = buffer->buf;
-  ssize_t len = buffer->len;
-  buffer->len = 0;
-  return len;
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len) {
-  int ret;
-  ssize_t r;
   struct archive *a = archive_read_new();
 
   archive_read_support_filter_all(a);
   archive_read_support_format_all(a);
+  archive_read_support_format_empty(a);
+  archive_read_support_format_raw(a);
 
-  Buffer buffer = {buf, len};
-  archive_read_open(a, &buffer, NULL, reader_callback, NULL);
+  archive_read_open_memory(a, buf, len);
 
-  std::vector<uint8_t> data_buffer(getpagesize(), 0);
   struct archive_entry *entry;
   while(1) {
-    ret = archive_read_next_header(a, &entry);
+    int ret = archive_read_next_header(a, &entry);
     if (ret == ARCHIVE_EOF || ret == ARCHIVE_FATAL)
       break;
     if (ret == ARCHIVE_RETRY)
       continue;
-    while ((r = archive_read_data(a, data_buffer.data(),
-            data_buffer.size())) > 0)
+
+    const void* buff;
+    size_t size;
+    int64_t offset;
+    while ((ret = archive_read_data_block(a, &buff, &size, &offset)) > 0)
       ;
-    if (r == ARCHIVE_FATAL)
+    if (ret == ARCHIVE_FATAL)
       break;
   }
 
