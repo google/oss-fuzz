e99f9c008abd925b9652b1c59c8d0fd07e966005
0x34d <ajsinghyadav00@gmail.com>
2022-10-17 15:17:35 +0530

    modified build.sh
    Signed-off-by: 0x34d <ajsinghyadav00@gmail.com>

--- projects/mosquitto/build.sh
+++ projects/mosquitto/build.sh
@@ -18,10 +18,11 @@ export CXXFLAGS="$CFLAGS"
 export LDFLAGS="$CFLAGS"
 make WITH_STATIC_LIBRARIES=yes -j$(nproc)
 
-pushd $SRC/mosquitto/fuzzing/
+pushd fuzzing/
 make
 cp FuzzPropRecv $OUT/FuzzPropRecv
 cp FuzzReqRes $OUT/FuzzReqRes
+popd
 
 pushd $SRC/oss-fuzz-bloat/mosquitto/
 cp FuzzPropRecv_seed_corpus.zip $OUT/FuzzPropRecv_seed_corpus.zip
