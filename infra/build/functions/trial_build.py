16fb6232dc3e3748446b269855f70aea25afb6d6
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2022-10-26 19:38:31 -0400

    Fix introspector trial builds (#8867)

--- infra/build/functions/trial_build.py
+++ infra/build/functions/trial_build.py
@@ -307,8 +307,12 @@ def trial_build_main(args=None, local_base_build=True):
     build_and_push_test_images.build_and_push_images(  # pylint: disable=unexpected-keyword-arg
         test_image_suffix)
   else:
-    build_and_push_test_images.gcb_build_and_push_images(
-        test_image_suffix, introspector=introspector)
+    if list(args.sanitizers) != ['introspector']:
+      build_and_push_test_images.gcb_build_and_push_images(
+          test_image_suffix, False)
+    if introspector:
+      build_and_push_test_images.gcb_build_and_push_images(
+          test_image_suffix, introspector=introspector)
 
   return _do_test_builds(args, test_image_suffix)
 
