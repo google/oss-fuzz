c72407b5d08cbe8ce713cca8d23c13c2335d4cd2
AdamKorcz <adam@adalogics.com>
2022-10-30 05:58:24 +0000

    moby: add more fuzzers
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/moby/build.sh
+++ projects/moby/build.sh
@@ -18,9 +18,21 @@
 mv $SRC/moby/vendor.mod $SRC/moby/go.mod
 
 cp $SRC/mounts_fuzzer.go $SRC/moby/volume/mounts/
+cp $SRC/tailfile_fuzzer.go $SRC/moby/pkg/tailfile/
+cp $SRC/archive_fuzzers.go $SRC/moby/pkg/archive/
+cp $SRC/jsonfilelog_fuzzer.go $SRC/moby/daemon/logger/jsonfilelog/
 
 go mod tidy
+go get github.com/AdaLogics/go-fuzz-headers@latest
+go mod vendor
 
 mv $SRC/moby/volume/mounts/parser_test.go $SRC/moby/volume/mounts/parser_test_fuzz.go
 mv $SRC/moby/volume/mounts/validate_unix_test.go $SRC/moby/volume/mounts/validate_unix_test_fuzz.go
 compile_go_fuzzer github.com/docker/docker/volume/mounts FuzzParseLinux FuzzParseLinux
+
+compile_go_fuzzer github.com/docker/docker/pkg/archive FuzzUntar FuzzUntar
+compile_go_fuzzer github.com/docker/docker/pkg/archive FuzzDecompressStream FuzzDecompressStream
+compile_go_fuzzer github.com/docker/docker/pkg/archive FuzzApplyLayer FuzzApplyLayer
+compile_go_fuzzer github.com/docker/docker/pkg/tailfile FuzzTailfile FuzzTailfile
+compile_go_fuzzer github.com/docker/docker/daemon/logger/jsonfilelog FuzzLoggerDecode FuzzLoggerDecode
+
