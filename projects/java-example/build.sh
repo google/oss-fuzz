google/oss-fuzz

commit 5b1953b20174adca72d06a8b4e893a46591b7f4a

infra/java: Improve reproducibility of memory issues (#8736)
When reproducing, use slightly lower limits on heap and stack size so
that minimal changes to fuzz targets, fuzzer and runtime do not cause
memory issues to fail to reproduce.

projects/java-example/build.sh
@@ -39,15 +39,20 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java' -or -name '*FuzzerNative.java'); 
 
   cp default.options $OUT/"$fuzzer_basename".options
   # Create execution wrapper.
-  echo "#!/bin/sh
+  echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir/native \
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 \$this_dir/$driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=$RUNTIME_CLASSPATH \
 --target_class=$fuzzer_basename \
---jvm_args=\"-Xmx2048m\" \
+--jvm_args=\"\$mem_settings\" \
 \$@" > $OUT/$fuzzer_basename
   chmod +x $OUT/$fuzzer_basename
 done

projects/jetty/build.sh
@@ -43,15 +43,21 @@ for fuzzer in $(find $SRC -maxdepth 1 -name '*Fuzzer.java'); do
   cp $SRC/[$fuzzer_basename]*.class $OUT/
 
   # Create an execution wrapper that executes Jazzer with the correct arguments.
-  echo "#!/bin/sh
+  echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk/\" \
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=$RUNTIME_CLASSPATH \
 --target_class=$fuzzer_basename \
 -rss_limit_mb=0 \
+--jvm_args=\"\$mem_settings\" \
 \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
 done

projects/jmh/build.sh
@@ -57,20 +57,25 @@ for fuzzer in $(find ${SRC} -name '*Fuzzer.java'); do
 	if (echo ${stripped_path} | grep ".java$"); then
 		continue;
 	fi
-	
+
 	fuzzer_basename=$(basename -s .java $fuzzer)
 	fuzzer_classname=$(echo ${stripped_path} | sed 's|/|.|g');
-	
+
 	# Create an execution wrapper that executes Jazzer with the correct arguments.
-	
+
 	echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=${RUNTIME_CLASSPATH} \
 --target_class=${fuzzer_classname} \
---jvm_args=\"-Xmx2048m\" \
+--jvm_args=\"\$mem_settings\" \
 \$@" > $OUT/${fuzzer_basename}
 	chmod u+x $OUT/${fuzzer_basename}
 done

projects/mysql-connector-j/build.sh
@@ -21,7 +21,7 @@ ALL_JARS=""
 
 pushd "${SRC}/mysql-connector-j"
 	$ANT ${ANT_ARGS} dist
-	
+
 	#
 	# build directory contains only the emited jar and the lib/ population,
 	# all of which we depend on.
@@ -47,16 +47,21 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
   fuzzer_basename=$(basename -s .java $fuzzer)
 
   # Create an execution wrapper that executes Jazzer with the correct arguments.
-  echo "#!/bin/sh
+  echo "#!/bin/bash
 # start mysql and setup it
 \$(dirname \"\$0\")/entrypoint.sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=$RUNTIME_CLASSPATH \
 --target_class=$fuzzer_basename \
---jvm_args=\"-Xmx2048m\" \
+--jvm_args=\"\$mem_settings\" \
 --disabled_hooks=\"com.code_intelligence.jazzer.sanitizers.SqlInjection\" \
 \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename

projects/protobuf-java/build.sh
@@ -52,14 +52,19 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
   cp $SRC/$fuzzer_basename.class $OUT/
 
   # Create an execution wrapper that executes Jazzer with the correct arguments.
-  echo "#!/bin/sh
+  echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\" \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=$RUNTIME_CLASSPATH \
 --target_class=$fuzzer_basename \
---jvm_args=\"-Xmx2048m\" \
+--jvm_args=\"\$mem_settings\" \
 \$@" > $OUT/$fuzzer_basename
   chmod +x $OUT/$fuzzer_basename
 done

projects/retrofit/build.sh
@@ -46,15 +46,21 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
   mv $SRC/[$fuzzer_basename]*.class $OUT/retrofit2
 
   # Create an execution wrapper that executes Jazzer with the correct arguments.
-  echo "#!/bin/sh
+  echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-11/\" \
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-11/lib/server\":\$this_dir \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=$RUNTIME_CLASSPATH \
 --target_class=retrofit2.$fuzzer_basename \
 -rss_limit_mb=0 \
+--jvm_args=\"\$mem_settings\" \
 \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename
 done

projects/rxjava/build.sh
@@ -63,14 +63,19 @@ for fuzzer in $(find ${SRC} -name '*Fuzzer.java'); do
 	fuzzer_classname=$(echo ${stripped_path} | sed 's|/|.|g');
 
 	# Create an execution wrapper that executes Jazzer with the correct arguments.
-	echo "#!/bin/sh
+	echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=${RUNTIME_CLASSPATH} \
 --target_class=${fuzzer_classname} \
---jvm_args=\"-Xmx2048m\" \
+--jvm_args=\"\$mem_settings\" \
 \$@" > $OUT/${fuzzer_basename}
 	chmod u+x $OUT/${fuzzer_basename}
 done

projects/spring-amqp/build.sh
@@ -43,15 +43,20 @@ for fuzzer in $(find $SRC -maxdepth 1 -name '*Fuzzer.java'); do
   cp $SRC/[$fuzzer_basename]*.class $OUT/
 
   # Create an execution wrapper that executes Jazzer with the correct arguments.
-  echo "#!/bin/sh
+  echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
+if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+  mem_settings='-Xmx1900m:-Xss900k'
+else
+  mem_settings='-Xmx2048m:-Xss1024k'
+fi
 \$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
 --cp=$RUNTIME_CLASSPATH \
 --target_class=$fuzzer_basename \
---jvm_args=\"-Xmx2048m\" \
+--jvm_args=\"\$mem_settings\" \
 --instrumentation_includes=org.springframework.** \
 \$@" > $OUT/$fuzzer_basename
   chmod u+x $OUT/$fuzzer_basename

commit e0e5394b9f5fdda75251e501b56439c1f4a50e0d

java: Fix bash escaping in memory setting logic
These issues where introduced by
5b1953b20174adca72d06a8b4e893a46591b7f4a and prevent the new logic from
applying correctly.

projects/java-example/build.sh
@@ -44,7 +44,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java' -or -name '*FuzzerNative.java'); 
 this_dir=\$(dirname \"\$0\")
 LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir/native \
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/jetty/build.sh
@@ -47,7 +47,7 @@ for fuzzer in $(find $SRC -maxdepth 1 -name '*Fuzzer.java'); do
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk/\" \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/jmh/build.sh
@@ -66,7 +66,7 @@ for fuzzer in $(find ${SRC} -name '*Fuzzer.java'); do
 	echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/mysql-connector-j/build.sh
@@ -52,7 +52,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 \$(dirname \"\$0\")/entrypoint.sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/protobuf-java/build.sh
@@ -55,7 +55,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
   echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/retrofit/build.sh
@@ -50,7 +50,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-11/\" \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/rxjava/build.sh
@@ -66,7 +66,7 @@ for fuzzer in $(find ${SRC} -name '*Fuzzer.java'); do
 	echo "#!/bin/bash
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-amqp/build.sh
@@ -48,7 +48,7 @@ for fuzzer in $(find $SRC -maxdepth 1 -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-boot-actuator/build.sh
@@ -47,7 +47,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-boot/build.sh
@@ -63,7 +63,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-cloud-commons/build.sh
@@ -83,7 +83,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-data-jpa/build.sh
@@ -45,7 +45,7 @@ for fuzzer in $(find $SRC -maxdepth 1 -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-data-keyvalue/build.sh
@@ -55,7 +55,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-data-mongodb/build.sh
@@ -47,7 +47,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-data-redis/build.sh
@@ -49,7 +49,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-framework/build.sh
@@ -75,7 +75,7 @@ function create_fuzz_targets() {
         JAVA_OPTS=\"-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog\" \
         JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
         LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-        if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+        if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
             mem_settings='-Xmx1900m:-Xss900k'
         else
             mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-integration/build.sh
@@ -46,7 +46,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-ldap/build.sh
@@ -67,7 +67,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-retry/build.sh
@@ -57,7 +57,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-security/build.sh
@@ -62,7 +62,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-17/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'

projects/spring-shell/build.sh
@@ -54,7 +54,7 @@ function create_fuzz_targets() {
     # LLVMFuzzerTestOneInput for fuzzer detection.
     this_dir=\$(dirname \"\$0\")
     JAVA_HOME=\"\$this_dir/open-jdk-17/\" \
-    if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+    if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
       mem_settings='-Xmx1900m:-Xss900k'
     else
       mem_settings='-Xmx2048m:-Xss1024k'

projects/tomcat/build.sh
@@ -57,7 +57,7 @@ for fuzzer in $(find $SRC -name '*Fuzzer.java'); do
 this_dir=\$(dirname \"\$0\")
 JAVA_HOME=\"\$this_dir/open-jdk-11/\" \
 LD_LIBRARY_PATH=\"\$this_dir/open-jdk-11/lib/server\":\$this_dir \
-if [[ \"$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
+if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
   mem_settings='-Xmx1900m:-Xss900k'
 else
   mem_settings='-Xmx2048m:-Xss1024k'