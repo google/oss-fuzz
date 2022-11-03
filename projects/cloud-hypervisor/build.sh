db3f1af4d5778cffeb0c4be285fd5f30e63ef299
Bo Chen <chen.bo@intel.com>
2022-11-03 14:44:35 -0700

    cloud-hypervisor: Add new fuzzer (#8922)
    New fuzzer is added for virtio-console.
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>
    
    Signed-off-by: Bo Chen <chen.bo@intel.com>

--- projects/cloud-hypervisor/build.sh
+++ projects/cloud-hypervisor/build.sh
@@ -18,6 +18,7 @@ cargo fuzz build -O
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/balloon $OUT/
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/block $OUT/
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/cmos $OUT/
+cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/console $OUT/
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/http_api $OUT/
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/iommu $OUT/
 cp $SRC/cloud-hypervisor/fuzz/target/x86_64-unknown-linux-gnu/release/mem $OUT/
