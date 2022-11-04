bb89c6ebd47e2c56d8b587b9327b7b9c4ec3e6ce
Jonathan Metzman <metzman@chromium.org>
2022-11-04 09:13:25 -0400

    Fix

--- infra/tools/wycheproof/launcher.py
+++ infra/tools/wycheproof/launcher.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+"""Script for running wycheproof."""
+
 import logging
 import sys
 import subprocess
@@ -8,7 +26,7 @@ def main():
     return 1
 
   test_app = sys.argv[1]
-  return subprocess.run([test_app], check=True).return_code
+  return subprocess.run([test_app], check=False).return_code
 
 
 if __name__ == '__main__':
