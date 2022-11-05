3bd34f5d7d14c1163624787f34232ef704f63738
henryrneh <henryharry93@gmail.com>
2022-11-05 23:18:00 +0100

    apache-commons: Fix build failures and configure all apache-commons project correctly

--- projects/apache-commons/build.sh
+++ projects/apache-commons/build.sh
@@ -18,60 +18,9 @@
 # Move seed corpus and dictionary.
 mv $SRC/{*.zip,*.dict} $OUT
 
-PROJECTS="compress imaging geometry math"
-GEOMETRY_MODULE="commons-geometry-io-euclidean"
-MATH_MODULE="commons-math-legacy"
-
-for project in $PROJECTS; do
-  cd $SRC/commons-$project
-  MAVEN_ARGS="--no-transfer-progress -Dmaven.test.skip=true -Djavac.src.version=15 -Djavac.target.version=15 -Djdk.version=15"
-  CURRENT_VERSION=$($MVN org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate \
-  -Dexpression=project.version -q -DforceStdout)
-
-  if [ $project = 'geometry' ]; then
-    # commons-geometry is a multi-module project and requires special handling in order
-    # to build and extract the proper module (commons-geometry-io-euclidean)
-    $MVN package org.apache.maven.plugins:maven-shade-plugin:3.2.4:shade -am -pl $GEOMETRY_MODULE $MAVEN_ARGS
-    cp "$GEOMETRY_MODULE/target/$GEOMETRY_MODULE-$CURRENT_VERSION.jar" $OUT/commons-$project.jar
-  elif [ $project = 'math' ]; then
-    # math also is a multi-library project
-    # further, math's libraries are named 'math4' rather then 'math'
-    $MVN package org.apache.maven.plugins:maven-shade-plugin:3.2.4:shade -am -pl $MATH_MODULE $MAVEN_ARGS
-    cp "$MATH_MODULE/target/commons-math4-legacy-$CURRENT_VERSION.jar" $OUT/commons-$project.jar
-  else
-    $MVN package org.apache.maven.plugins:maven-shade-plugin:3.2.4:shade $MAVEN_ARGS
-    cp "target/commons-$project-$CURRENT_VERSION.jar" $OUT/commons-$project.jar
-  fi
-
-  ALL_JARS="commons-$project.jar"
-
-  # The classpath at build-time includes the project jars in $OUT as well as the
-  # Jazzer API.
-  BUILD_CLASSPATH=$(echo $ALL_JARS | xargs printf -- "$OUT/%s:"):$JAZZER_API_PATH
-
-  # All .jar and .class files lie in the same directory as the fuzzer at runtime.
-  RUNTIME_CLASSPATH=$(echo $ALL_JARS | xargs printf -- "\$this_dir/%s:"):\$this_dir
-
-  for fuzzer in $(find $SRC -iname "$project"'*Fuzzer.java'); do
-    fuzzer_basename=$(basename -s .java $fuzzer)
-    javac -cp $BUILD_CLASSPATH $fuzzer
-    cp $SRC/$fuzzer_basename.class $OUT/
-
-    # Create an execution wrapper that executes Jazzer with the correct arguments.
-    echo "#!/bin/bash
-# LLVMFuzzerTestOneInput for fuzzer detection.
-this_dir=\$(dirname \"\$0\")
-if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
-  mem_settings='-Xmx1900m:-Xss900k'
-else
-  mem_settings='-Xmx2048m:-Xss1024k'
-fi
-LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
-\$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
---cp=$RUNTIME_CLASSPATH \
---target_class=$fuzzer_basename \
---jvm_args=\"\$mem_settings:-Djava.awt.headless=true\" \
-\$@" > $OUT/$fuzzer_basename
-    chmod +x $OUT/$fuzzer_basename
-  done
+for project in $SRC/apache-commons-*; do
+  pushd $(basename "$project")
+    chmod u+x build.sh
+    ./build.sh
+  popd
 done
