e6a880567ad13ee66003596313ca519eeb7c0a64
0x34d <ajsinghyadav00@gmail.com>
2022-11-06 17:29:32 +0530

    [pjsip] initial integration
    Signed-off-by: 0x34d <ajsinghyadav00@gmail.com>

---
+++ projects/pjsip/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -eu
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="$CFLAGS"
+
+./configure \
+--disable-ffmpeg --disable-ssl --disable-opus --disable-libuuid \
+--disable-speex-aec --disable-speex-codec \
+--disable-g7221-codec --disable-gsm-codec --disable-ilbc-codec \
+--disable-resample --disable-libsrtp --disable-libwebrtc --disable-libyuv
+
+make -j$(nproc)
+
+pushd fuzzer/
+make
+cp fuzz-json $OUT/fuzz-json
+cp fuzz-xml $OUT/fuzz-xml
+cp fuzz-sdp $OUT/fuzz-sdp
+cp fuzz-stun $OUT/fuzz-stun
+cp fuzz-sip $OUT/fuzz-sip
+popd
+
+pushd $SRC/oss-fuzz-bloat/pjsip/
+cp fuzz-json_seed_corpus.zip $OUT/fuzz-json_seed_corpus.zip
+cp fuzz-xml_seed_corpus.zip $OUT/fuzz-xml_seed_corpus.zip
+cp fuzz-sdp_seed_corpus.zip $OUT/fuzz-sdp_seed_corpus.zip
+cp fuzz-stun_seed_corpus.zip $OUT/fuzz-stun_seed_corpus.zip
+cp fuzz-sip_seed_corpus.zip $OUT/fuzz-sip_seed_corpus.zip
+popd
---
+++ projects/pjsip/fuzzer/fuzz-json.c
@@ -0,0 +1,80 @@
+/* Copyright 2022 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <pjlib.h>
+#include <pjlib-util.h>
+
+#include <pjlib-util/json.h>
+#include <pj/log.h>
+#include <pj/string.h>
+
+#define kMinInputLength 10
+#define kMaxInputLength 1024
+
+pj_pool_factory *mem;
+
+int Json_parse(char *DataFx,size_t Size){
+    
+    int ret = 0;
+    pj_pool_t *pool;
+    pj_json_elem *elem;
+    pj_json_err_info err;
+
+    pool = pj_pool_create(mem, "json", 1000, 1000, NULL);
+
+    elem = pj_json_parse(pool, DataFx,(unsigned *)&Size, &err);
+
+    if (!elem)
+        ret = 1;
+
+    pj_pool_release(pool);
+
+    return ret;
+}
+
+extern int
+LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{/*pjproject/pjlib-util/src/pjlib-util-test/json_test.c*/
+
+    if (Size < kMinInputLength || Size > kMaxInputLength){
+        return 1;
+    }
+
+/*Add Extra byte */
+    char *DataFx;
+    DataFx = (char *)calloc((Size+1),sizeof(char));
+    memcpy((void *)DataFx,(void *)Data,Size);
+
+/*init*/
+    int ret = 0;
+    pj_caching_pool caching_pool;
+    mem = &caching_pool.factory;
+
+    pj_log_set_level(0);
+
+    ret = pj_init();
+    ret = pjlib_util_init();
+
+    pj_dump_config();
+    pj_caching_pool_init( &caching_pool, &pj_pool_factory_default_policy, 0);
+
+/*Calls*/
+    ret = Json_parse(DataFx,Size);
+
+    free(DataFx);
+
+    return ret;
+}
