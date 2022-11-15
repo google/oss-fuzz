d5925e49a59a1e7d08ef910522bfd188bbacfef5
Jonathan Metzman <metzman@chromium.org>
2022-11-15 17:00:46 -0500

    Fix

--- infra/tools/wycheproof/launcher.py
+++ infra/tools/wycheproof/launcher.py
@@ -27,8 +27,7 @@ def main():
     logging.error('Usage: %s <test_app> <testcase>.', sys.argv[0])
     return 1
 
-  test_app = sys.argv[1]
-  return subprocess.run([test_app], check=False).returncode
+  return subprocess.run(sys.argv[1:], check=False).returncode
 
 
 if __name__ == '__main__':
