cba42195383f0d6a38088130ba5950e31aceda4e
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-11-19 18:20:13 +0000

    moby: add more fuzzers (#9017)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/moby/build.sh
+++ projects/moby/build.sh
@@ -29,6 +29,11 @@ cp $SRC/etchosts_fuzzer.go $SRC/moby/libnetwork/etchosts/
 cp $SRC/oci_fuzzer.go $SRC/moby/oci/
 cp $SRC/jsonlogbytes_fuzzer.go $SRC/moby/daemon/logger/jsonfilelog/jsonlog/
 
+cp $SRC/jsonmessage_fuzzer.go $SRC/moby/pkg/jsonmessage/
+cp $SRC/backend_build_fuzzer.go $SRC/moby/api/server/backend/build/
+cp $SRC/remotecontext_fuzzer.go $SRC/moby/builder/remotecontext/
+cp $SRC/containerstream_fuzzer.go $SRC/moby/container/stream/
+
 
 rm $SRC/moby/pkg/archive/example_changes.go
 rm $SRC/moby/daemon/logger/plugin_unsupported.go
@@ -41,13 +46,16 @@ go mod vendor
 mv $SRC/moby/volume/mounts/parser_test.go $SRC/moby/volume/mounts/parser_test_fuzz.go
 mv $SRC/moby/volume/mounts/validate_unix_test.go $SRC/moby/volume/mounts/validate_unix_test_fuzz.go
 compile_go_fuzzer github.com/docker/docker/volume/mounts FuzzParseLinux FuzzParseLinux
-
+compile_go_fuzzer github.com/docker/docker/pkg/jsonmessage FuzzDisplayJSONMessagesStream FuzzDisplayJSONMessagesStream
+rm $SRC/moby/vendor/github.com/cilium/ebpf/internal/btf/fuzz.go
+compile_go_fuzzer github.com/docker/docker/api/server/backend/build FuzzsanitizeRepoAndTags FuzzsanitizeRepoAndTags
+compile_go_fuzzer github.com/docker/docker/builder/remotecontext FuzzreadAndParseDockerfile FuzzreadAndParseDockerfile
+compile_go_fuzzer github.com/docker/docker/container/stream FuzzcopyEscapable FuzzcopyEscapable
 compile_go_fuzzer github.com/docker/docker/pkg/archive FuzzUntar FuzzUntar
 compile_go_fuzzer github.com/docker/docker/pkg/archive FuzzDecompressStream FuzzDecompressStream
 compile_go_fuzzer github.com/docker/docker/pkg/archive FuzzApplyLayer FuzzApplyLayer
 compile_go_fuzzer github.com/docker/docker/pkg/tailfile FuzzTailfile FuzzTailfile
 compile_go_fuzzer github.com/docker/docker/daemon/logger/jsonfilelog FuzzLoggerDecode FuzzLoggerDecode
-
 compile_go_fuzzer github.com/docker/docker/libnetwork/etchosts FuzzAdd FuzzAdd
 compile_go_fuzzer github.com/docker/docker/oci FuzzAppendDevicePermissionsFromCgroupRules FuzzAppendDevicePermissionsFromCgroupRules
 compile_go_fuzzer github.com/docker/docker/daemon/logger/jsonfilelog/jsonlog FuzzJSONLogsMarshalJSONBuf FuzzJSONLogsMarshalJSONBuf
