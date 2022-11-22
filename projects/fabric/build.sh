58b49c8401cf73c0f8d95ed514e3f9e0fa37cd2c
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-11-22 12:29:51 +0000

    fabric: fix broken build (#9035)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/fabric/build.sh
+++ projects/fabric/build.sh
@@ -17,27 +17,25 @@
 
 rm -r $SRC/fabric/cmd/cryptogen
 
+cp $SRC/ccprovider_fuzzer.go ./core/common/ccprovider/
+cp $SRC/persistence_fuzzer.go ./core/chaincode/persistence/mock/
+cp $SRC/policydsl_fuzzer.go $SRC/fabric/common/policydsl/
+cp $SRC/msp_fuzzer.go $SRC/fabric/msp/
+cp $SRC/fabenc_fuzzer.go $SRC/fabric/common/flogging/fabenc/
+
 cd $SRC/instrumentation && go run main.go $SRC/fabric && cd $SRC/fabric
 go mod tidy && go mod vendor
 
-
-cp $SRC/persistence_fuzzer.go ./core/chaincode/persistence/mock/
 go get github.com/AdaLogics/go-fuzz-headers
+go mod tidy
 go mod vendor
+
 compile_go_fuzzer github.com/hyperledger/fabric/core/chaincode/persistence/mock FuzzPersistence fuzz_persistence
 compile_go_fuzzer github.com/hyperledger/fabric/core/chaincode/persistence/mock FuzzChaincodePackageStreamerMetadatabytes FuzzChaincodePackageStreamerMetadatabytes
 compile_go_fuzzer github.com/hyperledger/fabric/core/chaincode/persistence/mock FuzzParseChaincodePackage FuzzParseChaincodePackage
-
-cp $SRC/ccprovider_fuzzer.go ./core/common/ccprovider/
 compile_go_fuzzer github.com/hyperledger/fabric/core/common/ccprovider FuzzExtractFileEntries FuzzExtractFileEntries
-
-cp $SRC/policydsl_fuzzer.go $SRC/fabric/common/policydsl/
 compile_go_fuzzer github.com/hyperledger/fabric/common/policydsl FuzzFromString fuzz_from_string
-
-cp $SRC/msp_fuzzer.go $SRC/fabric/msp/
 compile_go_fuzzer github.com/hyperledger/fabric/msp FuzzDeserializeIdentity fuzz_deserialize_identity
-
-cp $SRC/fabenc_fuzzer.go $SRC/fabric/common/flogging/fabenc/
 compile_go_fuzzer github.com/hyperledger/fabric/common/flogging/fabenc FuzzParseFormat fuzz_parse_format
 
 cp $SRC/*.options $OUT/
