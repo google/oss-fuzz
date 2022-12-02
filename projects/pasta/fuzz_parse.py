b1f99d8af25407d480b34314cd11cea72c8b640f
DavidKorczynski <david@adalogics.com>
2022-12-02 18:17:15 +0000

    pasta: initial integration (#9114)
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ projects/pasta/fuzz_parse.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import atheris
+
+import pasta
+
+# For detecting security-sensitive issues
+import pysecsan
+pysecsan.sanlib.sanitizer_log_level = 30
+pysecsan.add_hooks()
+
+
+@atheris.instrument_func
+def TestOneInput(data):
+  fdp = atheris.FuzzedDataProvider(data)
+  try:
+    pasta.parse(fdp.ConsumeUnicodeNoSurrogates(8096))
+  except (
+    SyntaxError, # thrown by ast fast
+    ValueError,  # thrown by ast fast
+    pasta.base.annotate.AnnotationError, # pasta-thrown error
+    MemoryError # For stack overflows
+  ):
+    pass
+
+
+def main():
+  atheris.instrument_all()
+  atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+  atheris.Fuzz()
+
+
+if __name__ == "__main__":
+  main()
