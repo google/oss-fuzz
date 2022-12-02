5e8a91ab86f886d247f5698a27f9a2314a4862ff
AdamKorcz <adam@adalogics.com>
2022-12-02 19:27:18 +0000

    Add another fuzzer
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/knative/build.sh
+++ projects/knative/build.sh
@@ -18,3 +18,11 @@
 cp $SRC/json_fuzzer.go $SRC/pkg/webhook/json/
 mv $SRC/pkg/webhook/json/decode_test.go $SRC/pkg/webhook/json/decode_test_fuzz.go
 compile_go_fuzzer knative.dev/pkg/webhook/json FuzzJsonDecode fuzz_json_decode
+
+cp $SRC/fuzz_activatornet.go $SRC/serving/pkg/activator/net/
+cd $SRC/serving
+mv pkg/activator/net/throttler_test.go pkg/activator/net/throttler_test_fuzz.go
+mv pkg/activator/net/revision_backends_test.go pkg/activator/net/revision_backends_test_fuzz.go
+printf "package net\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/serving/pkg/activator/net/registerfuzzdep.go
+go mod tidy && go mod vendor
+compile_native_go_fuzzer knative.dev/serving/pkg/activator/net FuzzNewRevisionThrottler FuzzNewRevisionThrottler
