95a5086ba45fbb1388a35ddac553497cc34e84c7
Oliver Chang <oliverchang@users.noreply.github.com>
2022-12-07 15:03:41 +1100

    Remove network call from github_scarecrow.
    This doesn't work in our fuzzing env.

--- projects/github_scarecrow/fakelib.py
+++ projects/github_scarecrow/fakelib.py
@@ -1,10 +1,9 @@
-import requests
+
 RAN_ONCE = False
 
 def do_something(data):
     global RAN_ONCE
     if not RAN_ONCE:
-        print(requests.get('https://google.com'))
         RAN_ONCE = True
     if not data:
         return
