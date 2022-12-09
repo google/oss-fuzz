8477ffcccf71bdcbf7748933649ad87dca95e406
DavidKorczynski <david@adalogics.com>
2022-12-09 10:57:22 +0000

    mrab-regex: initial integration (#9152)
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ projects/mrab-regex/fuzz_regex.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import atheris
+import regex
+
+
+def TestOneInput(data):
+  fdp = atheris.FuzzedDataProvider(data)
+
+  # Random compilation
+  try:
+    x = regex.compile(fdp.ConsumeUnicodeNoSurrogates(sys.maxsize))
+  except regex._regex_core.error:
+    pass
+  except ValueError:
+    # Compile throws several ValueErrors
+    pass
+
+  # Target regex.sub
+  try:
+    s1 = fdp.ConsumeUnicodeNoSurrogates(sys.maxsize)
+    s2 = fdp.ConsumeUnicodeNoSurrogates(sys.maxsize)
+    s3 = fdp.ConsumeUnicodeNoSurrogates(sys.maxsize)
+    regex.sub(s1, s2, s3)
+  except regex._regex_core.error:
+    pass
+  except ValueError:
+    # Compile throws several ValueErrors
+    pass
+
+
+def main():
+  atheris.instrument_all()
+  atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+  atheris.Fuzz()
+
+
+if __name__ == "__main__":
+  main()
