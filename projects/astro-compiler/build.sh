647e4eee3f5aac0b46a4252bb1b70d37819b6c92
Caleb Jasik <calebjasik@jasik.xyz>
2022-12-20 18:15:57 -0600

    `go mod vendor` is unneeded

--- projects/astro-compiler/build.sh
+++ projects/astro-compiler/build.sh
@@ -19,7 +19,7 @@ go install github.com/AdamKorcz/go-118-fuzz-build@latest
 
 # Fuzz internal/transform
 printf "package transform\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/compiler/internal/transform/registerfuzzdep.go
-go mod tidy && go mod vendor
+go mod tidy
 go get -u github.com/AdamKorcz/go-118-fuzz-build/testing
 
 compile_native_go_fuzzer github.com/withastro/compiler/internal/transform FuzzScopeHTML fuzz_scope_html
