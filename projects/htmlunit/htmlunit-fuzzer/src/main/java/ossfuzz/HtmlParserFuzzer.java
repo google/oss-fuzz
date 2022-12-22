caf7186ee5ee0e1005191f2404d2708f0d8a7b36
aschaich <108736614+aschaich@users.noreply.github.com>
2022-12-23 06:28:28 +0900

    [htmlunit] Initial Commit (#9248)

---
+++ projects/htmlunit/htmlunit-fuzzer/src/main/java/ossfuzz/HtmlParserFuzzer.java
@@ -0,0 +1,75 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package ossfuzz;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.StringWebResponse; 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HtmlParserFuzzer {
+
+	private FuzzedDataProvider fuzzedDataProvider;
+
+	public HtmlParserFuzzer(FuzzedDataProvider fuzzedDataProvider) {
+
+		this.fuzzedDataProvider = fuzzedDataProvider;
+
+	}
+
+	BrowserVersion getBrowserVersion() {
+		/*
+		 * from src/test/java/com/gargoylesoftware/htmlunit/WebTestCase.java
+		 */
+		return new BrowserVersion.BrowserVersionBuilder(BrowserVersion.BEST_SUPPORTED)
+                    .setApplicationName("FLAG_ALL_BROWSERS")
+                    .build();
+	}
+
+	void test() {
+		try {
+			WebClient webClient = new WebClient(getBrowserVersion());
+			WebResponse webResponse = new StringWebResponse(fuzzedDataProvider.consumeRemainingAsString(), new URL("http://localhost.edu/index.html"));
+			HtmlPage page = new HtmlPage(webResponse, webClient.getCurrentWindow());
+        
+			/*
+			 * net.sourceforge.htmlunit.corejs.javascript.EvaluatorException
+			 * seems to be fatal
+			 */
+			webClient.getOptions().setThrowExceptionOnScriptError(false);
+
+			webClient.getCurrentWindow().setEnclosedPage(page);
+			webClient.getPageCreator().getHtmlParser().parse(webResponse, page, false, false);
+		} catch (IllegalArgumentException e) {
+		} catch (MalformedURLException e) {
+		} catch (IOException e) {
+		}
+
+	}
+
+	public static void fuzzerTestOneInput(FuzzedDataProvider fuzzedDataProvider) {
+
+		HtmlParserFuzzer testFixture = new HtmlParserFuzzer(fuzzedDataProvider);
+		testFixture.test();
+	}
+}
