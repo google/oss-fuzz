7f00ff0af0be0a2e27aad16fd3229fb26d15acc6
David Korczynski <david@adalogics.com>
2022-12-29 03:56:42 -0800

    nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/ply/build.sh
+++ projects/ply/build.sh
@@ -17,6 +17,6 @@
 pip3 install .
 
 # Build fuzzers in $OUT.
-for fuzzer in $(find ./ -name 'fuzz_*.py'); do
+for fuzzer in $(find $SRC/ -name 'fuzz_*.py'); do
   compile_python_fuzzer $fuzzer
 done
