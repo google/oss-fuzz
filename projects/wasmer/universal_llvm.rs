b181472c3d488b4ef57b9549527883d7333aefb0
manunio <max.manu.nair@gmail.com>
2022-12-29 19:18:51 +0530

    wasmer: initial integration (#9276)
    Hi, [ Wasmer](https://github.com/wasmerio/wasmer) is a fast and secure
    [WebAssembly](https://webassembly.org/) runtime that enables super
    lightweight containers to run anywhere: from Desktop to the Cloud, Edge
    and IoT devices.
    - It has close to 2 million downloads as per
    [crates.io](https://crates.io/crates/wasmer).
    - It is being used by projects like:
      - [swc](https://github.com/swc-project/swc)
      - [zellij](https://github.com/zellij-org/zellij)
      - [youki](https://github.com/containers/youki)
      - [lunatic](https://github.com/lunatic-solutions/lunatic)
      - [mun](https://github.com/mun-lang/mun)
      - [locutus](https://github.com/freenet/locutus)

---
+++ projects/wasmer/universal_llvm.rs
@@ -0,0 +1,75 @@
+/*
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+*/
+#![no_main]
+
+use libfuzzer_sys::{arbitrary, arbitrary::Arbitrary, fuzz_target};
+use wasm_smith::{Config, ConfiguredModule};
+use wasmer::{imports, CompilerConfig, Instance, Module, Store};
+use wasmer_compiler_llvm::LLVM;
+
+#[derive(Arbitrary, Debug, Default, Copy, Clone)]
+struct NoImportsConfig;
+impl Config for NoImportsConfig {
+    fn max_imports(&self) -> usize {
+        0
+    }
+    fn max_memory_pages(&self) -> u32 {
+        // https://github.com/wasmerio/wasmer/issues/2187
+        65535
+    }
+    fn allow_start_export(&self) -> bool {
+        false
+    }
+}
+#[derive(Arbitrary)]
+struct WasmSmithModule(ConfiguredModule<NoImportsConfig>);
+impl std::fmt::Debug for WasmSmithModule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&wasmprinter::print_bytes(self.0.to_bytes()).unwrap())
+    }
+}
+
+fuzz_target!(|module: WasmSmithModule| {
+    let wasm_bytes = module.0.to_bytes();
+
+    if let Ok(path) = std::env::var("DUMP_TESTCASE") {
+        use std::fs::File;
+        use std::io::Write;
+        let mut file = File::create(path).unwrap();
+        file.write_all(&wasm_bytes).unwrap();
+        return;
+    }
+
+    let mut compiler = LLVM::default();
+    compiler.canonicalize_nans(true);
+    compiler.enable_verifier();
+    let mut store = Store::new(compiler);
+    let module = Module::new(&store, &wasm_bytes).unwrap();
+    match Instance::new(&mut store, &module, &imports! {}) {
+        Ok(_) => {}
+        Err(e) => {
+            let error_message = format!("{}", e);
+            if error_message.starts_with("RuntimeError: ")
+                && error_message.contains("out of bounds")
+            {
+                return;
+            }
+            panic!("{}", e);
+        }
+    }
+});
