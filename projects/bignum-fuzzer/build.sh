fabe7d7ec443d0890ce071b9a385748d6d32a380
Guido Vranken <guidovranken@users.noreply.github.com>
2023-01-04 12:11:55 +0100

    [cryptofuzz, bignum-fuzzer] Use libgmp repo again (#9348)
    I recently configured several projects to use a stable source archive of
    libgmp, instead of cloning from the repository, because the certificate
    gmplib.org had expired: https://github.com/google/oss-fuzz/pull/9314
    
    gmplib.org have fixed their certificate so with this PR I'm cloning from
    the repo again.

--- projects/bignum-fuzzer/build.sh
+++ projects/bignum-fuzzer/build.sh
@@ -15,10 +15,6 @@
 #
 ################################################################################
 
-cd $SRC/
-tar --lzip -xvf gmp-6.2.1.tar.lz
-mv $SRC/gmp-6.2.1/ $SRC/libgmp/
-
 # Install Rust nightly
 #curl https://sh.rustup.rs -sSf | sh -s -- -y
 #source $HOME/.cargo/env
