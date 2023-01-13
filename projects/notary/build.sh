275a316d523f863384a95ffeb88e37fe6aac5e14
AdamKorcz <adam@adalogics.com>
2023-01-13 18:35:27 +0000

    notary: move fuzzers to cncf-fuzzing
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/notary/build.sh
+++ projects/notary/build.sh
@@ -14,15 +14,4 @@
 # limitations under the License.
 #
 ################################################################################
-
-sed 's/go 1.17/go 1.19/g' -i $SRC/notary/go.mod
-
-cp $SRC/fuzz_trustmanager_test.go $SRC/notary/trustmanager/
-printf "package trustmanager\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/notary/trustmanager/registerfuzzdep.go
-go mod tidy && go mod vendor
-compile_go_fuzzer github.com/theupdateframework/notary/cryptoservice/fuzz Fuzz fuzz
-
-mv $SRC/notary/trustmanager/keys_test.go $SRC/notary/trustmanager/keys_test_fuzz.go
-mv $SRC/notary/trustmanager/keystore_test.go $SRC/notary/trustmanager/keystore_test_fuzz.go
-compile_native_go_fuzzer github.com/theupdateframework/notary/trustmanager FuzzImportKeysSimple FuzzImportKeysSimple
-compile_native_go_fuzzer github.com/theupdateframework/notary/trustmanager FuzzImportKeysStructured FuzzImportKeysStructured
+$SRC/cncf-fuzzing/projects/notary/build.sh
