231e2217c7e0e0e7aa813b42c0cf1bf387cda698
Jonathan Metzman <metzman@chromium.org>
2023-01-30 22:03:42 -0500

    fmt

--- infra/build/functions/build_and_push_test_images.py
+++ infra/build/functions/build_and_push_test_images.py
@@ -97,11 +97,10 @@ def gcb_build_and_push_images(test_image_suffix):
     main_tag, test_tag = get_image_tags(base_image, test_image_suffix)
     test_tags.append(test_tag)
     directory = os.path.join('infra', 'base-images', base_image)
-    step = build_lib.get_docker_build_step(
-        [main_tag, test_tag],
-        directory,
-        use_buildkit_cache=True,
-        src_root='.')
+    step = build_lib.get_docker_build_step([main_tag, test_tag],
+                                           directory,
+                                           use_buildkit_cache=True,
+                                           src_root='.')
     steps.append(step)
 
   overrides = {'images': test_tags}
