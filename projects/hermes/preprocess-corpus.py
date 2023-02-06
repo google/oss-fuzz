7addd586fa2cb3358715c10b4f357aac49ca21ef
Edward <14011954+0xedward@users.noreply.github.com>
2023-02-06 07:29:45 -0500

    Remove select tests from test262, flow, esprima, and mjsunit from corpus
    Hermes's testsuite marks some tests in test262, flow, esprima and mjsunit as
    tests not to run for one reason or another (e.g. unsupported features,
    etc). This commit removes those tests from the seed corpus by removing
    the tests explicit marked to be skipped in the `SKIP_LIST` and
    `PERMANENT_SKIP_LIST` in `hermes/utils/testsuite/testsuite_skiplist.py`,
    removing tests in mjsunit that use v8 runtime functions, and excludes
    tests that have keywords from `UNSUPPORTED_FEATURES` and
    `PERMANENT_UNSUPPORTED_FEATURES` in
    `hermes/utils/testsuite/testsuite_skiplist.py` in the file contents or
    in the path name.

--- projects/hermes/preprocess-corpus.py
+++ projects/hermes/preprocess-corpus.py
@@ -13,25 +13,115 @@
 #
 ################################################################################
 
-from pathlib import Path
+from pathlib import Path, PosixPath
 import re
+import sys
+import os
+import fnmatch
+from typing import List, Set
+import shutil
 
-excluded_CVEs_or_CRBugs = [
-    'CR410030',
-    'CR445267',
-]
+excluded_CVEs_or_CRBugs = ["CR410030", "CR445267", "CR1150371"]
 
-keywords_to_exclude = [
-    'WebAssembly',
-    'Worker',
-]
+keywords_to_exclude = {
+    "WebAssembly",
+    "Worker",
+}
 
 # v8 runtime functions regex - https://chromium.googlesource.com/v8/v8/+/refs/heads/main/src/runtime/runtime.h
 v8_runtime_function_pattern = r"%\w+\("
 
-if __name__ == '__main__':
-    v8_pocs_dir = Path('./v8-vulnerabilities/pocs')
-    v8_pocs = [x for x in v8_pocs_dir.glob('**/*.js') if v8_pocs_dir.is_dir()]
+
+def get_js_files(path: str) -> Set[PosixPath]:
+    path = Path(path)
+    js_files = {x for x in path.glob("**/*.js") if path.is_dir()}
+    if not js_files:
+        print(f"No JS files found in {path}")
+    return js_files
+
+
+if __name__ == "__main__":
+    # Exclude tests marked by Hermes test suite to skip from corpus
+    test262_tests = get_js_files("./test262/test")
+    esprima_tests = get_js_files("./hermes/external/esprima/test_fixtures")
+    flow_tests = get_js_files("./hermes/external/flowtest/test/flow")
+    mjsunit_tests = get_js_files("./v8/test/mjsunit")
+
+    hermes_skiplist_path = Path("./hermes/utils/testsuite/testsuite_skiplist.py")
+    if not hermes_skiplist_path.is_file():
+        for file in os.listdir("./hermes"):
+            if fnmatch.fnmatch(file, "testsuite_skiplist.py"):
+                hermes_skiplist_path = Path(file)
+                break
+
+    shutil.copy(hermes_skiplist_path, "./testsuite_skiplist.py")
+    from testsuite_skiplist import (
+        SKIP_LIST,
+        PERMANENT_SKIP_LIST,
+        UNSUPPORTED_FEATURES,
+        PERMANENT_UNSUPPORTED_FEATURES,
+    )
+
+    # Exclude tests in mjsunit using v8 runtime functions
+    for test in mjsunit_tests.copy():
+        test_code = test.open().read()
+        if re.search(v8_runtime_function_pattern, test_code):
+            print(f"Removed: {test}")
+            test.unlink()
+            mjsunit_tests.discard(test)
+
+    # Exclude tests marked explicitly to skip in Hermes test suite
+    tests_to_remove = (
+        {test_name for test_name in test262_tests if str(test_name) in SKIP_LIST}
+        | {test_name for test_name in esprima_tests if str(test_name) in SKIP_LIST}
+        | {test_name for test_name in flow_tests if str(test_name) in SKIP_LIST}
+        | {
+            test_name
+            for test_name in mjsunit_tests
+            if str(test_name).lstrip("v8/test/") in SKIP_LIST
+        }
+        | {
+            test_name
+            for test_name in test262_tests
+            if str(test_name) in PERMANENT_SKIP_LIST
+        }
+        | {
+            test_name
+            for test_name in esprima_tests
+            if str(test_name) in PERMANENT_SKIP_LIST
+        }
+        | {
+            test_name
+            for test_name in flow_tests
+            if str(test_name) in PERMANENT_SKIP_LIST
+        }
+        | {
+            test_name
+            for test_name in mjsunit_tests
+            if str(test_name).lstrip("v8/test/") in PERMANENT_SKIP_LIST
+        }
+    )
+    for test in tests_to_remove:
+        print(f"Removed: {test}")
+        test.unlink()
+        test262_tests.discard(test)
+        esprima_tests.discard(test)
+        mjsunit_tests.discard(test)
+        flow_tests.discard(test)
+
+    # Exclude tests by keywords found in file contents
+    keywords_to_exclude.update(UNSUPPORTED_FEATURES, PERMANENT_UNSUPPORTED_FEATURES)
+    all_tests = test262_tests | esprima_tests | flow_tests | mjsunit_tests
+    for test in all_tests:
+        test_code = test.open().read()
+        for keyword in keywords_to_exclude:
+            if keyword in test_code or keyword in str(test):
+                print(f"Removed: {test}")
+                test.unlink()
+                break
+
+    # Filter v8 bugs before including in corpus
+    v8_pocs = get_js_files("./v8-vulnerabilities/pocs")
     for poc in v8_pocs:
         try:
             # Exclude specific CVEs or CR bugs
