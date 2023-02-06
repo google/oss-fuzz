7dd752f2863d58bcc35207ac46c8fdbbc89ee2ad
Jonathan Metzman <metzman@chromium.org>
2023-02-06 14:55:46 -0500

    Make many projects centipede compatible
    Previously, they would break because they incorrectly used $CC to
    link fuzz targets.

--- projects/hiredis/build.sh
+++ projects/hiredis/build.sh
@@ -21,5 +21,5 @@ mv fuzzing/format_command_fuzzer.c .
 $CC $CFLAGS -std=c99 -pedantic -c -O3 -fPIC \
 	format_command_fuzzer.c -o format_command_fuzzer.o
 
-$CC $CFLAGS -O3 -fPIC $LIB_FUZZING_ENGINE format_command_fuzzer.o \
+$CXX $CXXFLAGS -O3 -fPIC $LIB_FUZZING_ENGINE format_command_fuzzer.o \
 	-o $OUT/format_command_fuzzer libhiredis.a
