383dc91556092bd57be4035ee615a64c6fc2de1f
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-02-07 13:44:15 -0800

    perf(serde_urlencoded): Add new fuzzer, to improve coverage (#9612)
    The original fuzzer makes some assumptions on specific properties.
    Namely that Deserialise->Serialise->Deserialise was completely
    symmetrical. However support for some parts of the API only includes
    Serialising, so to fix this I've added the reverse/inverse fuzzer, which
    improves code coverage substantially.
    
    Signed-off-by: Nathaniel Brough <nathaniel.brough@gmail.com>

--- projects/serde_urlencoded/build.sh
+++ projects/serde_urlencoded/build.sh
@@ -14,5 +14,11 @@
 #
 ################################################################################
 cd $SRC/serde_urlencoded
-cargo fuzz build
-cp $SRC/serde_urlencoded/fuzz/target/x86_64-unknown-linux-gnu/release/roundtrip $OUT/
+cargo fuzz build -O
+
+FUZZ_TARGET_OUTPUT_DIR=fuzz/target/x86_64-unknown-linux-gnu/release
+for f in fuzz/fuzz_targets/*.rs
+do
+    FUZZ_TARGET_NAME=$(basename ${f%.*})
+    cp $FUZZ_TARGET_OUTPUT_DIR/$FUZZ_TARGET_NAME $OUT/
+done
