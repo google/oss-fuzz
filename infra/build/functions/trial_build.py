2aa85ce34ccb1669160b449e20e1a1508692f4e3
Jonathan Metzman <metzman@chromium.org>
2023-02-06 23:33:57 -0500

    fast test

--- infra/build/functions/trial_build.py
+++ infra/build/functions/trial_build.py
@@ -341,7 +341,9 @@ def trial_build_main(args=None, local_base_build=True):
     build_and_push_test_images.build_and_push_images(  # pylint: disable=unexpected-keyword-arg
         test_image_suffix)
   else:
-    build_and_push_test_images.gcb_build_and_push_images(test_image_suffix)
+    # !!!
+    # build_and_push_test_images.gcb_build_and_push_images(test_image_suffix)
+    pass
   return _do_test_builds(args, test_image_suffix)
 
 
