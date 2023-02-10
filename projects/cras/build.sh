73eb252b3635e47bc576c77a0560319e451d126a
Li-Yu Yu <aaronyu@google.com>
2023-02-11 03:57:06 +0800

    cras: Fix coverage build
    Like bazel_build_fuzz_tests we should copy our sources.
    https://github.com/google/oss-fuzz/blob/16de98e2cf5efe2bd34703eefd4f9bf2e0c51579/infra/base-images/base-builder/bazel_build_fuzz_tests#L73

--- projects/cras/build.sh
+++ projects/cras/build.sh
@@ -67,5 +67,25 @@ mv ${WORK}/build/fuzzer/* ${OUT}/
 
 zip -j ${OUT}/rclient_message_corpus.zip ${SRC}/adhd/cras/src/fuzz/corpus/*
 cp "${SRC}/adhd/cras/src/fuzz/cras_hfp_slc.dict" "${OUT}/cras_hfp_slc.dict"
-# Add *.rs soft link for coverage build
-ln -s ${SRC}/adhd/cras/src/server/rust/src/* ${SRC}
+
+if [ "$SANITIZER" = "coverage" ]; then
+    echo "Collecting the repository source files for coverage tracking."
+
+    ln -s ${SRC}/adhd/cras/src/server/rust/src/* ${SRC}
+
+    declare -r COVERAGE_SOURCES="${OUT}/proc/self/cwd"
+    mkdir -p "${COVERAGE_SOURCES}"
+    declare -r RSYNC_FILTER_ARGS=(
+        "--include" "*.h"
+        "--include" "*.cc"
+        "--include" "*.hpp"
+        "--include" "*.cpp"
+        "--include" "*.c"
+        "--include" "*.inc"
+        "--include" "*/"
+        "--exclude" "*"
+    )
+    rsync -avLk "${RSYNC_FILTER_ARGS[@]}" \
+        "$(bazel info execution_root)/" \
+        "${COVERAGE_SOURCES}/"
+fi
