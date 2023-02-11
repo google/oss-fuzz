a27e250a2ef4c72314679232de2ca2b2701cb4ed
David Korczynski <david@adalogics.com>
2023-02-11 03:23:20 -0800

    fix year
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/pem/fuzz_pem.py
+++ projects/pem/fuzz_pem.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ def TestOneInput(data):
 
 def main():
     atheris.instrument_all()
-    atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+    atheris.Setup(sys.argv, TestOneInput)
     atheris.Fuzz()
 
 
