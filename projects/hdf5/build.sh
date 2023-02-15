fa242ade7b753238c12df8ed110849d840850216
DavidKorczynski <david@adalogics.com>
2023-02-15 12:02:07 +0000

    hdf5: add dataset fuzzer (#9711)
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/hdf5/build.sh
+++ projects/hdf5/build.sh
@@ -39,5 +39,10 @@ cd $SRC/hdf5
 $CC $CFLAGS  -std=c99 -c \
   -I/src/hdf5/src -I/src/hdf5/build-dir/src -I./src/H5FDsubfiling/ \
   $SRC/h5_read_fuzzer.c
-
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE h5_read_fuzzer.o ./build-dir/bin/libhdf5.a -lz -o $OUT/h5_read_fuzzer
+
+
+$CC $CFLAGS  -std=c99 -c \
+  -I/src/hdf5/src -I/src/hdf5/build-dir/src -I./src/H5FDsubfiling/ \
+  $SRC/h5_extended_fuzzer.c
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE h5_extended_fuzzer.o ./build-dir/bin/libhdf5.a -lz -o $OUT/h5_extended_fuzzer
