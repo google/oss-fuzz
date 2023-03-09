0f20edc6b001425c42de3583f398ee3ad0517297
David Korczynski <david@adalogics.com>
2023-03-09 07:35:34 -0800

    black: enable pysecsan
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/black/build.sh
+++ projects/black/build.sh
@@ -16,6 +16,13 @@
 ################################################################################
 
 pip3 install .
+
+if [ "$SANITIZER" = "address" ]
+then
+  # Enable pysecsan
+  export ENABLE_PYSECSAN="1"
+fi
+
 # Build fuzzers in $OUT.
 for fuzzer in $(find $SRC -name 'fuzz_*.py'); do
   compile_python_fuzzer $fuzzer --add-data 'src/blib2to3:blib2to3'
