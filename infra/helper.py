4d5c0ee890d6c3811523ff7fda0814dc7ef0f149
Dongge Liu <donggeliu@google.com>
2023-04-03 13:35:46 +1000

    Download corpora can target multiple fuzz targets

--- infra/helper.py
+++ infra/helper.py
@@ -428,6 +428,8 @@ def get_parser():  # pylint: disable=too-many-statements,too-many-locals
   download_corpora_parser = subparsers.add_parser(
       'download_corpora', help='Download all corpora for a project.')
   download_corpora_parser.add_argument('--fuzz-target',
+                                       action='extend',
+                                       nargs='+',
                                        help='specify name of a fuzz target')
   download_corpora_parser.add_argument('--public',
                                        action='store_true',
@@ -1149,7 +1151,7 @@ def download_corpora(args):
       return False
 
   if args.fuzz_target:
-    fuzz_targets = [args.fuzz_target]
+    fuzz_targets = args.fuzz_target
   else:
     fuzz_targets = _get_fuzz_targets(args.project)
 
