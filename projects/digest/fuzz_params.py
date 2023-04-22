c293d805d87308dc7f399aa0dbc08e8e562ad11f
sg3-141-592 <5122866+sg3-141-592@users.noreply.github.com>
2023-04-22 08:15:01 +0000

    Adding coverage to digest for parser

---
+++ projects/digest/fuzz_params.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from unittest.mock import patch
+import atheris
+
+import digest
+import hashlib
+
+
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+    encoder = fdp.PickValueInList(list(hashlib.algorithms_available))
+    buffsize = str(fdp.ConsumeInt(16))
+    stdin = fdp.ConsumeBytes(sys.maxsize)
+    try:
+        with patch(
+            "sys.argv", ["python", "-b", buffsize, encoder, "fuzz_digest.py"]
+        ), patch("sys.stdin", stdin):
+            digest.main()
+    except SystemExit:
+        pass
+
+
+def main():
+    atheris.instrument_all()
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
