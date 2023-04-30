7b6076fb0d7dd323a3cbcecbe9281b55a7049943
Shane F. Carr <shane@unicode.org>
2023-04-29 18:07:17 -0700

    Update build.sh

--- projects/myanmar-tools/build.sh
+++ projects/myanmar-tools/build.sh
@@ -26,7 +26,7 @@ make all
 # Note: don't use the myanmartools_fuzz CMake target directly because we want
 # to link with LIB_FUZZING_ENGINE instead of the default fuzzer.
 $CXX $CXXFLAGS -std=c++11 -I../public -L. \
-    -Wl,-Bstatic -lunwind -llzma -lmyanmartools_static -Wl,-Bdynamic \
-    -o $OUT/zawgyi_detector_fuzz_target \
     ../zawgyi_detector_fuzz_target.cpp \
+    -Wl,-Bstatic -lmyanmartools_static -lglog -lunwind -llzma -lmyanmartools_static -Wl,-Bdynamic \
+    -o $OUT/zawgyi_detector_fuzz_target \
     $LIB_FUZZING_ENGINE
