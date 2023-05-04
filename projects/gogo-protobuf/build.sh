71f1b703dda67ac757b4bf33a2bbd8e0fe70c020
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-05-04 14:47:35 +0100

    [gogo-protobuf] initial integration (#10238)
    Signed-off-by: AdamKorcz <adam@adalogics.com>

---
+++ projects/gogo-protobuf/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eu
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+mkdir fuzzing
+cp $SRC/fuzz_test.go ./fuzzing/
+cd fuzzing
+go mod tidy
+printf "package fuzzing\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > register.go
+go mod tidy
+echo building
+compile_native_go_fuzzer github.com/gogo/protobuf/fuzzing FuzzProtoUnmarshal FuzzProtoUnmarshal
