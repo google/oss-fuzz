fcbc0aa98f55bc7a734bbd2e2448dcc26a92bb66
Henry Lin <henryharry93@gmail.com>
2023-05-24 13:56:27 +0200

    Update several projects (#10357)
    Fix several issues in yamlbeans, apache-cxf and spring-amqp.

--- projects/spring-amqp/project-parent/fuzz-targets/src/test/java/com/example/AllowedListDeserializingMessageConverterFuzzer.java
+++ projects/spring-amqp/project-parent/fuzz-targets/src/test/java/com/example/AllowedListDeserializingMessageConverterFuzzer.java
@@ -1,7 +1,7 @@
-// Copyright 2022 Google LLC
+// Copyright 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in co  mpliance with the License.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -12,26 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
+package com.example;
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.code_intelligence.jazzer.api.FuzzerSecurityIssueLow;
-
-import org.springframework.amqp.core.MessageDeliveryMode;
-import org.springframework.amqp.support.converter.*;
-
-import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import com.code_intelligence.jazzer.junit.FuzzTest;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.support.converter.*;
+
+import java.util.*;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 
-public class AllowedListDeserializingMessageConverterFuzzer {
-    public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+class AllowedListDeserializingMessageConverterFuzzer {
+    @FuzzTest
+    void myFuzzTest(FuzzedDataProvider data) {
         int cn = 29;
         int dummyNum = data.consumeInt(0, 2^cn - 1);
         DummyClass dummyClass = new DummyClass();
@@ -46,6 +48,7 @@ public class AllowedListDeserializingMessageConverterFuzzer {
                             } catch (NullPointerException e) {
                             }
                         }
+                        break;
                     case 1:
                         for (int i = 0; i < data.consumeInt(0, 50); i++) {
                             try {
@@ -53,6 +56,7 @@ public class AllowedListDeserializingMessageConverterFuzzer {
                             } catch (NullPointerException e) {
                             }
                         }
+                        break;
                     case 2:
                         for (int i = 0; i < data.consumeInt(0, 50); i++) {
                             try {
@@ -60,6 +64,7 @@ public class AllowedListDeserializingMessageConverterFuzzer {
                             } catch (NullPointerException e) {
                             }
                         }
+                        break;
                     case 3:
                         for (int i = 0; i < data.consumeInt(0, 50); i++) {
                             try {
@@ -67,88 +72,114 @@ public class AllowedListDeserializingMessageConverterFuzzer {
                             } catch (NullPointerException e) {
                             }
                         }
+                        break;
                     case 4:
                         Integer[] integerArr = new Integer[data.consumeInt(0, 50)];
                         for (int i = 0; i < integerArr.length; i++) {
                             integerArr[i] = data.consumeInt();
                         }
                         dummyClass._asList = Arrays.asList(integerArr);
+                        break;
                     case 5:
                         dummyClass._intArray = data.consumeInts(50);
+                        break;
                     case 6:
                         dummyClass._longArray = data.consumeLongs(50);
+                        break;
                     case 7:
                         dummyClass._shortArray = data.consumeShorts(50);
+                        break;
                     case 8:
                         float[] floatArr = new float[data.consumeInt(0, 50)];
                         for (int i = 0; i < floatArr.length; i++) {
                             floatArr[i] = data.consumeFloat();
                         }
                         dummyClass._floatArray = floatArr;
+                        break;
                     case 9:
                         double[] doubleArr = new double[data.consumeInt(0, 50)];
                         for (int i = 0; i < doubleArr.length; i++) {
                             doubleArr[i] = data.consumeDouble();
                         }
                         dummyClass._doubleArray = doubleArr;
+                        break;
                     case 10:
                         dummyClass._byteArray = data.consumeBytes(50);
+                        break;
                     case 11:
                         char[] charArr = new char[data.consumeInt(0, 50)];
                         for (int i = 0; i < charArr.length; i++) {
                             charArr[i] = data.consumeChar();
                         }
                         dummyClass._charArray = charArr;
+                        break;
                     case 12:
                         dummyClass._boolArray = data.consumeBooleans(50);
+                        break;
                     case 13:
                         String[] strArr = new String[data.consumeInt(0, 50)];
                         for (int i = 0; i < strArr.length; i++) {
                             strArr[i] = data.consumeString(50);
                         }
                         dummyClass._stringArray = strArr;
+                        break;
                     case 14:
                         BitSet bitSet = BitSet.valueOf(data.consumeLongs(50));
                         dummyClass._bitSet = bitSet;
+                        break;
                     case 15:
                         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                         try {
                             dummyClass._date = dateFormat.parse(data.consumeString(50));
                         } catch (ParseException e) {
                         }
+                        break;
                     case 16:
                         TimeZone tz = TimeZone.getTimeZone(data.consumeString(50));
                         dummyClass._timeZone = tz;
+                        break;
                     case 17:
                         Calendar calendar = new GregorianCalendar(data.consumeInt(),data.consumeInt(),data.consumeInt());
                         dummyClass._calendar = calendar;
+                        break;
                     case 18:
                         Locale locale = new Locale(data.consumeString(50));
                         dummyClass._locale = locale;
+                        break;
                     case 19:
                         Integer[] integerArr2 = new Integer[data.consumeInt(0, 50)];
                         for (int i = 0; i < integerArr2.length; i++) {
                             integerArr2[i] = data.consumeInt();
                         }
                         dummyClass._integerArray = integerArr2;
+                        break;
                     case 20:
                         dummyClass._boolean = data.consumeBoolean();
+                        break;
                     case 21:
                         dummyClass._char = data.consumeChar();
+                        break;
                     case 22:
                         dummyClass._byte = data.consumeByte();
+                        break;
                     case 23:
                         dummyClass._short = data.consumeShort();
+                        break;
                     case 24:
                         dummyClass._int = data.consumeInt();
+                        break;
                     case 25:
                         dummyClass._float = data.consumeFloat();
+                        break;
                     case 26:
                         dummyClass._long = data.consumeLong();
+                        break;
                     case 27:
                         dummyClass._double = data.consumeDouble();
+                        break;
                     case 28:
                         dummyClass._string = data.consumeString(50);
+                        break;
                 }
             }
         }
@@ -170,7 +201,6 @@ public class AllowedListDeserializingMessageConverterFuzzer {
         String str14 = data.consumeString(100);
         String str15 = data.consumeString(100);
         String str16 = data.consumeString(100);
-
 
         SerializerMessageConverter converter = new SerializerMessageConverter();
         converter.setAllowedListPatterns(Collections.singletonList(""));
@@ -208,7 +238,7 @@ public class AllowedListDeserializingMessageConverterFuzzer {
         throw new FuzzerSecurityIssueLow("Bypass allowed list!");
     }
 
-        public static class DummyClass implements Serializable {
+    public static class DummyClass implements Serializable {
         public TreeMap<String, Integer> _treeMap;
         public List<String> _arrayList;
         public Set<String> _hashSet;
