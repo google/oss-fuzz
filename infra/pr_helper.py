ba38b2a99e08b0547f094a713dc4eca7b0210490
Holly Gong <gongh@google.com>
2023-06-02 15:13:50 +1000

    resolve comments

--- infra/pr_helper.py
+++ infra/pr_helper.py
@@ -29,65 +29,13 @@ GITHUB_URL = 'https://github.com/'
 API_URL = 'https://api.github.com'
 BASE_URL = f'{API_URL}/repos/{OWNER}/{REPO}'
 BRANCH = 'master'
-
-
-def get_projects_path(pr_number, headers):
-  """Gets the current project path."""
-  response = requests.get(f'{BASE_URL}/pulls/{pr_number}/files',
-                          headers=headers)
-
-  projects_path = set()
-  for file in response.json():
-    file_path = file['filename']
-    dir_path = os.path.dirname(file_path)
-    if 'projects' in dir_path:
-      projects_path.add(dir_path)
-  return list(projects_path)
-
-
-def get_author_email(pr_number, headers):
-  """Retrieves the author's email address for a pull request,
-  including non-public emails."""
-  commits_response = requests.get(f'{BASE_URL}/pulls/{pr_number}/commits',
-                                  headers=headers)
-  if not commits_response.ok:
-    return None
-  email = commits_response.json()[0]['commit']['author']['email']
-  return email
-
-
-def get_project_yaml(project_path, headers):
-  """Gets the project yaml file."""
-  contents_url = f'{BASE_URL}/contents/{project_path}/project.yaml'
-  return get_yaml_file_content(contents_url, headers)
-
-
-def get_yaml_file_content(contents_url, headers):
-  """Gets yaml file content."""
-  response = requests.get(contents_url, headers=headers)
-  if not response.ok:
-    return {}
-  content = base64.b64decode(response.json()['content']).decode('UTF-8')
-  return yaml.safe_load(content)
-
-
-def get_integrated_project_info(pr_number, headers):
-  """Gets the new integrated project."""
-  response = requests.get(f'{BASE_URL}/pulls/{pr_number}/files',
-                          headers=headers)
-
-  for file in response.json():
-    file_path = file['filename']
-    if 'project.yaml' in file_path:
-      return get_yaml_file_content(file['contents_url'], headers)
-
-  return None
+CRITICALITY_SCORE_PATH = '/home/runner/go/bin/criticality_score'
 
 
 def get_criticality_score(repo_url):
   """Gets the criticality score of the project."""
   report = subprocess.run([
-      '/home/runner/go/bin/criticality_score', '--format', 'json',
+      CRITICALITY_SCORE_PATH, '--format', 'json',
       '-gcp-project-id=clusterfuzz-external', '-depsdev-disable', repo_url
   ],
                           capture_output=True,
@@ -104,45 +52,6 @@ def is_known_contributor(content, email):
           email in content.get('auto_ccs', []))
 
 
-def has_author_modified_project(project_path, pr_author, headers):
-  """Checks if the author has modified this project before."""
-  commits_response = requests.get(
-      f'{BASE_URL}/commits?path={project_path}&author={pr_author}',
-      headers=headers)
-
-  if not commits_response.ok or not commits_response.json():
-    return False
-
-  commit = commits_response.json()[0]
-  return True, commit['sha']
-
-
-def get_pull_request_url(commit, headers):
-  """Gets the pull request url."""
-  pr_response = requests.get(f'{BASE_URL}/commits/{commit}/pulls',
-                             headers=headers)
-  if not pr_response.ok:
-    return None
-  return pr_response.json()[0]['html_url']
-
-
-def is_author_internal_member(pr_author, headers):
-  """Returns if the author is an internal member."""
-  response = requests.get(f'{BASE_URL}/contents/infra/MAINTAINERS.csv',
-                          headers=headers)
-  if not response.ok:
-    return False
-
-  maintainers = base64.b64decode(response.json()['content']).decode('UTF-8')
-  for line in maintainers.split('\n'):
-    print(f"username: {line.split(',')[2]}")
-    if pr_author == line.split(',')[2]:
-      save_env(None, None, True)
-      return True
-
-  return False
-
-
 def save_env(message, is_ready_for_merge, is_internal=False):
   """Saves the outputs as environment variables."""
   with open(os.environ['GITHUB_ENV'], 'a') as github_env:
@@ -153,38 +62,36 @@ def save_env(message, is_ready_for_merge, is_internal=False):
 
 def main():
   """Verifies if a PR is ready for merge."""
-  token = os.environ['GITHUBTOKEN']
-  pr_author = os.environ['PRAUTHOR']
-  pr_number = os.environ['PRNUMBER']
-  os.environ['GITHUB_AUTH_TOKEN'] = token
-  headers = {
-      'Authorization': f'Bearer {token}',
-      'X-GitHub-Api-Version': '2022-11-28'
-  }
   message = ''
   is_ready_for_merge = True
+  github = GithubHandler()
+  pr_author = github.get_pr_author()
 
   # Bypasses PRs of the internal members.
-  if is_author_internal_member(pr_author, headers):
-    return
+  # if github.is_author_internal_member():
+  #   return
 
   # Gets all modified projects path.
-  projects_path = get_projects_path(pr_number, headers)
-  email = get_author_email(pr_number, headers)
+  projects_path = github.get_projects_path()
+  email = github.get_author_email()
 
   for project_path in projects_path:
     project_url = f'{GITHUB_URL}/{OWNER}/{REPO}/tree/{BRANCH}/{project_path}'
-    content_dict = get_project_yaml(project_path, headers)
+    content_dict = github.get_project_yaml(project_path)
 
     # Gets information for the new integrating project
     if not content_dict:
       is_ready_for_merge = False
-      new_project = get_integrated_project_info(pr_number, headers)
+      new_project = github.get_integrated_project_info()
       repo_url = new_project.get('main_repo')
       if repo_url is not None:
         message += (f'@{pr_author} is integrating a new project:<br/>'
                     f'- Main repo: {repo_url}<br/> - Criticality score: '
                     f'{get_criticality_score(repo_url)}<br/>')
+      else:
+        message += (f'@{pr_author} is integrating a new project, '
+                    'but the `repo_url` is missing. '
+                    'The criticality score cannot be computed.<br/>')
       continue
 
     # Checks if the author is in the contact list.
@@ -195,7 +102,7 @@ def main():
       continue
 
     # Checks the previous commits.
-    has_commit = has_author_modified_project(project_path, pr_author, headers)
+    has_commit = github.has_author_modified_project(project_path)
     if not has_commit:
       message += (
           f'@{pr_author} is a new contributor to '
@@ -210,7 +117,7 @@ def main():
                   f'[{project_path}]({project_url}). The previous commit was '
                   f'{GITHUB_URL}/{OWNER}/{REPO}/commit/{commit_sha}<br/>')
 
-    pr_url = get_pull_request_url(commit_sha, headers)
+    pr_url = github.get_pull_request_url(commit_sha)
     if pr_url is not None:
       pr_message = (f'@{pr_author} has previously contributed to '
                     f'[{project_path}]({project_url}). '
@@ -218,6 +125,107 @@ def main():
     message += pr_message
 
   save_env(message, is_ready_for_merge, False)
+
+
+class GithubHandler:
+  """Github requests handler."""
+
+  def __init__(self):
+    self._pr_author = os.environ['PRAUTHOR']
+    self._token = os.environ['GITHUBTOKEN']
+    self._pr_number = os.environ['PRNUMBER']
+    self._headers = {
+        'Authorization': f'Bearer {self._token}',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+    os.environ['GITHUB_AUTH_TOKEN'] = self._token
+
+  def get_pr_author(self):
+    """Gets the pr author user name."""
+    return self._pr_author
+
+  def get_projects_path(self):
+    """Gets the current project path."""
+    response = requests.get(f'{BASE_URL}/pulls/{self._pr_number}/files',
+                            headers=self._headers)
+
+    projects_path = set()
+    for file in response.json():
+      file_path = file['filename']
+      dir_path = os.path.dirname(file_path)
+      if dir_path is not None and dir_path.split('/')[0] == 'projects':
+        projects_path.add(dir_path)
+    return list(projects_path)
+
+  def get_author_email(self):
+    """Retrieves the author's email address for a pull request,
+    including non-public emails."""
+    commits_response = requests.get(
+        f'{BASE_URL}/pulls/{self._pr_number}/commits', headers=self._headers)
+    if not commits_response.ok:
+      return None
+    email = commits_response.json()[0]['commit']['author']['email']
+    return email
+
+  def get_project_yaml(self, project_path):
+    """Gets the project yaml file."""
+    contents_url = f'{BASE_URL}/contents/{project_path}/project.yaml'
+    return self.get_yaml_file_content(contents_url)
+
+  def get_yaml_file_content(self, contents_url):
+    """Gets yaml file content."""
+    response = requests.get(contents_url, headers=self._headers)
+    if not response.ok:
+      return {}
+    content = base64.b64decode(response.json()['content']).decode('UTF-8')
+    return yaml.safe_load(content)
+
+  def get_integrated_project_info(self):
+    """Gets the new integrated project."""
+    response = requests.get(f'{BASE_URL}/pulls/{self._pr_number}/files',
+                            headers=self._headers)
+
+    for file in response.json():
+      file_path = file['filename']
+      if 'project.yaml' in file_path:
+        return self.get_yaml_file_content(file['contents_url'])
+
+    return None
+
+  def get_pull_request_url(self, commit):
+    """Gets the pull request url."""
+    pr_response = requests.get(f'{BASE_URL}/commits/{commit}/pulls',
+                               headers=self._headers)
+    if not pr_response.ok:
+      return None
+    return pr_response.json()[0]['html_url']
+
+  def is_author_internal_member(self):
+    """Returns if the author is an internal member."""
+    response = requests.get(f'{BASE_URL}/contents/infra/MAINTAINERS.csv',
+                            headers=self._headers)
+    if not response.ok:
+      return False
+
+    maintainers = base64.b64decode(response.json()['content']).decode('UTF-8')
+    for line in maintainers.split('\n'):
+      if self._pr_author == line.split(',')[2]:
+        save_env(None, None, True)
+        return True
+
+    return False
+
+  def has_author_modified_project(self, project_path):
+    """Checks if the author has modified this project before."""
+    commits_response = requests.get(
+        f'{BASE_URL}/commits?path={project_path}&author={self._pr_author}',
+        headers=self._headers)
+
+    if not commits_response.ok or not commits_response.json():
+      return False
+
+    commit = commits_response.json()[0]
+    return True, commit['sha']
 
 
 if __name__ == '__main__':
