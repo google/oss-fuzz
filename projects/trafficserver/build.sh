0d99caf2c22f438e87bc001baff448dcb9b5e896
Kit Chan <kichan@apache.org>
2023-06-08 02:17:50 -0700

    Turn on FuzzHTTP fuzzer on trafficserver project (#10460)
    Co-authored-by: Oliver Chang <oliverchang@users.noreply.github.com>

--- projects/trafficserver/build.sh
+++ projects/trafficserver/build.sh
@@ -14,14 +14,21 @@
 # limitations under the License.
 #
 ################################################################################
+
+# don't use __cxa_atexit for coverage sanitizer
+if [[ $SANITIZER = coverage ]]
+then
+    export CXXFLAGS="$CXXFLAGS -fno-use-cxa-atexit"
+fi
+
 autoreconf -if
-./configure --enable-debug
+./configure --enable-debug --enable-malloc-allocator
 make -j$(nproc) --ignore-errors
 
 pushd fuzzer/
 make
 cp FuzzEsi $OUT/FuzzEsi
-#cp FuzzHTTP $OUT/FuzzHTTP
+cp FuzzHTTP $OUT/FuzzHTTP
 popd
 
 pushd $SRC/oss-fuzz-bloat/trafficserver/
@@ -29,6 +36,7 @@ cp FuzzEsi_seed_corpus.zip $OUT/FuzzEsi_seed_corpus.zip
 cp FuzzHTTP_seed_corpus.zip $OUT/FuzzHTTP_seed_corpus.zip
 popd
 
-mkdir $OUT/lib
+mkdir -p $OUT/lib
+cp lib/swoc/.libs/libtsswoc*.so* $OUT/lib/.
 cp src/tscore/.libs/libtscore.so* $OUT/lib/.
 cp src/tscpp/util/.libs/libtscpputil.so* $OUT/lib/.
