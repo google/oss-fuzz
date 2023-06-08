dc4326c34b817c5c9d91f80be396fd9f42329de6
Jonathan Metzman <metzman@chromium.org>
2023-06-08 12:01:32 -0400

    only

--- infra/build/functions/build_and_run_coverage.py
+++ infra/build/functions/build_and_run_coverage.py
@@ -265,7 +265,8 @@ def get_build_steps(  # pylint: disable=too-many-locals, too-many-arguments
   # # Corpus research steps.
   # if config.corpus_research:
   # !!!
-  build_steps.extend(get_corpus_research_steps(project, config))
+  # build_steps.extend(get_corpus_research_steps(project, config))
+  build_steps = get_corpus_research_steps(project, config)
 
   build_steps.append(
       build_lib.http_upload_step(latest_report_info_body,
@@ -287,7 +288,45 @@ def get_corpus_research_steps(project, config):
           '/workspace/research-corpus'
       ],
   })
+  #   build_steps.append({
+  #     'name': build_lib.get_runner_image_name(config.test_image_suffix),
+  #     'env': coverage_env,
+  #     'args': [
+  #         'bash', '-c',
+  #         ('for f in /corpus/*.zip; do unzip -q $f -d ${f%.*} || ('
+  #          'echo "Failed to unpack the corpus for $(basename ${f%.*}). '
+  #          'This usually means that corpus backup for a particular fuzz '
+  #          'target does not exist. If a fuzz target was added in the last '
+  #          '24 hours, please wait one more day. Otherwise, something is '
+  #          'wrong with the fuzz target or the infrastructure, and corpus '
+  #          'pruning task does not finish successfully." && exit 1'
+  #          '); done && coverage || (echo "' + failure_msg + '" && false)')
+  #     ],
+  #     'volumes': [{
+  #         'name': 'corpus',
+  #         'path': '/corpus'
+  #     }],
+  # })
   # 2.
+  # steps.append({
+  #     'name': 'gcr.io/oss-fuzz-base/base-runner-research',
+
+  #     'args': [
+  #         '-m', 'cp', '-r',
+  #         f'gs://oss-fuzz-corpus-research/corpus/{project.name}',
+  #         '/workspace/research-corpus'
+  #     ],
+  #     'volumes': [{
+  #         'name': 'corpus',
+  #         'path': '/corpus'
+  #     },
+  #     {
+  #         'name': 'research-corpus',
+  #         'path': '/research-corpus'
+  #     }
+  #                 ],
+
+  # })
   return steps
 
 
