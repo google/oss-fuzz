19e3caf153cf8b0b721b101a3c76cfdddb3d29d2
Philippe Antoine <p.antoine@catenacyber.fr>
2023-07-03 14:06:27 +0200

    openssl: special patch to get target for NAME_CONSTRAINTS_check

--- projects/openssl/build.sh
+++ projects/openssl/build.sh
@@ -63,6 +63,9 @@ function build_fuzzers() {
 }
 
 cd $SRC/openssl/
+# special patch to get NAME_CONSTRAINTS_check/punycode
+sed -i -e 's/DECLARE_ASN1_ALLOC_FUNCTIONS(NAME_CONSTRAINTS)/DECLARE_ASN1_FUNCTIONS(NAME_CONSTRAINTS)/' include/openssl/x509v3.h.in
+sed -i -e 's/IMPLEMENT_ASN1_ALLOC_FUNCTIONS(NAME_CONSTRAINTS)/IMPLEMENT_ASN1_FUNCTIONS(NAME_CONSTRAINTS)/' crypto/x509/v3_ncons.c
 build_fuzzers ""
 $CC $CFLAGS -c fuzz/driver.c -o fuzz/driver.o -I include
 ls include/openssl/*.h.in | while read i; do python3 $SRC/ngolo.py $i; done
