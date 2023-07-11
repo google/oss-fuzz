ec26870f583f554a732be57109c7234cc39306f1
David Korczynski <david@adalogics.com>
2023-07-10 17:26:09 -0700

    nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/heimdal/fuzz_json.c
+++ projects/heimdal/fuzz_json.c
@@ -1,3 +1,15 @@
+/* Copyright 2023 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -6,11 +18,11 @@
 #include "heimbase.h"
 
 int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
-  // Creat null-terminated string
   char *null_terminated = (char *)malloc(size + 1);
   memcpy(null_terminated, (char *)data, size);
   null_terminated[size] = '\0';
 
+  // Call various json handlers
   heim_error_t error = NULL;
   heim_object_t o = heim_json_create(null_terminated, 20, 0, &error);
   if (error == NULL) {
