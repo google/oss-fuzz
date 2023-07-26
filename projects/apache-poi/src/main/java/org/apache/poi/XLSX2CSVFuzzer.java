70329be8763f653b995ee6c32d4db43777c5e173
Dominik Stadler <dominik.stadler@gmx.at>
2023-07-26 08:54:35 +0200

    Add license header and fix IDE warnings

--- projects/apache-poi/src/main/java/org/apache/poi/XLSX2CSVFuzzer.java
+++ projects/apache-poi/src/main/java/org/apache/poi/XLSX2CSVFuzzer.java
@@ -1,3 +1,19 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
 package org.apache.poi;
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
@@ -17,7 +33,7 @@ import org.xml.sax.SAXException;
 
 public class XLSX2CSVFuzzer {
 
-    private FuzzedDataProvider fuzzedDataProvider;
+    private final FuzzedDataProvider fuzzedDataProvider;
 
     public XLSX2CSVFuzzer(FuzzedDataProvider fuzzedDataProvider) {
         this.fuzzedDataProvider = fuzzedDataProvider;
@@ -28,7 +44,7 @@ public class XLSX2CSVFuzzer {
             final ByteArrayOutputStream baos = new ByteArrayOutputStream();
             PrintStream out = new PrintStream(baos, true, StandardCharsets.UTF_8.name());
             String string = fuzzedDataProvider.consumeRemainingAsString();
-            InputStream in = new ByteArrayInputStream(string.getBytes("UTF-8"));
+            InputStream in = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
             OPCPackage p = OPCPackage.open(in);
             XLSX2CSV xlsx2csv = new XLSX2CSV(p, out, 5);
             xlsx2csv.process();
