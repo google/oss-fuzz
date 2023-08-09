27a40582e3d0177c8849754dbe331754caeb7d59
Henry Lin <henryharry93@gmail.com>
2023-08-09 19:51:21 -0400

    Fix coverage build failure for multiple Java projects (#10766)
    Fix coverage build failure for:
    
    java-jwt
    ion-java
    g-http-java-client
    jose4j
    apache-commons-text
    jsqlparser
    tablesaw
    zt-zip
    json-flattener
    opencsv
    calcite
    mvel
    spring-amqp
    univocity-parsers
    async-http-client
    pdfbox
    itext7
    presto

--- projects/opencsv/build.sh
+++ projects/opencsv/build.sh
@@ -19,7 +19,7 @@ PROJECT=opencsv
 PROJECT_GROUP_ID=com.opencsv
 PROJECT_ARTIFACT_ID=opencsv
 MAIN_REPOSITORY=https://git.code.sf.net/p/opencsv/source/
-MAVEN_ARGS="-Djavac.src.version=15 -Djavac.target.version=15 -Denforcer.skip=true -DskipTests -Dgpg.skip"
+MAVEN_ARGS="-Djavac.src.version=11 -Djavac.target.version=11 -Denforcer.skip=true -DskipTests -Dgpg.skip"
 
 function set_project_version_in_fuzz_targets_dependency {
   PROJECT_VERSION=$(cd $PROJECT && $MVN org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
@@ -66,16 +66,16 @@ else
   # LLVMFuzzerTestOneInput comment for fuzzer detection by infrastructure.
   this_dir=\$(dirname \"\$0\")
   if [[ \"\$@\" =~ (^| )-runs=[0-9]+($| ) ]]; then
-    mem_settings='-Xmx1900m -Xss900k'
+    mem_settings='-Xmx1900m:-Xss900k'
   else
-    mem_settings='-Xmx2048m -Xss1024k'
+    mem_settings='-Xmx2048m:-Xss1024k'
   fi
-  java -cp $RUNTIME_CLASSPATH \
-  \$mem_settings \
-  com.code_intelligence.jazzer.Jazzer \
-  --target_class=com.example.$fuzzer_basename \
-  --disabled_hooks=com.code_intelligence.jazzer.sanitizers.RegexInjection \
-  \$@" > $OUT/$fuzzer_basename
+  LD_LIBRARY_PATH=\"$JVM_LD_LIBRARY_PATH\":\$this_dir \
+\$this_dir/jazzer_driver --agent_path=\$this_dir/jazzer_agent_deploy.jar \
+--cp=$RUNTIME_CLASSPATH \
+--target_class=com.example.$fuzzer_basename \
+--jvm_args=\"\$mem_settings\" \
+\$@" > $OUT/$fuzzer_basename
     chmod u+x $OUT/$fuzzer_basename
   done
 
