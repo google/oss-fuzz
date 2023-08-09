bc91e81ca6202a206fc00abe024b5dd5808a2bc6
434b <17012133+0xricksanchez@users.noreply.github.com>
2023-08-09 14:52:21 +0200

    [sharp]: update fuzz harness (#10813)
    Cleans up and improves the fuzz harness for the JavaScript library
    `sharp` a bit, as the earlier harness had some issues like e.g.: faulty
    API calls

--- projects/sharp/fuzz.js
+++ projects/sharp/fuzz.js
@@ -36,96 +36,135 @@ module.exports.fuzz = async function(data) {
     };
     const width = provider.consumeIntegralInRange(1, 10000);
     const height = provider.consumeIntegralInRange(1, 10000);
-    const gravity = provider.consumeString(provider.consumeIntegralInRange(0, 64));
+    const gravity = provider.consumeString(provider.consumeIntegralInRange(0, 1000));
     const top = provider.consumeIntegralInRange(1, 1000);
     const bottom = provider.consumeIntegralInRange(1, 1000);
     const left = provider.consumeIntegralInRange(1, 1000);
     const right = provider.consumeIntegralInRange(1, 1000);
-    const background = provider.consumeString(provider.consumeIntegralInRange(0, 64));
+    const background = provider.consumeString(provider.consumeIntegralInRange(0, 1000));
     const withMeta = provider.consumeBoolean();
-    const input = Buffer.from(provider.consumeRemainingAsBytes());
-
-    sharp(input)
-      .resize(width, height)
-      .toBuffer((_err, _out) => { });
-
     const degrees = provider.consumeIntegralInRange(0, 361);
-    sharp(input)
-      .rotate(degrees)
-      .toBuffer((_err, _out) => { });
+    const gamma = provider.consumeFloatInRange(0.0, 4.0);
+    var input = "";
+    if (provider.consumeBoolean() == true) {
+      input = Buffer.from(provider.consumeBytes(provider.consumeIntegralInRange(1, 100000)));
+    } else {
+      input = Buffer.from(provider.consumeRemainingAsBytes());
+    }
+    if (input.length == 0) {
+      return;
+    }
 
-    sharp(input)
-      .flip()
-      .toBuffer((_err, _out) => { });
 
-    sharp(input)
-      .flop()
-      .toBuffer((_err, _out) => { });
+    switch (provider.consumeIntegralInRange(0, 20)) {
+      case 0:
+        sharp(input)
+          .rotate(degrees)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 1:
+        sharp(input)
+          .flip()
+          .toBuffer((_err, _out) => { });
+        break;
+      case 2:
+        sharp(input)
+          .flop()
+          .toBuffer((_err, _out) => { });
+        break;
+      case 3:
+        sharp(input)
+          .grayscale()
+          .toBuffer((_err, _out) => { });
+        break;
+      case 4:
+        sharp(input, options)
+          .composite([{ input: input, gravity: gravity }], options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 5:
+        sharp(input, options)
+          .extend({ top: top, bottom: bottom, left: left, right: right, background: background }, options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 6:
+        sharp(input, options)
+          .trim(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 7:
+        sharp(input, options)
+          .median(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 8:
+        sharp(input, options)
+          .unflatten(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 9:
+        sharp(input, options)
+          .flatten(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 10:
+        sharp(input, options)
+          .gamma(gamma)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 11:
+        sharp(input, options)
+          .gif(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 12:
+        sharp(input, options)
+          .clahe(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 13:
+        //console.log(`case 13`);
+        sharp(input, options)
+          .withMetadata(withMeta)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 14:
+        sharp(input, options)
+          .jpeg(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 15:
+        sharp(input, options)
+          .png(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 16:
+        sharp(input, options)
+          .webp(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 17:
+        sharp(input, options)
+          .tiff(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 18:
+        sharp(input, options)
+          .avif(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 19:
+        sharp(input, options)
+          .negate(options)
+          .toBuffer((_err, _out) => { });
+        break;
+      case 20:
+        sharp(input)
+          .resize(width, height)
+          .toBuffer((_err, _out) => { });
+        break;
 
-    sharp(input)
-      .grayscale()
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .composite([{ input: input, gravity: gravity }], options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .extend({ top: top, bottom: bottom, left: left, right: right, background: background }, options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .trim(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .flatten(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .background(background, options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .embed(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .max()
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .min()
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .ignoreAspectRatio()
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .withMetadata(withMeta)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .jpeg(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .png(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .webp(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .tiff(options)
-      .toBuffer((_err, _out) => { });
-
-    sharp(input, options)
-      .avif(options)
-      .toBuffer((_err, _out) => { });
-
+    }
   } catch (error) {
     if (!ignoredError(error)) throw error;
   }
@@ -139,7 +178,10 @@ const ignored = [
   "Input Buffer is empty",
   "Expected valid gravity",
   "Expected one of:",
-  "Expected integer between"
+  "Expected integer",
+  "Expected number",
+  "Expected object or string",
+  "Unable to parse",
 ];
 
 
