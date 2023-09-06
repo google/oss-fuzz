c838ec6116535e50da9218932bf451d5eb3fd4da
Dominik Stadler <dominik.stadler@gmx.at>
2023-09-06 11:20:23 +0200

    apache-poi: expect one more exception (#10919)
    One more expected exception when font-libraries are missing and add
    slightly more coverage.
    
    This should fix
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=61501

--- projects/apache-poi/src/main/java/org/apache/poi/POIXSSFFuzzer.java
+++ projects/apache-poi/src/main/java/org/apache/poi/POIXSSFFuzzer.java
@@ -37,10 +37,12 @@ public class POIXSSFFuzzer {
 				swb.write(NullOutputStream.INSTANCE);
 			}
 		} catch (NoClassDefFoundError e) {
-			// only allow one missing class related to Font-handling
+			// only allow some missing classes related to Font-handling
 			// we cannot install JDK font packages in oss-fuzz images currently
 			// see https://github.com/google/oss-fuzz/issues/7380
-			if (!e.getMessage().contains("java.awt.Font")) {
+			if (!e.getMessage().contains("java.awt.Font") &&
+					!e.getMessage().contains("java.awt.Toolkit") &&
+					!e.getMessage().contains("sun.awt.X11FontManager")) {
 				throw e;
 			}
 		} catch (IOException | POIXMLException | RecordFormatException | IllegalStateException |
@@ -51,6 +53,15 @@ public class POIXSSFFuzzer {
 		try (OPCPackage pkg = OPCPackage.open(new ByteArrayInputStream(input))) {
 			try (XSSFEventBasedExcelExtractor extractor = new XSSFEventBasedExcelExtractor(pkg)) {
 				POIFuzzer.checkExtractor(extractor);
+			}
+		} catch (NoClassDefFoundError e) {
+			// only allow some missing classes related to Font-handling
+			// we cannot install JDK font packages in oss-fuzz images currently
+			// see https://github.com/google/oss-fuzz/issues/7380
+			if (!e.getMessage().contains("java.awt.Font") &&
+					!e.getMessage().contains("java.awt.Toolkit") &&
+					!e.getMessage().contains("sun.awt.X11FontManager")) {
+				throw e;
 			}
 		} catch (IOException | XmlException | OpenXML4JException | POIXMLException | RecordFormatException |
 				IllegalStateException | IllegalArgumentException e) {
