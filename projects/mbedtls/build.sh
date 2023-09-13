ab017e5c62cd1ab11e806b7843d54ed2489d1158
Gilles Peskine <gilles.peskine@arm.com>
2023-09-13 19:49:03 +0200

    mbedtls: test experimental features (#9824)
    In the mbedtls fuzzer, enable some non-default features that are
    supposed to be secure and so should be fuzzed. In particular:
    
    * Test both with and without `MBEDTLS_USE_PSA_CRYPTO` (which has a major
    impact on X.509 and TLS).
    * Test both with and without `MBEDTLS_ECDH_VARIANT_EVEREST_ENABLED` and
    `MBEDTLS_RSA_NO_CRT` (which affect X25519 and RSA respectively).
    * Enable non-default features (in particular, at this time, PKCS#7).
    
    Fixes https://github.com/Mbed-TLS/mbedtls/issues/6708.
    
    ---------
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>
    Co-authored-by: jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>

--- projects/mbedtls/build.sh
+++ projects/mbedtls/build.sh
@@ -15,17 +15,23 @@
 #
 ################################################################################
 
-# build project
-perl scripts/config.pl set MBEDTLS_PLATFORM_TIME_ALT
+# build project (default configuration + experimental features)
+scripts/config.py full
+# Allow repeatability of time-based calculations.
+scripts/config.py set MBEDTLS_PLATFORM_TIME_ALT
+# MBEDTLS_USE_PSA_CRYPTO significantly changes how X.509 and TLS work.
+# In this build, use the default setting.
+scripts/config.py unset MBEDTLS_USE_PSA_CRYPTO
 mkdir build
 cd build
 cmake -DENABLE_TESTING=OFF ..
 # build including fuzzers
 make -j$(nproc) all
 cp programs/fuzz/fuzz_* $OUT/
+cd ..
 
 # build corpuses
-cd ../programs
+cd programs
 cp -r ../../openssl/fuzz/corpora/crl fuzz/corpuses/
 cp -r ../../openssl/fuzz/corpora/x509 fuzz/corpuses/
 cp -r ../../boringssl/fuzz/privkey_corpus fuzz/corpuses/
@@ -39,8 +45,35 @@ zip -r fuzz/fuzz_dtlsclient_seed_corpus.zip fuzz/corpuses/dtlsclient
 zip -r fuzz/fuzz_dtlsserver_seed_corpus.zip fuzz/corpuses/dtlsserver
 zip -r fuzz/fuzz_client_seed_corpus.zip fuzz/corpuses/client
 zip -r fuzz/fuzz_server_seed_corpus.zip fuzz/corpuses/server
-
 cd fuzz
 # export other associated stuff
 cp *.options $OUT/
 cp fuzz_*_seed_corpus.zip $OUT/
+cd ../..
+
+# build project (some non-default compile-time settings)
+scripts/config.py full
+# Allow repeatability of time-based calculations.
+scripts/config.py set MBEDTLS_PLATFORM_TIME_ALT
+# Toggle some options that affect how some parts of the library work.
+scripts/config.py set MBEDTLS_USE_PSA_CRYPTO
+scripts/config.py set MBEDTLS_ECDH_VARIANT_EVEREST_ENABLED
+scripts/config.py set MBEDTLS_RSA_NO_CRT
+mkdir build-usepsa
+cd build-usepsa
+cmake -DENABLE_TESTING=OFF ..
+# just build the fuzzers
+make -j$(nproc) -C programs/fuzz
+cd programs/fuzz
+for x in fuzz_*; do
+  cp $x $OUT/usepsa-$x
+  # use the same ancilliary data as the default-build executables
+  if [ -e $OUT/${x}_seed_corpus.zip ]; then
+    ln -s ${x}_seed_corpus.zip $OUT/usepsa-${x}_seed_corpus.zip
+  fi
+  if [ -e $OUT/${x}.options ]; then
+    ln -s ${x}.options $OUT/usepsa-${x}.options
+  fi
+done
+cd ../..
+cd ..
