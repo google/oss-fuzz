81ba32d03586b4e8745ce3ff159b16f3946ea05f
0xcha1 <145190829+0xcha1@users.noreply.github.com>
2023-09-16 14:43:27 +0000

    more effective fuzzing harness created

--- projects/markdown-it-js/fuzz.js
+++ projects/markdown-it-js/fuzz.js
@@ -1,50 +1,40 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-////////////////////////////////////////////////////////////////////////////////
+const MarkdownIt = require('markdown-it');
 
-const { FuzzedDataProvider } = require('@jazzer.js/core');
-const markdownIt = require('markdown-it');
-const md = markdownIt();
+/**
+ * @param { Buffer } data
+ */
+module.exports.fuzz = function (data) {
+    const s = data.toString();
+    try {
+        // Using commonmark mode
+        const mdCommonMark = new MarkdownIt('commonmark');
+        mdCommonMark.render(s);
 
-module.exports.fuzz = function(data) {
-  try {
-    const provider = new FuzzedDataProvider(data);
-    const markdownContent = provider.consumeString(provider.consumeIntegralInRange(1, 4096));
+        // Using default mode
+        const mdDefault = new MarkdownIt();
+        mdDefault.render(s);
 
-    // Render the markdown content
-    const htmlOutput = md.render(markdownContent);
+        // Enabling everything
+        const mdEverythingEnabled = new MarkdownIt({
+            html: true,
+            linkify: true,
+            typographer: true
+        });
+        mdEverythingEnabled.render(s);
 
-    // Optionally, you can test other functionalities of markdown-it
-    // For example, you can test the inline rendering:
-    const inlineOutput = md.renderInline(markdownContent);
-
-    // If markdown-it had more functionalities like plugins, options, etc., 
-    // you would include them here in a similar manner, using the FuzzedDataProvider 
-    // to generate random inputs for those functionalities.
-
-  } catch (error) {
-    if (!ignoredError(error)) throw error;
-  }
+        // Using full options list with defaults
+        const mdFullOptions = new MarkdownIt({
+            html: false,
+            xhtmlOut: false,
+            breaks: false,
+            langPrefix: 'language-',
+            linkify: false,
+            typographer: false,
+            quotes: '“”‘’',
+            highlight: function (/*str, lang*/) { return ''; }
+        });
+        mdFullOptions.render(s);
+    } catch (e) {
+        throw e;
+    }
 };
-
-function ignoredError(error) {
-  // List of error messages that you want the fuzzer to ignore.
-  // For example, if markdown-it throws specific errors for invalid inputs 
-  // that you're not interested in, you can list them here.
-  const ignored = [
-    // "Example error message to ignore",
-  ];
-  return Boolean(ignored.find((message) => error.message.indexOf(message) !== -1));
-}
