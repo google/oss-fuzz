0725ab721c67b256af53cd75f9c385ba7a1d7fe2
bcapuano <bcapuano@asu.edu>
2023-09-24 16:07:18 -0400

    Remove pyvex fuzzers from ossfuzz projects dir

--- projects/pyvex/build.sh
+++ projects/pyvex/build.sh
@@ -20,7 +20,7 @@ cd "$SRC"/archinfo
 pip3 install .
 
 cd "$SRC"/pyvex
-pip3 install .
+pip3 install .[testing]
 
 # Generate a simple binary for the corpus
 echo -ne "start:\n\txor %edi, %edi\nmov \$60, %eax\nsyscall" > /tmp/corpus.s
@@ -28,6 +28,6 @@ clang -Os -s /tmp/corpus.s -nostdlib -nostartfiles -m32 -o corpus
 zip -r "$OUT"/irsb_fuzzer_seed_corpus.zip corpus
 
 # Build fuzzers in $OUT
-for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
+for fuzzer in $(find $SRC -name 'fuzzing/*_fuzzer.py'); do
   compile_python_fuzzer "$fuzzer" --add-binary="pyvex/lib/libpyvex.so:pyvex/lib"
 done
