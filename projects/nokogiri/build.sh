d1f46425b76b109293d01d7252a971eb120ce1b2
fuzzy-boiii23a <fuzzy.boiiii23a@gmail.com>
2023-10-18 06:25:19 -0700

    fixed hardcoding compiler flags

--- projects/nokogiri/build.sh
+++ projects/nokogiri/build.sh
@@ -19,57 +19,36 @@ export SANITIZER_LINK=""
 
 if [ "$FUZZING_ENGINE" = "centipede" ]
 then
-  export CXXFLAGS="-fsanitize-coverage=trace-pc-guard,pc-table,trace-cmp -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fno-builtin -gline-tables-only"
-  export CFLAGS="-fsanitize-coverage=trace-pc-guard,pc-table,trace-cmp -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fno-builtin -gline-tables-only"
   export ENGINE_LINK="$LIB_FUZZING_ENGINE -lc++"
 fi
 if [ "$FUZZING_ENGINE" = "libfuzzer" ]
 then
-  export CXXFLAGS="-fsanitize=fuzzer-no-link"
-  export CFLAGS="-fsanitize=fuzzer-no-link"
   export ENGINE_LINK="$(find $(llvm-config --libdir) -name libclang_rt.fuzzer-x86_64.a | head -1)"
 fi
 if [ "$FUZZING_ENGINE" = "honggfuzz" ]
 then
-  export CXXFLAGS="-fsanitize-coverage=trace-pc-guard,indirect-calls,trace-cmp"
-  export CFLAGS="-fsanitize-coverage=trace-pc-guard,indirect-calls,trace-cmp"
   export ENGINE_LINK="$(find . -name honggfuzz.a)"
 fi
 if [ "$FUZZING_ENGINE" = "afl" ]
 then
-  export CXXFLAGS="-fsanitize=fuzzer-no-link -fsanitize-coverage=trace-pc-guard,indirect-calls,trace-cmp"
-  export CFLAGS="-fsanitize=fuzzer-no-link -fsanitize-coverage=trace-pc-guard,indirect-calls,trace-cmp"
   export ENGINE_LINK="$(find . -name libAFLDriver.a | head -1) $(find . -name afl-compiler-rt-64.o | head -1)"
 fi
 
 if [ "$SANITIZER" = "undefined" ]
 then
-  export SANITIZER_OPTS="-fsanitize=undefined"
   export SANITIZER_LINK="$(find $(llvm-config --libdir) -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1)"
 fi
 if [ "$SANITIZER" = "address" ]
 then
-  export SANITIZER_OPTS="-fsanitize=address"
   export SANITIZER_LINK="$(find $(llvm-config --libdir) -name libclang_rt.asan_cxx-x86_64.a | head -1)"
-fi
-if [ "$SANITIZER" = "coverage" ]
-then
-  export SANITIZER_OPTS="-g -fprofile-instr-generate -fcoverage-mapping"
-  export SANITIZER_LINK=""
 fi
 if [ "$SANITIZER" = "memory" ]
 then
-  export SANITIZER_OPTS="-fsanitize=memory -fPIE -pie -Wno-unused-command-line-argument"
   export SANITIZER_LINK="$(find $($LLVM_CONFIG --libdir) -name libclang_rt.msan_cxx-x86_64.a | head -1)"
 fi
-if [ "$SANITIZER" = "introspector" ]
-then
-  export SANITIZER_OPTS="-fno-omit-frame-pointer -gline-tables-only -fuse-ld=gold -flto -Wno-unused-command-line-argument"
-  export SANITIZER_LINK=""
-fi
 
-export CXXFLAGS="-O0 -fno-inline-functions $CXXFLAGS $SANITIZER_OPTS"
-export CFLAGS="-O0 -fno-inline-functions  $CFLAGS $SANITIZER_OPTS"
+export CXXFLAGS="$CXXFLAGS $SANITIZER_OPTS"
+export CFLAGS="$CFLAGS $SANITIZER_OPTS"
 cd nokogiri/gumbo-parser/src && make clean && make && cd -
 $CXX $CXXFLAGS -o parse_fuzzer parse_fuzzer.cc nokogiri/gumbo-parser/src/libgumbo.a $ENGINE_LINK $SANITIZER_LINK
 mv parse_fuzzer $OUT/parse_fuzzer
