26a17da70bbf35b333442b24f8eb5ea79e37b9b6
viktoriia-lsg <viktoriia.reitsenshtein@leviathansecurity.com>
2023-10-31 13:08:48 +0200

    Add clang-format

--- projects/lcms/cms_virtual_profile_fuzzer.c
+++ projects/lcms/cms_virtual_profile_fuzzer.c
@@ -10,86 +10,92 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <stdint.h>
 #include "lcms2.h"
+#include <stdint.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
-if (size < 16) {
+  if (size < 16) {
     return 0;
-}
+  }
 
-cmsHPROFILE hInProfile = cmsOpenProfileFromMem(data, size);
-if (!hInProfile){
+  cmsHPROFILE hInProfile = cmsOpenProfileFromMem(data, size);
+  if (!hInProfile) {
     return 0;
-}
+  }
 
-cmsHPROFILE hOutProfile = cmsCreate_sRGBProfile();
-if (!hOutProfile){
+  cmsHPROFILE hOutProfile = cmsCreate_sRGBProfile();
+  if (!hOutProfile) {
     cmsCloseProfile(hInProfile);
     return 0;
-}
-cmsColorSpaceSignature srcCS = cmsGetColorSpace(hInProfile);
-cmsUInt32Number nSrcComponents = cmsChannelsOf(srcCS);
-cmsUInt32Number srcFormat;
-if (srcCS == cmsSigLabData) {
+  }
+  cmsColorSpaceSignature srcCS = cmsGetColorSpace(hInProfile);
+  cmsUInt32Number nSrcComponents = cmsChannelsOf(srcCS);
+  cmsUInt32Number srcFormat;
+  if (srcCS == cmsSigLabData) {
     srcFormat =
         COLORSPACE_SH(PT_Lab) | CHANNELS_SH(nSrcComponents) | BYTES_SH(0);
-} else {
+  } else {
     srcFormat =
         COLORSPACE_SH(PT_ANY) | CHANNELS_SH(nSrcComponents) | BYTES_SH(1);
-}
-cmsHTRANSFORM hTransform = cmsCreateTransform(hInProfile, srcFormat, hOutProfile, TYPE_BGR_8, *((const uint32_t *)data+3) % 16,*((const uint32_t *)data+2));
-                                 
-cmsCloseProfile(hInProfile);
-cmsCloseProfile(hOutProfile);
-if (!hTransform){
-    return 0;
-}
+  }
+  cmsHTRANSFORM hTransform = cmsCreateTransform(
+      hInProfile, srcFormat, hOutProfile, TYPE_BGR_8,
+      *((const uint32_t *)data + 3) % 16, *((const uint32_t *)data + 2));
 
-cmsFloat64Number version;
-if (*((const uint32_t *)data+3) % 2 == 0){
+  cmsCloseProfile(hInProfile);
+  cmsCloseProfile(hOutProfile);
+  if (!hTransform) {
+    return 0;
+  }
+
+  cmsFloat64Number version;
+  if (*((const uint32_t *)data + 3) % 2 == 0) {
     version = 3.4;
-}
-else{
+  } else {
     version = 4.4;
-}
+  }
 
-//cmsTransform2DeviceLink
-cmsHPROFILE devicelinkProfile = cmsTransform2DeviceLink(hTransform, version, *((const uint32_t *)data+2));
+  // cmsTransform2DeviceLink
+  cmsHPROFILE devicelinkProfile = cmsTransform2DeviceLink(
+      hTransform, version, *((const uint32_t *)data + 2));
 
-//clean up
-cmsDeleteTransform(hTransform);
-if (devicelinkProfile){
+  // clean up
+  cmsDeleteTransform(hTransform);
+  if (devicelinkProfile) {
     cmsCloseProfile(devicelinkProfile);
-}
+  }
 
-//cmsCreateLinearizationDeviceLink
-cmsToneCurve* tone = cmsBuildGamma(NULL, *((const uint32_t *)data+3));
-if (!tone){
+  // cmsCreateLinearizationDeviceLink
+  cmsToneCurve *tone = cmsBuildGamma(NULL, *((const uint32_t *)data + 3));
+  if (!tone) {
     return 0;
-}
-//15 curves, so it can handle all color spaces
-cmsToneCurve* rgb_curves[15] = {tone,tone,tone,tone,tone,tone,tone,tone,tone,tone,tone,tone,tone,tone,tone};
-cmsHPROFILE linearizationDeviceLinkProfile = cmsCreateLinearizationDeviceLink(srcCS, rgb_curves);
+  }
+  // 15 curves, so it can handle all color spaces
+  cmsToneCurve *rgb_curves[15] = {tone, tone, tone, tone, tone,
+                                  tone, tone, tone, tone, tone,
+                                  tone, tone, tone, tone, tone};
+  cmsHPROFILE linearizationDeviceLinkProfile =
+      cmsCreateLinearizationDeviceLink(srcCS, rgb_curves);
 
-cmsFreeToneCurve(tone);
+  cmsFreeToneCurve(tone);
 
-if (linearizationDeviceLinkProfile){
+  if (linearizationDeviceLinkProfile) {
     cmsCloseProfile(linearizationDeviceLinkProfile);
-}
+  }
 
-//cmsCreateInkLimitingDeviceLink
-cmsFloat64Number limit;
-if (*((const uint32_t *)data+3) % 2 == 0){
+  // cmsCreateInkLimitingDeviceLink
+  cmsFloat64Number limit;
+  if (*((const uint32_t *)data + 3) % 2 == 0) {
     limit = 0;
-}else{
+  } else {
     limit = 400;
-}
-cmsHPROFILE limitingDeviceLinkProfile = cmsCreateInkLimitingDeviceLink(cmsSigCmykData, limit);
-if (limitingDeviceLinkProfile){
+  }
+  cmsHPROFILE limitingDeviceLinkProfile =
+      cmsCreateInkLimitingDeviceLink(cmsSigCmykData, limit);
+  if (limitingDeviceLinkProfile) {
     cmsCloseProfile(limitingDeviceLinkProfile);
-}
+  }
 
-return 0;
+  return 0;
 }
