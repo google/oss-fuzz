9edc260e0a52fce9001330817ba706e0fd66e006
Maksym Vatsyk <maksym.vatsyk@leviathansecurity.com>
2023-11-28 13:41:34 +0100

    fix corpora for decode_raw_lzma2 fuzzer

--- projects/xz/build.sh
+++ projects/xz/build.sh
@@ -43,7 +43,7 @@ find $SRC/xz/tests/files -name "*.lz" \
 -exec zip -ujq $OUT/fuzz_decode_lzip_seed_corpus.zip "{}" \;
 find $SRC/xz/tests/files -name "*.lzma_raw" \
 -exec zip -ujq $OUT/fuzz_decode_raw_seed_corpus.zip "{}" \;
-find $SRC/xz/tests/files ! -name "*.lzma2*" \
+find $SRC/xz/tests/files -name "*.lzma2*" \
 -exec zip -ujq $OUT/fuzz_decode_raw_lzma2_seed_corpus.zip "{}" \;
 find $SRC/xz/tests/files -name "*.xz" \
 -exec zip -ujq $OUT/fuzz_decode_stream_seed_corpus.zip "{}" \;
