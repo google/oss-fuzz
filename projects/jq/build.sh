510a22d3c0ab8bb7da9c798b8ef4e8dfca4b0ca8
DavidKorczynski <david@adalogics.com>
2023-11-30 13:56:41 +0000

    jq: add new fuzzer (#11294)
    Ref: https://github.com/jqlang/jq/pull/2959
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/jq/build.sh
+++ projects/jq/build.sh
@@ -51,3 +51,8 @@ $CC $CFLAGS -c tests/jq_fuzz_parse_stream.c \
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./jq_fuzz_parse_stream.o \
     ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_parse_stream -I./src
+
+$CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./tests/jq_fuzz_execute.cpp \
+    -I./src \
+    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    -o $OUT/jq_fuzz_execute -I./src
