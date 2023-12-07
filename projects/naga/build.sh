9efc8e74a6e9bdb74803f40241b52bce381ad7e3
manunio <max.manu.nair@gmail.com>
2023-12-07 17:40:06 +0530

    naga: match upstream (#11329)
    Naga have been moved to https://github.com/gfx-rs/wgpu/tree/trunk/naga

--- projects/naga/build.sh
+++ projects/naga/build.sh
@@ -15,7 +15,7 @@
 #
 ################################################################################
 
-cargo fuzz build -O
-fuzz_release=fuzz/target/x86_64-unknown-linux-gnu/release
+cargo +nightly fuzz build -O
+fuzz_release=$SRC/wgpu/target/x86_64-unknown-linux-gnu/release
 cp $fuzz_release/spv_parser $OUT/
 cp $fuzz_release/wgsl_parser $OUT/
