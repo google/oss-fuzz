561a2c357aeab72ddfe4bac6de393a302a245c7e
Hugh Wang <hghwng@gmail.com>
2023-12-12 23:56:16 +0800

    binutils: fix corpus name mismatch (#11352)

--- projects/binutils/build.sh
+++ projects/binutils/build.sh
@@ -167,7 +167,7 @@ then
 fi
 
 # Copy seeds out
-for fuzzname in readelf_pef readelf_elf32_csky readelf_elf64_mmix readelf_elf32_littlearm readelf_elf32_bigarm objdump objdump_safe nm objcopy bdf windres addr2line dwarf; do
+for fuzzname in readelf_pef readelf_elf32_csky readelf_elf64_mmix readelf_elf32_littlearm readelf_elf32_bigarm objdump objdump_safe nm objcopy bfd windres addr2line dwarf; do
   cp $SRC/binary-samples/oss-fuzz-binutils/general_seeds.zip $OUT/fuzz_${fuzzname}_seed_corpus.zip
 done
 # Seed targeted the pef file format
