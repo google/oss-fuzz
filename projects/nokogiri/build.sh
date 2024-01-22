google/oss-fuzz

commit 92bb9a25eb6e3fdd7aa1ae9b4dd86ef04ed4cf38

Upstream nokogiri (#11494)
Fuzzers are now maintained upstream, tested locally with docker and all
looks good

projects/nokogiri/Dockerfile
@@ -19,6 +19,4 @@ RUN apt-get update && apt-get --no-install-recommends install -y build-essential
 
 RUN git clone https://github.com/sparklemotion/nokogiri.git
 
-RUN wget https://github.com/fuzzy-boiii23a/oss-fuzz-corpus/raw/main/nokogiri_corpus.zip
-
-COPY build.sh parse_fuzzer.cc gumbo.dict $SRC/
+COPY build.sh $SRC

projects/nokogiri/build.sh
@@ -14,8 +14,5 @@
 #
 ###############################################################################
 
-cd nokogiri/gumbo-parser/src && make clean && make && cd -
-$CXX $CXXFLAGS -o parse_fuzzer parse_fuzzer.cc nokogiri/gumbo-parser/src/libgumbo.a $LIB_FUZZING_ENGINE
-mv parse_fuzzer $OUT/parse_fuzzer
-mv gumbo.dict $OUT/parse_fuzzer.dict
-mv nokogiri_corpus.zip $OUT/parse_fuzzer_seed_corpus.zip
+cd nokogiri/gumbo-parser
+make oss-fuzz

projects/nokogiri/gumbo.dict
@@ -1,560 +1,0 @@
-#
-# AFL dictionary for HTML parsers
-# -------------------------------
-#
-# A basic collection of HTML string likely to matter to HTML parsers.
-#
-# Created by Michal Zalewski <lcamtuf@google.com>
-#
-
-tag_a="<a>"
-tag_abbr="<abbr>"
-tag_acronym="<acronym>"
-tag_address="<address>"
-tag_annotation_xml="<annotation-xml>"
-tag_applet="<applet>"
-tag_area="<area>"
-tag_article="<article>"
-tag_aside="<aside>"
-tag_audio="<audio>"
-tag_b="<b>"
-tag_base="<base>"
-tag_basefont="<basefont>"
-tag_bdi="<bdi>"
-tag_bdo="<bdo>"
-tag_bgsound="<bgsound>"
-tag_big="<big>"
-tag_blink="<blink>"
-tag_blockquote="<blockquote>"
-tag_body="<body>"
-tag_br="<br>"
-tag_button="<button>"
-tag_canvas="<canvas>"
-tag_caption="<caption>"
-tag_center="<center>"
-tag_cite="<cite>"
-tag_code="<code>"
-tag_col="<col>"
-tag_colgroup="<colgroup>"
-tag_data="<data>"
-tag_datalist="<datalist>"
-tag_dd="<dd>"
-tag_del="<del>"
-tag_desc="<desc>"
-tag_details="<details>"
-tag_dfn="<dfn>"
-tag_dir="<dir>"
-tag_div="<div>"
-tag_dl="<dl>"
-tag_dt="<dt>"
-tag_em="<em>"
-tag_embed="<embed>"
-tag_fieldset="<fieldset>"
-tag_figcaption="<figcaption>"
-tag_figure="<figure>"
-tag_font="<font>"
-tag_footer="<footer>"
-tag_foreignobject="<foreignobject>"
-tag_form="<form>"
-tag_frame="<frame>"
-tag_frameset="<frameset>"
-tag_h1="<h1>"
-tag_h2="<h2>"
-tag_h3="<h3>"
-tag_h4="<h4>"
-tag_h5="<h5>"
-tag_h6="<h6>"
-tag_head="<head>"
-tag_header="<header>"
-tag_hgroup="<hgroup>"
-tag_hr="<hr>"
-tag_html="<html>"
-tag_i="<i>"
-tag_iframe="<iframe>"
-tag_image="<image>"
-tag_img="<img>"
-tag_input="<input>"
-tag_ins="<ins>"
-tag_isindex="<isindex>"
-tag_kbd="<kbd>"
-tag_keygen="<keygen>"
-tag_label="<label>"
-tag_legend="<legend>"
-tag_li="<li>"
-tag_link="<link>"
-tag_listing="<listing>"
-tag_main="<main>"
-tag_malignmark="<malignmark>"
-tag_map="<map>"
-tag_mark="<mark>"
-tag_marquee="<marquee>"
-tag_math="<math>"
-tag_menu="<menu>"
-tag_menuitem="<menuitem>"
-tag_meta="<meta>"
-tag_meter="<meter>"
-tag_mglyph="<mglyph>"
-tag_mi="<mi>"
-tag_mn="<mn>"
-tag_mo="<mo>"
-tag_ms="<ms>"
-tag_mtext="<mtext>"
-tag_multicol="<multicol>"
-tag_nav="<nav>"
-tag_nextid="<nextid>"
-tag_nobr="<nobr>"
-tag_noembed="<noembed>"
-tag_noframes="<noframes>"
-tag_noscript="<noscript>"
-tag_object="<object>"
-tag_ol="<ol>"
-tag_optgroup="<optgroup>"
-tag_option="<option>"
-tag_output="<output>"
-tag_p="<p>"
-tag_param="<param>"
-tag_plaintext="<plaintext>"
-tag_pre="<pre>"
-tag_progress="<progress>"
-tag_q="<q>"
-tag_rb="<rb>"
-tag_rp="<rp>"
-tag_rt="<rt>"
-tag_rtc="<rtc>"
-tag_ruby="<ruby>"
-tag_s="<s>"
-tag_samp="<samp>"
-tag_script="<script>"
-tag_section="<section>"
-tag_select="<select>"
-tag_small="<small>"
-tag_source="<source>"
-tag_spacer="<spacer>"
-tag_span="<span>"
-tag_strike="<strike>"
-tag_strong="<strong>"
-tag_style="<style>"
-tag_sub="<sub>"
-tag_summary="<summary>"
-tag_sup="<sup>"
-tag_svg="<svg>"
-tag_table="<table>"
-tag_tbody="<tbody>"
-tag_td="<td>"
-tag_template="<template>"
-tag_textarea="<textarea>"
-tag_tfoot="<tfoot>"
-tag_th="<th>"
-tag_thead="<thead>"
-tag_time="<time>"
-tag_title="<title>"
-tag_tr="<tr>"
-tag_track="<track>"
-tag_tt="<tt>"
-tag_u="<u>"
-tag_ul="<ul>"
-tag_var="<var>"
-tag_video="<video>"
-tag_wbr="<wbr>"
-tag_xmp="<xmp>"
-
-
-# attributes
-
-"accept"
-"accept-charset"
-"accesskey"
-"action"
-"align"
-"allow"
-"alt"
-"async"
-"autocapitalize"
-"autocomplete"
-"autofocus"
-"autoplay"
-"background"
-"bgcolor"
-"border"
-"capture"
-"challenge"
-"charset"
-"checked"
-"cite"
-"class"
-"code"
-"codebase"
-"color"
-"cols"
-"colspan"
-"content"
-"contenteditable"
-"contextmenu"
-"controls"
-"coords"
-"crossorigin"
-"csp"
-"data"
-"data-"
-"datetime"
-"decoding"
-"default"
-"defer"
-"dir"
-"dirname"
-"disabled"
-"download"
-"draggable"
-"dropzone"
-"enctype"
-"enterkeyhint"
-"for"
-"form"
-"formaction"
-"formenctype"
-"formmethod"
-"formnovalidate"
-"formtarget"
-"headers"
-"height"
-"hidden"
-"high"
-"href"
-"hreflang"
-"http-equiv"
-"icon"
-"id"
-"importance"
-"integrity"
-"inputmode"
-"ismap"
-"itemprop"
-"keytype"
-"kind"
-"label"
-"lang"
-"language"
-"loading"
-"list"
-"loop"
-"low"
-"manifest"
-"max"
-"maxlength"
-"minlength"
-"media"
-"method"
-"min"
-"multiple"
-"muted"
-"name"
-"novalidate"
-"onabort"
-"onactivate"
-"onafterprint"
-"onafterupdate"
-"onanimationend"
-"onanimationiteration"
-"onanimationstart"
-"onautocomplete"
-"onautocompleteerror"
-"onbeforeactivate"
-"onbeforecopy"
-"onbeforecut"
-"onbeforedeactivate"
-"onbeforeeditfocus"
-"onbeforepaste"
-"onbeforeprint"
-"onbeforeunload"
-"onbeforeupdate"
-"onbegin"
-"onblur"
-"onbounce"
-"oncancel"
-"oncanplay"
-"oncanplaythrough"
-"oncellchange"
-"onchange"
-"onclick"
-"onclose"
-"oncompassneedscalibration"
-"oncontextmenu"
-"oncontrolselect"
-"oncopy"
-"oncuechange"
-"oncut"
-"ondataavailable"
-"ondatasetchanged"
-"ondatasetcomplete"
-"ondblclick"
-"ondeactivate"
-"ondevicelight"
-"ondevicemotion"
-"ondeviceorientation"
-"ondeviceproximity"
-"ondrag"
-"ondragdrop"
-"ondragend"
-"ondragenter"
-"ondragexit"
-"ondragleave"
-"ondragover"
-"ondragstart"
-"ondrop"
-"ondurationchange"
-"onemptied"
-"onend"
-"onended"
-"onerror"
-"onerrorupdate"
-"onexit"
-"onfilterchange"
-"onfinish"
-"onfocus"
-"onfocusin"
-"onfocusout"
-"onformchange "
-"onforminput "
-"ongesturechange"
-"ongestureend"
-"ongesturestart"
-"onhashchange"
-"onhelp"
-"oninput"
-"oninvalid"
-"onkeydown"
-"onkeypress"
-"onkeyup"
-"onlanguagechange"
-"onlayoutcomplete"
-"onload"
-"onloadeddata"
-"onloadedmetadata"
-"onloadstart"
-"onlosecapture"
-"onmediacomplete"
-"onmediaerror"
-"onmessage"
-"onmousedown"
-"onmouseenter"
-"onmouseleave"
-"onmousemove"
-"onmouseout"
-"onmouseover"
-"onmouseup"
-"onmousewheel"
-"onmove"
-"onmoveend"
-"onmovestart"
-"onmozfullscreenchange"
-"onmozfullscreenerror"
-"onmozpointerlockchange"
-"onmozpointerlockerror"
-"onmsgesturechange"
-"onmsgesturedoubletap"
-"onmsgesturehold"
-"onmsgesturerestart"
-"onmsinertiastart"
-"onmspointercancel"
-"onmspointerdown"
-"onmspointerenter"
-"onmspointerhover"
-"onmspointerleave"
-"onmspointermove"
-"onmspointerout"
-"onmspointerover"
-"onmspointerup"
-"onoffline"
-"ononline"
-"onorientationchange"
-"onoutofsync"
-"onpagehide"
-"onpageshow"
-"onpaste"
-"onpause"
-"onplay"
-"onplaying"
-"onpopstate"
-"onprogress"
-"onpropertychange"
-"onratechange"
-"onreadystatechange"
-"onreceived"
-"onrepeat"
-"onreset"
-"onresize"
-"onresizeend"
-"onresizestart"
-"onresume"
-"onreverse"
-"onrowdelete"
-"onrowenter"
-"onrowexit"
-"onrowinserted"
-"onrowsdelete"
-"onrowsinserted"
-"onscroll"
-"onsearch"
-"onseek"
-"onseeked"
-"onseeking"
-"onselect"
-"onselectionchange"
-"onselectstart"
-"onshow"
-"onstalled"
-"onstart"
-"onstop"
-"onstorage"
-"onsubmit"
-"onsuspend"
-"onsynchrestored"
-"ontimeerror"
-"ontimeupdate"
-"ontoggle"
-"ontouchcancel"
-"ontouchend"
-"ontouchmove"
-"ontouchstart"
-"ontrackchange"
-"ontransitionend"
-"onunload"
-"onurlflip"
-"onuserproximity"
-"onvolumechange"
-"onwaiting"
-"onwebkitanimationend"
-"onwebkitanimationiteration"
-"onwebkitanimationstart"
-"onwebkitmouseforcechanged"
-"onwebkitmouseforcedown"
-"onwebkitmouseforceup"
-"onwebkitmouseforcewillbegin"
-"onwebkittransitionend"
-"onwebkitwillrevealbottom"
-"onwheel"
-"onzoom"
-"open"
-"optimum"
-"pattern"
-"ping"
-"placeholder"
-"poster"
-"preload"
-"radiogroup"
-"readonly"
-"referrerpolicy"
-"rel"
-"required"
-"reversed"
-"rows"
-"rowspan"
-"sandbox"
-"scope"
-"scoped"
-"selected"
-"shape"
-"size"
-"sizes"
-"slot"
-"span"
-"spellcheck"
-"src"
-"srcdoc"
-"srclang"
-"srcset"
-"start"
-"step"
-"style"
-"summary"
-"tabindex"
-"target"
-"title"
-"translate"
-"type"
-"usemap"
-"value"
-"width"
-"wrap"
-#
-# AFL dictionary for XML
-# ----------------------
-#
-# Several basic syntax elements and attributes, modeled on libxml2.
-#
-# Created by Michal Zalewski <lcamtuf@google.com>
-#
-
-attr_encoding=" encoding=\"1\""
-attr_generic=" a=\"1\""
-attr_href=" href=\"1\""
-attr_standalone=" standalone=\"no\""
-attr_version=" version=\"1\""
-attr_xml_base=" xml:base=\"1\""
-attr_xml_id=" xml:id=\"1\""
-attr_xml_lang=" xml:lang=\"1\""
-attr_xml_space=" xml:space=\"1\""
-attr_xmlns=" xmlns=\"1\""
-
-entity_builtin="&lt;"
-entity_decimal="&#1;"
-entity_external="&a;"
-entity_hex="&#x1;"
-
-string_any="ANY"
-string_brackets="[]"
-string_cdata="CDATA"
-string_col_fallback=":fallback"
-string_col_generic=":a"
-string_col_include=":include"
-string_dashes="--"
-string_empty="EMPTY"
-string_empty_dblquotes="\"\""
-string_empty_quotes="''"
-string_entities="ENTITIES"
-string_entity="ENTITY"
-string_fixed="#FIXED"
-string_id="ID"
-string_idref="IDREF"
-string_idrefs="IDREFS"
-string_implied="#IMPLIED"
-string_nmtoken="NMTOKEN"
-string_nmtokens="NMTOKENS"
-string_notation="NOTATION"
-string_parentheses="()"
-string_pcdata="#PCDATA"
-string_percent="%a"
-string_public="PUBLIC"
-string_required="#REQUIRED"
-string_schema=":schema"
-string_system="SYSTEM"
-string_ucs4="UCS-4"
-string_utf16="UTF-16"
-string_utf8="UTF-8"
-string_xmlns="xmlns:"
-
-tag_attlist="<!ATTLIST"
-tag_cdata="<![CDATA["
-tag_close="</a>"
-tag_doctype="<!DOCTYPE"
-tag_element="<!ELEMENT"
-tag_entity="<!ENTITY"
-tag_ignore="<![IGNORE["
-tag_include="<![INCLUDE["
-tag_notation="<!NOTATION"
-tag_open="<a>"
-tag_open_close="<a />"
-tag_open_exclamation="<!"
-tag_open_q="<?"
-tag_sq2_close="]]>"
-tag_xml_q="<?xml?>"
-
-encoding_utf="UTF-"
-encoding_iso1="ISO-8859"
-encoding_iso3="ISO-10646-UCS"
-encoding_iso5="ISO-LATIN-1"
-encoding_jis="SHIFT_JIS"
-encoding_utf7="UTF-7"
-encoding_utf16le="UTF-16BE"
-encoding_utf16le="UTF-16LE"
-encoding_ascii="US-ASCII"

projects/nokogiri/parse_fuzzer.cc
@@ -1,85 +1,0 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-////////////////////////////////////////////////////////////////////////////////
-
-
-#include "nokogiri/gumbo-parser/src/nokogiri_gumbo.h"
-#include <stdint.h>
-
-int SanityCheckPointers(const char* input, size_t input_length, const GumboNode* node, int depth) {
-  if (node->type == GUMBO_NODE_DOCUMENT || depth > 400) {
-    return -1;
-  }
-  if (node->type == GUMBO_NODE_ELEMENT) {
-    const GumboElement* element = &node->v.element;
-    const GumboVector* attributes = &element->attributes;
-
-    for (unsigned int i = 0; i < attributes->length; ++i) {
-      const GumboAttribute* attribute = static_cast<const GumboAttribute*>(attributes->data[i]);
-      if (!attribute)
-      {
-        return -1;
-      }
-    }
-    const GumboVector* children = &element->children;
-    for (unsigned int i = 0; i < children->length; ++i) {
-      const GumboNode* child = static_cast<const GumboNode*>(children->data[i]);
-      if (!child)
-      {
-        return -1;
-      }
-      SanityCheckPointers(input, input_length, child, depth + 1);
-    }
-  } else {
-    const GumboText* text = &node->v.text;
-    if (!text)
-    {
-      return -1;
-    }
-  }
-
-  return 0;
-}
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  if (size < 10)
-  {
-      return 0;
-  }
-  
-  GumboOptions options = kGumboDefaultOptions;
-  GumboOutput* output;
-  GumboNode* root;
-
-  output = gumbo_parse_with_options(&options, (char*)data, size);
-  root = output->document;
-  
-  int result = SanityCheckPointers((char*)data, size, output->root, 0);
-
-  if (result < 0)
-  {
-    if (output) {
-      gumbo_destroy_output(output);
-    }
-
-    return -1;
-  }
-    
-  if (output) {
-    gumbo_destroy_output(output);
-  }
-
-	return 0;
-}

projects/nokogiri/project.yaml
@@ -7,9 +7,7 @@ auto_ccs:
   - "nokogiri-oss-fuzz@googlegroups.com"
 main_repo: "https://github.com/sparklemotion/nokogiri.git"
 sanitizers:
-  - address
   - undefined
-  - memory
 fuzzing_engines:
   - afl
   - centipede

commit 413bb8be7a15e616dd262b24f53f08a22a19a08a

fix(nokogiri): pass CFLAGS and CXXFLAGS to the make command (#11527)
Without these flags being set properly, the build has been failing, see
https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=65904

Also: use a shallow git clone when building the container

(Solves issue introduced in #11494 /cc @fuzzy-boiii23a)

projects/nokogiri/Dockerfile
@@ -17,6 +17,6 @@
 FROM gcr.io/oss-fuzz-base/base-builder
 RUN apt-get update && apt-get --no-install-recommends install -y build-essential make curl wget
 
-RUN git clone https://github.com/sparklemotion/nokogiri.git
+RUN git clone --depth=1 https://github.com/sparklemotion/nokogiri.git
 
 COPY build.sh $SRC

projects/nokogiri/build.sh
@@ -15,4 +15,4 @@
 ###############################################################################
 
 cd nokogiri/gumbo-parser
-make oss-fuzz
+make oss-fuzz CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS"