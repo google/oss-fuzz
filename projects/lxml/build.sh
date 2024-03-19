9b461f53ffb951109e4fe7b43c58d5e647f5dd61
David Lakin <github@themoderndev.com>
2024-03-19 05:18:02 -0400

    Include compiled extension artifacts in pyinstaller bundle
    Also adds some lxml build time env variables to include more coverage
    related metadata in build artifacts.

--- projects/lxml/build.sh
+++ projects/lxml/build.sh
@@ -17,10 +17,16 @@
 
 export LIBXML2_VERSION=2.12.6
 export LIBXSLT_VERSION=1.1.39
-export STATIC_DEPS=true
-export CFLAGS="$CFLAGS -fPIC"
+export STATICBUILD=true
+export CFLAGS="$CFLAGS -fPIC -DCYTHON_CLINE_IN_TRACEBACK=1"
 
-if [ "$SANITIZER" = undefined ]; then
+if [[ $SANITIZER = *coverage* ]]; then
+  export COVERAGE=true
+  export OPTION_WITH_COVERAG=true
+  export OPTION_WITH_CLINE=true
+fi
+
+if [[ "$SANITIZER" = 'undefined' ]]; then
   # libiconv-1.17 (a dependency of libxml2) triggers several UBSAN errors similar to:
   # `utf32le.h:30:59: runtime error: left shift of 225 by 24 places cannot be represented in type 'int'`
   # Disabling the UBSAN shift check, while not ideal, appears to be the simplest way to avoid hindering the fuzzer
@@ -29,16 +35,15 @@ if [ "$SANITIZER" = undefined ]; then
   export CFLAGS="$CFLAGS -fno-sanitize=shift"
   export CXXFLAGS="$CXXFLAGS -fno-sanitize=shift"
 fi
-python3 -u setup.py build --static-deps --with-cython
+python3 -u setup.py build --with-cython
 python3 -m pip install .
 
-FUZZ_TARGETS_DIR="$SRC/fuzz_targets"
 SEED_DATA_DIR="$SRC/seed_data"
 
 find "$SEED_DATA_DIR" \( -name '*_seed_corpus.zip' -o -name '*.dict' \)  ! -name '__base.dict' -exec printf 'Copying: %s\n' {} \; -exec cp {} "$OUT" \;
 
-find "$FUZZ_TARGETS_DIR" -name 'fuzz_*.py' -print0 | while IFS= read -r -d $'\0' fuzz_harness; do
-   LD_PRELOAD=$OUT/sanitizer_with_fuzzer.so ASAN_OPTIONS=detect_leaks=0 compile_python_fuzzer "$fuzz_harness"
+find "$SRC" -name 'fuzz_*.py' -print0 | while IFS= read -r -d $'\0' fuzz_harness; do
+   compile_python_fuzzer "$fuzz_harness" --add-data="$SRC/lxml/build/lib.linux-x86_64-cpython-38/lxml:."
 
   if [[ -r "$SEED_DATA_DIR/dicts/__base.dict" ]]; then
     fuzz_harness_basename=$(basename "$fuzz_harness")
