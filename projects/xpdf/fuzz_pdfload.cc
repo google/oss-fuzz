5d1bf134628f6ec92831a1d3c653031d59d0a3e3
Julien Voisin <jvoisin@users.noreply.github.com>
2024-03-21 11:59:22 +0100

    Fix the xpdf fuzzer (#11716)
    The number of pages is starting at `1`, and not at `0`
    
    This should fix #11711

--- projects/xpdf/fuzz_pdfload.cc
+++ projects/xpdf/fuzz_pdfload.cc
@@ -96,7 +96,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
                 xfa.free();
             }
 
-            for (size_t i = 0; i < doc.getNumPages(); i++) {
+            for (size_t i = 1; i <= doc.getNumPages(); i++) {
               doc.getLinks(i);
               auto page = doc.getCatalog()->getPage(i);
               if (!page->isOk()) {
