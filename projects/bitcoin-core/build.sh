03eab76be48215007567add245182bcf5aa1bbbf
MarcoFalke <6399679+MarcoFalke@users.noreply.github.com>
2024-05-04 12:06:41 +0200

    bitcoin-core: Re-enable msan

--- projects/bitcoin-core/build.sh
+++ projects/bitcoin-core/build.sh
@@ -85,6 +85,7 @@ make -j$(nproc)
 
 # Replace the magic string with the actual name of each fuzz target
 for fuzz_target in ${FUZZ_TARGETS[@]}; do
+  df --human-readable ./src
   python3 -c "c_str_target=b\"${fuzz_target}\x00\";c_str_magic=b\"$MAGIC_STR\";dat=open('./src/test/fuzz/fuzz','rb').read();dat=dat.replace(c_str_magic, c_str_target+c_str_magic[len(c_str_target):]);open(\"$OUT/$fuzz_target\",'wb').write(dat)"
 
   chmod +x "$OUT/$fuzz_target"
