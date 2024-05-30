e517b6d5b59cb2588a0207a3233541940c9af842
Leo <leonate@gmail.com>
2024-05-30 23:38:44 +0200

    Some debug commands

--- projects/jxls/build.sh
+++ projects/jxls/build.sh
@@ -20,6 +20,10 @@ java --version
 
 mv $SRC/*.dict $OUT
 
+pwd
+
+ls .
+
 CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
 
 mvn package
