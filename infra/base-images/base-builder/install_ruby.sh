3f18090a58d9907186855737a2050d376b914832
Advenam Tacet <advenam.tacet@trailofbits.com>
2024-06-28 05:49:19 +0200

    Resolve TODOs

--- infra/base-images/base-builder/install_ruby.sh
+++ infra/base-images/base-builder/install_ruby.sh
@@ -16,7 +16,6 @@
 ################################################################################
 
 apt update
-# TODO: we should install a specific version of ruby
 apt install -y lsb-release software-properties-common gnupg2 binutils xz-utils libyaml-dev
 gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash
