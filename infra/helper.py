9716d93d286d3257c477c5331ab4fb675218049d
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2024-07-09 11:45:44 -0400

    Add more missing ruby references (#12180)

--- infra/helper.py
+++ infra/helper.py
@@ -85,6 +85,7 @@ LANGUAGE_TO_BASE_BUILDER_IMAGE = {
     'javascript': 'base-builder-javascript',
     'jvm': 'base-builder-jvm',
     'python': 'base-builder-python',
+    'ruby': 'base-builder-ruby',
     'rust': 'base-builder-rust',
     'swift': 'base-builder-swift'
 }
