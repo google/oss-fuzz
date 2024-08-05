31f727a890bfc7abb94515e7aa04c9b30f93489b
Dongge Liu <donggeliu@google.com>
2024-08-05 14:20:54 +1000

    Cleanup script

--- projects/libiec61850/chronos.sh
+++ projects/libiec61850/chronos.sh
@@ -1,42 +1,52 @@
-# Usage: Source this file at the beginning or at least before the fuzz target compilation command:
-# source chronos.sh
+# Usage:
+# 1. Set FUZZ_TARGET (e.g., in Dockerfile)
+# 2. Source this file before compiling the fuzz target (source chronos.sh).
 
-# Initialize re-run.sh
-echo "#!/bin/bash" > re-run.sh
-echo "source /src/saved_vars.sh" >> re-run.sh
-chmod +x re-run.sh
+export START_RECORDING="false"
+RECOMPILE_ENV="/usr/local/bin/recompile_env.sh"
 
-export FUZZ_TARGET="fuzz_mms_decode.c"
-export LOGGING="false"
 
-# Logging Function
-log_command() {
-    local cmd="$@"
-    local log_file="$SRC/re-run.sh"
-    local current_dir=$(pwd)
-    echo "cd \"$current_dir\"" >> $log_file
-    echo "$cmd" >> $log_file
+# Initialize the recompile script.
+initialize_recompile_script() {
+    export RECOMPILE_SCRIPT="/usr/local/bin/recompile"
+    echo "#!/bin/bash" > "$RECOMPILE_SCRIPT"
+    echo "source $RECOMPILE_ENV" >> "$RECOMPILE_SCRIPT" 
+    chmod +x "$RECOMPILE_SCRIPT" 
 }
 
-# Command Execution and Logging Function
-execute_or_log_command() {
-    local cmd="$BASH_COMMAND"
-    if [[ "$cmd" == *"$FUZZ_TARGET"* ]]; then
-        declare -p > /src/saved_vars.sh
-        log_command "$cmd"
-        echo "Simulated (logged) execution of: $cmd"
-        export LOGGING="true"
-        # kill -SIGINT $$  # Skip execution of subsequent commands
-        BASH_COMMAND=":"  # Override the command with a no-op
-    elif [ "$LOGGING" = "true" ]; then
-        log_command "$cmd"
-        echo "Simulated (logged) execution of: $cmd"
-        # kill -SIGINT $$  # Skip execution of subsequent commands
-        BASH_COMMAND=":"  # Override the command with a no-op
+
+# Execute or record command for recompilation.
+execute_or_record_command() {
+    record_command() {
+        echo "cd \"$(pwd)\"" >> "$RECOMPILE_SCRIPT"
+        echo "$@" >> "$RECOMPILE_SCRIPT"
+    }
+
+    # Check if any element in the command array contains the FUZZ_TARGET.
+   if [[ "$BASH_COMMAND" == *"$FUZZ_TARGET"* ]]; then
+       export START_RECORDING="true"
+       # Save all environment variables, excluding read-only ones
+       declare -p | grep -Ev 'declare -[^ ]*r[^ ]*' > "$RECOMPILE_ENV"
+   fi
+
+    if [[ "$START_RECORDING" == "true" ]]; then
+        record_command "$BASH_COMMAND"
+        echo "Simulated (logged) execution of: $BASH_COMMAND"
     fi
 }
 
-# Set up trap for DEBUG to intercept commands
-trap 'execute_or_log_command' DEBUG
-shopt -s extdebug
-set -T  # Ensure trap works in subshells and functions
+
+main() {
+    # Initialize.
+    initialize_recompile_script
+    
+    # Set up trap for DEBUG to intercept commands.
+    trap 'execute_or_record_command' DEBUG
+
+    # Enable extended debugging mode
+    shopt -s extdebug  
+    # Ensure trap works in subshells and functions.
+    set -T  
+}
+
+main
