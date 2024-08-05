3dcab5093d42346812bb720cbfa167553b3a2d4d
Dongge Liu <donggeliu@google.com>
2024-08-05 15:29:50 +1000

    fix lint: add headers

--- infra/experimental/chronos/chronos.sh
+++ infra/experimental/chronos/chronos.sh
@@ -1,3 +1,19 @@
+# Copyright 2024 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
 # Usage:
 # 1. Set FUZZ_TARGET (e.g., in Dockerfile)
 # 2. Source this file before compiling the fuzz target (source chronos.sh).
