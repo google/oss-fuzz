8faa1875131b5726c2da78d4ef64764c29b4e1b4
Volkan Yazıcı <volkan@yazi.ci>
2024-08-06 14:34:04 +0200

    Fix links in the license headers

--- projects/apache-logging-log4j2/build.sh
+++ projects/apache-logging-log4j2/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eu
-# Copyright 2024 Google Inc.
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      https://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
