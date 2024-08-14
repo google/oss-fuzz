e7e40d52db2c27cd218e965e40471a21021159f8
At0m <rijan.conf@gmail.com>
2024-08-14 13:47:08 -0400

    Update coverage_atheris_fuzzer.py

--- .clusterfuzzlite/coverage_atheris_fuzzer.py
+++ .clusterfuzzlite/coverage_atheris_fuzzer.py
@@ -19,38 +19,33 @@ import os
 from unittest import mock
 import sys
 
+# Updated sys.path for module imports
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cifuzz'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cifuzz'))
 
 with atheris.instrument_imports():
-  import get_coverage
+    import get_coverage
 
 REPO_PATH = '/src/curl'
 PROJECT_NAME = 'curl'
 
-with mock.patch('get_coverage._get_oss_fuzz_fuzzer_stats_dir_url',
-                return_value="randomurl"):
-  oss_fuzz_coverage = get_coverage.OSSFuzzCoverage(REPO_PATH, PROJECT_NAME)
-
+# Mocking the method to get OSS-Fuzz fuzzer stats directory URL
+with mock.patch('get_coverage._get_oss_fuzz_fuzzer_stats_dir_url', return_value="randomurl"):
+    oss_fuzz_coverage = get_coverage.OSSFuzzCoverage(REPO_PATH, PROJECT_NAME)
 
 def TestOneInput(data):
-  try:
-    decoded_json = json.loads(data)
-  except (json.decoder.JSONDecodeError, UnicodeDecodeError):
-    # Wart
-    return oss_fuzz_coverage.get_files_covered_by_target('fuzz-target')
+    try:
+        decoded_json = json.loads(data)
+    except (json.decoder.JSONDecodeError, UnicodeDecodeError):
+        return oss_fuzz_coverage.get_files_covered_by_target('fuzz-target')
 
-  with mock.patch('get_coverage.OSSFuzzCoverage.get_target_coverage',
-                  return_value=decoded_json):
-    oss_fuzz_coverage.get_files_covered_by_target('fuzz-target')
-  return 0
-
+    with mock.patch('get_coverage.OSSFuzzCoverage.get_target_coverage', return_value=decoded_json):
+        oss_fuzz_coverage.get_files_covered_by_target('fuzz-target')
+    return 0
 
 def main():
-  atheris.Setup(sys.argv, TestOneInput)
-  atheris.Fuzz()
-
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
 
 if __name__ == '__main__':
-  main()
+    main()
