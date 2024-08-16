75338477f8c09edcc8fc30fbe93c55bca2b6a8f8
Philippe Antoine <p.antoine@catenacyber.fr>
2024-08-16 13:26:05 +0200

    ecc-diff-fuzzer: use clang for quickjs

--- projects/ecc-diff-fuzzer/build.sh
+++ projects/ecc-diff-fuzzer/build.sh
@@ -122,15 +122,16 @@ make install
 #quickjs
 (
 cd quickjs
+sed -i -e 's/CFLAGS=/CFLAGS+=/' Makefile
 if [ "$ARCHITECTURE" = 'i386' ]; then
     make qjsc
     cp qjsc /usr/local/bin/
     make clean
     # Makefile should not override CFLAGS
     sed -i -e 's/CFLAGS=/CFLAGS+=/' Makefile
-    CFLAGS="-m32" make libquickjs.a
+    CFLAGS="-m32" CONFIG_CLANG=y make libquickjs.a
 else
-    make && make install
+    CONFIG_CLANG=y make && make install
 fi
 cp quickjs*.h /usr/local/include/
 cp libquickjs.a /usr/local/lib/
