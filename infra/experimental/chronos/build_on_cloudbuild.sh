d865c6d3fac6beaa204156bd50e8da7639d94553
Oliver Chang <ochang@google.com>
2024-10-01 16:28:11 +1000

    async

--- infra/experimental/chronos/build_on_cloudbuild.sh
+++ infra/experimental/chronos/build_on_cloudbuild.sh
@@ -19,6 +19,7 @@ FUZZ_TARGET=$2
 FUZZING_LANGUAGE=$3
 
 gcloud builds submit "https://github.com/google/oss-fuzz" \
+  --async \
   --git-source-revision=master \
   --config=cloudbuild.yaml \
   --substitutions=_PROJECT=$PROJECT,_FUZZ_TARGET=$FUZZ_TARGET,_FUZZING_LANGUAGE=$FUZZING_LANGUAGE \
