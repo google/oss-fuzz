792d53e5f7947ceea1d923bf43aaccefe8374880
Arthur Chan <arthur.chan@adalogics.com>
2024-10-23 15:58:41 +0000

    Fix license
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/modp_b64/build.sh
+++ projects/modp_b64/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 #
-# Copyright 2023 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
