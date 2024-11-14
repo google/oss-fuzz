55afb330a59816108ab510aec72414e09e8bdad9
Esra Ercan <ercan.esra96@gmail.com>
2024-11-14 16:24:04 +0300

    Update fuzz_target.cc

--- projects/cjsonobject/fuzz_target.cc
+++ projects/cjsonobject/fuzz_target.cc
@@ -1,18 +1,27 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
-#include "../CJsonObject.hpp"  // Adjust the include path if necessary
+#include "../CJsonObject.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-    // Convert the input data to a string for JSON parsing
     std::string jsonContent(reinterpret_cast<const char*>(data), size);
-
-    // Parse the JSON content
     neb::CJsonObject jsonObj;
     if (!jsonObj.Parse(jsonContent)) {
-        // Parsing failed, but this is expected with random inputs
         return 0;
     }
-
-    // Display parsed JSON (optional, can be removed for performance)
     std::cout << "Parsed JSON content:\n" << jsonObj.ToString() << std::endl;
 
     return 0;
