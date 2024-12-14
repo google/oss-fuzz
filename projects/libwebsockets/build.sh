d6135207701a2b5e26953e3c21d9ceb0401d9d98
Segwaz <chloe.cano@axeinos.co>
2024-12-14 16:16:44 +0000

    libwebsockets - html and http headers parsing fuzz targets

--- projects/libwebsockets/build.sh
+++ projects/libwebsockets/build.sh
@@ -21,11 +21,40 @@ cd $DIR
 sed -i 's/-Werror//g' ./CMakeLists.txt
 mkdir build && cd build
 cmake -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
-      -DCMAKE_EXE_LINKER_FLAGS="$CFLAGS" -DCMAKE_SHARED_LINKER_FLAGS="$CFLAGS" ..
+      -DCMAKE_EXE_LINKER_FLAGS="$CFLAGS" -DCMAKE_SHARED_LINKER_FLAGS="$CFLAGS" \
+      -DLWS_WITH_CUSTOM_HEADERS=ON \
+      -DLWS_ROLE_WS=ON \
+      ..
 make -j8
 
 cd $DIR
-$CXX $CFLAGS $LIB_FUZZING_ENGINE -I$DIR/build/include \
-	-o $OUT/lws_upng_inflate_fuzzer lws_upng_inflate_fuzzer.cpp \
-	-L$DIR/build/lib -l:libwebsockets.a \
-	-L/usr/lib/x86_64-linux-gnu/ -l:libssl.so -l:libcrypto.so
+
+INCLUDE_DIRS="-I$DIR/build/include \
+	-I$DIR/build \
+	-I$DIR/lib/core/ \
+	-I$DIR/lib/plat/unix -I$DIR/lib/tls/ \
+	-I$DIR/lib/secure-streams/ \
+	-I$DIR/lib/event-libs/ \
+	-I$DIR/lib/system/smd \
+	-I$DIR/lib/system/metrics/ \
+	-I$DIR/lib/core-net \
+	-I$DIR/lib/roles \
+	-I$DIR/lib/roles/http \
+	-I$DIR/lib/roles/h1 \
+	-I$DIR/lib/roles/h2 \
+	-I$DIR/lib/roles/ws"
+
+FUZZER_SRCS=(
+    "lws_lhs_fuzzer.cpp"
+    "lws_upng_inflate_fuzzer.cpp"
+    "lws_parse_uri_fuzzer.cpp"
+    "lws_parse_fuzzer.cpp"
+)
+
+for FUZZER in "${FUZZER_SRCS[@]}"; do
+    FUZZER_NAME=$(basename "$FUZZER" .cpp)
+    $CXX $CFLAGS $LIB_FUZZING_ENGINE $INCLUDE_DIRS \
+        -o "$OUT/$FUZZER_NAME" $FUZZER \
+        -L"$DIR/build/lib" -l:libwebsockets.a \
+        -L/usr/lib/x86_64-linux-gnu/ -l:libssl.so -l:libcrypto.so
+done
