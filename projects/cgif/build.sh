92221390587038bf52249f06d66e9da926a402a1
Daniel Löbl <dloebl.2000@gmail.com>
2025-01-28 19:05:29 +0100

    cgif: add file fuzzer

--- projects/cgif/build.sh
+++ projects/cgif/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2023 Google Inc.
+# Copyright 2025 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,11 @@ meson install -C build
 meson test -C build
 
 cp "build/fuzz/cgif_fuzzer_seed_corpus.zip" $OUT/.
+cp "build/fuzz/cgif_file_fuzzer_seed_corpus.zip" $OUT/.
 
-# build cgif's fuzz target
+# build cgif's fuzz targets
 $CXX $CXXFLAGS -o "$OUT/cgif_fuzzer" -I"$WORK/include" \
   $LIB_FUZZING_ENGINE fuzz/cgif_fuzzer.c "$WORK/lib/libcgif.a"
+
+$CXX $CXXFLAGS -o "$OUT/cgif_file_fuzzer" -I"$WORK/include" \
+  $LIB_FUZZING_ENGINE fuzz/cgif_file_fuzzer.c "$WORK/lib/libcgif.a"
