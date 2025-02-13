489ed279844fe422ce72dda8cfd081918c32fc7e
DavidKorczynski <david@adalogics.com>
2025-02-13 23:42:34 +0000

    jq: fix build (#13041)
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/jq/build.sh
+++ projects/jq/build.sh
@@ -25,41 +25,41 @@ make -j$(nproc)
 $CC $CFLAGS -c tests/jq_fuzz_parse.c \
     -I./src -o ./jq_fuzz_parse.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./jq_fuzz_parse.o \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_parse -I./src
 
 $CC $CFLAGS -c tests/jq_fuzz_compile.c \
     -I./src -o ./jq_fuzz_compile.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./jq_fuzz_compile.o \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_compile -I./src
 
 $CC $CFLAGS -c tests/jq_fuzz_load_file.c \
     -I./src -o ./jq_fuzz_load_file.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./jq_fuzz_load_file.o \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_load_file -I./src
 
 $CC $CFLAGS -c tests/jq_fuzz_parse_extended.c \
     -I./src -o ./jq_fuzz_parse_extended.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./jq_fuzz_parse_extended.o \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_parse_extended -I./src
 
 $CC $CFLAGS -c tests/jq_fuzz_parse_stream.c \
     -I./src -o ./jq_fuzz_parse_stream.o
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./jq_fuzz_parse_stream.o \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_parse_stream -I./src
 
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./tests/jq_fuzz_execute.cpp \
     -I./src \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a\
     -o $OUT/jq_fuzz_execute -I./src
 
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./tests/jq_fuzz_fixed.cpp \
     -I./src \
-    ./.libs/libjq.a ./modules/oniguruma/src/.libs/libonig.a \
+    ./.libs/libjq.a ./vendor/oniguruma/src/.libs/libonig.a \
     -o $OUT/jq_fuzz_fixed -I./src
 
 
