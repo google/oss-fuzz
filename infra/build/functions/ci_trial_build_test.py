082ad439c3b3f9a530503f3ccea1672d7cf5a447
Andre Nogueira Ribeiro <andrenribeiro@google.com>
2025-03-07 18:19:24 +0000

    Integrate OSS-Fuzz on Demand into trial_build GCB trigger flow

--- infra/build/functions/ci_trial_build_test.py
+++ infra/build/functions/ci_trial_build_test.py
@@ -30,8 +30,8 @@ class GetLatestGCBrunCommandTest(unittest.TestCase):
                          'coverage address --fuzzing-engine libfuzzer')
     comments = [mock_comment]
     expected_command = [
-        'aiohttp', '--sanitizer', 'coverage', 'address', '--fuzzing-engine',
-        'libfuzzer'
+        'trial_build.py', 'aiohttp', '--sanitizer', 'coverage', 'address',
+        '--fuzzing-engine', 'libfuzzer'
     ]
     actual_command = ci_trial_build.get_latest_gcbrun_command(comments)
     self.assertEqual(expected_command, actual_command)
@@ -47,8 +47,71 @@ class GetLatestGCBrunCommandTest(unittest.TestCase):
                            'coverage address --fuzzing-engine libfuzzer')
     comments = [mock_comment_1, mock_comment_2]
     expected_command = [
-        'skcms', '--sanitizer', 'coverage', 'address', '--fuzzing-engine',
-        'libfuzzer'
+        'trial_build.py', 'skcms', '--sanitizer', 'coverage', 'address',
+        '--fuzzing-engine', 'libfuzzer'
     ]
     actual_command = ci_trial_build.get_latest_gcbrun_command(comments)
     self.assertEqual(expected_command, actual_command)
+
+  def test_oss_fuzz_on_demand_command_parsing(self):
+    """Tests that the OSS-Fuzz on demand commands from GitHub comments are
+    parsed properly."""
+    mock_comment = mock.MagicMock()
+    mock_comment.body = ('/gcbrun oss_fuzz_on_demand.py aiohttp --sanitizer '
+                         'coverage address --fuzzing-engine libfuzzer')
+    comments = [mock_comment]
+    expected_command = [
+        'oss_fuzz_on_demand.py', 'aiohttp', '--sanitizer', 'coverage',
+        'address', '--fuzzing-engine', 'libfuzzer'
+    ]
+    actual_command = ci_trial_build.get_latest_gcbrun_command(comments)
+    self.assertEqual(expected_command, actual_command)
+
+class ExecCommandFromGithubTest(unittest.TestCase):
+  """Tests for exec_command_from_github."""
+
+  def test_exec_command(self):
+    """Tests if exec_command_from_github is generating the correct command."""
+    test_cases = [
+        {
+            "comments": [('/gcbrun oss_fuzz_on_demand.py aiohttp --sanitizer'
+                          'coverage address --fuzzing-engine libfuzzer')],
+            "latest_command": ['oss_fuzz_on_demand.py', 'aiohttp',
+                               '--sanitizer', 'coverage', 'address',
+                               '--fuzzing-engine', 'libfuzzer'],
+            "expected_command": ['aiohttp', '--sanitizer', 'coverage',
+                                 'address', '--fuzzing-engine', 'libfuzzer',
+                                 '--repo', 'test_repo', '--branch',
+                                 'test_branch'],
+            "trial_build_called": False,
+        },
+        {
+            "comments": ['/gcbrun trial_build.py my_project'],
+            "latest_command": ['trial_build.py', 'my_project'],
+            "expected_command": (['my_project', '--repo', 'test_repo',
+                                  '--branch', 'test_branch']),
+            "trial_build_called": True,
+        },
+    ]
+    for i, test_case in enumerate(test_cases):
+      with self.subTest(i=i):
+        with mock.patch('ci_trial_build.get_comments',
+                        return_value=test_case["comments"]), \
+             mock.patch('ci_trial_build.get_latest_gcbrun_command',
+                        return_value=test_case["latest_command"]), \
+             mock.patch('oss_fuzz_on_demand.oss_fuzz_on_demand_main') as (
+                 mock_oss_fuzz_on_demand), \
+             mock.patch('trial_build.trial_build_main') as (
+                 mock_trial_build_trial_build_main):
+
+          ci_trial_build.exec_command_from_github(0, "test_repo", "test_branch")
+
+          if test_case["trial_build_called"]:
+            mock_trial_build_trial_build_main.assert_called_once_with(
+                test_case["expected_command"], local_base_build=False)
+            mock_oss_fuzz_on_demand.assert_not_called()
+          else:
+            mock_trial_build_trial_build_main.assert_not_called()
+            mock_oss_fuzz_on_demand.assert_called_once_with(
+                test_case["expected_command"])
+                
