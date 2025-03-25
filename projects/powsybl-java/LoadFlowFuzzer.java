ff73c5f07332a6e538efab242e1a08c7a8f9b890
Arthur Chan <arthur.chan@adalogics.com>
2025-03-25 11:28:43 +0000

    powsybl-java: Add and update fuzzers (#13160)
    This PR adds and updates fuzzers for more java modules in the powsybl
    library.
    
    ---------
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

---
+++ projects/powsybl-java/LoadFlowFuzzer.java
@@ -0,0 +1,219 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.powsybl.cgmes.conversion.*;
+import com.powsybl.commons.*;
+import com.powsybl.commons.datasource.*;
+import com.powsybl.commons.report.*;
+import com.powsybl.computation.local.*;
+import com.powsybl.contingency.*;
+import com.powsybl.ieeecdf.converter.*;
+import com.powsybl.iidm.network.*;
+import com.powsybl.loadflow.*;
+import com.powsybl.loadflow.LoadFlowParameters.*;
+import com.powsybl.matpower.converter.*;
+import com.powsybl.nad.*;
+import com.powsybl.openloadflow.*;
+import com.powsybl.powerfactory.converter.*;
+import com.powsybl.powerfactory.model.*;
+import com.powsybl.psse.converter.*;
+import com.powsybl.security.*;
+import com.powsybl.sensitivity.*;
+import com.powsybl.shortcircuit.*;
+import com.powsybl.sld.*;
+import com.powsybl.ucte.converter.*;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class LoadFlowFuzzer {
+  private static Path tempFile;
+
+  public static void fuzzerInitialize() {
+    try {
+      tempFile = Files.createTempFile("fuzz-", "-fuzz");
+      tempFile.toFile().deleteOnExit();
+    } catch (Throwable ignored) {
+      tempFile = null;
+    }
+  }
+
+  private static Contingency getContingency(FuzzedDataProvider data) {
+    List<ContingencyElement> allElements = new ArrayList<>();
+    allElements.add(new BatteryContingency("fuzz"));
+    allElements.add(new BranchContingency("fuzz"));
+    allElements.add(new BusbarSectionContingency("fuzz"));
+    allElements.add(new BusContingency("fuzz"));
+    allElements.add(new DanglingLineContingency("fuzz"));
+    allElements.add(new GeneratorContingency("fuzz"));
+    allElements.add(new HvdcLineContingency("fuzz"));
+    allElements.add(new LineContingency("fuzz"));
+    allElements.add(new LoadContingency("fuzz"));
+    allElements.add(new ShuntCompensatorContingency("fuzz"));
+    allElements.add(new StaticVarCompensatorContingency("fuzz"));
+    allElements.add(new SwitchContingency("fuzz"));
+    allElements.add(new ThreeWindingsTransformerContingency("fuzz"));
+    allElements.add(new TieLineContingency("fuzz"));
+    allElements.add(new TwoWindingsTransformerContingency("fuzz"));
+
+    // Build random contingency with repetition
+    Contingency contingency = new Contingency("Fuzz");
+    for (Integer i = 0; i < 10; i++) {
+      ContingencyElement elem = allElements.get(data.consumeInt(0, allElements.size() - 1));
+      contingency.addElement(elem);
+    }
+    return contingency;
+  }
+
+  public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+    // 14 Doubles + 11 Integers + 15 Booleans + 7 pick values + bytes for network
+    Integer requiredBytes = (14 * 8) + (11 * 4) + (15 * 1) + (7 * 4) + 1;
+    if (data.remainingBytes() < requiredBytes) {
+      return;
+    }
+
+    try {
+      Importer importer = null;
+      int choice = data.consumeInt(1, 6);
+      double[] randomDouble = new double[12];
+      for (int i = 0; i < randomDouble.length; i++) {
+        randomDouble[i] = data.consumeDouble();
+      }
+
+      Contingency contingency = getContingency(data);
+
+      LoadFlowParameters loadFlowParameters = new LoadFlowParameters();
+      loadFlowParameters.setBalanceType(data.pickValue(EnumSet.allOf(BalanceType.class)));
+      loadFlowParameters.setVoltageInitMode(data.pickValue(EnumSet.allOf(VoltageInitMode.class)));
+      loadFlowParameters.setConnectedComponentMode(
+          data.pickValue(EnumSet.allOf(ConnectedComponentMode.class)));
+      loadFlowParameters.setDc(data.consumeBoolean());
+      loadFlowParameters.setDcPowerFactor(data.consumeDouble());
+      loadFlowParameters.setDcUseTransformerRatio(data.consumeBoolean());
+      loadFlowParameters.setDistributedSlack(data.consumeBoolean());
+      loadFlowParameters.setHvdcAcEmulation(data.consumeBoolean());
+      loadFlowParameters.setNoGeneratorReactiveLimits(data.consumeBoolean());
+      loadFlowParameters.setPhaseShifterRegulationOn(data.consumeBoolean());
+      loadFlowParameters.setReadSlackBus(data.consumeBoolean());
+      loadFlowParameters.setShuntCompensatorVoltageControlOn(data.consumeBoolean());
+      loadFlowParameters.setSimulShunt(data.consumeBoolean());
+      loadFlowParameters.setTransformerVoltageControlOn(data.consumeBoolean());
+      loadFlowParameters.setTwtSplitShuntAdmittance(data.consumeBoolean());
+      loadFlowParameters.setUseReactiveLimits(data.consumeBoolean());
+
+      ContingencyContext context =
+          new ContingencyContext("ID", data.pickValue(EnumSet.allOf(ContingencyContextType.class)));
+      SensitivityFactor factor =
+          new SensitivityFactor(
+              data.pickValue(EnumSet.allOf(SensitivityFunctionType.class)),
+              "ID",
+              data.pickValue(EnumSet.allOf(SensitivityVariableType.class)),
+              "ID",
+              data.consumeBoolean(),
+              context);
+
+      Properties properties = new Properties();
+      properties.setProperty("solver", data.pickValue(new String[] {"DEFAULT", "NEWTON", "GAUSS"}));
+      properties.setProperty("convergence", String.valueOf(data.consumeDouble()));
+
+      ReadOnlyMemDataSource ds = new ReadOnlyMemDataSource("fuzz");
+      ds.putData("fuzz", data.consumeRemainingAsBytes());
+
+      switch (choice) {
+        case 1:
+          importer = new CgmesImport();
+          break;
+        case 2:
+          importer = new IeeeCdfImporter();
+          break;
+        case 3:
+          importer = new MatpowerImporter();
+          break;
+        case 4:
+          importer = new PowerFactoryImporter();
+          break;
+        case 5:
+          importer = new PsseImporter();
+          break;
+        case 6:
+          importer = new UcteImporter();
+          break;
+        default:
+          return;
+      }
+
+      if (importer != null) {
+        Network network = importer.importData(ds, NetworkFactory.findDefault(), properties);
+        if (network.getBusView().getBuses().spliterator().getExactSizeIfKnown() > 0
+            && network.getGeneratorCount() > 0) {
+
+          for (Bus bus : network.getBusView().getBuses()) {
+            bus.setV(Math.abs(randomDouble[0]));
+            bus.setAngle(randomDouble[1]);
+          }
+          for (Generator generator : network.getGenerators()) {
+            generator.setMaxP(Math.abs(randomDouble[2]));
+            generator.setMinP(Math.max(0, randomDouble[3]));
+            generator.setTargetV(randomDouble[4]);
+            generator.setTargetP(randomDouble[5]);
+            generator.setTargetQ(randomDouble[6]);
+            generator.setRatedS(Math.max(1.0, randomDouble[7]));
+          }
+          for (Load load : network.getLoads()) {
+            load.setP0(randomDouble[8]);
+            load.setQ0(randomDouble[9]);
+          }
+          for (Line line : network.getLines()) {
+            line.setR(randomDouble[10]);
+            line.setX(randomDouble[11]);
+          }
+
+          LoadFlow.run(network, loadFlowParameters);
+          new OpenLoadFlowProvider()
+              .run(
+                  network,
+                  LocalComputationManager.getDefault(),
+                  "Fuzz",
+                  loadFlowParameters,
+                  ReportNode.NO_OP);
+
+          List<Fault> faults = new ArrayList<>();
+          faults.add(new BranchFault("id", "elemId", randomDouble[12]));
+          faults.add(new BusFault("id", "elemId"));
+          ShortCircuitAnalysis.run(network, faults);
+
+          List<Contingency> contingencies = new ArrayList<>();
+          contingencies.add(contingency);
+          SecurityAnalysis.run(network, contingencies);
+
+          List<SensitivityFactor> factors = new ArrayList<>();
+          factors.add(factor);
+          SensitivityAnalysis.run(network, factors);
+
+          if (tempFile != null) {
+            NetworkAreaDiagram.draw(network, tempFile);
+            SingleLineDiagram.draw(network, "fuzz", tempFile);
+          }
+        }
+      }
+    } catch (PowsyblException
+        | UncheckedIOException
+        | PowerFactoryException
+        | IllegalArgumentException e) {
+      // Fuzzer: silently ignore
+    }
+  }
+}
---
+++ projects/powsybl-java/SparseMatrixFuzzer.java
@@ -0,0 +1,60 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.powsybl.commons.exceptions.UncheckedClassNotFoundException;
+import com.powsybl.math.matrix.MatrixException;
+import com.powsybl.math.matrix.SparseMatrix;
+import java.io.ByteArrayInputStream;
+import java.io.UncheckedIOException;
+
+public class SparseMatrixFuzzer {
+
+  public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+    try {
+      int row = data.consumeInt(1, 10);
+      int col = data.consumeInt(1, 10);
+
+      // Prepare matrix with constructor
+      SparseMatrix matrix =
+          new SparseMatrix(row, col, new int[col + 1], new int[row + 1], new double[row + 1]);
+      for (int j = 0; j < col; j++) {
+        for (int i = 0; i < row; i++) {
+          double value = data.consumeDouble();
+          matrix.set(i, j, data.consumeDouble());
+        }
+      }
+      matrix.reset();
+
+      // Fuzz operational methods
+      matrix.setRgrowthThreshold(data.consumeDouble());
+      matrix.decomposeLU();
+      matrix.transpose().decomposeLU();
+
+      // Fuzz deserailisation
+      ByteArrayInputStream input = new ByteArrayInputStream(data.consumeRemainingAsBytes());
+      SparseMatrix other = SparseMatrix.read(input);
+      matrix.times(other);
+      matrix.add(other, 1, 1);
+      matrix.equals(other);
+    } catch (MatrixException
+        | UncheckedIOException
+        | IllegalArgumentException
+        | UncheckedClassNotFoundException
+        | ClassCastException e) {
+      // Known exceptions
+    }
+  }
+}
