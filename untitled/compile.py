41e9a5993428160553b474d2d3edcdfdb0f7d7b6
Carlo Lemos <carlolemos@google.com>
2025-04-09 14:57:24 +0000

    first draft of the new cflags compilation

---
+++ untitled/compile.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+"""Note: This is not a source file that is built and run as part of the build.
+
+It is copied into the OSS-Fuzz container image and run there as part of the
+instrumentation process.
+"""
+
+import os
+import shutil
+import subprocess
+
+
+def main():
+  # Copy fuzzing_engine.cc to the src directory so it is available to the
+  # indexer.
+  shutil.copyfile("/untitled/fuzzing_engine.cc",
+                  "/src/untitled_fuzzing_engine.cc")
+
+  # Compile and link the fuzzing engine.
+  subprocess.run([
+      "/untitled/clang++",
+      "-c",
+      "-Wall",
+      "-Wextra",
+      "-pedantic",
+      "-std=c++20",
+      "-glldb",
+      "-O0",
+      "/src/untitled_fuzzing_engine.cc",
+      "-o",
+      "/out/untitled_fuzzing_engine.o",
+      "-gen-cdb-fragment-path", "/out/cdb", "-Qunused-arguments",
+      "--sysroot=/sysroot",
+      "-I/usr/grte/v5/include/",
+      "-I/toolchain/lib/clang/google3-trunk/include",
+      "-I/toolchain/include/c++/v1/",
+  ], capture_output=False, check=True)
+
+  subprocess.run([
+      "ar",
+      "rcs",
+      "/untitled/fuzzing_engine.a",
+      "/out/untitled_fuzzing_engine.o",
+  ], capture_output=False, check=True)
+
+  # Set up the symlink to the fuzzing engine.
+  try:
+    os.unlink("/usr/lib/libFuzzingEngine.a")
+  except FileNotFoundError:
+    pass
+
+  os.symlink("/untitled/fuzzing_engine.a", "/usr/lib/libFuzzingEngine.a")
+
+  # Run the main OSS-Fuzz compile script.
+  subprocess.run(["/usr/local/bin/compile"], capture_output=False, check=True)
+
+  for root, dirs, files in os.walk("/out"):
+    for dir_name in dirs:
+      dir_path = os.path.join(root, dir_name)
+      os.chmod(dir_path, 0o755)
+    for file_name in files:
+      file_path = os.path.join(root, file_name)
+      if os.access(file_path, os.X_OK):
+        os.chmod(file_path, 0o755)
+      else:
+        os.chmod(file_path, 0o644)
+
+
+if __name__ == "__main__":
+  main()
---
+++ untitled/fuzzing_engine.cc
@@ -0,0 +1,70 @@
+// Note: This is not a source file that is built and run on google3/prod. It is
+// copied into the OSS-Fuzz container image and compiled there as part of the
+// instrumentation process.
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t data[], size_t n);
+
+__attribute__((weak)) int LLVMFuzzerInitialize(
+    __attribute__((unused)) int* argc, __attribute__((unused)) char*** argv) {
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    // Special-case because curl invokes the fuzzer binaries without arguments
+    // during make, and will fail if they don't return success.
+    if (strstr(argv[0], "curl_fuzzer")) {
+      fprintf(stderr, "Exiting early for curl_fuzzer\n");
+      exit(EXIT_SUCCESS);
+    }
+
+    fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  int fd = open(argv[1], O_RDONLY);
+  if (fd == -1) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+
+  const off_t end_offset = lseek(fd, 0, SEEK_END);
+  if (end_offset == static_cast<off_t>(-1)) {
+    perror("lseek SEEK_END");
+    exit(EXIT_FAILURE);
+  }
+
+  if (lseek(fd, 0, SEEK_SET) == -1) {
+    perror("lseek SEEK_SET");
+    exit(EXIT_FAILURE);
+  }
+
+  const size_t size = static_cast<size_t>(end_offset);
+
+  void* mapping = mmap(nullptr, static_cast<size_t>(size),
+                       PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  if (mapping == MAP_FAILED) {
+    perror("mmap");
+    exit(EXIT_FAILURE);
+  }
+  close(fd);
+
+  int res = LLVMFuzzerInitialize(&argc, &argv);
+  if (res != 0) {
+    return res;
+  }
+
+  res = LLVMFuzzerTestOneInput(static_cast<uint8_t*>(mapping), size);
+
+  munmap(mapping, size);
+  return res;
+}
