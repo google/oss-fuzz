01f3c10b7b51fb6d5872083cb8c8a5d572b3834b
Jonathan Metzman <metzman@chromium.org>
2025-05-20 14:56:47 -0400

    Fix issues.
    1. Some builds using relative paths were not handled correctly.
    2. Fix issues instead of reverting in #13341 by copying necessary .so

--- infra/base-images/base-builder/indexer/index_build.py
+++ infra/base-images/base-builder/indexer/index_build.py
@@ -89,10 +89,11 @@ class Manifest:
   is_oss_fuzz: bool = False
 
 
-def _is_elf(file: Path) -> bool:
+def _is_excludable_elf(file: Path) -> bool:
   """Returns whether a file is an ELF file."""
   with file.open('rb') as f:
-    return f.read(4) == b'\x7fELF'
+    return (f.read(4) == b'\x7fELF' and not
+            str(file.absolute()).startswith('/out/lib/'))
 
 
 def save_build(
@@ -120,10 +121,10 @@ def save_build(
               continue
 
             file = Path(root, file)
-            if exclude_build_artifacts and _is_elf(file):
+            if exclude_build_artifacts and _is_excludable_elf(file):
               continue
 
-            if only_include_target and _is_elf(file):
+            if only_include_target and _is_excludable_elf(file):
               # Skip ELF files that aren't the relevant target (unless it's a
               # shared library).
               if file.name != only_include_target and file.suffix != '.so':
