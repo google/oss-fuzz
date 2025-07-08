d84287c2fa0031372f8974cbbb27a8f8362c50a5
Adam Korczynski <adam@adalogics.com>
2025-07-08 11:15:06 +0100

    brotli: add run_tests.sh
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- projects/brotli/run_tests.sh
+++ projects/brotli/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2016 Google Inc.
+#!/bin/bash -eu
+# Copyright 2025 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +15,5 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y cmake libtool make
-
-RUN git clone --depth 1 https://github.com/google/brotli.git
-WORKDIR brotli
-COPY build.sh run_tests.sh $SRC/
+cd $SRC/brotli
+make test
