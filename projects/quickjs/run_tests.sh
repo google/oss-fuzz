1f4f01467f5f61097cc3d1f4c848872a5be196f1
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2025-07-09 17:06:31 +0100

    quickjs: add run_test.sh (#13572)
    Adds `run_tests.sh` for the quickjs project.
    
    `run_tests.sh` is used as part of Chronos with cached builds:
    https://github.com/google/oss-fuzz/tree/master/infra/experimental/chronos#check-tests
    
    output from `infra/experimental/chronos/check_tests.sh quickjs c`
    ```
    ./qjs tests/test_closure.js
    ./qjs tests/test_language.js
    ./qjs --std tests/test_builtin.js
    ./qjs tests/test_loop.js
    ./qjs tests/test_bigint.js
    ./qjs tests/test_cyclic_import.js
    ./qjs tests/test_std.js
    ./qjs tests/test_bjson.js
    ./qjs examples/test_point.js
    ```
    
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- projects/quickjs/run_tests.sh
+++ projects/quickjs/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2020 Google Inc.
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +15,10 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make
-RUN git clone --depth 1 https://github.com/bellard/quickjs quickjs
-RUN git clone --depth 1 https://github.com/renatahodovan/quickjs-corpus
-WORKDIR $SRC/quickjs
-COPY build.sh run_tests.sh run_tests_patch.diff $SRC/
+# At the time of writing, `tests/test_worker.js`
+# fails from latest master, so we remove it.
+# This is being tracked in https://github.com/bellard/quickjs/issues/390.
+# A TODO is to track when it has been patched
+# upstream and enable all tests.
+git apply $SRC/run_tests_patch.diff
+CONFIG_CLANG=y make test
