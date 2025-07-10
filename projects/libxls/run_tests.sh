e99f394199c0a3b3dbcf75f184041efdca8ee010
Adam Korczynski <adam@adalogics.com>
2025-07-10 14:50:17 +0100

    libxls: add run_tests.sh
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- projects/libxls/run_tests.sh
+++ projects/libxls/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2019 Evan Miller
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +14,4 @@
 # limitations under the License.
 #
 ################################################################################
-
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf autoconf-archive automake gettext libtool
-
-RUN git clone --depth 1 https://github.com/libxls/libxls libxls
-WORKDIR libxls
-COPY build.sh run_tests.sh $SRC/
+ASAN_OPTIONS=detect_leaks=0 make check
