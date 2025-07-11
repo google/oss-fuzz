93d69fb729e1aa5b5d16c7e20268b295e8c0f881
Carlo Lemos <55899543+vitaliset@users.noreply.github.com>
2025-07-11 19:58:01 -0300

    cmark: add run_tests.sh (#13588)
    Adds `run_tests.sh` to the cmark project.
    
    `run_tests.sh` is used as part of Chronos with cached builds:
    https://github.com/google/oss-fuzz/tree/master/infra/experimental/chronos#check-tests
    
    Output of ./infra/experimental/chronos/check_tests.sh cmark c++:
    ```
    Running tests...
    Test project /src/cmark/build
        Start 1: api_test
    1/5 Test #1: api_test .........................   Passed    0.03 sec
        Start 2: html_normalization
    2/5 Test #2: html_normalization ...............   Passed    0.22 sec
        Start 3: spectest_executable
    3/5 Test #3: spectest_executable ..............   Passed   10.47 sec
        Start 4: smartpuncttest_executable
    4/5 Test #4: smartpuncttest_executable ........   Passed    0.34 sec
        Start 5: regressiontest_executable
    5/5 Test #5: regressiontest_executable ........   Passed    0.44 sec
    
    100% tests passed, 0 tests failed out of 5
    
    Total Test time (real) =  11.51 sec
    ```

--- projects/cmark/run_tests.sh
+++ projects/cmark/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2017 Google Inc.
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +15,5 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make cmake python3
-RUN git clone --depth 1 https://github.com/commonmark/cmark.git cmark
-WORKDIR cmark
-COPY run_tests.sh build.sh *.dict *.options $SRC/
+cd build
+make test
