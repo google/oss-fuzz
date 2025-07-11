daa9915235e2025de14075cf04e7c01ffb6acba9
Carlo Lemos <55899543+vitaliset@users.noreply.github.com>
2025-07-11 20:24:06 -0300

    zlib: add run_tests.sh (#13592)
    Adds `run_tests.sh` to the zlib project.
    
    `run_tests.sh` is used as part of Chronos with cached builds:
    https://github.com/google/oss-fuzz/tree/master/infra/experimental/chronos#check-tests
    
    Output of ./infra/experimental/chronos/check_tests.sh zlib c:
    ```
    hello world
    zlib version 1.3.1.1-motley = 0x1311, compile flags = 0xa9
    uncompress(): hello, hello!
    gzread(): hello, hello!
    gzgets() after gzseek:  hello!
    inflate(): hello, hello!
    large_inflate(): OK
    after inflateSync(): hello, hello!
    inflate with dictionary: hello, hello!
                    *** zlib test OK ***
    hello world
    zlib version 1.3.1.1-motley = 0x1311, compile flags = 0xa9
    uncompress(): hello, hello!
    gzread(): hello, hello!
    gzgets() after gzseek:  hello!
    inflate(): hello, hello!
    large_inflate(): OK
    after inflateSync(): hello, hello!
    inflate with dictionary: hello, hello!
                    *** zlib shared test OK ***
    hello world
    zlib version 1.3.1.1-motley = 0x1311, compile flags = 0xa9
    uncompress(): hello, hello!
    gzread(): hello, hello!
    gzgets() after gzseek:  hello!
    inflate(): hello, hello!
    large_inflate(): OK
    after inflateSync(): hello, hello!
    inflate with dictionary: hello, hello!
                    *** zlib 64-bit test OK ***
    ```

--- projects/zlib/run_tests.sh
+++ projects/zlib/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2016 Google Inc.
+#!/bin/bash -eu
+# Copyright 2025 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +15,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake libtool
-RUN git clone --depth 1 -b develop https://github.com/madler/zlib.git
-WORKDIR zlib
-COPY run_tests.sh build.sh *_fuzzer.c* $SRC/
+make check
