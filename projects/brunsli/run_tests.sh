af4a695b6adcd595ea4ec870e51b036b4366ce98
Carlo Lemos <carlolemos@google.com>
2025-07-13 22:09:13 +0000

    tests brunsli

--- projects/brunsli/run_tests.sh
+++ projects/brunsli/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2020 Google Inc.
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y cmake libtool make
-
-RUN git clone --depth 1 https://github.com/google/brunsli.git && \
-    cd brunsli && \
-    git submodule update --init --depth 1
-WORKDIR brunsli
-COPY run_tests.sh build.sh $SRC/
+make test
