aabb59f055c4c129c5ae313b44f5e032474c17c7
David Korczynski <david@adalogics.com>
2025-07-18 08:31:36 -0700

    readstat: add run_tests.sh
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/readstat/run_tests.sh
+++ projects/readstat/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2019 Evan Miller
+#!/bin/bash -eux
+# Copyright 2025 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-################################################################################
+###############################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake gettext libtool zip zlib1g-dev
-
-RUN git clone --depth 1 https://github.com/WizardMac/ReadStat readstat
-WORKDIR readstat
-COPY *.sh $SRC/
+make check
