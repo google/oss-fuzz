3306ffbced81d576d56370cadebdd9df03c0964d
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2025-07-19 20:36:36 +0100

    libcue: add run_tests.sh (#13682)
    `run_tests.sh` is used as part of Chronos with cached builds:
    https://github.com/google/oss-fuzz/tree/master/infra/experimental/chronos#check-tests
    
    `infra/experimental/chronos/check_tests.sh libcue c`
    ```
    Running tests...                                                                                 
    Test project /work                                                                               
        Start 1: standard_cue_test                                                                   
    1/6 Test #1: standard_cue_test ................   Passed    0.01 sec                             
        Start 2: single_file_idx_00_test                                                             
    2/6 Test #2: single_file_idx_00_test ..........   Passed    0.01 sec                             
        Start 3: multiple_files_test                                                                 
    3/6 Test #3: multiple_files_test ..............   Passed    0.01 sec                             
        Start 4: noncompliant_test                                                                                                                                                                     
    4/6 Test #4: noncompliant_test ................   Passed    0.01 sec                             
        Start 5: issue10_test                                                                        
    5/6 Test #5: issue10_test .....................   Passed    0.01 sec       
        Start 6: 99_tracks_test                                                                      
    6/6 Test #6: 99_tracks_test ...................   Passed    0.01 sec                             
                                                                                                     
    100% tests passed, 0 tests failed out of 6                                                       
                                                                                                     
    Total Test time (real) =   0.06 sec                                                              
    --------------------------------------------------------                                         
    Total time taken to replay tests: 1
    ```
    
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- projects/libcue/run_tests.sh
+++ projects/libcue/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2023 Google LLC
+#!/bin/bash -eux
+# Copyright 2025 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-################################################################################
-
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake libtool bison flex
-
-RUN git clone --depth 1 https://github.com/lipnitsk/libcue.git libcue
-
-WORKDIR libcue
-COPY run_tests.sh build.sh $SRC/
+###############################################################################
+cd $WORK
+make test
