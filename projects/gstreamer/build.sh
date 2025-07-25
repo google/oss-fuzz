3238e6b695aab04c8ba6cc107705845db6dc1f8d
David Korczynski <david@adalogics.com>
2025-07-25 09:04:26 -0700

    gstreamer: fix build
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/gstreamer/build.sh
+++ projects/gstreamer/build.sh
@@ -15,4 +15,17 @@
 #
 ################################################################################
 
+# gnutls requires autoconf 2.71 minimum which is not available in the Ubuntu 20 base image
+# Skip this step if a newer base image is used
+if grep -q -F "20.04" /etc/os-release ; then
+    cd /tmp
+    wget https://archive.ubuntu.com/ubuntu/pool/main/a/autoconf/autoconf_2.71-2_all.deb
+    # Ensure file is not modified or corrupted before install
+    if echo "96b528889794c4134015a63c75050f93d8aecdf5e3f2a20993c1433f4c61b80e autoconf_2.71-2_all.deb" | sha256sum --check --status ; then
+        # Install but use G option to prevent downgrade in case this is
+        dpkg -i -G /tmp/autoconf_2.71-2_all.deb
+    fi
+fi
+
+
 $SRC/gstreamer/ci/fuzzing/build-oss-fuzz.sh
