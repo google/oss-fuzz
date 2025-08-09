afa5837e73b8ccadcb5fc3516f91bd3ae5fa7865
Lars Möllendorf <iniparser@gmail.com>
2025-08-09 20:14:24 +0200

    Add iniparser
    Signed-off-by: Lars Möllendorf <lars@moellendorf.eu>

---
+++ projects/iniparser/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
+# Copyright 2025 Lars Möllendorf <lars@moellendorf.eu>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+tree -L 2 $SRC && \
+cmake -B $WORK \
+-DBUILD_FUZZER=ON  \
+-DCMAKE_C_COMPILER=clang \
+-DCMAKE_BUILD_TYPE=DEBUG $SRC/iniparser && \
+cmake --build $WORK && \
+tree -L 2 $WORK && \
+cp $WORK/fuzz/iniparser_fuzzer $OUT/ && \
+zip -r $OUT/iniparser_fuzzer_seed_corpus.zip $SRC/iniparser/samples/ && \
+tree -L 2 $OUT
