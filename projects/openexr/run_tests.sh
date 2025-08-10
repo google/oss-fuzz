24ebc76baaecca5cec65e281ef24cb2a868cde83
Oliver Chang <ochang@google.com>
2025-08-10 06:25:25 +0000

    openexr: add run_tests.sh

---
+++ projects/openexr/run_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+#
+# Build the OpenEXR fuzzers, via the "oss_fuzz" CMake target.
+#
+# The "oss_fuzz" target is specially constructed to operate in this
+# env. It queries the environment for the values of
+# LIB_FUZZING_ENGINE, OUT, and CC/CC_FLAGS, and CXX/CXX_FLAGS. The
+# installed fuzzers go in the OUT directory.
+#
+
+set -x
+
+BUILD_DIR=$WORK/_build.oss-fuzz
+
+cd $BUILD_DIR
+make OpenEXRTest OpenEXRCoreTest IexTest OpenEXRUtilTest -j$(nproc)
+
+CTEST_PARALLEL_LEVEL=$(nproc) make test
