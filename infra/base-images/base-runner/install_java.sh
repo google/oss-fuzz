479d1393a5894d932974100eb27052b4e5e1e6a0
Tim Allison <tallison@apache.org>
2025-08-26 06:19:32 -0400

    Update jdk 17 version to unbreak the PDFBox build (#13860)
    This updates the version of jdk17 to 17.0.16+8. 
    
    This update is required to build PDFBox:
    https://issues.apache.org/jira/browse/PDFBOX-6051?focusedCommentId=18015721&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-18015721
    
    PDFBox is no longer building in ossfuzz: 
    
    https://oss-fuzz-build-logs.storage.googleapis.com/log-907a41b7-64fc-4a03-a728-a4b5bbf8042a.txt
    
    and the error message is consistent with PDFBOX-6051:
    
    ```
    Fatal error compiling: error: invalid flag: -proc:full
    ```
    
    I confirmed locally that `infra/base-images/all.sh` creates a jvm base
    that in turn allows pdfbox to build.
    
    I have NOT tested this across all projects.
    
    ---------
    
    Co-authored-by: DavidKorczynski <david@adalogics.com>

--- infra/base-images/base-runner/install_java.sh
+++ infra/base-images/base-runner/install_java.sh
@@ -31,12 +31,12 @@ case $(uname -m) in
       ;;
 esac
 
-wget -q https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-"$ARCHITECTURE"_bin.tar.gz -O /tmp/openjdk-17.0.2_linux-"$ARCHITECTURE"_bin.tar.gz
+wget -q https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.16+8/OpenJDK17U-jdk_"$ARCHITECTURE"_linux_hotspot_17.0.16_8.tar.gz -O /tmp/openjdk-17.0.16_linux-"$ARCHITECTURE"_bin.tar.gz
 wget -q https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-"$ARCHITECTURE"_bin.tar.gz -O /tmp/openjdk-15.0.2_linux-"$ARCHITECTURE"_bin.tar.gz
 cd /tmp
 mkdir -p $JAVA_HOME
-tar -xz --strip-components=1 -f openjdk-17.0.2_linux-"$ARCHITECTURE"_bin.tar.gz --directory $JAVA_HOME
-rm -f openjdk-17.0.2_linux-"$ARCHITECTURE"_bin.tar.gz
+tar -xz --strip-components=1 -f openjdk-17.0.16_linux-"$ARCHITECTURE"_bin.tar.gz --directory $JAVA_HOME
+rm -f openjdk-17.0.16_linux-"$ARCHITECTURE"_bin.tar.gz
 rm -rf $JAVA_HOME/jmods $JAVA_HOME/lib/src.zip
 
 # Install OpenJDK 15 and trim its size by removing unused components. Some projects only run with Java 15.
