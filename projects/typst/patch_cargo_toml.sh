232fb3037d44d0c9b856341659bb832229053513
David Korczynski <david@adalogics.com>
2025-08-30 22:05:47 +0100

    typst: fix build
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/typst/patch_cargo_toml.sh
+++ projects/typst/patch_cargo_toml.sh
@@ -1,24 +1,21 @@
-# Copyright 2024 Google LLC
+#!/bin/bash
+# Copyright 2025 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-################################################################################
+# This script adds the required cargo features to use the edition2024 feature
 
-FROM gcr.io/oss-fuzz-base/base-builder-rust
+set -eux
 
-RUN rustup toolchain install nightly
-RUN git clone --depth 1 https://github.com/typst/typst.git typst
-WORKDIR typst
-COPY build.sh $SRC/
-COPY patch_cargo_toml.sh $SRC/
-RUN chmod +x $SRC/patch_cargo_toml.sh && $SRC/patch_cargo_toml.sh
+find /src/typst -name Cargo.toml | while read -r cargo_toml; do
+  sed -i '1s;^;cargo-features = ["edition2024"]\n;' "$cargo_toml"
+done
