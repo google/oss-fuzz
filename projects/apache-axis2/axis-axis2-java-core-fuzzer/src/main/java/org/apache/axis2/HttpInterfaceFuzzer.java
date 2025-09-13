7a72922aa90a0005b070fd4b58f0acdf5cac645c
David Korczynski <david@adalogics.com>
2025-09-13 11:46:33 +0100

    apache-axis2: fix build
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/apache-axis2/axis-axis2-java-core-fuzzer/src/main/java/org/apache/axis2/HttpInterfaceFuzzer.java
+++ projects/apache-axis2/axis-axis2-java-core-fuzzer/src/main/java/org/apache/axis2/HttpInterfaceFuzzer.java
@@ -12,7 +12,6 @@ import java.net.http.HttpResponse;
 
 import org.apache.axis2.kernel.SimpleAxis2Server;
 
-
 public class HttpInterfaceFuzzer extends SimpleAxis2Server {
 
     private FuzzedDataProvider fuzzedDataProvider;
@@ -25,40 +24,49 @@ public class HttpInterfaceFuzzer extends SimpleAxis2Server {
     }
 
     void test() {
-        try{
+        // Begin cleanup
+        try {
+            stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        try {
             start();
 
             var client = HttpClient.newHttpClient();
-            URI uri = new URI("http://localhost:6060/axis2/services/StockQuoteService/" + fuzzedDataProvider.consumeRemainingAsString());
+            String host = "localhost";
+            int port = getPort(); // Get the dynamically assigned port
+            URI uri = new URI("http://" + host + ":" + port + "/axis2/services/StockQuoteService/" + fuzzedDataProvider.consumeRemainingAsString());
             var request = HttpRequest.newBuilder(uri)
                         .GET()
                         .build();
             var reponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (MalformedURLException e) {
-            
-        } catch (IOException e) {
-
-        } catch (URISyntaxException e) {
-
-        } catch (InterruptedException e) {
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stop();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
-
-        try {
-            stop();
-        } catch (Exception ex) {
-
-        }
-
-
     }
 
-    public static void fuzzerTestOneInput(FuzzedDataProvider fuzzedDataProvider)  throws Exception {
-
+    public static void fuzzerTestOneInput(FuzzedDataProvider fuzzedDataProvider) throws Exception {
         HttpInterfaceFuzzer fixture = new HttpInterfaceFuzzer(fuzzedDataProvider);
         fixture.test();
 
         fixture = null;
-        Thread.sleep(100); // good old way to get sockets closed.
+        Thread.sleep(100); // Ensure closed state ready for next instance
+    }
+
+    private int getPort() {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            return socket.getLocalPort();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 6060; // Backup in failure of auto assign
+        }
     }
 }
