0730eabfeeebaaac0d27ca9e399c8808dea16040
OSS-Fuzz Team <no-reply@google.com>
2025-09-16 15:46:25 -0700

    Add structures to support incremental indexing to the indexer database
    Indexer-PiperOrigin-RevId: 807878968

--- infra/indexer/index/sqlite.cc
+++ infra/indexer/index/sqlite.cc
@@ -30,6 +30,13 @@
 namespace oss_fuzz {
 namespace indexer {
 namespace {
+
+// Note: We could in principle enforce UNIQUE constraints on `reference` foreign
+// key pairs, as well as those of `virtual_method_link` and
+// `entity_translation_unit` (as an extreme, non-ID fields of e.g. `location`
+// could also be made into a UNIQUE tuple). But those are unique by construction
+// now and we hope to avoid the overhead of checking those constraints.
+
 const char kCreateDb[] =
     "PRAGMA foreign_keys = ON;\n"
     "PRAGMA user_version = " SCHEMA_VERSION
@@ -87,6 +94,25 @@ const char kCreateDb[] =
     "CREATE INDEX virtual_method_link_parent ON virtual_method_link("
     "  parent_entity_id);\n";
 
+const char kCreateIncrementalIndexingSupportTables[] =
+    "CREATE TABLE translation_unit(\n"
+    "  id       INTEGER PRIMARY KEY,\n"
+    "  path     TEXT);\n"
+    "\n"
+    "CREATE TABLE entity_translation_unit(\n"
+    "  id           INTEGER PRIMARY KEY,\n"
+    "  entity_id    INT NOT NULL,\n"
+    "  tu_id        INT NOT NULL,\n"
+    "  FOREIGN KEY (entity_id) REFERENCES entity(id),\n"
+    "  FOREIGN KEY (tu_id) REFERENCES translation_unit(id));\n"
+    "\n"
+    "CREATE TABLE reference_translation_unit(\n"
+    "  id              INTEGER PRIMARY KEY,\n"
+    "  reference_id    INT NOT NULL,\n"
+    "  tu_id           INT NOT NULL,\n"
+    "  FOREIGN KEY (reference_id) REFERENCES reference(id),\n"
+    "  FOREIGN KEY (tu_id) REFERENCES translation_unit(id));\n";
+
 const char kInsertLocation[] =
     "INSERT INTO location\n"
     "  (id, dirname, basename, start_line, end_line)\n"
@@ -107,6 +133,21 @@ const char kInsertReference[] =
 const char kInsertLink[] =
     "INSERT INTO virtual_method_link\n"
     "  (id, parent_entity_id, child_entity_id)\n"
+    "  VALUES (?1, ?2, ?3);";
+
+const char kInsertTranslationUnit[] =
+    "INSERT INTO translation_unit\n"
+    "  (id, path)\n"
+    "  VALUES (?1, ?2);";
+
+const char kInsertEntityTranslationUnit[] =
+    "INSERT INTO entity_translation_unit\n"
+    "  (id, entity_id, tu_id)\n"
+    "  VALUES (?1, ?2, ?3);";
+
+const char kInsertReferenceTranslationUnit[] =
+    "INSERT INTO reference_translation_unit\n"
+    "  (id, reference_id, tu_id)\n"
     "  VALUES (?1, ?2, ?3);";
 
 const char kFinalizeDb[] =
@@ -163,8 +204,8 @@ bool InsertLocations(sqlite3* db, absl::Span<const Location> locations) {
 
 bool InsertEntities(sqlite3* db, absl::Span<const Entity> entities) {
   // `substitute_entity_id` foreign key can refer to a yet-unadded entity.
-  if (sqlite3_exec(db, "PRAGMA foreign_keys = OFF;", nullptr,
-                   nullptr, nullptr) != SQLITE_OK) {
+  if (sqlite3_exec(db, "PRAGMA foreign_keys = OFF;", nullptr, nullptr,
+                   nullptr) != SQLITE_OK) {
     LOG(ERROR) << "sqlite disabling foreign keys failed: `"
                << sqlite3_errmsg(db) << "`";
     return false;
@@ -261,8 +302,8 @@ bool InsertEntities(sqlite3* db, absl::Span<const Entity> entities) {
   std::move(cleanup).Cancel();
   sqlite3_finalize(insert_entity);
 
-  if (sqlite3_exec(db, "PRAGMA foreign_keys = ON;", nullptr, nullptr, nullptr)
-          != SQLITE_OK) {
+  if (sqlite3_exec(db, "PRAGMA foreign_keys = ON;", nullptr, nullptr,
+                   nullptr) != SQLITE_OK) {
     LOG(ERROR) << "sqlite re-enabling foreign keys failed: `"
                << sqlite3_errmsg(db) << "`";
     return false;
@@ -320,7 +361,7 @@ bool InsertVirtualMethodLinks(sqlite3* db,
     return false;
   }
 
-  for (ReferenceId i = 0; i < links.size(); ++i) {
+  for (VirtualMethodLinkId i = 0; i < links.size(); ++i) {
     const VirtualMethodLink& link = links[i];
     if (sqlite3_bind_int64(insert_link, 1, i) != SQLITE_OK ||
         sqlite3_bind_int64(insert_link, 2, link.parent()) != SQLITE_OK ||
@@ -332,7 +373,7 @@ bool InsertVirtualMethodLinks(sqlite3* db,
     }
 
     if (sqlite3_step(insert_link) != SQLITE_DONE) {
-      LOG(ERROR) << "sqlite executing insert_reference failed: `"
+      LOG(ERROR) << "sqlite executing insert_link failed: `"
                  << sqlite3_errmsg(db) << "`";
       sqlite3_finalize(insert_link);
       return false;
@@ -343,6 +384,126 @@ bool InsertVirtualMethodLinks(sqlite3* db,
   }
 
   sqlite3_finalize(insert_link);
+  return true;
+}
+
+bool InsertTranslationUnits(
+    sqlite3* db, absl::Span<const TranslationUnit> translation_units) {
+  sqlite3_stmt* insert_tu = nullptr;
+  if (sqlite3_prepare_v2(db, kInsertTranslationUnit,
+                         sizeof(kInsertTranslationUnit), &insert_tu,
+                         nullptr) != SQLITE_OK) {
+    LOG(ERROR) << "sqlite compiling prepared statement failed: `"
+               << sqlite3_errmsg(db) << "`";
+    return false;
+  }
+
+  for (TranslationUnitId i = 0; i < translation_units.size(); ++i) {
+    const TranslationUnit& tu = translation_units[i];
+    if (sqlite3_bind_int64(insert_tu, 1, i) != SQLITE_OK ||
+        sqlite3_bind_text(insert_tu, 2, tu.index_path().data(),
+                          tu.index_path().size(), SQLITE_STATIC) != SQLITE_OK) {
+      LOG(ERROR) << "sqlite binding insert_tu failed: `" << sqlite3_errmsg(db)
+                 << "`";
+      sqlite3_finalize(insert_tu);
+      return false;
+    }
+
+    if (sqlite3_step(insert_tu) != SQLITE_DONE) {
+      LOG(ERROR) << "sqlite executing insert_tu failed: `" << sqlite3_errmsg(db)
+                 << "`";
+      sqlite3_finalize(insert_tu);
+      return false;
+    }
+
+    sqlite3_reset(insert_tu);
+    sqlite3_clear_bindings(insert_tu);
+  }
+
+  sqlite3_finalize(insert_tu);
+  return true;
+}
+
+bool InsertEntityTranslationUnits(
+    sqlite3* db,
+    absl::Span<const EntityTranslationUnit> entity_translation_units) {
+  sqlite3_stmt* insert_entity_tu = nullptr;
+  if (sqlite3_prepare_v2(db, kInsertEntityTranslationUnit,
+                         sizeof(kInsertEntityTranslationUnit),
+                         &insert_entity_tu, nullptr) != SQLITE_OK) {
+    LOG(ERROR) << "sqlite compiling prepared statement failed: `"
+               << sqlite3_errmsg(db) << "`";
+    return false;
+  }
+
+  for (EntityTranslationUnitId i = 0; i < entity_translation_units.size();
+       ++i) {
+    const EntityTranslationUnit& entity_tu = entity_translation_units[i];
+    if (sqlite3_bind_int64(insert_entity_tu, 1, i) != SQLITE_OK ||
+        sqlite3_bind_int64(insert_entity_tu, 2, entity_tu.entity_id()) !=
+            SQLITE_OK ||
+        sqlite3_bind_int64(insert_entity_tu, 3, entity_tu.tu_id()) !=
+            SQLITE_OK) {
+      LOG(ERROR) << "sqlite binding insert_entity_tu failed: `"
+                 << sqlite3_errmsg(db) << "`";
+      sqlite3_finalize(insert_entity_tu);
+      return false;
+    }
+
+    if (sqlite3_step(insert_entity_tu) != SQLITE_DONE) {
+      LOG(ERROR) << "sqlite executing insert_entity_tu failed: `"
+                 << sqlite3_errmsg(db) << "`";
+      sqlite3_finalize(insert_entity_tu);
+      return false;
+    }
+
+    sqlite3_reset(insert_entity_tu);
+    sqlite3_clear_bindings(insert_entity_tu);
+  }
+
+  sqlite3_finalize(insert_entity_tu);
+  return true;
+}
+
+bool InsertReferenceTranslationUnits(
+    sqlite3* db,
+    absl::Span<const ReferenceTranslationUnit> reference_translation_units) {
+  sqlite3_stmt* insert_reference_tu = nullptr;
+  if (sqlite3_prepare_v2(db, kInsertReferenceTranslationUnit,
+                         sizeof(kInsertReferenceTranslationUnit),
+                         &insert_reference_tu, nullptr) != SQLITE_OK) {
+    LOG(ERROR) << "sqlite compiling prepared statement failed: `"
+               << sqlite3_errmsg(db) << "`";
+    return false;
+  }
+
+  for (ReferenceTranslationUnitId i = 0; i < reference_translation_units.size();
+       ++i) {
+    const ReferenceTranslationUnit& reference_tu =
+        reference_translation_units[i];
+    if (sqlite3_bind_int64(insert_reference_tu, 1, i) != SQLITE_OK ||
+        sqlite3_bind_int64(insert_reference_tu, 2,
+                           reference_tu.reference_id()) != SQLITE_OK ||
+        sqlite3_bind_int64(insert_reference_tu, 3, reference_tu.tu_id()) !=
+            SQLITE_OK) {
+      LOG(ERROR) << "sqlite binding insert_reference_tu failed: `"
+                 << sqlite3_errmsg(db) << "`";
+      sqlite3_finalize(insert_reference_tu);
+      return false;
+    }
+
+    if (sqlite3_step(insert_reference_tu) != SQLITE_DONE) {
+      LOG(ERROR) << "sqlite executing insert_reference_tu failed: `"
+                 << sqlite3_errmsg(db) << "`";
+      sqlite3_finalize(insert_reference_tu);
+      return false;
+    }
+
+    sqlite3_reset(insert_reference_tu);
+    sqlite3_clear_bindings(insert_reference_tu);
+  }
+
+  sqlite3_finalize(insert_reference_tu);
   return true;
 }
 }  // anonymous namespace
@@ -391,6 +552,39 @@ bool SaveAsSqlite(const FlatIndex& index, const std::string& path) {
     return false;
   }
 
+  if (index.incremental_indexing_metadata.has_value()) {
+    const IncrementalIndexingMetadata& metadata =
+        *index.incremental_indexing_metadata;
+
+    LOG(INFO) << "creating incremental indexing support tables";
+    if (sqlite3_exec(db, kCreateIncrementalIndexingSupportTables, nullptr,
+                     nullptr, &error) != SQLITE_OK) {
+      LOG(ERROR) << "incremental indexing support table creation failed: `"
+                 << error << "`";
+      sqlite3_close(db);
+      return false;
+    }
+
+    LOG(INFO) << "inserting translation units";
+    if (!InsertTranslationUnits(db, metadata.translation_units)) {
+      sqlite3_close(db);
+      return false;
+    }
+
+    LOG(INFO) << "inserting entity - translation unit pairs";
+    if (!InsertEntityTranslationUnits(db, metadata.entity_translation_units)) {
+      sqlite3_close(db);
+      return false;
+    }
+
+    LOG(INFO) << "inserting reference - translation unit pairs";
+    if (!InsertReferenceTranslationUnits(
+            db, metadata.reference_translation_units)) {
+      sqlite3_close(db);
+      return false;
+    }
+  }
+
   LOG(INFO) << "finalizing database";
   if (sqlite3_exec(db, kFinalizeDb, nullptr, nullptr, &error) != SQLITE_OK) {
     LOG(ERROR) << "database finalization failed: `" << error << "`";
@@ -427,5 +621,6 @@ bool SaveAsSqlite(const FlatIndex& index, const std::string& path) {
   sqlite3_close(db);
   return backup_success;
 }
+
 }  // namespace indexer
 }  // namespace oss_fuzz
