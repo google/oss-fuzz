180babbb3682706f3edb4eaf8145d7f7d0f7bb41
Adam Korczynski <adam@adalogics.com>
2025-09-30 22:25:29 +0000

    nodejs: migrate fuzzers to oss-fuzz
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- projects/nodejs/build.sh
+++ projects/nodejs/build.sh
@@ -14,6 +14,17 @@
 # limitations under the License.
 #
 ################################################################################
+rm $SRC/node/test/fuzzers/*
+cp $SRC/fuzz_sources/* $SRC/node/test/fuzzers/
+
+# Add the fuzzers to node.gyp
+mkdir $SRC/modify-node-gyp
+cd $SRC/modify-node-gyp
+go mod init modify-node-gyp
+mv $SRC/add_fuzzers_to_node_gyp.go ./main.go
+go run main.go $SRC/node/node.gyp /tmp/updated-node.gyp
+mv /tmp/updated-node.gyp $SRC/node/node.gyp
+
 cd $SRC/node
 
 # Coverage build takes very long and time outs in the CI which blocks changes. Ignore Coverage build in OSS-Fuzz CI for now:
@@ -31,75 +42,62 @@ if [[ "$SANITIZER" = coverage ]]; then
 fi
 
 # Build node
+export CXXFLAGS="$CXXFLAGS -std=c++20 -stdlib=libc++"
+export GN_ARGS='use_custom_libcxx=true'
 export LDFLAGS="$CXXFLAGS"
+export LDFLAGS="$LDFLAGS -stdlib=libc++"
 export LD="$CXX"
 ./configure --with-ossfuzz
 
 # Ensure we build with few processors if memory gets exhausted
 if [[ "$SANITIZER" = coverage ]]; then
-	for mrkpath in \
-        	fuzz_buffer_includes.target.mk \
-        	fuzz_buffer_equals.target.mk \
-        	fuzz_buffer_compare.target.mk \
-        	fuzz_blob.target.mk \
-        	fuzz_zlib_gzip_createUnzip.target.mk \
-	        fuzz_zlib_createBrotliDecompress.target.mk \
-	        fuzz_zlib_brotliDecompress.target.mk \
-	        fuzz_zlib_brotliCompress.target.mk \
-	        fuzz_string_decoder.target.mk \
-	        fuzz_querystring_parse.target.mk \
-	        fuzz_path_join.target.mk \
-	        fuzz_stream1.target.mk \
-	        fuzz_strings.target.mk \
-	        fuzz_diffieHellmanPEM.target.mk \
-	        fuzz_createPrivateKeyPEM.target.mk \
-	        fuzz_createPrivateKeyDER.target.mk \
-	        fuzz_path_extname.target.mk \
-	        fuzz_path_normalize.target.mk \
-	        fuzz_path_relative.target.mk \
-	        fuzz_createPrivateKeyJWK.target.mk \
-	        fuzz_path_format.target.mk \
-	        fuzz_ClientHelloParser.target.mk \
-	        fuzz_diffieHellmanJWK.target.mk \
-	        fuzz_path_basename.target.mk \
-	        fuzz_path_isAbsolute.target.mk \
-       		fuzz_tls_socket_request.target.mk \
-	        fuzz_diffieHellmanDER.target.mk \
-    		fuzz_path_toNamespacedPath.target.mk \
-        	fuzz_path_parse.target.mk \
-        	fuzz_httpparser1.target.mk \
-        	fuzz_path_dirname.target.mk \
-        	fuzz_x509.target.mk \
-        	fuzz_fs_write_read_append.target.mk \
-        	fuzz_sign_verify.target.mk \
-        	fuzz_path_resolve.target.mk \
-        	fuzz_fs_write_open_read.target.mk \
-        	libnode.target.mk
-	do
-        	echo "sed'ing ${mrkpath}"
-        	sed -i 's/BUILDTYPE))/BUILDTYPE)) -fprofile-instr-generate -fcoverage-mapping/g' "$SRC/node/out/${mrkpath}"
-	done
-	make -j 3 || make -j1
+    for mrkpath in \
+        fuzz_buffer_includes.target.mk \
+        fuzz_buffer_equals.target.mk \
+        fuzz_buffer_compare.target.mk \
+        fuzz_blob.target.mk \
+        fuzz_zlib_gzip_createUnzip.target.mk \
+        fuzz_zlib_createBrotliDecompress.target.mk \
+        fuzz_zlib_brotliDecompress.target.mk \
+        fuzz_zlib_brotliCompress.target.mk \
+        fuzz_string_decoder.target.mk \
+        fuzz_querystring_parse.target.mk \
+        fuzz_path_join.target.mk \
+        fuzz_stream1.target.mk \
+        fuzz_strings.target.mk \
+        fuzz_diffieHellmanPEM.target.mk \
+        fuzz_createPrivateKeyPEM.target.mk \
+        fuzz_createPrivateKeyDER.target.mk \
+        fuzz_path_extname.target.mk \
+        fuzz_path_normalize.target.mk \
+        fuzz_path_relative.target.mk \
+        fuzz_createPrivateKeyJWK.target.mk \
+        fuzz_path_format.target.mk \
+        fuzz_ClientHelloParser.target.mk \
+        fuzz_diffieHellmanJWK.target.mk \
+        fuzz_path_basename.target.mk \
+        fuzz_path_isAbsolute.target.mk \
+        fuzz_tls_socket_request.target.mk \
+        fuzz_diffieHellmanDER.target.mk \
+        fuzz_path_toNamespacedPath.target.mk \
+        fuzz_path_parse.target.mk \
+        fuzz_httpparser1.target.mk \
+        fuzz_path_dirname.target.mk \
+        fuzz_x509.target.mk \
+        fuzz_fs_write_read_append.target.mk \
+        fuzz_sign_verify.target.mk \
+        fuzz_path_resolve.target.mk \
+        fuzz_fs_write_open_read.target.mk \
+        libnode.target.mk
+    do
+        echo "sed'ing ${mrkpath}"
+        sed -i 's/BUILDTYPE))/BUILDTYPE)) -fprofile-instr-generate -fcoverage-mapping/g' "$SRC/node/out/${mrkpath}"
+    done
+    make -j3 || make -j1
 else
-	make -j$(nproc) || make -j1
+        make -j$(nproc) || make -j1
 fi
 
 # Copy all fuzzers to OUT folder 
 cp out/Release/fuzz_* ${OUT}/
-
-# Create seed for fuzz_env
-mkdir fuzz_env_seed
-find ./test -name '*.js' -exec cp {} ./fuzz_env_seed/ \;
-cd fuzz_env_seed
-# Remove small files:
-find -size -5k -delete
-# Remove large files:
-find -size +30k -delete
-zip $OUT/fuzz_env_seed_corpus.zip ./*
-# Add more seeds
-cd $SRC/node/test/fuzzers/seed/fuzz_env
-zip $OUT/fuzz_env_seed_corpus.zip ./*
-
-cd $SRC/node/test/fuzzers/seed/fuzz_x509
-zip $OUT/fuzz_x509_seed_corpus.zip ./*
 
