2fa830e6a610d80cddbb629647e0de9e552a598f
David Korczynski <david@adalogics.com>
2025-10-01 07:30:23 -0700

    infra: enable Ruby fuzzing
    Follow up to https://github.com/google/oss-fuzz/pull/12034 and
    https://github.com/google/oss-fuzz/pull/12034#issuecomment-2203043223
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- infra/base-images/base-builder/install_ruby.sh
+++ infra/base-images/base-builder/install_ruby.sh
@@ -18,7 +18,10 @@
 apt update
 apt install -y lsb-release software-properties-common gnupg2 binutils xz-utils libyaml-dev
 gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash
+
+curl -sSL https://get.rvm.io > ruby_installation.sh
+chmod +x ruby_installation.sh
+bash ruby_installation.sh stable
 
 . /etc/profile.d/rvm.sh
 
