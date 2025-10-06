51d2018d0e3e74b02bf5b9451d927e263354ef2b
jhrag13 <jhrag13@gmail.com>
2025-10-06 15:34:00 -0400

    Add Apache 2.0 license headers to fuzz targets

--- projects/gson/GsonParserFuzzer.java
+++ projects/gson/GsonParserFuzzer.java
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
@@ -5,7 +19,7 @@ import java.io.StringReader;
 
 public class GsonParserFuzzer {
     private static final Gson gson = new Gson();
-    
+
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
         try {
             String jsonInput = data.consumeRemainingAsString();
@@ -13,7 +27,7 @@ public class GsonParserFuzzer {
             testStreamParsing(jsonInput);
         } catch (Exception e) {}
     }
-    
+
     private static void testJsonParsing(String jsonInput) {
         try {
             JsonElement element = JsonParser.parseString(jsonInput);
@@ -21,7 +35,7 @@ public class GsonParserFuzzer {
             else if (element.isJsonArray()) element.getAsJsonArray();
         } catch (Exception e) {}
     }
-    
+
     private static void testStreamParsing(String jsonInput) {
         try {
             JsonReader reader = new JsonReader(new StringReader(jsonInput));
--- projects/gson/GsonSerializationFuzzer.java
+++ projects/gson/GsonSerializationFuzzer.java
@@ -1,28 +1,42 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.google.gson.*;
 import java.util.*;
 
 public class GsonSerializationFuzzer {
     private static final Gson gson = new Gson();
-    
+
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
         try {
             TestData testData = new TestData();
             testData.name = data.consumeString(50);
             testData.value = data.consumeInt();
             testData.tags = new ArrayList<>();
-            
+
             int count = data.consumeInt(0, 5);
             for (int i = 0; i < count; i++) {
                 testData.tags.add(data.consumeString(20));
             }
-            
+
             String json = gson.toJson(testData);
             TestData parsed = gson.fromJson(json, TestData.class);
-            
+
         } catch (Exception e) {}
     }
-    
+
     static class TestData {
         public String name;
         public int value;
