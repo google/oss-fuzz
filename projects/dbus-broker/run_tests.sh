d1fb6e7fa07ed1bed85ee06dc36041e0daec600c
Arthur Chan <arthur.chan@adalogics.com>
2025-12-12 23:44:24 +0000

    dbus-broker: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/dbus-broker/run_tests.sh
+++ projects/dbus-broker/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2022 Google LLC
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,14 +15,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder-rust:ubuntu-24-04
-RUN git clone --depth 1 https://github.com/bus1/dbus-broker
-
-# RUSTUP_TOOLCHAIN is set explicitly to match the latest images
-# to get around https://github.com/google/oss-fuzz/issues/14220.
-# It should be removed once that issue is resolved.
-ENV RUSTUP_TOOLCHAIN nightly-2025-09-05
-
-RUN cargo install bindgen-cli
-WORKDIR dbus-broker
-COPY *.c build.sh run_tests.sh $SRC/
+meson test -C build -j$(nproc)
