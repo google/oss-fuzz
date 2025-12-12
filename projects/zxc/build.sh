0462a2c0ca3df9cdaef3890b125a6a69c75be8fb
hellobertrand <5901952+hellobertrand@users.noreply.github.com>
2025-12-12 15:46:11 +0100

    Updates copyright notice
    Updates the copyright notice from Google Inc. to Google LLC.

--- projects/zxc/build.sh
+++ projects/zxc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2025 Google Inc.
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 ################################################################################
-
 
 SRC_DIR="$SRC/zxc_project"
 
