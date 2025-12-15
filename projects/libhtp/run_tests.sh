537c8005ba4c9de026b2fa3550663280d25d6175
Arthur Chan <arthur.chan@adalogics.com>
2025-12-15 23:46:47 +0800

    libhtp: Add run_tests.sh (#14504)
    Adds run_tests.sh for the libhtp project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/experimental/chronos#check-tests
    
    ---------
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/libhtp/run_tests.sh
+++ projects/libhtp/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2018 Google Inc.
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +14,4 @@
 # limitations under the License.
 #
 ################################################################################
-
-FROM gcr.io/oss-fuzz-base/base-builder-rust
-RUN apt-get update && apt-get install -y make autoconf automake libtool zlib1g-dev liblzma-dev
-RUN git clone --depth 1 https://github.com/OISF/libhtp.git libhtp
-WORKDIR $SRC/libhtp
-COPY run_tests.sh build.sh $SRC/
+make check
