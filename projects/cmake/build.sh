15530ebdd1500edcda478f04deceb486dbeaf240
Leslie P. Polzer <polzer@fastmail.com>
2025-12-16 12:09:42 +0000

    cmake: Add 8 fuzzers and enable MSan support
    Expand CMake fuzzing coverage from 1 to 9 fuzzers:
    
    - xml_parser_fuzzer (existing)
    - cmListFileLexerFuzzer: CMakeLists.txt lexer
    - cmGeneratorExpressionFuzzer: $<...> generator expressions
    - cmELFFuzzer: ELF binary parser
    - cmArchiveExtractFuzzer: tar/zip extraction
    - cmFileLockFuzzer: file(LOCK) command
    - cmExprParserFuzzer: math() expressions
    - cmPkgConfigParserFuzzer: .pc file parsing
    - cmJSONParserFuzzer: JSON/CMakePresets
    
    Enable MSan (MemorySanitizer) support by:
    - Using bundled libraries (CMAKE_USE_SYSTEM_LIBRARIES=OFF)
    - Disabling OpenSSL (avoids uninstrumented code)
    - Removing libssl-dev from Dockerfile
    
    This addresses OSS-Fuzz issue #6294 which had MSan disabled.

--- projects/cmake/build.sh
+++ projects/cmake/build.sh
@@ -15,27 +15,145 @@
 #
 ################################################################################
 
-# Build CMake.
+# Build CMake with all bundled dependencies for proper sanitizer support.
+# MSan requires all code to be instrumented, so we use bundled libraries
+# and disable OpenSSL (which would require building from source).
+
 mkdir build-dir && cd build-dir
-../bootstrap
+
+# Configure CMake
+# - Use bundled libraries (required for MSan - all code must be instrumented)
+# - Disable OpenSSL (not needed for fuzzing, avoids uninstrumented code)
+# - Disable server mode (deprecated)
+# - Disable tests (not needed for fuzzing)
+cmake .. \
+  -DCMAKE_C_COMPILER="${CC}" \
+  -DCMAKE_CXX_COMPILER="${CXX}" \
+  -DCMAKE_C_FLAGS="${CFLAGS}" \
+  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -DCMAKE_USE_SYSTEM_LIBRARIES=OFF \
+  -DCMAKE_USE_OPENSSL=OFF \
+  -DBUILD_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release
+
+# Build CMakeLib (contains all the code we want to fuzz)
 make -j$(nproc) CMakeLib
 
-# Build fuzzers.
+# Store paths for fuzzer linking
+CMAKE_SOURCE="${SRC}/CMake"
+CMAKE_BUILD="${CMAKE_SOURCE}/build-dir"
+CMAKE_LIB_DIR="${CMAKE_BUILD}/Source"
+UTILITIES_DIR="${CMAKE_BUILD}/Utilities"
+
+# Common include paths
+INCLUDE_FLAGS="-I${CMAKE_SOURCE}/Source"
+INCLUDE_FLAGS="${INCLUDE_FLAGS} -I${CMAKE_BUILD}/Source"
+INCLUDE_FLAGS="${INCLUDE_FLAGS} -I${CMAKE_SOURCE}/Utilities/std"
+INCLUDE_FLAGS="${INCLUDE_FLAGS} -I${CMAKE_SOURCE}/Utilities/cmlibuv/include"
+INCLUDE_FLAGS="${INCLUDE_FLAGS} -I${CMAKE_SOURCE}/Utilities/cmlibrhash"
+INCLUDE_FLAGS="${INCLUDE_FLAGS} -I${CMAKE_SOURCE}/Utilities/cm3p"
+INCLUDE_FLAGS="${INCLUDE_FLAGS} -I${CMAKE_SOURCE}/Utilities"
+
+# Common libraries to link (order matters for static linking!)
+COMMON_LIBS="${CMAKE_LIB_DIR}/libCMakeLib.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmexpat/libcmexpat.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmlibarchive/libarchive/libcmlibarchive.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmliblzma/libcmliblzma.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmzstd/libcmzstd.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmzlib/libcmzlib.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmbzip2/libcmbzip2.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmcurl/lib/libcmcurl.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmnghttp2/libcmnghttp2.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmjsoncpp/libcmjsoncpp.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmlibrhash/libcmlibrhash.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmlibuv/libcmlibuv.a"
+COMMON_LIBS="${COMMON_LIBS} ${UTILITIES_DIR}/cmllpkgc/libcmllpkgc.a"
+
+# System libs needed
+SYS_LIBS="-lpthread -ldl -lrt"
+
+# Function to build a fuzzer
+build_fuzzer() {
+    local name=$1
+    local source=$2
+    echo "Building ${name}..."
+    $CXX $CXXFLAGS ${INCLUDE_FLAGS} -c "${source}" -o "${name}.o"
+    $CXX $CXXFLAGS $LIB_FUZZING_ENGINE "${name}.o" ${COMMON_LIBS} ${SYS_LIBS} -o "$OUT/${name}"
+}
+
+# Build fuzzers
 cd ../Tests/Fuzzing
-$CXX $CXXFLAGS -I../../Source \
-	-I../../build-dir/Source \
-	-c xml_parser_fuzzer.cc \
-	-o xml_parser_fuzzer.o 
 
+# 1. XML Parser Fuzzer (existing)
+build_fuzzer "xml_parser_fuzzer" "xml_parser_fuzzer.cc"
 
-export cmexpat_dir="${SRC}/CMake/build-dir/Utilities/cmexpat/CMakeFiles/cmexpat.dir/lib"
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE \
-		xml_parser_fuzzer.o -o $OUT/xml_parser_fuzzer \
-		../../build-dir/Source/CMakeFiles/CMakeLib.dir/cmXMLParser.cxx.o \
-		$cmexpat_dir/xmlparse.c.o \
-		$cmexpat_dir/xmlrole.c.o \
-		$cmexpat_dir/xmltok.c.o	
+# 2. ListFile Lexer Fuzzer
+build_fuzzer "cmListFileLexerFuzzer" "cmListFileLexerFuzzer.cxx"
 
+# 3. Generator Expression Fuzzer
+build_fuzzer "cmGeneratorExpressionFuzzer" "cmGeneratorExpressionFuzzer.cxx"
 
-# Build seed corpus
-zip $OUT/xml_parser_fuzzer_seed_corpus.zip $SRC/fuzzing-corpus/xml/test.xml
+# 4. ELF Fuzzer
+build_fuzzer "cmELFFuzzer" "cmELFFuzzer.cxx"
+
+# 5. Archive Extract Fuzzer
+build_fuzzer "cmArchiveExtractFuzzer" "cmArchiveExtractFuzzer.cxx"
+
+# 6. File Lock Fuzzer
+build_fuzzer "cmFileLockFuzzer" "cmFileLockFuzzer.cxx"
+
+# 7. Expression Parser Fuzzer
+build_fuzzer "cmExprParserFuzzer" "cmExprParserFuzzer.cxx"
+
+# 8. PkgConfig Parser Fuzzer
+build_fuzzer "cmPkgConfigParserFuzzer" "cmPkgConfigParserFuzzer.cxx"
+
+# 9. JSON Parser Fuzzer
+build_fuzzer "cmJSONParserFuzzer" "cmJSONParserFuzzer.cxx"
+
+# Build seed corpora
+echo "Building seed corpora..."
+
+# Helper function for corpus
+build_corpus() {
+    local name=$1
+    local dir=$2
+    if [ -d "${dir}" ]; then
+        zip -j "$OUT/${name}_seed_corpus.zip" "${dir}"/* 2>/dev/null || true
+    fi
+}
+
+# XML corpus (existing)
+zip -j $OUT/xml_parser_fuzzer_seed_corpus.zip \
+    $SRC/fuzzing-corpus/xml/*.xml 2>/dev/null || true
+
+# New corpora
+build_corpus "cmListFileLexerFuzzer" "corpus/listfile"
+build_corpus "cmGeneratorExpressionFuzzer" "corpus/genex"
+build_corpus "cmELFFuzzer" "corpus/elf"
+build_corpus "cmArchiveExtractFuzzer" "corpus/archive"
+build_corpus "cmFileLockFuzzer" "corpus/filelock"
+build_corpus "cmExprParserFuzzer" "corpus/expr"
+build_corpus "cmPkgConfigParserFuzzer" "corpus/pkgconfig"
+build_corpus "cmJSONParserFuzzer" "corpus/json"
+
+# Copy dictionaries
+echo "Copying dictionaries..."
+for dict in *.dict; do
+    if [ -f "$dict" ]; then
+        # Map dict to fuzzer name
+        fuzzer_name="${dict%.dict}"
+        case "$fuzzer_name" in
+            cmListFileLexer) cp "$dict" "$OUT/cmListFileLexerFuzzer.dict" ;;
+            cmGeneratorExpression) cp "$dict" "$OUT/cmGeneratorExpressionFuzzer.dict" ;;
+            cmELF) cp "$dict" "$OUT/cmELFFuzzer.dict" ;;
+            cmArchiveExtract) cp "$dict" "$OUT/cmArchiveExtractFuzzer.dict" ;;
+            cmExprParser) cp "$dict" "$OUT/cmExprParserFuzzer.dict" ;;
+            cmPkgConfigParser) cp "$dict" "$OUT/cmPkgConfigParserFuzzer.dict" ;;
+            cmJSONParser) cp "$dict" "$OUT/cmJSONParserFuzzer.dict" ;;
+            *) cp "$dict" "$OUT/" ;;
+        esac
+    fi
+done
+
+echo "Build complete! Built 9 fuzzers with corpora and dictionaries."
