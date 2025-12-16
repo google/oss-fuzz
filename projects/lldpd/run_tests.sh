0578c3fe29cea7c151279f743bfe44e21d8fcf78
Arthur Chan <arthur.chan@adalogics.com>
2025-12-16 07:32:11 +0000

    lldpd: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/lldpd/run_tests.sh
+++ projects/lldpd/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2022 Google LLC
+#!/bin/bash -eu
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +14,4 @@
 # limitations under the License.
 #
 ################################################################################
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y autoconf libtool-bin pkg-config check
-RUN git clone --recursive https://github.com/lldpd/lldpd.git
-COPY build.sh run_tests.sh $SRC/
-WORKDIR $SRC/lldpd/
+make check -j$(nproc)
