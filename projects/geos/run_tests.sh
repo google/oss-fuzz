240b176dd5b4261c4af721faf84b71467aac7858
Arthur Chan <arthur.chan@adalogics.com>
2025-12-18 11:18:00 +0000

    geos: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/geos/run_tests.sh
+++ projects/geos/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2021 Google LLC
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +15,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y cmake
-# fallback to github if main git server is not responding
-RUN git clone --depth 1 https://git.osgeo.org/gitea/geos/geos.git || git clone --depth 1 https://github.com/libgeos/geos.git
-COPY run_tests.sh build.sh $SRC
-COPY patch.diff $SRC
-WORKDIR $SRC/geos
-RUN git apply ../patch.diff
+ctest --test-dir build
