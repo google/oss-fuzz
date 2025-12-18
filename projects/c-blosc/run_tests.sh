e045612c86a47acb5e4497e3e4b2730b7df8c151
Arthur Chan <arthur.chan@adalogics.com>
2025-12-18 19:23:04 +0800

    c-blosc: Add run_tests.sh (#14567)
    Adds run_tests.sh for the c-blosc project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    ---------
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/c-blosc/run_tests.sh
+++ projects/c-blosc/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2019 Google Inc.
+#!/bin/bash -eux
+# Copyright 2025 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-################################################################################
+###############################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y cmake make
-RUN git clone --depth 1 https://github.com/Blosc/c-blosc.git c-blosc
-WORKDIR c-blosc
-COPY run_tests.sh build.sh $SRC/
+# Disable leak sanitizer and run unit testing
+export ASAN_OPTIONS="detect_leaks=0"
+ctest --test-dir .
