afcad5fe53f7947a8621c5de309c7e019610a960
Arthur Chan <arthur.chan@adalogics.com>
2025-12-18 19:01:56 +0800

    fftw3: Add run_tests.sh (#14583)
    Adds run_tests.sh for the fftw3 project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/fftw3/run_tests.sh
+++ projects/fftw3/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2023 Google LLC
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-################################################################################
+###############################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-
-RUN apt-get update && \
-    apt-get install -y build-essential ocaml ocamlbuild texinfo \
-    autoconf automake indent libtool libnum-ocaml-dev fig2dev
-
-RUN git clone https://github.com/FFTW/fftw3 fftw3
-COPY run_tests.sh build.sh *.cc $SRC/
-WORKDIR $SRC/fftw3
+make check
