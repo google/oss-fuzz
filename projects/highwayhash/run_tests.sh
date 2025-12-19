dc91468154371dcc3e055d97fd36fccba2049164
Arthur Chan <arthur.chan@adalogics.com>
2025-12-19 19:10:05 +0800

    highwayhash: Add run_tests.sh (#14600)
    Adds run_tests.sh for the highwayhash project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/highwayhash/run_tests.sh
+++ projects/highwayhash/run_tests.sh
@@ -1,4 +1,5 @@
-# Copyright 2023 Google LLC
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +15,5 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake libtool
-RUN git clone --depth 1 https://github.com/google/highwayhash highwayhash
-WORKDIR highwayhash
-COPY run_tests.sh build.sh $SRC/
+# Run built unit test cases
+./build/highwayhash_test && ./build/sip_hash_test && ./build/vector_test
