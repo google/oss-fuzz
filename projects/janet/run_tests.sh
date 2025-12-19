4d8f94ff94470fb61fa13b62ef4b626e19961e1d
Arthur Chan <arthur.chan@adalogics.com>
2025-12-19 09:18:56 +0000

    janet: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/janet/run_tests.sh
+++ projects/janet/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2020 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +16,5 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make cmake
-RUN git clone https://github.com/janet-lang/janet
-
-WORKDIR $SRC/janet
-COPY run_tests.sh build.sh $SRC/
+export ASAN_OPTIONS="detect_leaks=0"
+make test
