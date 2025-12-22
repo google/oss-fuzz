a31687bc6db326128f4bdc40983261804a4bad68
Arthur Chan <arthur.chan@adalogics.com>
2025-12-23 07:04:36 +0800

    xnnpack: Add run_tests.sh (#14624)
    Adds run_tests.sh for the xnnpack project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/xnnpack/run_tests.sh
+++ projects/xnnpack/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2023 Google LLC
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder:ubuntu-24-04
-RUN apt-get update && apt-get install -y make autoconf automake libtool libomp-dev libgomp1
-RUN git clone --depth 1 https://github.com/google/XNNPACK xnnpack
-WORKDIR xnnpack
-COPY run_tests.sh build.sh *.cc $SRC/
+ctest --test-dir build
