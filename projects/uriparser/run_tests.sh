4d33f90c0c808d560c6f7cd7c3b723c73dc1cfad
Arthur Chan <arthur.chan@adalogics.com>
2025-12-22 11:18:59 +0000

    uriparser: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/uriparser/run_tests.sh
+++ projects/uriparser/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2020 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake wget sudo libtool cmake
-RUN git clone --depth 1 https://github.com/uriparser/uriparser uriparser
-WORKDIR uriparser
-COPY run_tests.sh build.sh "$SRC/"
-
-RUN git clone https://github.com/google/googletest.git -b v1.15.2 && \
-    cd googletest && cmake . && make install && cd .. && rm -rf googletest
+ctest --test-dir build
