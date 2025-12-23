f79d2b54dc1a0d656ab792713955c502e287180d
Arthur Chan <arthur.chan@adalogics.com>
2025-12-23 03:57:59 +0000

    tinygltf: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/tinygltf/run_tests.sh
+++ projects/tinygltf/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2021 Google LLC
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +16,5 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN pip3 install meson ninja
-RUN git clone --depth 1 https://github.com/syoyo/tinygltf.git
-WORKDIR $SRC/tinygltf
-COPY run_tests.sh build.sh $SRC/
+# Enter tests directory and run the two unit test cases
+cd tests && ./tester && ./tester_noexcept
