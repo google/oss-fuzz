71e64c88dfcdaea57c735f70c70a6318f337e44d
Arthur Chan <arthur.chan@adalogics.com>
2025-12-29 11:05:13 +0000

    openvswitch: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/openvswitch/run_tests.sh
+++ projects/openvswitch/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2018 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,13 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake \
-    libtool python3 python3-pip \
-    libz-dev libssl-dev wget
-RUN pip3 install six
-RUN git clone --depth 1 https://github.com/openvswitch/ovs.git openvswitch
-RUN git clone --depth 1 https://github.com/openvswitch/ovs-fuzzing-corpus.git \
-    ovs-fuzzing-corpus
-WORKDIR openvswitch
-COPY run_tests.sh build.sh $SRC/
+make check -j$(nproc)
