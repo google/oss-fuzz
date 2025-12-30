5d5da479e96f914d91aff09f7ce48e199fc2a0a8
Arthur Chan <arthur.chan@adalogics.com>
2025-12-30 09:12:14 +0000

    libical: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/libical/run_tests.sh
+++ projects/libical/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2019 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,14 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-
-# Install OpenJDK
-RUN apt-get update && \
-    apt-get install -y openjdk-11-jdk && \
-    rm -rf /var/lib/apt/lists/*
-
-RUN git clone --depth 1 https://github.com/libical/libical.git
-COPY run_tests.sh build.sh $SRC
-COPY *.cc $SRC/
-WORKDIR libical
+ctest --test-dir $SRC/libical -j$(nproc)
