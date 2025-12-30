168de546c4c85ea8d21a776898d2143cf45be5e9
Arthur Chan <arthur.chan@adalogics.com>
2025-12-30 09:58:03 +0000

    libtasn1: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/libtasn1/run_tests.sh
+++ projects/libtasn1/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2019 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,14 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake libtool bison
-
-RUN git clone git://git.savannah.gnu.org/gnulib.git
-ENV GNULIB_TOOL=$SRC/gnulib/gnulib-tool
-ENV GNULIB_SRCDIR=$SRC/gnulib
-
-RUN git clone https://gitlab.com/gnutls/libtasn1.git
-
-WORKDIR libtasn1
-COPY run_tests.sh build.sh $SRC/
+make check -C tests -j$(nproc)
