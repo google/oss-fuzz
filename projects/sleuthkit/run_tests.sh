4850bd157ece872e90a4c60dcb48712c434803cf
Arthur Chan <arthur.chan@adalogics.com>
2025-12-31 02:05:30 +0800

    sleuthkit: Add run_tests.sh (#14646)
    Adds run_tests.sh for the sleuthkit project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    ---------
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/sleuthkit/run_tests.sh
+++ projects/sleuthkit/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2020 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y make autoconf automake libtool libz-dev
-RUN git clone --depth 1 https://github.com/sleuthkit/sleuthkit sleuthkit
-WORKDIR sleuthkit
-COPY run_tests.sh build.sh replay_build.sh $SRC/
+make check -j$(nproc)
