65e24169d0d6ee8e4cc73dda3f259bbb578e1b33
Arthur Chan <arthur.chan@adalogics.com>
2025-12-31 02:02:12 +0800

    openthread: Add run_tests.sh (#14682)
    Adds run_tests.sh for the openthread project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/openthread/run_tests.sh
+++ projects/openthread/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2017 Google Inc.
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +16,5 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-
-RUN apt-get update && apt-get install -y cmake ninja-build
-RUN git clone --depth 1 https://github.com/openthread/openthread
-
-WORKDIR openthread
-COPY run_tests.sh build.sh *.options $SRC/
+# Skip Nexus unit test which requires network connection
+ctest --test-dir build -E "nexus*"
