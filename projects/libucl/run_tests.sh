aef3ab9d5fcca1cb6a975b5ce42bf819c2f726ec
Arthur Chan <arthur.chan@adalogics.com>
2025-12-31 19:16:07 +0800

    libucl: Add run_tests.sh (#14709)
    Adds run_tests.sh for the libucl project.
    
    run_tests.sh is used as part of Chronos with cached builds:
    
    https://github.com/google/oss-fuzz/tree/master/infra/chronos#chronos-feature--running-tests-of-a-project
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/libucl/run_tests.sh
+++ projects/libucl/run_tests.sh
@@ -1,6 +1,5 @@
-#!/bin/bash -eux
-
-# Copyright 2021 Google LLC
+#!/bin/bash -eu
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,13 +15,5 @@
 #
 ################################################################################
 
-cp $SRC/ucl_add_string_fuzzer.options $OUT/
-
-./autogen.sh --force && ./configure
-make
-
-$CC $CFLAGS -c tests/fuzzers/ucl_add_string_fuzzer.c \
-  -DHAVE_CONFIG_H -I./src -I./include src/.libs/libucl.a -I./ \
-  -o $OUT/ucl_add_string_fuzzer.o
-
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE $OUT/ucl_add_string_fuzzer.o -DHAVE_CONFIG_H -I./src -I./include src/.libs/libucl.a -I. -o $OUT/ucl_add_string_fuzzer
+export ASAN_OPTIONS="detect_leaks=0"
+make check -C tests -j$(nproc)
