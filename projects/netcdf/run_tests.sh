607d1742482b5bdb99c6170a058e9fa4a50c7c94
Arthur Chan <arthur.chan@adalogics.com>
2025-12-31 09:17:39 +0000

    netcdf: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/netcdf/run_tests.sh
+++ projects/netcdf/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2021 Google LLC
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y cmake m4 zlib1g-dev libcurlpp-dev libcurl4-openssl-dev
-RUN git clone --depth 1 https://github.com/Unidata/netcdf-c
-COPY run_tests.sh build.sh $SRC
-WORKDIR $SRC/netcdf-c
+ctest --test-dir $SRC/netcdf-c/build -j$(nproc)
