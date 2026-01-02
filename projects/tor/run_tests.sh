0ff617ab6a1520286d1435c2c966ebcd9f473b86
Arthur Chan <arthur.chan@adalogics.com>
2026-01-02 12:44:23 +0000

    tor: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

---
+++ projects/tor/run_tests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eu
+#
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# Run all successful unit testing
+make check -j$(nproc) TESTS="src/test/test-slow src/test/test-timers \
+    src/test/test_keygen.sh src/test/test_key_expiration.sh \
+    src/test/test_zero_length_keys.sh src/test/test_workqueue_cancel.sh \
+    src/test/test_workqueue_efd.sh src/test/test_workqueue_efd2.sh \
+    src/test/test_workqueue_pipe.sh src/test/test_workqueue_pipe2.sh \
+    src/test/test_workqueue_socketpair.sh src/test/test_switch_id.sh \
+    src/test/test_cmdline.sh src/test/unittest_part1.sh \
+    src/test/unittest_part2.sh src/test/unittest_part3.sh \
+    src/test/unittest_part4.sh src/test/unittest_part5.sh \
+    src/test/unittest_part6.sh src/test/unittest_part7.sh \
+    src/test/unittest_part8.sh src/test/test_ntor.sh src/test/test_hs_ntor.sh \
+    scripts/maint/practracker/test_practracker.sh \
+    scripts/maint/run_check_subsystem_order.sh \
+    src/test/test_rebind.sh src/test/test_include.sh \
+    scripts/maint/checkSpaceTest.sh"
+
+# Skip four failing unit test cases
+# make check TEST="src/test/test-memwipe src/test/test-workqueuec src/test/test_parseconf.sh src/test/test_bt.sh"
