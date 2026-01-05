9b86b1690ea47928dd152a64900805ab5f429e64
Arthur Chan <arthur.chan@adalogics.com>
2026-01-05 10:28:44 +0000

    spotify-json: Add run_tests.sh
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/spotify-json/run_tests.sh
+++ projects/spotify-json/run_tests.sh
@@ -1,4 +1,6 @@
-# Copyright 2021 Google LLC
+#!/bin/bash -eu
+#
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +16,4 @@
 #
 ################################################################################
 
-FROM gcr.io/oss-fuzz-base/base-builder:ubuntu-24-04
-RUN apt-get update && apt-get install -y libboost-dev libboost-test-dev \
-      libboost-system-dev libboost-chrono-dev
-RUN git clone https://github.com/spotify/spotify-json
-
-WORKDIR $SRC/spotify-json
-COPY run_tests.sh build.sh $SRC/
+make test -C $SRC/spotify-json/build-tests -j$(nproc)
