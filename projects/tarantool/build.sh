cb5e661fb7e95d7e862e93649e75dc0746666237
Sergey Bronnikov <estetus@gmail.com>
2026-01-30 12:59:40 +0300

    tarantool: disable coverage for Lua tests
    See also #14859
    Follows up #14656

--- projects/tarantool/build.sh
+++ projects/tarantool/build.sh
@@ -119,8 +119,11 @@ done
 # is libFuzzer-based.
 # UndefinedBehaviorSanitizer is not supported,
 # see https://github.com/tarantool/tarantool/issues/12216.
+# Code coverage is not supported,
+# see https://github.com/google/oss-fuzz/issues/14859.
 if [[ "$FUZZING_ENGINE" != libfuzzer ]] ||
-   [[ "$SANITIZER" == "undefined" ]]; then
+   [[ "$SANITIZER" == "undefined" ]] ||
+   [[ "$SANITIZER" == "coverage" ]]; then
   exit
 fi
 
