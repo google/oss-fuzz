12194852a167f66c5c11cecdc57a95e832360879
Yoo_LCD <youichi0929@outlook.jp>
2026-03-12 20:31:42 +0900

    Add license header to build.sh

--- projects/go-github/build.sh
+++ projects/go-github/build.sh
@@ -1,8 +1,23 @@
-#!/bin/bash -eu 
- 
-cd $SRC/go-github 
-go mod download 
-go install github.com/AdamKorcz/go-118-fuzz-build@latest 
-go get github.com/AdamKorcz/go-118-fuzz-build/testing 
- 
-compile_native_go_fuzzer github.com/google/go-github/v84/github FuzzParseWebHook fuzz_parse_webhook 
+#!/bin/bash -eu
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd $SRC/go-github
+go mod download
+go install github.com/AdamKorcz/go-118-fuzz-build@latest
+go get github.com/AdamKorcz/go-118-fuzz-build/testing
+
+compile_native_go_fuzzer github.com/google/go-github/v84/github FuzzParseWebHook fuzz_parse_webhook
