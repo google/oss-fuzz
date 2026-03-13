ae77668ca2e4d052052bad3b9bd4e7bc78685279
Ze Sheng <108382772+OwenSanzas@users.noreply.github.com>
2026-03-13 21:42:40 +0000

    add license header and fix brace indentation

--- projects/unbound/parse_packet_fuzzer.c
+++ projects/unbound/parse_packet_fuzzer.c
@@ -1,3 +1,15 @@
+/* Copyright 2021 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include "config.h"
 #include "util/regional.h"
 #include "util/fptr_wlist.h"
@@ -12,7 +24,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len)
 		if (!region) {
 			abort();
 		}
-}
+	}
 	sldns_buffer pktbuf;
 	sldns_buffer_init_frm_data(&pktbuf, (void*)buf, len);
 
