eaaa53a3544743a18fe307ee5139d360b6881c98
Stan Ulbrych <stan@python.org>
2026-04-13 21:28:19 +0100

    Update to LLC

--- projects/pypy3/build.sh
+++ projects/pypy3/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# Copyright 2026 Google Inc.
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 ################################################################################
 
 case $SANITIZER in
--- projects/pypy3/run_tests.sh
+++ projects/pypy3/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2026 Google Inc.
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 ################################################################################
