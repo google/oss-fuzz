f84ee97889cb71c303dd2a3a139eb1fd985836d9
David Korczynski <david@adalogics.com>
2026-04-29 14:00:36 -0700

    tremor: extend project
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ projects/tremor/vorbis_synthesis_fuzzer.cc
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <cstdint>
+#include "ivorbiscodec.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  if (Size < 10) return 0;
+
+  vorbis_info vi;
+  vorbis_comment vc;
+  vorbis_info_init(&vi);
+  vorbis_comment_init(&vc);
+
+  const uint8_t *ptr = Data;
+  size_t remaining = Size;
+
+  // We'll use the first 3 "packets" for headers
+  for (int i = 0; i < 3; ++i) {
+    if (remaining < 2) break;
+    size_t p_size = (ptr[0] << 8) | ptr[1];
+    ptr += 2;
+    remaining -= 2;
+    if (p_size > remaining) p_size = remaining;
+    
+    if (p_size > 0) {
+      ogg_packet op;
+      op.packet = (unsigned char *)ptr;
+      op.bytes = p_size;
+      op.b_o_s = (i == 0);
+      op.e_o_s = 0;
+      op.granulepos = 0;
+      op.packetno = i;
+      
+      vorbis_synthesis_headerin(&vi, &vc, &op);
+      
+      ptr += p_size;
+      remaining -= p_size;
+    }
+  }
+
+  vorbis_dsp_state vd;
+  if (vorbis_synthesis_init(&vd, &vi) == 0) {
+    vorbis_block vb;
+    vorbis_block_init(&vd, &vb);
+    
+    while (remaining > 2) {
+      size_t p_size = (ptr[0] << 8) | ptr[1];
+      ptr += 2;
+      remaining -= 2;
+      if (p_size > remaining) p_size = remaining;
+
+      if (p_size > 0) {
+        ogg_packet op;
+        op.packet = (unsigned char *)ptr;
+        op.bytes = p_size;
+        op.b_o_s = 0;
+        op.e_o_s = (remaining == p_size);
+        op.granulepos = -1;
+        op.packetno = 0;
+
+        if (vorbis_synthesis(&vb, &op) == 0) {
+          vorbis_synthesis_blockin(&vd, &vb);
+        }
+        
+        ogg_int32_t **pcm;
+        int samples = vorbis_synthesis_pcmout(&vd, &pcm);
+        if (samples > 0) {
+          vorbis_synthesis_read(&vd, samples);
+        }
+
+        ptr += p_size;
+        remaining -= p_size;
+      }
+    }
+    vorbis_block_clear(&vb);
+    vorbis_dsp_clear(&vd);
+  }
+
+  vorbis_info_clear(&vi);
+  vorbis_comment_clear(&vc);
+  
+  return 0;
+}
