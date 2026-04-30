c0edb81e0fdd648eb1f58126c0b41ec0df9efd5b
DavidKorczynski <david@adalogics.com>
2026-04-30 10:54:21 +0100

    kamailio: build all fuzzing harnesses
    Adds generic way of building the fuzzing harnesses.

--- projects/kamailio/build.sh
+++ projects/kamailio/build.sh
@@ -28,19 +28,13 @@ cd src
 mkdir objects && find . -name "*.o" -exec cp {} ./objects/ \;
 ar -r libkamilio.a ./objects/*.o
 cd ../
-$CC $CFLAGS -c ./misc/fuzz/fuzz_uri.c \
-    -DFAST_LOCK -D__CPU_i386 ./src/libkamilio.a \
-    -I./src/ -I./src/core/parser -ldl -lresolv -lm
 
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE fuzz_uri.o -o $OUT/fuzz_uri \
-    -DFAST_LOCK -D__CPU_i386 ./src/libkamilio.a \
-    -I./src/ -I./src/core/parser -ldl -lresolv -lm
-
-$CC $CFLAGS  ./misc/fuzz/fuzz_parse_msg.c -c \
-    -DFAST_LOCK -D__CPU_i386 ./src/libkamilio.a \
-    -I./src/ -I./src/core/parser -ldl -lresolv -lm
-
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE fuzz_parse_msg.o -o $OUT/fuzz_parse_msg \
-    -DFAST_LOCK -D__CPU_i386 ./src/libkamilio.a \
-    -I./src/ -I./src/core/parser -ldl -lresolv -lm
-
+for fuzzer in misc/fuzz/fuzz_*.c; do
+    fuzzer_name=$(basename "$fuzzer" .c)
+    $CC $CFLAGS -c "$fuzzer" \
+        -DFAST_LOCK -D__CPU_i386 ./src/libkamilio.a \
+        -I./src/ -I./src/core/parser -ldl -lresolv -lm
+    $CXX $CXXFLAGS $LIB_FUZZING_ENGINE "$fuzzer_name.o" -o "$OUT/$fuzzer_name" \
+        -DFAST_LOCK -D__CPU_i386 ./src/libkamilio.a \
+        -I./src/ -I./src/core/parser -ldl -lresolv -lm
+done
