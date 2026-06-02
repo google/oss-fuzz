ffc5039673a32f005b2f65a1ca14c658b64934e0
DavidKorczynski <david@adalogics.com>
2026-06-02 10:55:11 +0100

    duckdb: build newly added harness (#15606)
    Ref https://github.com/duckdb/duckdb/pull/22958
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/duckdb/build.sh
+++ projects/duckdb/build.sh
@@ -18,9 +18,17 @@
 make relassert CRASH_ON_ASSERT=1 DISABLE_SANITIZER=1
 EXTENSION_LIBS=$(find ./build/relassert/extension/ -name "*.a")
 THIRD_PARTY_LIBS=$(find ./build/relassert/third_party/ -name "*.a")
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./test/ossfuzz/parse_fuzz_test.cpp \
-    -o $OUT/parse_fuzz_test -I./ -I./src/include -I./third_party/fmt/include \
-    -Wl,--start-group \
-    ./build/relassert/src/libduckdb_static.a \
-    ${EXTENSION_LIBS} ${THIRD_PARTY_LIBS} \
-    -Wl,--end-group
+
+# Build each OSS-Fuzz harness in test/ossfuzz/ against the statically-linked
+# DuckDB library plus all built-in extensions (parquet is built by default and
+# its static lib is picked up by EXTENSION_LIBS above). The parquet harness
+# (parquet_fuzz_test) needs the parquet extension's read_parquet/parquet_schema
+# table functions, which are registered when libparquet_extension.a is linked.
+for harness in parse_fuzz_test parquet_fuzz_test; do
+    $CXX $CXXFLAGS $LIB_FUZZING_ENGINE ./test/ossfuzz/${harness}.cpp \
+        -o $OUT/${harness} -I./ -I./src/include -I./third_party/fmt/include \
+        -Wl,--start-group \
+        ./build/relassert/src/libduckdb_static.a \
+        ${EXTENSION_LIBS} ${THIRD_PARTY_LIBS} \
+        -Wl,--end-group
+done
