01619dd67921414b538e9c45a23cb3c3d6944fb4
Alexey Ivanov <rbtz@ph34r.me>
2026-06-03 04:37:56 -0700

    zstd-seekable-format-go: add frame cache fuzzers

--- projects/zstd-seekable-format-go/build.sh
+++ projects/zstd-seekable-format-go/build.sh
@@ -18,15 +18,19 @@
 cd "$SRC/zstd-seekable-format-go/pkg"
 
 # The native Go fuzzer helper rewrites _test.go files into regular Go files.
-# Keep the external-package examples out of that package-local fuzz build.
+# Keep external-package examples out of package-local fuzz builds.
 rm example_test.go
+rm framecache/example_test.go
 
 FUZZERS_PACKAGE=github.com/SaveTheRbtz/zstd-seekable-format-go/pkg
+FRAMECACHE_FUZZERS_PACKAGE="$FUZZERS_PACKAGE/framecache"
 
 compile_native_go_fuzzer_v2 "$FUZZERS_PACKAGE" FuzzReader FuzzReader
 compile_native_go_fuzzer_v2 "$FUZZERS_PACKAGE" FuzzReaderConst FuzzReaderConst
 compile_native_go_fuzzer_v2 "$FUZZERS_PACKAGE" FuzzRoundTrip FuzzRoundTrip
 compile_native_go_fuzzer_v2 "$FUZZERS_PACKAGE" FuzzCorruptSeekTable FuzzCorruptSeekTable
+compile_native_go_fuzzer_v2 "$FUZZERS_PACKAGE" FuzzReaderFrameCacheConcurrentReadAt FuzzReaderFrameCacheConcurrentReadAt
+compile_native_go_fuzzer_v2 "$FRAMECACHE_FUZZERS_PACKAGE" FuzzFrameCacheAccessPatterns FuzzFrameCacheAccessPatterns
 
 zip -j "$OUT/FuzzReader_seed_corpus.zip" testdata/fuzz/FuzzReader/*
 zip -j "$OUT/FuzzRoundTrip_seed_corpus.zip" testdata/fuzz/FuzzRoundTrip/*
