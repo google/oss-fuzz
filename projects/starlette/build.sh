7be700b3dda8249dabf1eb3ecdd95342791fef69
can olgun <canolgun@gmail.com>
2026-06-12 12:47:04 +0300

    Add OSS-Fuzz for starlette + pyyaml

--- projects/starlette/build.sh
+++ projects/starlette/build.sh
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-pip3 install atheris pyyaml
-compile_python_fuzzer fuzz_pyyaml.py
+pip3 install atheris starlette
+compile_python_fuzzer fuzz_starlette.py
