9d915f48d83ecda011cea322a4c10e1888c883c3
can olgun <canolgun@gmail.com>
2026-06-12 10:36:24 +0300

    Fix license headers

--- projects/cobra/build.sh
+++ projects/cobra/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -eu
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 go mod download
-compile_go_fuzzer github.com/spf13/cobra
-github.com/go-chi/chi/v5 FuzzCobraExecute
-FuzzChiRouteMatch fuzz_cobra
+compile_go_fuzzer github.com/spf13/cobra FuzzCobraExecute fuzz_cobra
