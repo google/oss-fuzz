5583a998e6f6d5b92dc8a6f240084684162888c0
MR.SS <sajjadrahimnouri@gmail.com>
2026-06-22 21:39:49 +0330

    new changes

--- projects/pypdf/build.sh
+++ projects/pypdf/build.sh
@@ -1,23 +1,16 @@
 #!/bin/bash -eu
-
-# Install pypdf
 pip3 install .
-
 # Build a high-quality seed corpus to maximize code coverage
 mkdir -p corpus_temp
 cd corpus_temp
-
 # Download diverse PDFs (Encrypted, Forms, Images, Text)
 curl -sLO https://raw.githubusercontent.com/mozilla/pdf.js/master/test/pdfs/tracemonkey.pdf
 curl -sLO https://raw.githubusercontent.com/mozilla/pdf.js/master/test/pdfs/bug1056586.pdf
 curl -sLO https://raw.githubusercontent.com/mozilla/pdf.js/master/test/pdfs/xfa_form_calc_check.pdf
 
-# Zip them up for OSS-Fuzz
 zip -q $OUT/pypdf_fuzzer_seed_corpus.zip *.pdf
 cd ..
 rm -rf corpus_temp
-
-# Compile the Python fuzzer
 for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
   compile_python_fuzzer $fuzzer
 done
