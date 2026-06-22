ba0a0ceff2ae166d0e35ec3fa2f66ccebf37c0b9
MR.SS <sajjadrahimnouri@gmail.com>
2026-06-23 01:30:25 +0330

    Add required Google copyright headers

--- projects/pypdf/pypdf_fuzzer.py
+++ projects/pypdf/pypdf_fuzzer.py
@@ -1,7 +1,23 @@
+#!/usr/bin/python3
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
 import sys
 import atheris
 import io
-
 with atheris.instrument_imports():
     from pypdf import PdfReader, PdfWriter
     from pypdf.errors import PdfReadError, PdfStreamError, ParseError
@@ -61,8 +77,6 @@ def TestOneInput(data):
         writer2.write(io.BytesIO())
 
     except Exception:
-        # We catch all standard Python exceptions (ValueError, AttributeError, OverflowError, etc.)
-        # We are strictly hunting for DoS (Timeouts, OOM) and Native crashes.
         pass
 
 def main():
