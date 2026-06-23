506f27a1343aefc4b0a6d7b4270741d4df3f14b4
David Cook <dcook@divviup.org>
2026-06-23 18:23:01 -0500

    trust-dns: Enable debug assertions (#15795)
    This enables debug assertions when building Hickory DNS (née Trust-DNS).
    I recently caught an [interesting
    bug](https://github.com/hickory-dns/hickory-dns/issues/3743) by running
    a debug build of a fuzzer locally, so we may stand to benefit from doing
    the same in OSS-Fuzz.

--- projects/trust-dns/build.sh
+++ projects/trust-dns/build.sh
@@ -15,5 +15,5 @@
 #
 ################################################################################
 
-cargo fuzz build -O
+cargo fuzz build -O --debug-assertions
 cp fuzz/target/x86_64-unknown-linux-gnu/release/message $OUT/
