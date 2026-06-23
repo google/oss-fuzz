578490e7402054742d00e7c48addc284dde20d62
mozamils <smuzamel@gmail.com>
2026-06-23 15:27:41 -0600

    fix: Added dependency `github.com/AdamKorcz/go-118-fuzz-build/strong` to all fuzzers. Locally ran instance goes through each fuzzer successfully now.

--- projects/ssmctl/build.sh
+++ projects/ssmctl/build.sh
@@ -21,27 +21,23 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #################################################################################
-cd $GOPATH/src/github.com/MozamilS/ssmctl/
+cd $GOPATH/src/github.com/rhysmcneill/ssmctl/
 
 go mod download
 go mod tidy
 
-echo "=== FUZZ FUNCTIONS FOUND ==="
-grep -r "^func Fuzz" $GOPATH/src/github.com/MozamilS/ssmctl/internal/ssm/
-echo "=== END FUZZ FUNCTIONS ==="
-
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzSanitizeBasename fuzz_sanitizebasename
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzRemoteBaseName fuzz_remotebasename
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzDownloadBase64Decoding fuzz_downloadbase64decoding
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzAllInstanceIDs fuzz_allinstanceids
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzFirstInstance fuzz_firstinstance
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzShellQuote fuzz_shellquote
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzPowerShellQuote fuzz_powershellquote
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzNoInstancesFound fuzz_noinstancesfound
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzNormalizeWindowsPath fuzz_normalizewindowspath
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzTargetInfoFromInstance fuzz_targetinfofrommodule
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzParseRemoteFlag fuzz_parseremotsflag
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzNameTag fuzz_nametag
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzParseArg fuzz_parsearg
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzParseS3URL fuzz_parses3url
-compile_native_go_fuzzer github.com/MozamilS/ssmctl/internal/ssm FuzzListInstancesFiltering fuzz_list
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzSanitizeBasename fuzz_sanitizebasename
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzRemoteBaseName fuzz_remotebasename
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzDownloadBase64Decoding fuzz_downloadbase64decoding
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzAllInstanceIDs fuzz_allinstanceids
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzFirstInstance fuzz_firstinstance
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzShellQuote fuzz_shellquote
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzPowerShellQuote fuzz_powershellquote
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzNoInstancesFound fuzz_noinstancesfound
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzNormalizeWindowsPath fuzz_normalizewindowspath
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzTargetInfoFromInstance fuzz_targetinfofrommodule
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzParseRemoteFlag fuzz_parseremotsflag
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzNameTag fuzz_nametag
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzParseArg fuzz_parsearg
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzParseS3URL fuzz_parses3url
+compile_native_go_fuzzer github.com/rhysmcneill/ssmctl/internal/ssm FuzzListInstancesFiltering fuzz_list
