8912900fa9de5a05be1ddc42a7c1dcd7b19f6693
David Korczynski <david@adalogics.com>
2026-07-10 06:31:12 -0700

    nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- projects/pigweed/build.sh
+++ projects/pigweed/build.sh
@@ -30,6 +30,21 @@ echo "Building project using Bazel wrapper."
 #   TODO(b/370523804) / https://github.com/bazel-contrib/rules_fuzzing/issues/257
 sed -i 's/name = "rules_fuzzing", version = "0.6.0"/name = "rules_fuzzing", version = "0.8.0"/' MODULE.bazel
 
+# pigweed's .bazelrc `non_hermetic` config (which we enable below so OSS-Fuzz can
+# inject its own fuzzing compilers) passes
+# `--extra_toolchains=@local_config_cc_toolchains//...`. Under the Bazel 9
+# toolchain used by OSS-Fuzz, that apparent repo name is only visible to a module
+# that explicitly `use_repo`s it from rules_cc's cc_configure extension; pigweed's
+# MODULE.bazel does not, so analysis fails with "No repository visible as
+# '@local_config_cc_toolchains' from main repository". Make the repo visible.
+cat >> MODULE.bazel <<'EOF'
+
+# Added by OSS-Fuzz: expose the auto-detected system C++ toolchain repo so that
+# `--config non_hermetic` can resolve --extra_toolchains=@local_config_cc_toolchains//...
+cc_configure_ext = use_extension("@rules_cc//cc:extensions.bzl", "cc_configure_extension")
+use_repo(cc_configure_ext, "local_config_cc_toolchains")
+EOF
+
 export BAZEL_FUZZ_TEST_QUERY="
 let all_fuzz_tests = attr(tags, \"fuzz-test\", \"//...\") in
 let lang_fuzz_tests = attr(generator_function, \"pw_cc_fuzz_test\", \$all_fuzz_tests) in
