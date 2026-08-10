231f31f6334a218488dd1c3535c7219f1a68a05f
gurusai-voleti <gvoleti@google.com>
2026-08-10 18:56:14 +0530

    Update gcb.py

--- infra/cifuzz/platform_config/gcb.py
+++ infra/cifuzz/platform_config/gcb.py
@@ -37,4 +37,4 @@ class PlatformConfig(platform_config.BasePlatformConfig):
   @property
   def filestore(self):
     """Returns the filestore used to store persistent data."""
-    return os.environ.get('FILESTORE', 'gcloud storage')
+    return os.environ.get('FILESTORE', 'gsutil')
