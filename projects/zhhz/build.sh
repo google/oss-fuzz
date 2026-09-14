1a9ec1d0d1bdcbe2c338e7c7a02e89dd4207b8f7
Li Junhao <edwin.jh.lee@gmail.com>
2026-09-14 20:07:54 +0800

    Add Apache-2.0 license header to build.sh and Dockerfile
    googleapis/repo-automation-bots header-check failed without the
    canonical Google copyright + Apache-2.0 license preamble. Match the
    format used in image-rs/build.sh so header-check passes and
    trial-build can run.

--- projects/zhhz/build.sh
+++ projects/zhhz/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -eu
+# Copyright 2026 Google LLC
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
 # OSS-Fuzz build script for zhhz.
 # Invoked by ClusterFuzz in the gcr.io/oss-fuzz-base/base-builder-rust
 # image, with $SANITIZER set to one of address|memory|undefined.
@@ -9,7 +23,7 @@
 #   $OUT/zhhz-fuzz-convert_roundtrip   (libFuzzer binary)
 #   $OUT/zhhz-fuzz-fmm_segment_seed_corpus.zip
 #   $OUT/zhhz-fuzz-convert_roundtrip_seed_corpus.zip
-#
+################################################################################
 
 # The base-builder-rust image has pinned nightly Rust + cargo-fuzz pre-installed.
 
